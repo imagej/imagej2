0d8eeaec867bdbac8bd118ee556283a92b781461
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2010-01-19 18:56:15 +0000

    clean up warnings in file
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1031 be23795f-a60e-40d4-ba19-055798872e57

--- ij/util/ToolsTest.java
+++ ij/util/ToolsTest.java
@@ -4,18 +4,12 @@ import java.awt.Color;
 
 import static org.junit.Assert.*;
 
-import java.awt.Color;
-import java.util.StringTokenizer;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.util.StringTokenizer;
-
-import java.text.*;
 
 public class ToolsTest {
 
