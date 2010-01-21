5dbb993152fc900bbf7a39049f8e9392ae33ab01
Barry DeZonia <bdezonia@wisc.edu>
2010-01-21 12:29:40 -0600

    Added IJInfo class to hold info related to underlying IJ version, etc. Useful for defining more rigorous tests for future use that currently fail with original IJ.
    This used to be revision r1037.

---
+++ ij/io/IJInfo.java
@@ -0,0 +1,9 @@
+package ij.io;
+
+// class to hold various info about underlying IJ
+public class IJInfo {
+	// BDZ: I had an issue where I was generating tests that failed with the original IJ. Rather than comment out the
+	//   tests I have created this constant for runtime checking so that more rigorous tests can be exercised after
+	//   the refactor and when those changed systems have been hardened.
+	public static final testVsOrigIJ = true;
+}
