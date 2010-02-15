57e8000240aa9c57ce2055f891e55287d0409ab9
Barry DeZonia <bdezonia@wisc.edu>
2010-02-15 10:10:33 -0600

    IJInfo moved from package ij.io to package src. Other files edited to reflect this change.
    This used to be revision r1093.

--- ij/io/FileInfoTest.java
+++ ij/io/FileInfoTest.java
@@ -2,14 +2,13 @@ package ij.io;
 
 import static org.junit.Assert.*;
 
-import java.io.ByteArrayInputStream;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import ij.io.FileInfo;
 
 public class FileInfoTest {
