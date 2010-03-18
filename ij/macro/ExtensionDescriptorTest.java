a675f8b25e6740d5a239f9c26f2e5df07999bf03
Rick <Rick@be23795f-a60e-40d4-ba19-055798872e57>
2010-03-18 20:17:32 +0000

    Switched back to testing... focus on the macro's package
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1129 be23795f-a60e-40d4-ba19-055798872e57

---
+++ ij/macro/ExtensionDescriptorTest.java
@@ -0,0 +1,94 @@
+package ij.macro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExtensionDescriptorTest {
+
+	@Test
+	public void testExtensionDescriptor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewDescriptorStringMacroExtensionIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewDescriptorStringMacroExtension() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewDescriptorStringMacroExtensionInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewDescriptorStringMacroExtensionIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewDescriptorStringMacroExtensionIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewDescriptorStringMacroExtensionIntIntIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNewDescriptorStringMacroExtensionIntegerArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOptionalArg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsOutputArg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRawType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckArguments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTypeName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testParseArgumentList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvertVariable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvertOutputType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDispatch() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ ij/macro/VariableTest.java
@@ -0,0 +1,94 @@
+package ij.macro;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VariableTest {
+
+	@Test
+	public void testVariable() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVariableDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVariableIntDoubleString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVariableIntDoubleStringVariableArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVariableByteArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVariableIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVariableDoubleArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testConvertToDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetValue() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+}
