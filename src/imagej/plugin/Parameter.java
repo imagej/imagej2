cb65306458ccbd4ba525ca50ca7b58fc790e626d
GBH <GBH@be23795f-a60e-40d4-ba19-055798872e57>
2010-03-26 20:08:51 +0000

    The Grant Scheme for Abstract Plugins and stuff... and his first commit.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1151 be23795f-a60e-40d4-ba19-055798872e57

--- src/imagej/plugin/Parameter.java
+++ src/imagej/plugin/Parameter.java
@@ -1,16 +1,31 @@
 package imagej.plugin;
 
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
+/*
  * @author Johannes Schindelin johannes.schindelin at imagejdev.org
+ * @author Grant Harris gharris at mbl.edu
  */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Parameter {
-	String label() default "";
-	boolean isOutput() default false;
+    // attributes...
+	String label() default "";        // label to display in input widget
+    int digits() default 2;           // number of digits to right of decimal point
+	  int columns() default 6;          // width of field in characters
+    String units() default "";        // a string displayed to the right of the field
+    boolean required() default false; //++ gbh
+    String widget() default "";       // widget to use for input
+    //
+	  boolean output() default false;
 }
+
+/*
+How to set default value of parameter
+Persistence?
+ */
--- src/imagej/plugin/PlugInException.java
+++ src/imagej/plugin/PlugInException.java
@@ -1,8 +1,6 @@
 package imagej.plugin;
 
-/**
- * @author Johannes Schindelin johannes.schindelin at imagejdev.org
- */
+
 public class PlugInException extends Exception {
 	public PlugInException() {
 		super();
