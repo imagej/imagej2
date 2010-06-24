fe2d45bfb762da93c5dfbc10de592fd8e3ffb4d6
Barry DeZonia <bdezonia@wisc.edu>
2010-06-24 16:28:36 -0500

    Added another test image
    This used to be revision r1238.

--- tests/ij/gui/WandTest.java
+++ tests/ij/gui/WandTest.java
@@ -10,6 +10,11 @@ public class WandTest {
 
 	// data
 	
+	private static byte[] Pixels7x3 = new byte[]
+	                                   	    {0,0,0,0,0,0,0,
+	                                   		 0,0,1,1,1,0,0,
+	                                   		 0,0,0,0,0,0,0};
+	
 	private static byte[] Pixels5x5 = new byte[]
 	    {0,0,0,0,0,
 		 0,0,1,0,0,
@@ -83,30 +88,54 @@ public class WandTest {
 		w.autoOutline(2, 2, 1.0, 1.0, 0);
 		outlineTest(w,new int[]{4,3,3,2,2,1,1,2,2,3,3,4},new int[]{3,3,4,4,3,3,2,2,1,1,2,2});
 
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 1.0, 1.0, 0);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
+
 		// LEGACY_MODE
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 1.0, 1.0, Wand.LEGACY_MODE);
 		outlineTest(w,new int[]{3,2,2,3},new int[]{2,2,3,3});
+
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 1.0, 1.0, Wand.LEGACY_MODE);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
 
 		// EIGHT_CONNECTED
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 1.0, 1.0, Wand.EIGHT_CONNECTED);
 		outlineTest(w,new int[]{4,3,3,2,2,1,1,2,2,3,3,4},new int[]{3,3,4,4,3,3,2,2,1,1,2,2});
 
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 1.0, 1.0, Wand.EIGHT_CONNECTED);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
+
 		// FOUR_CONNECTED
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 1.0, 1.0, Wand.FOUR_CONNECTED);
 		outlineTest(w,new int[]{3,4,4,3},new int[]{2,2,3,3});
+
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 1.0, 1.0, Wand.FOUR_CONNECTED);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
 
 		// LEGACY_MODE AND EIGHT_CONNECTED
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 1.0, 1.0, Wand.LEGACY_MODE|Wand.EIGHT_CONNECTED);
 		outlineTest(w,new int[]{3,2,2,3},new int[]{2,2,3,3});
 
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 1.0, 1.0, Wand.LEGACY_MODE|Wand.EIGHT_CONNECTED);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
+
 		// LEGACY_MODE AND FOUR_CONNECTED
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 1.0, 1.0, Wand.LEGACY_MODE|Wand.FOUR_CONNECTED);
 		outlineTest(w,new int[]{3,2,2,3},new int[]{2,2,3,3});
+
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 1.0, 1.0, Wand.LEGACY_MODE|Wand.FOUR_CONNECTED);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
 	}
 
 	@Test
@@ -115,6 +144,10 @@ public class WandTest {
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 1.0, 1.0);
 		outlineTest(w,new int[]{3,2,2,3},new int[]{2,2,3,3});
+
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 1.0, 1.0, 0);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
 	}
 
 	@Test
@@ -123,6 +156,10 @@ public class WandTest {
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 1, 1);
 		outlineTest(w,new int[]{3,2,2,3},new int[]{2,2,3,3});
+
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 1, 1);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
 	}
 
 	@Test
@@ -131,6 +168,10 @@ public class WandTest {
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2);
 		outlineTest(w,new int[]{3,2,2,3},new int[]{3,3,2,2});
+
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
 	}
 
 	@Test
@@ -140,30 +181,54 @@ public class WandTest {
 		w.autoOutline(2, 2, 0.5, 0);
 		outlineTest(w,new int[]{5,0,0,5},new int[]{5,5,0,0});
 
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 0.5, 0);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
+
 		// LEGACY_MODE
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 0.5, Wand.LEGACY_MODE);
 		outlineTest(w,new int[]{5,0,0,5},new int[]{5,5,0,0});
+
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 0.5, Wand.LEGACY_MODE);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
 
 		// EIGHT_CONNECTED
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 0.5, Wand.EIGHT_CONNECTED);
 		outlineTest(w,new int[]{5,0,0,5},new int[]{5,5,0,0});
 
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 0.5, Wand.EIGHT_CONNECTED);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
+
 		// FOUR_CONNECTED
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 0.5, Wand.FOUR_CONNECTED);
 		outlineTest(w,new int[]{3,2,2,3},new int[]{3,3,2,2});
+
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 0.5, Wand.FOUR_CONNECTED);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
 
 		// LEGACY_MODE and EIGHT_CONNECTED
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 0.5, Wand.LEGACY_MODE|Wand.EIGHT_CONNECTED);
 		outlineTest(w,new int[]{5,0,0,5},new int[]{5,5,0,0});
 
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 0.5, Wand.LEGACY_MODE|Wand.EIGHT_CONNECTED);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
+
 		// LEGACY_MODE and FOUR_CONNECTED
 		w = newWand(5,5,Pixels5x5);
 		w.autoOutline(2, 2, 0.5, Wand.LEGACY_MODE|Wand.FOUR_CONNECTED);
 		outlineTest(w,new int[]{3,2,2,3},new int[]{3,3,2,2});
+
+		w = newWand(7,3,Pixels7x3);
+		w.autoOutline(2, 1, 0.5, Wand.LEGACY_MODE|Wand.FOUR_CONNECTED);
+		outlineTest(w,new int[]{5,2,2,5},new int[]{2,2,1,1});
 	}
 
 	@Test
