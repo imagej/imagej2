8179b4535ba50635e7b520ab1d940417a83f9a34
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2010-07-28 21:18:27 +0000

    Further implementation of main() and applyTable().
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1265 be23795f-a60e-40d4-ba19-055798872e57

--- src/ij/process/ImgLibProcessor.java
+++ src/ij/process/ImgLibProcessor.java
@@ -1,5 +1,6 @@
 package ij.process;
 
+import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
 import ij.ImageStack;
@@ -12,11 +13,18 @@ import java.io.File;
 import javax.swing.JFileChooser;
 
 import loci.common.DataTools;
+import mpicbg.imglib.container.Container;
 import mpicbg.imglib.container.ContainerFactory;
 import mpicbg.imglib.container.array.ArrayContainerFactory;
+import mpicbg.imglib.container.imageplus.ImagePlusContainer;
+import mpicbg.imglib.cursor.Cursor;
 import mpicbg.imglib.cursor.LocalizableByDimCursor;
+import mpicbg.imglib.exception.ImgLibException;
 import mpicbg.imglib.image.Image;
+import mpicbg.imglib.image.display.imagej.ImageJFunctions;
 import mpicbg.imglib.io.LOCI;
+
+import mpicbg.imglib.type.Type;
 import mpicbg.imglib.type.numeric.RealType;
 import mpicbg.imglib.type.numeric.integer.ByteType;
 import mpicbg.imglib.type.numeric.integer.IntType;
@@ -52,40 +60,80 @@ public class ImgLibProcessor<T extends RealType<T>> extends ImageProcessor {
 	// TODO: How can we use generics here without breaking javac?
 	@SuppressWarnings("rawtypes")
 	private final RealType type;
-
+	private final int[] extraDimensions;
 	/**
-   * Dimensional coordinates for this processor's plane.
-   * Array length equals <code>imageData.getDimensions().length - 2</code>,
-   * since the first two dimensions are assumed to be X and Y, respectively.
-   */
-	private final int[] coords;
-
+	 * */
 	private byte[] pixels8;
 
-	public ImgLibProcessor(Image<T> img, T type, int[] coords) {
+	public ImgLibProcessor(Image<T> img, T type ) {
 		this.imageData = img;
 		this.type = type;
 		final int[] dims = img.getDimensions();
+		
+		//define a measure of the planes
+		extraDimensions = new int[dims.length - 2];
+		
+		
 		if (dims.length < 2) {
 			throw new IllegalArgumentException("Image must be at least 2-D");
+		} else {
+			//populate the array
+		
+			for (int i = 0;i < extraDimensions.length;i++)
+			{
+				extraDimensions[i] = dims[i+2];
+			}
 		}
+
 		this.width = dims[0]; // TODO: Dimensional labels are safer way to find X
 		this.height = dims[1]; // TODO: Dimensional labels are safer way to find Y
-		if (coords == null) {
-			this.coords = new int[dims.length - 2];
-		}
-		else if (coords.length == dims.length - 2) {
-			this.coords = coords.clone();
-		}
-		else {
-			throw new IllegalArgumentException("Dimensional mismatch: image is " +
-				dims.length + "-D but coords array length is " + coords.length);
-		}
+	
 	}
 
 	@Override
 	public void applyTable(int[] lut) {
-    throw new RuntimeException("Unimplemented");
+		if (type instanceof UnsignedByteType)
+		{
+			if (lut.length!=256)
+				throw new IllegalArgumentException("lut.length!=256");
+			
+			 //Fill the image with data - first get a cursor
+			final Cursor<UnsignedByteType> imageCursor = (Cursor<UnsignedByteType>) imageData.createCursor( );
+
+			for (final UnsignedByteType pixel:imageCursor)
+			{
+				//System.out.print("pix "+pixel.get()+" lut"+lut[pixel.get()]);
+				pixel.set( lut[ pixel.get() ] );
+				//System.out.println( " value is now " + pixel.get()  );
+   			}
+			
+			//close the cursor?
+			imageCursor.close( );
+			
+			//TODO - we are not doing old Roi code yet
+		}
+		else if (type instanceof UnsignedShortType)
+		{
+			if (lut.length!=65536)
+				throw new IllegalArgumentException("lut.length!=65536");
+			
+			 //Fill the image with data - first get a cursor
+			final Cursor<UnsignedShortType> imageCursor = (Cursor<UnsignedShortType>) imageData.createCursor( );
+
+			for (final UnsignedShortType pixel:imageCursor)
+			{
+				//System.out.print("pix "+pixel.get()+" lut"+lut[pixel.get()]);
+				pixel.set( lut[ pixel.get() ] );
+				//System.out.println( " value is now " + pixel.get()  );
+   			}
+			
+			//close the cursor?
+			imageCursor.close( );
+			
+			//TODO - we are not doing old Roi code yet
+		}
+		else
+			throw new RuntimeException("applyTable() : unsupported pixel layout "+type.toString());
 
 	}
 
@@ -456,39 +504,39 @@ public class ImgLibProcessor<T extends RealType<T>> extends ImageProcessor {
 	public Object getPixelsArray() {
 		if (type instanceof ByteType) {
 			Image<ByteType> im = (Image) imageData;
-			return getPlaneBytes(im, width, height, coords);
+			return getPlaneBytes(im, width, height, extraDimensions);
 		}
 		if (type instanceof UnsignedByteType) {
 			Image<UnsignedByteType> im = (Image) imageData;
-			return getPlaneUnsignedBytes(im, width, height, coords);
+			return getPlaneUnsignedBytes(im, width, height, extraDimensions);
 		}
 		if (type instanceof ShortType) {
 			Image<ShortType> im = (Image) imageData;
-			return getPlaneShorts(im, width, height, coords);
+			return getPlaneShorts(im, width, height, extraDimensions);
 		}
 		if (type instanceof UnsignedShortType) {
 			Image<UnsignedShortType> im = (Image) imageData;
-			return getPlaneUnsignedShorts(im, width, height, coords);
+			return getPlaneUnsignedShorts(im, width, height, extraDimensions);
 		}
 		if (type instanceof IntType) {
 			Image<IntType> im = (Image) imageData;
-			return getPlaneInts(im, width, height, coords);
+			return getPlaneInts(im, width, height, extraDimensions);
 		}
 		if (type instanceof UnsignedIntType) {
 			Image<UnsignedIntType> im = (Image) imageData;
-			return getPlaneUnsignedInts(im, width, height, coords);
+			return getPlaneUnsignedInts(im, width, height, extraDimensions);
 		}
 		if (type instanceof LongType) {
 			Image<LongType> im = (Image) imageData;
-			return getPlaneLongs(im, width, height, coords);
+			return getPlaneLongs(im, width, height, extraDimensions);
 		}
 		if (type instanceof FloatType) {
 			Image<FloatType> im = (Image) imageData;
-			return getPlaneFloats(im, width, height, coords);
+			return getPlaneFloats(im, width, height, extraDimensions);
 		}
 		if (type instanceof DoubleType) {
 			Image<DoubleType> im = (Image) imageData;
-			return getPlaneDoubles(im, width, height, coords);
+			return getPlaneDoubles(im, width, height, extraDimensions);
 		}
 		return getPlaneData();
 	}
@@ -499,7 +547,7 @@ public class ImgLibProcessor<T extends RealType<T>> extends ImageProcessor {
 		final double[] data = new double[width * height];
 		final LocalizableByDimCursor<T> cursor =
 			imageData.createLocalizableByDimCursor();
-		final int[] pos = makePosArray(coords);
+		final int[] pos = makePosArray(extraDimensions);
 		int index = 0;
 		for (int y=0; y<height; y++) {
 			pos[1] = y;
@@ -692,6 +740,57 @@ public class ImgLibProcessor<T extends RealType<T>> extends ImageProcessor {
 		return pos;
 	}
 
+	public static <T extends RealType<T>> void display(Image<T> img,
+			    String title)
+			  {
+			    ImagePlus imp = null;
+			    Container<T> c = img.getContainer();
+			    if (c instanceof ImagePlusContainer<?, ?>) {
+			      ImagePlusContainer<T, ?> ipc = (ImagePlusContainer<T, ?>) c;
+			      try {
+			        imp = ipc.getImagePlus();
+			      }
+			      catch (ImgLibException exc) {
+			        IJ.log("Warning: " + exc.getMessage());
+			      }
+			    }
+			    if (imp == null) {
+			      imp = ImageJFunctions.copyToImagePlus(img);
+			    }
+			    if (title != null) imp.setTitle(title);
+			    imp.show();
+			  }
+
+	//public <T extends Type<T>> ImgLibProcessor(final Image<T> img)
+	//{
+	//	
+	//}
+/*	
+	public static <T extends RealType<T>> ImagePlus createImagePlus(final Image<T> img)
+	{
+		ImageProcessor processor = new ImgLibProcessor<T>(img, img.createCursor().getType() );
+		ImagePlus imp = new ImagePlus(img.getName(),processor);
+		return imp;
+	}
+*/
+	
+	public static ImagePlus createImagePlus(final Image<?> img)
+	{
+		ImageProcessor processor = null;
+		
+		Type runtimeT = img.createCursor().getType();
+		
+		if (runtimeT instanceof UnsignedByteType)
+		{
+			processor = new ImgLibProcessor<UnsignedByteType>((Image<UnsignedByteType>)img, new UnsignedByteType());
+		}
+			
+		if (processor == null)
+			throw new IllegalArgumentException("no processor type was matched");
+		
+		return new ImagePlus(img.getName(),processor);
+	}
+	
 	public static void main(String[] args) {
 		final JFileChooser chooser = new JFileChooser();
 		int rval = chooser.showOpenDialog(null);
@@ -699,12 +798,39 @@ public class ImgLibProcessor<T extends RealType<T>> extends ImageProcessor {
 		final File file = chooser.getSelectedFile();
 		final String fileName = file.getAbsolutePath();
 		final ContainerFactory containerFactory = new ArrayContainerFactory();
-		final Image<UnsignedByteType> image =
-			LOCI.openLOCIUnsignedByteType(fileName, containerFactory);
+		//Image<?> image = LOCI.openLOCI( fileName, containerFactory );
+		final Image<UnsignedByteType> image = LOCI.openLOCIUnsignedByteType(fileName, containerFactory);
+		
+		// make our image plus from inglib image
+		final ImagePlus imp = createImagePlus(image);
+		
+		// methods to test to make sure they work
+		
+		// invert()
+		// works : imp.getProcessor().invert();
+		
+		// applyTable()
+		
+		int[] lut = new int[256];
+		for (int i = 0; i < 256; i++)
+			lut[i] = 255-i;
+		
+		// works: imp.getProcessor().applyTable(lut);
+		
+		// TODO
+		//convolve(float[] kernel, int kernelWidth, int kernelHeight)
+		//convolve3x3(int[] kernel)
+		//copyBits(ImageProcessor ip, int xloc, int yloc, int mode)
+		// etc.
+		
+		new ImageJ();
+		imp.show();
+
+		/*
 		final int[] dims = image.getDimensions();
 		final int width = dims[0];
 		final int height = dims[1];
-		final int sizeZ = dims[2];
+		final int sizeZ = 1;
 		final int[] coords = new int[1];
 		final ImageStack imageStack = new ImageStack(width, height);
 		for (int i =0; i<sizeZ; i++) {
@@ -713,9 +839,7 @@ public class ImgLibProcessor<T extends RealType<T>> extends ImageProcessor {
 				new UnsignedByteType(), coords);
 			imageStack.addSlice("" + (i + 1), ip);
 		}
-		ImagePlus imp = new ImagePlus(fileName, imageStack);
-    new ImageJ();
-    imp.show();
-	}
+		*/
 
+	}
 }
