a41dcfea814e4c02c54667b1c699afdc20479232
Barry DeZonia <bdezonia@wisc.edu>
2010-08-11 13:09:11 -0500

    further refactoring and tests
    This used to be revision r1311.

---
+++ src/ij/process/TypeManager.java
@@ -0,0 +1,43 @@
+package ij.process;
+
+import mpicbg.imglib.type.numeric.IntegerType;
+import mpicbg.imglib.type.numeric.RealType;
+import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
+import mpicbg.imglib.type.numeric.integer.UnsignedIntType;
+import mpicbg.imglib.type.numeric.integer.UnsignedShortType;
+
+public class TypeManager {
+
+	// TODO is there a better way? ask.
+	//   Note - needed to go from type T to type RealType as our Hudson wouldn't build even though Eclipse can 
+	public static boolean isUnsignedType(RealType t) {
+		return (
+			(t instanceof UnsignedByteType) ||
+			(t instanceof UnsignedIntType) ||
+			(t instanceof UnsignedShortType)
+		);
+	}
+
+	// TODO is there a better way? ask.
+	//   Note - needed to go from type T to type RealType as our Hudson wouldn't build even though Eclipse can 
+	public static boolean isIntegralType(RealType t) {
+		return (t instanceof IntegerType);
+	}
+	
+	/**
+	 * Limits and returns the range of the input value
+	 * to the corresponding max and min values respective to the
+	 * underlying type.
+	 */
+	public static int boundIntValueToType(RealType type, int inputValue)
+	{
+		if (isIntegralType(type))
+		{
+			if (inputValue < type.getMinValue() ) inputValue = (int) type.getMinValue();
+			if (inputValue > type.getMaxValue() ) inputValue = (int) type.getMaxValue();
+		}
+
+		return inputValue;
+	}
+
+}
