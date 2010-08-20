237b40de96ec628510f49cc4619b3079bd6ffeac
Barry DeZonia <bdezonia@wisc.edu>
2010-08-20 17:27:16 -0500

    prep work before pulling out GenericBlitter
    This used to be revision r1334.

--- src/ij/process/TypeManager.java
+++ src/ij/process/TypeManager.java
@@ -2,15 +2,21 @@ package ij.process;
 
 import mpicbg.imglib.type.numeric.IntegerType;
 import mpicbg.imglib.type.numeric.RealType;
+import mpicbg.imglib.type.numeric.integer.ByteType;
+import mpicbg.imglib.type.numeric.integer.IntType;
+import mpicbg.imglib.type.numeric.integer.LongType;
+import mpicbg.imglib.type.numeric.integer.ShortType;
 import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
 import mpicbg.imglib.type.numeric.integer.UnsignedIntType;
 import mpicbg.imglib.type.numeric.integer.UnsignedShortType;
+import mpicbg.imglib.type.numeric.real.DoubleType;
+import mpicbg.imglib.type.numeric.real.FloatType;
 
 public class TypeManager {
 
 	// TODO is there a better way? ask.
 	//   Note - needed to go from type T to type RealType as our Hudson wouldn't build even though Eclipse can 
-	public static boolean isUnsignedType(RealType t) {
+	public static boolean isUnsignedType(RealType<?> t) {
 		return (
 			(t instanceof UnsignedByteType) ||
 			(t instanceof UnsignedIntType) ||
@@ -20,8 +26,8 @@ public class TypeManager {
 
 	// TODO is there a better way? ask.
 	//   Note - needed to go from type T to type RealType as our Hudson wouldn't build even though Eclipse can 
-	public static boolean isIntegralType(RealType t) {
-		return (t instanceof IntegerType);
+	public static boolean isIntegralType(RealType<?> t) {
+		return (t instanceof IntegerType<?>);
 	}
 	
 	/**
@@ -40,4 +46,35 @@ public class TypeManager {
 		return inputValue;
 	}
 
+	public static boolean sameKind(RealType<?> type1, RealType<?> type2)
+	{
+		if ((type1 instanceof ByteType) && (type2 instanceof ByteType))
+		 	return true;
+		
+		else if ((type1 instanceof UnsignedByteType) && (type2 instanceof UnsignedByteType))
+		 	return true;
+		
+		else if ((type1 instanceof ShortType) && (type2 instanceof ShortType))
+			return true;
+		
+		else if ((type1 instanceof UnsignedShortType) && (type2 instanceof UnsignedShortType))
+			return true;
+		
+		else if ((type1 instanceof IntType) && (type2 instanceof IntType))
+			return true;
+		
+		else if ((type1 instanceof UnsignedIntType) && (type2 instanceof UnsignedIntType))
+			return true;
+		
+		else if ((type1 instanceof LongType) && (type2 instanceof LongType))
+			return true;
+		
+		else if ((type1 instanceof FloatType) && (type2 instanceof FloatType))
+			return true;
+		
+		else if ((type1 instanceof DoubleType) && (type2 instanceof DoubleType))
+			return true;
+		
+		return false;
+	}
 }
