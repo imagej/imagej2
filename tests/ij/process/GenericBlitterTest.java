daf732962bcd386e1c2bd7b93f0ed40798c75adb
Barry DeZonia <bdezonia@wisc.edu>
2010-08-23 14:49:47 -0500

    create empty test skeletons for all new classes. actual testing takes place in ImgLibProcessorTest.
    This used to be revision r1341.

---
+++ tests/ij/process/GenericBlitterTest.java
@@ -0,0 +1,13 @@
+package ij.process;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class GenericBlitterTest {
+	@Test
+	public void testNothing()
+	{
+		// TODO : this class is exercised by ImgLibProcessor::copyBits(). Tested in ImgLibProcessorTest.
+		assertTrue(true);
+	}
+}
---
+++ tests/ij/process/SetFloatValuesOperationTest.java
@@ -0,0 +1,13 @@
+package ij.process;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SetFloatValuesOperationTest {
+	@Test
+	public void testNothing()
+	{
+		// TODO : this class is exercised by ImgLibProcessor::duplicate() and toFloat(). Tested in ImgLibProcessorTest.
+		assertTrue(true);
+	}
+}
