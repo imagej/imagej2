5edb0e269f08a596c926bc171d516f7b3f85631f
Curtis Rueden <ctrueden@wisc.edu>
2010-09-09 16:34:50 -0500

    Maven-based ImageJ project.
    This used to be revision r1384.

--- compile.bat
+++
@@ -1,5 +0,0 @@
-javac src\ij\ImageJ.java
-javac src\ij\plugin\*.java
-javac src\ij\plugin\filter\*.java
-javac src\ij\plugin\frame\*.java
-java ij.ImageJ
--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "Cleaning up ... "
-ant clean 
-echo "Building ImageJA ..."
-ant
