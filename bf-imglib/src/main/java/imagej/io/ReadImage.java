5934e70c6df93a075bde0098371dc0888705da29
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2010-09-15 18:52:03 +0000

    Skeleton for bf-imglib project.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1403 be23795f-a60e-40d4-ba19-055798872e57

---
+++ bf-imglib/src/main/java/imagej/io/ReadImage.java
@@ -0,0 +1,38 @@
+//
+// ReadImage.java
+//
+
+/*
+Bio-Formats support for ImgLib.
+Copyright (C) 2010, UW-Madison LOCI
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+package imagej.io;
+
+/**
+ * TODO
+ *
+ * <dl><dt><b>Source code:</b></dt>
+ * <dd><a href="http://dev.imagejdev.org/trac/java/browser/trunk/projects/bf-imglib/src/main/java/imagej/io/ReadImage.java">Trac</a>,
+ * <a href="http://dev.imagejdev.org/svn/java/trunk/projects/bf-imglib/src/main/java/imagej/io/ReadImage.java">SVN</a></dd></dl>
+ */
+public class ReadImage {
+
+  public static void main(String[] args) {
+  }
+
+}
