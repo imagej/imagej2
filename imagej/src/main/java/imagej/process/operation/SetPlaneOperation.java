dae0d435769659eae52b3ed0f08736df48b930b2
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2010-09-17 16:27:59 +0000

    attempt to improve clarity and speed of SetPlane oriented methods
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1418 be23795f-a60e-40d4-ba19-055798872e57

--- imagej/src/main/java/imagej/process/operation/SetPlaneOperation.java
+++ imagej/src/main/java/imagej/process/operation/SetPlaneOperation.java
@@ -10,76 +10,179 @@ public class SetPlaneOperation<T extends RealType<T>> extends PositionalSingleCu
 	public static enum PixelType {BYTE,SHORT,INT,FLOAT,DOUBLE,LONG};
 
 	// set in constructor
-	private Object pixels;
 	private PixelType pixType;
-	private boolean isUnsigned;
+	private PixelReader reader;
 	
 	// set before iteration
 	private int pixNum;
-	private RealType<?> type;
 	private boolean isIntegral;
 	
 	public SetPlaneOperation(Image<T> theImage, int[] origin, Object pixels, PixelType inputType, boolean isUnsigned)
 	{
 		super(theImage, origin, Span.singlePlane(theImage.getDimension(0), theImage.getDimension(1), theImage.getNumDimensions()));
-		this.pixels = pixels;
 		this.pixType = inputType;
-		this.isUnsigned = isUnsigned;
+		
+		switch (pixType)
+		{
+			case BYTE:
+				if (isUnsigned)
+					this.reader = new UnsignedByteReader(pixels);
+				else
+					this.reader = new ByteReader(pixels);
+				break;
+			case SHORT:
+				if (isUnsigned)
+					this.reader = new UnsignedShortReader(pixels);
+				else
+					this.reader = new ShortReader(pixels);
+				break;
+			case INT:
+				if (isUnsigned)
+					this.reader = new UnsignedIntReader(pixels);
+				else
+					this.reader = new IntReader(pixels);
+				break;
+			case LONG:
+				this.reader = new LongReader(pixels);
+				break;
+			case FLOAT:
+				this.reader = new FloatReader(pixels);
+				break;
+			case DOUBLE:
+				this.reader = new DoubleReader(pixels);
+				break;
+			default:
+				throw new IllegalArgumentException("unknown pixel type");
+		}
+		
 	}
 	
 	@Override
-	public void beforeIteration(RealType<T> type) {
+	public void beforeIteration(RealType<T> type)
+	{
 		this.pixNum = 0;
-		this.type = type;
 		this.isIntegral = TypeManager.isIntegralType(type);
 	}
 
 	@Override
-	public void insideIteration(int[] position, RealType<T> sample) {
+	public void insideIteration(int[] position, RealType<T> sample)
+	{
 
-		double inputPixValue = getPixValue(pixels, pixType, isUnsigned, this.pixNum++);
+		double pixelValue = reader.getValue(this.pixNum++);
 		
 		if (this.isIntegral)
-			inputPixValue = TypeManager.boundValueToType(this.type, inputPixValue);
+			pixelValue = TypeManager.boundValueToType(sample, pixelValue);
 		
-		sample.setReal(inputPixValue);
+		sample.setReal(pixelValue);
 	}
 
 	@Override
-	public void afterIteration() {
+	public void afterIteration()
+	{
 	}
 	
-	private double getPixValue(Object pixels, PixelType inputType, boolean unsigned, int pixNum)
+	private interface PixelReader
 	{
-		switch (inputType) {
-			case BYTE:
-				byte b = ((byte[])pixels)[pixNum];
-				if ((unsigned) && (b < 0))
-					return 256.0 + b;
-				else
-					return b;
-			case SHORT:
-				short s = ((short[])pixels)[pixNum];
-				if ((unsigned) && (s < 0))
-					return 65536.0 + s;
-				else
-					return s;
-			case INT:
-				int i = ((int[])pixels)[pixNum];
-				if ((unsigned) && (i < 0))
-					return 4294967296.0 + i;
-				else
-					return i;
-			case FLOAT:
-				return ((float[])pixels)[pixNum];
-			case DOUBLE:
-				return ((double[])pixels)[pixNum];
-			case LONG:
-				return ((long[])pixels)[pixNum];  // TODO : possible precision loss here. Also unsigned not supported here.
-			default:
-				throw new IllegalArgumentException("unknown pixel type");
+		double getValue(int i);
+	}
+	
+	private class ByteReader implements PixelReader
+	{
+		byte[] pixels;
+		
+		ByteReader(Object pixels) { this.pixels = (byte[]) pixels; }
+		
+		public double getValue(int i) { return pixels[i]; }
+	}
+	
+	private class UnsignedByteReader implements PixelReader
+	{
+		byte[] pixels;
+		
+		UnsignedByteReader(Object pixels) { this.pixels = (byte[]) pixels; }
+		
+		public double getValue(int i)
+		{
+			double pixel = pixels[i];
+			if (pixel < 0)
+				pixel = 256.0 + pixel;
+			return pixel;
 		}
 	}
-
+	
+	private class ShortReader implements PixelReader
+	{
+		short[] pixels;
+		
+		ShortReader(Object pixels) { this.pixels = (short[]) pixels; }
+		
+		public double getValue(int i) { return pixels[i]; }
+	}
+	
+	private class UnsignedShortReader implements PixelReader
+	{
+		short[] pixels;
+		
+		UnsignedShortReader(Object pixels) { this.pixels = (short[]) pixels; }
+		
+		public double getValue(int i)
+		{
+			double pixel = pixels[i];
+			if (pixel < 0)
+				pixel = 65536.0 + pixel;
+			return pixel;
+		}
+	}
+	
+	private class IntReader implements PixelReader
+	{
+		int[] pixels;
+		
+		IntReader(Object pixels) { this.pixels = (int[]) pixels; }
+		
+		public double getValue(int i) { return pixels[i]; }
+	}
+	
+	private class UnsignedIntReader implements PixelReader
+	{
+		int[] pixels;
+		
+		UnsignedIntReader(Object pixels) { this.pixels = (int[]) pixels; }
+		
+		public double getValue(int i)
+		{
+			double pixel = pixels[i];
+			if (pixel < 0)
+				pixel = 4294967296.0 + pixel;
+			return pixel;
+		}
+	}
+	
+	private class LongReader implements PixelReader
+	{
+		long[] pixels;
+		
+		LongReader(Object pixels) { this.pixels = (long[]) pixels; }
+		
+		public double getValue(int i) { return pixels[i]; }
+	}
+	
+	private class FloatReader implements PixelReader
+	{
+		float[] pixels;
+		
+		FloatReader(Object pixels) { this.pixels = (float[]) pixels; }
+		
+		public double getValue(int i) { return pixels[i]; }
+	}
+	
+	private class DoubleReader implements PixelReader
+	{
+		double[] pixels;
+		
+		DoubleReader(Object pixels) { this.pixels = (double[]) pixels; }
+		
+		public double getValue(int i) { return pixels[i]; }
+	}
 }
 
