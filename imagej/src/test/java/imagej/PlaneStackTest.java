45bb3410bd2c0e234e5ba78dadf47cc128656f61
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2010-10-26 16:52:19 +0000

    * make PlaneStack(Image<T> image) set the factory to image's factory   * add getPlane()   * get under test
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1608 be23795f-a60e-40d4-ba19-055798872e57

---
+++ imagej/src/test/java/imagej/PlaneStackTest.java
@@ -0,0 +1,265 @@
+package imagej;
+
+import static org.junit.Assert.*;
+import imagej.process.ImageUtils;
+
+import org.junit.Test;
+
+import mpicbg.imglib.container.array.ArrayContainerFactory;
+import mpicbg.imglib.image.Image;
+import mpicbg.imglib.type.numeric.integer.UnsignedByteType;
+import mpicbg.imglib.type.numeric.integer.UnsignedIntType;
+
+
+public class PlaneStackTest {
+	
+	@Test
+	public void testPlaneStackImage()
+	{
+		Image<UnsignedByteType> image =
+			ImageUtils.createImage(new UnsignedByteType(), new ArrayContainerFactory(), new int[]{2,3,4});
+		
+		PlaneStack<UnsignedByteType> stack = new PlaneStack<UnsignedByteType>(image);
+		
+		assertNotNull(stack);
+		assertEquals(4, stack.getNumPlanes());
+	}
+
+	@Test
+	public void testPlaneStackIntIntFactory()
+	{
+		PlaneStack<UnsignedIntType> stack;
+
+		try {
+			stack = new PlaneStack<UnsignedIntType>(-1,-1,new ArrayContainerFactory());
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+
+		try {
+			stack = new PlaneStack<UnsignedIntType>(0,0,new ArrayContainerFactory());
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+
+		stack = new PlaneStack<UnsignedIntType>(1,5,new ArrayContainerFactory());
+		assertNotNull(stack);
+		assertEquals(0, stack.getNumPlanes());
+	}
+
+	@Test
+	public void testGetStorage()
+	{
+		Image<UnsignedByteType> image =
+			ImageUtils.createImage(new UnsignedByteType(), new ArrayContainerFactory(), new int[]{2,3,4});
+		
+		PlaneStack<UnsignedByteType> stack = new PlaneStack<UnsignedByteType>(image);
+		
+		assertSame(image, stack.getStorage());
+	}
+
+	@Test
+	public void testGetNumPlanes()
+	{
+		Image<UnsignedByteType> image =
+			ImageUtils.createImage(new UnsignedByteType(), new ArrayContainerFactory(), new int[]{2,3,4});
+		
+		PlaneStack<UnsignedByteType> stack = new PlaneStack<UnsignedByteType>(image);
+		
+		assertEquals(4, stack.getNumPlanes());
+	}
+
+	@Test
+	public void testGetEndPosition()
+	{
+		Image<UnsignedByteType> image =
+			ImageUtils.createImage(new UnsignedByteType(), new ArrayContainerFactory(), new int[]{2,3,4});
+		
+		PlaneStack<UnsignedByteType> stack = new PlaneStack<UnsignedByteType>(image);
+		
+		assertEquals(4, stack.getEndPosition());
+		
+		stack.addPlane(true, new byte[6]);
+		
+		assertEquals(5, stack.getEndPosition());
+	}
+
+	@Test
+	public void testInsertPlane()
+	{
+		PlaneStack<UnsignedByteType> stack = new PlaneStack<UnsignedByteType>(2,3,new ArrayContainerFactory());
+		
+		stack.addPlane(true, new byte[]{1,1,1,1,1,1});
+		stack.addPlane(true, new byte[]{2,2,2,2,2,2});
+		stack.addPlane(true, new byte[]{3,3,3,3,3,3});
+		stack.addPlane(true, new byte[]{4,4,4,4,4,4});
+
+		// insert before beginning
+		try {
+			stack.insertPlane(-1, true, new byte[]{6,6,6,6,6,6});
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+		
+		// insert after end+1
+		try {
+			stack.insertPlane(5, true, new byte[]{6,6,6,6,6,6});
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+		
+		// insert at front
+		stack.insertPlane(0, true, new byte[]{6,6,6,6,6,6});
+		assertEquals(6,((byte[])stack.getPlane(0))[0]);
+		assertEquals(1,((byte[])stack.getPlane(1))[0]);
+		assertEquals(2,((byte[])stack.getPlane(2))[0]);
+		assertEquals(3,((byte[])stack.getPlane(3))[0]);
+		assertEquals(4,((byte[])stack.getPlane(4))[0]);
+		
+		// insert in middle
+		stack.insertPlane(3, true, new byte[]{7,7,7,7,7,7});
+		assertEquals(6,((byte[])stack.getPlane(0))[0]);
+		assertEquals(1,((byte[])stack.getPlane(1))[0]);
+		assertEquals(2,((byte[])stack.getPlane(2))[0]);
+		assertEquals(7,((byte[])stack.getPlane(3))[0]);
+		assertEquals(3,((byte[])stack.getPlane(4))[0]);
+		assertEquals(4,((byte[])stack.getPlane(5))[0]);
+
+		// insert at end
+		stack.insertPlane(6, true, new byte[]{8,8,8,8,8,8});
+		assertEquals(6,((byte[])stack.getPlane(0))[0]);
+		assertEquals(1,((byte[])stack.getPlane(1))[0]);
+		assertEquals(2,((byte[])stack.getPlane(2))[0]);
+		assertEquals(7,((byte[])stack.getPlane(3))[0]);
+		assertEquals(3,((byte[])stack.getPlane(4))[0]);
+		assertEquals(4,((byte[])stack.getPlane(5))[0]);
+		assertEquals(8,((byte[])stack.getPlane(6))[0]);
+	}
+
+	@Test
+	public void testAddPlane()
+	{
+		Image<UnsignedByteType> image =
+			ImageUtils.createImage(new UnsignedByteType(), new ArrayContainerFactory(), new int[]{2,3,4});
+		
+		PlaneStack<UnsignedByteType> stack = new PlaneStack<UnsignedByteType>(image);
+		
+		assertEquals(4, stack.getEndPosition());
+
+		for (int i = 0; i < 5; i++)
+		{
+			byte[] bytes = new byte[6];
+			
+			for (int k = 0; k < bytes.length; k++)
+				bytes[k] = (byte) (i+1);
+			
+			stack.addPlane(true, bytes);
+
+			assertEquals(4+i+1, stack.getEndPosition());
+			
+			byte[] data = (byte[]) stack.getPlane(4+i);
+			
+			assertEquals(i+1, data[0]);
+		}
+	}
+
+	@Test
+	public void testDeletePlane()
+	{
+		PlaneStack<UnsignedByteType> stack;
+		
+		stack = new PlaneStack<UnsignedByteType>(2,3,new ArrayContainerFactory());
+
+		// try to delete from an empty stack
+		try {
+			stack.deletePlane(0);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+		
+		// now populate
+		stack.addPlane(true, new byte[]{1,1,1,1,1,1});
+		stack.addPlane(true, new byte[]{2,2,2,2,2,2});
+		stack.addPlane(true, new byte[]{3,3,3,3,3,3});
+		stack.addPlane(true, new byte[]{4,4,4,4,4,4});
+		
+		// illegal argument : before beginning of stack
+		try {
+			stack.deletePlane(-1);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+
+		// illegal argument : after end of stack
+		try {
+			stack.deletePlane(4);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+		
+		// delete first
+		stack.deletePlane(0);
+		assertEquals(3, stack.getNumPlanes());
+		assertEquals(2,((byte[])stack.getPlane(0))[0]);
+		assertEquals(3,((byte[])stack.getPlane(1))[0]);
+		assertEquals(4,((byte[])stack.getPlane(2))[0]);
+
+		// delete middle
+		stack.deletePlane(1);
+		assertEquals(2, stack.getNumPlanes());
+		assertEquals(2,((byte[])stack.getPlane(0))[0]);
+		assertEquals(4,((byte[])stack.getPlane(1))[0]);
+		
+		// delete end
+		stack.deletePlane(1);
+		assertEquals(1, stack.getNumPlanes());
+		assertEquals(2,((byte[])stack.getPlane(0))[0]);
+	}
+
+
+	@Test
+	public void testGetPlane()
+	{
+		PlaneStack<UnsignedByteType> stack = new PlaneStack<UnsignedByteType>(2,3,new ArrayContainerFactory());
+		
+		stack.addPlane(true, new byte[]{1,1,1,1,1,1});
+		stack.addPlane(true, new byte[]{2,2,2,2,2,2});
+		stack.addPlane(true, new byte[]{3,3,3,3,3,3});
+		stack.addPlane(true, new byte[]{4,4,4,4,4,4});
+		
+		byte[] data;
+
+		try {
+			data = (byte[]) stack.getPlane(-1);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+		
+		data = (byte[]) stack.getPlane(0);
+		assertEquals(1, data[0]);
+		
+		data = (byte[]) stack.getPlane(1);
+		assertEquals(2, data[0]);
+		
+		data = (byte[]) stack.getPlane(2);
+		assertEquals(3, data[0]);
+		
+		data = (byte[]) stack.getPlane(3);
+		assertEquals(4, data[0]);
+
+		try {
+			data = (byte[]) stack.getPlane(4);
+			fail();
+		} catch (IllegalArgumentException e) {
+			assertTrue(true);
+		}
+	}
+}
