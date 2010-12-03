d354c8a200df20fdac32b89401e2f3df2f49a861
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2010-12-03 20:51:13 +0000

    make general dataset duplicator class
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1733 be23795f-a60e-40d4-ba19-055798872e57

--- ij2-common/src/main/java/imagej/dataset/DatasetFactory.java
+++ ij2-common/src/main/java/imagej/dataset/DatasetFactory.java
@@ -2,12 +2,9 @@ package imagej.dataset;
 
 import imagej.DataType;
 
-/** the interface that all Dataset factories must implement for creation and duplication of data */
+/** the interface that all Dataset factories must implement for creation of data */
 public interface DatasetFactory
 {
 	/** creates a Dataset according to own layout style of given type and dimensions */ 
 	Dataset createDataset(DataType type, int[] dimensions);
-	
-	/** creates a Dataset according to own layout style but whose shape and data values are copied from a given Dataset */ 
-	Dataset duplicateDataset(Dataset dataset);
 }
