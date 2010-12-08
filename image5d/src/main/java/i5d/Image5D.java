8bd78c407cbdcbddb1aa4cf49cfb87d911ad266c
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2010-12-08 19:37:09 +0000

    Update Image5D version to 1.2.3.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1767 be23795f-a60e-40d4-ba19-055798872e57

--- image5d/src/main/java/i5d/Image5D.java
+++ image5d/src/main/java/i5d/Image5D.java
@@ -59,7 +59,7 @@ public class Image5D extends ImagePlus {
     
     // All references to data of image size and larger should be set to null in Image5D.flush().
     
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.2.3";
     
 	static final int nDefaultDimensions = 5;
 	protected int nDimensions = nDefaultDimensions;
