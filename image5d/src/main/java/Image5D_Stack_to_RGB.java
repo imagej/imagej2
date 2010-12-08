0744bf0726e29efecab62c4056c0e267be8a947c
Curtis Rueden <ctrueden@wisc.edu>
2010-12-08 13:35:40 -0600

    Fix asynchronous painting bug with "Image5D Stack to RGB" command.
    This used to be revision r1766.

--- image5d/src/main/java/Image5D_Stack_to_RGB.java
+++ image5d/src/main/java/Image5D_Stack_to_RGB.java
@@ -1,8 +1,13 @@
 import i5d.Image5D;
+
 import ij.*;
+import ij.gui.ImageCanvas;
+import ij.measure.*;
 import ij.plugin.*;
 import ij.process.*;
-import ij.measure.*;
+
+import java.awt.Graphics;
+
 /** Converts the current timeframe of an Image5D to an RGB stack using the current 
  * view settings.
  * @author Joachim Walter
@@ -33,8 +38,18 @@ public class Image5D_Stack_to_RGB implements PlugIn {
         ImagePlus rgbImage = IJ.createImage(title+"-RGB", "RGB black", width, height, 1);
         ImageStack rgbStack = rgbImage.getStack();
         
+        ImageCanvas canvas = currentImage.getCanvas();
+        Graphics imageGfx = canvas.getGraphics();
         for (int i=1; i<=depth; i++) {
             currentImage.setSlice(i);
+
+            // HACK: force immediate (synchronous) repaint of image canvas
+            if (currentImage instanceof Image5D) {
+              ((Image5D) currentImage).updateImageAndDraw();
+            }
+            currentImage.updateAndDraw();
+            canvas.paint(imageGfx);
+
             currentImage.copy(false);
             
             ImagePlus rgbClip = ImagePlus.getClipboard();
@@ -48,6 +63,7 @@ public class Image5D_Stack_to_RGB implements PlugIn {
                 rgbStack.setSliceLabel(currentImage.getStack().getSliceLabel(1), 1);
             }
         }
+        imageGfx.dispose();
 
         currentImage.setSlice(currentSlice);
         rgbImage.setStack(null, rgbStack);
