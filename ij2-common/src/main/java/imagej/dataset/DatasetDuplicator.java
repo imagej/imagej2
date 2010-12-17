5abf18197c6f9a5321adf0f055be858e63df7b83
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2010-12-17 21:54:08 +0000

    improve so that Dataset copying never loses precision
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1854 be23795f-a60e-40d4-ba19-055798872e57

--- ij2-common/src/main/java/imagej/dataset/DatasetDuplicator.java
+++ ij2-common/src/main/java/imagej/dataset/DatasetDuplicator.java
@@ -19,23 +19,54 @@ public class DatasetDuplicator
 		
 		Dataset newDataset = factory.createDataset(type, dimensions);
 		
-		int[] position = Index.create(dimensions.length);
-		int[] origin = Index.create(dimensions.length);
-		int[] span = dimensions;
-
-		// TODO - there are should be faster ways of doing this. do this most general way for now
-		
-		while (Index.isValid(position, origin, span))
-		{
-			double value = dataset.getDouble(position);
-			
-			newDataset.setDouble(position, value);
-			
-			Index.increment(position, origin, span);
-		}
+		// choose the best way to copy to assure no precision loss
+		if (type.isFloat())
+			copyRealData(dataset, newDataset);
+		else
+			copyIntegralData(dataset, newDataset);
 		
 		newDataset.setMetaData(dataset.getMetaData());  // TODO - PROBABLY NEED TO CLONE THE METADATA HERE!!!!!!!!!!!
 		
 		return newDataset;
+	}
+	
+	// TODO - copying in the easiest and slowest way possible - do some speed up by indexing on planes to minimize Dataset subset lookup times
+	/** copy data as doubles */
+	private void copyRealData(Dataset fromDataset, Dataset toDataset)
+	{
+		int[] dimensions = fromDataset.getDimensions();
+		
+		int[] position = Index.create(dimensions.length);
+		int[] origin = Index.create(dimensions.length);
+		int[] span = dimensions;
+
+		while (Index.isValid(position, origin, span))
+		{
+			double value = fromDataset.getDouble(position);
+			
+			toDataset.setDouble(position, value);
+			
+			Index.increment(position, origin, span);
+		}
+	}
+
+	// TODO - copying in the easiest and slowest way possible - do some speed up by indexing on planes to minimize Dataset subset lookup times
+	/** copy data as longs */
+	private void copyIntegralData(Dataset fromDataset, Dataset toDataset)
+	{
+		int[] dimensions = fromDataset.getDimensions();
+		
+		int[] position = Index.create(dimensions.length);
+		int[] origin = Index.create(dimensions.length);
+		int[] span = dimensions;
+
+		while (Index.isValid(position, origin, span))
+		{
+			long value = fromDataset.getLong(position);
+			
+			toDataset.setLong(position, value);
+			
+			Index.increment(position, origin, span);
+		}
 	}
 }
