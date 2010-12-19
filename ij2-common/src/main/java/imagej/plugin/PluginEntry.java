99194906b4eb854b106156c3a43a1147ff4253d4
Curtis Rueden <ctrueden@wisc.edu>
2010-12-19 13:58:31 -0600

    Progress on plugin discovery and GUI construction:   * Clean up plugin finder logic.   * Add toplevel utility methods for plugin discovery and launch.   * Initial, proof-of-concept menu builder for IJ2 GUI.
    This used to be revision r1858.

--- ij2-common/src/main/java/imagej/plugin/PluginEntry.java
+++ ij2-common/src/main/java/imagej/plugin/PluginEntry.java
@@ -1,96 +1,77 @@
 package imagej.plugin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PluginEntry {
 
 	private String pluginClass;
-	private String parentMenu;
+	private List<String> menuPath;
 	private String label;
-	private String args;
-	private String menu;
-	
-	public void setPluginClass(String pluginClass) {
-		this.pluginClass = pluginClass;
-	}
-	
-	public String getPluginClass() {
-		return pluginClass;
-	}
-	
-	public void setParentMenu( String parentMenu ) 
-	{
-		this.parentMenu = parentMenu;
-	}
-	
-	public String getParentMenu() {
-		return parentMenu;
-	}
-	
-	public void setLabel(String label) {
-		this.label = label;
-	}
-	
-	public String getLabel() {
-		return label;
-	}
-	
-	public void setArgs(String args) {
-		this.args = args;
-	}
-	
-	public String getArgs() {
-		return args;
-	}
-	
-	public void setMenu(String menu) {
-		this.menu = menu;
-	}
-	
-	public String getMenu() {
-		return menu;
-	}
-	
-	public PluginEntry( String pluginClass, String parentMenu, String label )
-	{
-		this.pluginClass = pluginClass;
-		this.parentMenu = parentMenu;
-		this.label = label;
-		stripArgsFromClass();
-	}
-	
+	private String arg;
+
 	/**
 	 * Used to represent IJ1 plugin meta information
 	 * @param pluginClass - full class name
 	 * @param label - the menu label in IJ 1.43u
 	 */
 	public PluginEntry(String pluginClass, String label) {
-		this.pluginClass = pluginClass;
-		this.parentMenu = "";
-		this.label = label;
-		stripArgsFromClass();
+		this(pluginClass, new ArrayList<String>(), label, "");
 	}
-	// Strips the String between the "" which is what IJ uses
-	// as arguments.
-	private void stripArgsFromClass()
+
+	public PluginEntry(String pluginClass, List<String> menuPath,
+		String label, String arg)
 	{
-		if(this.pluginClass.contains("\""))
-		{
-			int firstInstance = this.pluginClass.indexOf('\"');
-			int secondInstance = this.pluginClass.indexOf('\"', firstInstance + 1);
-			this.args = this.pluginClass.substring(firstInstance + 1, secondInstance);
-			//System.out.println("The args are " + this.args + " for string " + this.pluginClass + " at index " + firstInstance + " " + secondInstance);
-			this.pluginClass = this.pluginClass.replace('\"' + this.args + '\"', "");
-		}
+		this.pluginClass = pluginClass;
+		this.menuPath = menuPath;
+		this.label = label;
+		this.arg = arg;
 	}
+
+	public void setPluginClass(String pluginClass) {
+		this.pluginClass = pluginClass;
+	}
+
+	public String getPluginClass() {
+		return pluginClass;
+	}
+
+	public void setMenuPath(List<String> menuPath) {
+		this.menuPath = menuPath;
+	}
+
+	public List<String> getMenuPath() {
+		return menuPath;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setArg(String arg) {
+		this.arg = arg;
+	}
+
+	public String getArg() {
+		return arg;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(pluginClass);
 		sb.append(" [menu = ");
-		sb.append(parentMenu);
-		sb.append(" > ");		
+		for (String menu : menuPath) {
+			sb.append(menu);
+			sb.append(" > ");
+		}
 		sb.append(label);
 		sb.append("]");
 		return sb.toString();
 	}
-	
+
 }
---
+++ ij2-common/src/main/java/imagej/plugin/PluginUtils.java
@@ -0,0 +1,42 @@
+package imagej.plugin;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.openide.util.Lookup;
+
+/** Utility class for discovering and launching plugins. */
+public final class PluginUtils {
+
+	private PluginUtils() {
+		// prohibit instantiation of utility class
+	}
+
+	public static List<PluginEntry> findPlugins() {
+		final Collection<? extends PluginFinder> finders =
+			Lookup.getDefault().lookupAll(PluginFinder.class);
+		List<PluginEntry> plugins = new ArrayList<PluginEntry>();
+		for (PluginFinder finder : finders) {
+			finder.findPlugins(plugins);
+		}
+		return plugins;
+	}
+
+	public static void runPlugin(PluginEntry entry) {
+		final Collection<? extends PluginRunner> runners =
+			Lookup.getDefault().lookupAll(PluginRunner.class);
+		for (PluginRunner runner : runners) {
+			try {
+				runner.runPlugin(entry);
+				return;
+			}
+			catch (PluginException e) {
+				// execution failed; try the next runner
+			}
+		}
+		throw new IllegalArgumentException(
+			"No compatible PluginRunners for PluginEntry: " + entry);
+	}
+
+}
