1df2b20cd3d9b5c970639e87463506ed25975672
Curtis Rueden <ctrueden@wisc.edu>
2010-12-20 14:14:49 -0600

    Move auto-plugin logic to imagej.plugin.auto package for now.
    This used to be revision r1868.

--- ij1-plugin/src/main/java/imagej/plugin/auto/Dimensions.java
+++ ij1-plugin/src/main/java/imagej/plugin/auto/Dimensions.java
@@ -21,7 +21,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-package imagej.plugin;
+package imagej.plugin.auto;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
