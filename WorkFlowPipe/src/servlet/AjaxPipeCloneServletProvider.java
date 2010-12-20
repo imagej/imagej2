a93f7f208bcfc247d928e6afd3180197352906bf
rlentz <rlentz@be23795f-a60e-40d4-ba19-055798872e57>
2010-12-20 21:01:40 +0000

    Added the fields name and description to the Layout entity
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1870 be23795f-a60e-40d4-ba19-055798872e57

--- WorkFlowPipe/src/servlet/AjaxPipeCloneServletProvider.java
+++ WorkFlowPipe/src/servlet/AjaxPipeCloneServletProvider.java
@@ -13,13 +13,14 @@ import org.json.JSONObject;
 import controller.PipesController;
 
 /**
- * ajax.pipe.clone passes four fields; _out:json, id:(32 character hex id of parent), rnd:(4 char number)
- * and .crumb:(11 digit alpha numeric)
+ * ajax.pipe.clone passes four fields; _out:json, id:(optional if saved, 32
+ * character hex id of parent), rnd:(4 char number) and .crumb:(11 digit alpha
+ * numeric)
  * 
- * The call return two json fields
- *   "ok":1 and "new_id":(new 32 character hex id)
+ * The call return two json fields "ok":1 and "new_id":(new 32 character hex id)
+ * 
  * @author ipatron
- *
+ * 
  */
 public class AjaxPipeCloneServletProvider extends HttpServlet {
 
@@ -29,54 +30,46 @@ public class AjaxPipeCloneServletProvider extends HttpServlet {
 	private static final long serialVersionUID = 1458365345236667188L;
 	private JSONObject json = new JSONObject();
 	private PipesController pipesController;
-	
-	public AjaxPipeCloneServletProvider( PipesController pipesController ) {
-		
+
+	public AjaxPipeCloneServletProvider(PipesController pipesController) {
+
 		this.pipesController = pipesController;
-		json.put( "ok", new Integer(1) );
 
 	}
 
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		
-		//call doPOST() to submit the id
-		try {
-			doPOST( request, response );
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+
+		// call doPOST() to submit the id
+		doPost(request, response);
 	}
 
-	protected void doPOST( HttpServletRequest request, HttpServletResponse response ) throws IOException, NoSuchAlgorithmException {
-		
-		//check for the four required parameters
-		String parentID = request.getParameter("id");
-		
-		//get the .crumb
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+
+		// get the .crumb
 		String crumb = request.getParameter(".crumb");
-		
-		//get the random number
+
+		// get the random number
 		String randomNumber = request.getParameter("rnd");
-		
-		//get the return format
+
+		// get the return format
 		String out = request.getParameter("_out");
-				
-		//TODO: add rnd and .crumb checks
-		
-		//get the response, a string containing the id of the clone
+
+		// TODO: add rnd and .crumb checks
+
+		// check for the four required parameters
+		String parentID = request.getParameter("id");
+
+		// get the response, a string containing the id of the clone
 		String newId = pipesController.clonePipe( parentID, crumb );
-		
-		//if output type is JSON
-		if(out=="json")
-			json.put( "new_id", newId );
-		
+		json.put("new_id", newId);
+		json.put("ok", new Integer(0) );
+
 		// generate and send the response
-		response.setContentType( "application/json" );
-		response.setHeader( "Cache-Control", "no-cache" );
-		response.getWriter().write( json.toString() );
+		response.setContentType("application/json");
+		response.setHeader("Cache-Control", "no-cache");
+		response.getWriter().write(json.toString());
 	}
 
 }
