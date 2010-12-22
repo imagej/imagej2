93affc6de422de64d30a3a2c56c4b2c9570fd821
Curtis Rueden <ctrueden@wisc.edu>
2010-12-22 11:57:59 -0600

    Make OpenImage (temporarily) prompt for a file using a JFileChooser.
    This used to be revision r1887.

--- ij2-gui/src/main/java/imagej/gui/OpenImage.java
+++ ij2-gui/src/main/java/imagej/gui/OpenImage.java
@@ -2,7 +2,11 @@ package imagej.gui;
 
 import java.io.IOException;
 
+import javax.swing.JFileChooser;
+
+import loci.formats.ChannelMerger;
 import loci.formats.FormatException;
+import loci.formats.gui.GUITools;
 import mpicbg.imglib.image.Image;
 import mpicbg.imglib.io.ImageOpener;
 import mpicbg.imglib.type.numeric.RealType;
@@ -14,7 +18,8 @@ import imagej.plugin.Parameter;
 import imagej.plugin.Plugin;
 
 @Plugin(
-  menu={
+//	menuPath="File>Import>Bio-Formats...",
+	menu={
     @Menu(label="File", weight=0, mnemonic='f'),
     @Menu(label="Import", weight=0, mnemonic='i'),
     @Menu(label="Bio-Formats...", weight=0, mnemonic='b')
@@ -26,14 +31,21 @@ public class OpenImage<T extends RealType<T>> implements IPlugin {
 	@Parameter(output=true)
 	private Dataset dataset;
 
-  @Override
-  public void run() {
-  	// open image
-  	final String id = "";
-  	final ImageOpener imageOpener = new ImageOpener();
-  	try {
+	@Override
+	public void run() {
+		// prompt for input file
+		final JFileChooser fileChooser =
+			GUITools.buildFileChooser(new ChannelMerger());
+		final int rval = fileChooser.showOpenDialog(null);
+		if (rval != JFileChooser.APPROVE_OPTION) return; // canceled
+		final String id = fileChooser.getSelectedFile().getAbsolutePath();
+
+		// open image
+		final ImageOpener imageOpener = new ImageOpener();
+		try {
 			final Image<T> img = imageOpener.openImage(id);
 			dataset = new ImgLibDataset<T>(img);
+			System.out.println("Image dimensions: " + dataset.getDimensions()[0] + " x " + dataset.getDimensions()[1]);//TEMP
 		}
 		catch (FormatException e) {
 			// TODO Auto-generated catch block
