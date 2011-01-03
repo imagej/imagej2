f229dd961f6bc86d9a8a9a223d2f4ea935b98d58
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2011-01-03 22:45:50 +0000

    replace bitwise >> with >>> where appropriate
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1948 be23795f-a60e-40d4-ba19-055798872e57

--- ij2-common/src/main/java/imagej/data/Unsigned12BitArrayAccessor.java
+++ ij2-common/src/main/java/imagej/data/Unsigned12BitArrayAccessor.java
@@ -132,44 +132,44 @@ public class Unsigned12BitArrayAccessor implements DataAccessor
 		switch (indexOfFirstNibble)
 		{
 			case 0:
-				setNibble(intIndexOfBucketStart,   0, ((value >> 0) & 15));
-				setNibble(intIndexOfBucketStart,   1, ((value >> 4) & 15));
-				setNibble(intIndexOfBucketStart,   2, ((value >> 8) & 15));
+				setNibble(intIndexOfBucketStart,   0, ((value >>> 0) & 15));
+				setNibble(intIndexOfBucketStart,   1, ((value >>> 4) & 15));
+				setNibble(intIndexOfBucketStart,   2, ((value >>> 8) & 15));
 				break;
 			case 1:
-				setNibble(intIndexOfBucketStart,   3, ((value >> 0) & 15));
-				setNibble(intIndexOfBucketStart,   4, ((value >> 4) & 15));
-				setNibble(intIndexOfBucketStart,   5, ((value >> 8) & 15));
+				setNibble(intIndexOfBucketStart,   3, ((value >>> 0) & 15));
+				setNibble(intIndexOfBucketStart,   4, ((value >>> 4) & 15));
+				setNibble(intIndexOfBucketStart,   5, ((value >>> 8) & 15));
 				break;
 			case 2:
-				setNibble(intIndexOfBucketStart,   6, ((value >> 0) & 15));
-				setNibble(intIndexOfBucketStart,   7, ((value >> 4) & 15));
-				setNibble(intIndexOfBucketStart+1, 0, ((value >> 8) & 15));
+				setNibble(intIndexOfBucketStart,   6, ((value >>> 0) & 15));
+				setNibble(intIndexOfBucketStart,   7, ((value >>> 4) & 15));
+				setNibble(intIndexOfBucketStart+1, 0, ((value >>> 8) & 15));
 				break;
 			case 3:
-				setNibble(intIndexOfBucketStart+1, 1, ((value >> 0) & 15));
-				setNibble(intIndexOfBucketStart+1, 2, ((value >> 4) & 15));
-				setNibble(intIndexOfBucketStart+1, 3, ((value >> 8) & 15));
+				setNibble(intIndexOfBucketStart+1, 1, ((value >>> 0) & 15));
+				setNibble(intIndexOfBucketStart+1, 2, ((value >>> 4) & 15));
+				setNibble(intIndexOfBucketStart+1, 3, ((value >>> 8) & 15));
 				break;
 			case 4:
-				setNibble(intIndexOfBucketStart+1, 4, ((value >> 0) & 15));
-				setNibble(intIndexOfBucketStart+1, 5, ((value >> 4) & 15));
-				setNibble(intIndexOfBucketStart+1, 6, ((value >> 8) & 15));
+				setNibble(intIndexOfBucketStart+1, 4, ((value >>> 0) & 15));
+				setNibble(intIndexOfBucketStart+1, 5, ((value >>> 4) & 15));
+				setNibble(intIndexOfBucketStart+1, 6, ((value >>> 8) & 15));
 				break;
 			case 5:
-				setNibble(intIndexOfBucketStart+1, 7, ((value >> 0) & 15));
-				setNibble(intIndexOfBucketStart+2, 0, ((value >> 4) & 15));
-				setNibble(intIndexOfBucketStart+2, 1, ((value >> 8) & 15));
+				setNibble(intIndexOfBucketStart+1, 7, ((value >>> 0) & 15));
+				setNibble(intIndexOfBucketStart+2, 0, ((value >>> 4) & 15));
+				setNibble(intIndexOfBucketStart+2, 1, ((value >>> 8) & 15));
 				break;
 			case 6:
-				setNibble(intIndexOfBucketStart+2, 2, ((value >> 0) & 15));
-				setNibble(intIndexOfBucketStart+2, 3, ((value >> 4) & 15));
-				setNibble(intIndexOfBucketStart+2, 4, ((value >> 8) & 15));
+				setNibble(intIndexOfBucketStart+2, 2, ((value >>> 0) & 15));
+				setNibble(intIndexOfBucketStart+2, 3, ((value >>> 4) & 15));
+				setNibble(intIndexOfBucketStart+2, 4, ((value >>> 8) & 15));
 				break;
 			case 7:
-				setNibble(intIndexOfBucketStart+2, 5, ((value >> 0) & 15));
-				setNibble(intIndexOfBucketStart+2, 6, ((value >> 4) & 15));
-				setNibble(intIndexOfBucketStart+2, 7, ((value >> 8) & 15));
+				setNibble(intIndexOfBucketStart+2, 5, ((value >>> 0) & 15));
+				setNibble(intIndexOfBucketStart+2, 6, ((value >>> 4) & 15));
+				setNibble(intIndexOfBucketStart+2, 7, ((value >>> 8) & 15));
 				break;
 			default:
 				throw new IllegalStateException();
