d6250797463ffb469a7ab980d941785b957ac814
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2011-01-03 20:14:59 +0000

    whitespace change
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@1943 be23795f-a60e-40d4-ba19-055798872e57

--- ij2-common/src/main/java/imagej/StorageType.java
+++ ij2-common/src/main/java/imagej/StorageType.java
@@ -3,6 +3,5 @@ package imagej;
 /** the data types that ImageJ uses for representing data on disk and in memory */
 public enum StorageType
 {
-	INT8, UINT8, INT16, UINT16, INT32, UINT32, FLOAT32, INT64, FLOAT64 
+	INT8, UINT8, INT16, UINT16, INT32, UINT32, FLOAT32, INT64, FLOAT64
 }
-
