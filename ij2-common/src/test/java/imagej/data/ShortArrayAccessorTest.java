bfb42fcbf209402ef04541babc66a6c9cd091743
Barry DeZonia <bdezonia@wisc.edu>
2011-01-04 10:32:23 -0600

    add array accessor test classes and fix related bugs
    This used to be revision r1950.

---
+++ ij2-common/src/test/java/imagej/data/ShortArrayAccessorTest.java
@@ -0,0 +1,84 @@
+package imagej.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ShortArrayAccessorTest {
+
+	private ShortArrayAccessor accessor;
+	
+	@Test
+	public void testByteArrayAccessor() {
+		short[] data = new short[]{1,2,3,4,5};
+		this.accessor = new ShortArrayAccessor(data);
+		assertNotNull(this.accessor);
+	}
+
+	@Test
+	public void testGetReal() {
+		short[] data = new short[]{1,2,3,4,5};
+		this.accessor = new ShortArrayAccessor(data);
+		assertNotNull(this.accessor);
+		assertEquals(1, this.accessor.getReal(0), 0);
+		assertEquals(2, this.accessor.getReal(1), 0);
+		assertEquals(3, this.accessor.getReal(2), 0);
+		assertEquals(4, this.accessor.getReal(3), 0);
+	}
+
+	@Test
+	public void testGetIntegral() {
+		short[] data = new short[]{1,2,3,4,5};
+		this.accessor = new ShortArrayAccessor(data);
+		assertNotNull(this.accessor);
+		assertEquals(1, this.accessor.getIntegral(0));
+		assertEquals(2, this.accessor.getIntegral(1));
+		assertEquals(3, this.accessor.getIntegral(2));
+		assertEquals(4, this.accessor.getIntegral(3));
+	}
+
+	@Test
+	public void testSetReal() {
+		short[] data = new short[]{1,2,3,4,5};
+		this.accessor = new ShortArrayAccessor(data);
+		assertNotNull(this.accessor);
+		
+		this.accessor.setReal(0, -1.0 + Short.MIN_VALUE);
+		assertEquals(Short.MIN_VALUE, this.accessor.getReal(0), 0);
+		
+		this.accessor.setReal(1, Short.MAX_VALUE+1.0);
+		assertEquals(Short.MAX_VALUE, this.accessor.getReal(1), 0);
+		
+		this.accessor.setReal(2, -13.4);
+		assertEquals(-13, this.accessor.getReal(2), 0);
+		
+		this.accessor.setReal(3, 68.6);
+		assertEquals(68, this.accessor.getReal(3), 0);
+		
+		this.accessor.setReal(4, 0);
+		assertEquals(0, this.accessor.getReal(4), 0);
+	}
+
+	@Test
+	public void testSetIntegral() {
+		short[] data = new short[]{1,2,3,4,5};
+		this.accessor = new ShortArrayAccessor(data);
+		assertNotNull(this.accessor);
+		
+		this.accessor.setIntegral(0, -1 + Short.MIN_VALUE);
+		assertEquals(Short.MIN_VALUE, this.accessor.getIntegral(0));
+		
+		this.accessor.setIntegral(1, Short.MAX_VALUE+1L);
+		assertEquals(Short.MAX_VALUE, this.accessor.getIntegral(1));
+		
+		this.accessor.setIntegral(2, -13);
+		assertEquals(-13, this.accessor.getIntegral(2));
+		
+		this.accessor.setIntegral(3, 68);
+		assertEquals(68, this.accessor.getIntegral(3));
+		
+		this.accessor.setIntegral(4, 0);
+		assertEquals(0, this.accessor.getIntegral(4));
+	}
+
+}
--- ij2-common/src/test/java/imagej/data/Unsigned12BitArrayAccessorTest.java
+++ ij2-common/src/test/java/imagej/data/Unsigned12BitArrayAccessorTest.java
@@ -31,44 +31,31 @@ public class Unsigned12BitArrayAccessorTest {
 
 	@Test
 	public void testSetReal() {
-		int[] values = new int[]{0,0,0};
+		int[] values = new int[]{-1,-1,-1};
 		this.accessor = new Unsigned12BitArrayAccessor(values);
 		
-		// try entry 0
-		assertEquals(0, this.accessor.getReal(0),  0);
-		this.accessor.setReal(0, 23);
-		assertEquals(23, this.accessor.getReal(0),  0);  // set it to 1
-		this.accessor.setReal(0, -1);
-		assertEquals(0, this.accessor.getReal(0),  0);  // set it to value out of lower range
-		this.accessor.setReal(0, 4096);
-		assertEquals(4095, this.accessor.getReal(0),  0);  // set it to value out of upper range
+		for (int v = 0; v < values.length; v++)
+		{
+			for (int i = 0; i < 4096; i++)
+			{
+				this.accessor.setReal(v, i);
+				assertEquals(i, this.accessor.getReal(v), 0);
+			}
+			
+			for (int i = -68000; i < 0; i+=1000)
+			{
+				this.accessor.setReal(v, 99);
+				this.accessor.setReal(v, i);
+				assertEquals(0, this.accessor.getReal(v), 0);
+			}
 
-		// try entry 3
-		assertEquals(0, this.accessor.getReal(3),  0);
-		this.accessor.setReal(3, 145);
-		assertEquals(145, this.accessor.getReal(3),  0);  // set it to 1
-		this.accessor.setReal(3, -1);
-		assertEquals(0, this.accessor.getReal(3),  0);  // set it to value out of lower range
-		this.accessor.setReal(3, 4096);
-		assertEquals(4095, this.accessor.getReal(3),  0);  // set it to value out of upper range
-
-		// try entry 5
-		assertEquals(0, this.accessor.getReal(5),  0);
-		this.accessor.setReal(5, 2010);
-		assertEquals(2010, this.accessor.getReal(5),  0);  // set it to 1
-		this.accessor.setReal(5, -1);
-		assertEquals(0, this.accessor.getReal(5),  0);  // set it to value out of lower range
-		this.accessor.setReal(5, 4096);
-		assertEquals(4095, this.accessor.getReal(5),  0);  // set it to value out of upper range
-		
-		// try entry 7
-		assertEquals(0, this.accessor.getReal(7),  0);
-		this.accessor.setReal(7, 3886);
-		assertEquals(3886, this.accessor.getReal(7),  0);  // set it to 1
-		this.accessor.setReal(7, -1);
-		assertEquals(0, this.accessor.getReal(7),  0);  // set it to value out of lower range
-		this.accessor.setReal(7, 4096);
-		assertEquals(4095, this.accessor.getReal(7),  0);  // set it to value out of upper range
+			for (int i = 4096; i < 68000; i+=1000)
+			{
+				this.accessor.setReal(v, 99);
+				this.accessor.setReal(v, i);
+				assertEquals(4095, this.accessor.getReal(v), 0);
+			}
+		}
 	}
 
 	@Test
@@ -88,44 +75,31 @@ public class Unsigned12BitArrayAccessorTest {
 
 	@Test
 	public void testSetIntegral() {
-		int[] values = new int[]{0,0,0};
+		int[] values = new int[]{-1,-1,-1};
 		this.accessor = new Unsigned12BitArrayAccessor(values);
 		
-		// try entry 0
-		assertEquals(0, this.accessor.getReal(0),  0);
-		this.accessor.setReal(0, 999);
-		assertEquals(999, this.accessor.getReal(0),  0);  // set it to 1
-		this.accessor.setReal(0, -1);
-		assertEquals(0, this.accessor.getReal(0),  0);  // set it to value out of lower range
-		this.accessor.setReal(0, 4096);
-		assertEquals(4095, this.accessor.getReal(0),  0);  // set it to value out of upper range
+		for (int v = 0; v < values.length; v++)
+		{
+			for (int i = 0; i < 4096; i++)
+			{
+				this.accessor.setIntegral(v, i);
+				assertEquals(i, this.accessor.getIntegral(v));
+			}
+			
+			for (int i = -68000; i < 0; i+=1000)
+			{
+				this.accessor.setIntegral(v, 99);
+				this.accessor.setIntegral(v, i);
+				assertEquals(0, this.accessor.getIntegral(v));
+			}
 
-		// try entry 1
-		assertEquals(0, this.accessor.getReal(1),  0);
-		this.accessor.setReal(1, 4040);
-		assertEquals(4040, this.accessor.getReal(1),  0);  // set it to 1
-		this.accessor.setReal(1, -1);
-		assertEquals(0, this.accessor.getReal(1),  0);  // set it to value out of lower range
-		this.accessor.setReal(1, 4096);
-		assertEquals(4095, this.accessor.getReal(1),  0);  // set it to value out of upper range
-
-		// try entry 4
-		assertEquals(0, this.accessor.getReal(4),  0);
-		this.accessor.setReal(4, 3333);
-		assertEquals(3333, this.accessor.getReal(4),  0);  // set it to 1
-		this.accessor.setReal(4, -1);
-		assertEquals(0, this.accessor.getReal(4),  0);  // set it to value out of lower range
-		this.accessor.setReal(4, 4096);
-		assertEquals(4095, this.accessor.getReal(4),  0);  // set it to value out of upper range
-		
-		// try entry 6
-		assertEquals(0, this.accessor.getReal(6),  0);
-		this.accessor.setReal(6, 2422);
-		assertEquals(2422, this.accessor.getReal(6),  0);  // set it to 1
-		this.accessor.setReal(6, -1);
-		assertEquals(0, this.accessor.getReal(6),  0);  // set it to value out of lower range
-		this.accessor.setReal(6, 4096);
-		assertEquals(4095, this.accessor.getReal(6),  0);  // set it to value out of upper range
+			for (int i = 4096; i < 68000; i+=1000)
+			{
+				this.accessor.setIntegral(v, 99);
+				this.accessor.setIntegral(v, i);
+				assertEquals(4095, this.accessor.getIntegral(v));
+			}
+		}
 	}
 
 }
