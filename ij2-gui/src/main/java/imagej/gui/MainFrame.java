e1e57a5eec4a2f1ac1dea5f3191162fe55dd88da
Curtis Rueden <ctrueden@wisc.edu>
2011-01-18 12:53:43 -0600

    Add simple status bar to IJ2 GUI.
    This used to be revision r2023.

--- ij2-gui/src/main/java/imagej/gui/MainFrame.java
+++ ij2-gui/src/main/java/imagej/gui/MainFrame.java
@@ -11,7 +11,6 @@ import java.awt.BorderLayout;
 import java.util.List;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
@@ -20,14 +19,15 @@ import javax.swing.border.EmptyBorder;
 /** A simple and dumb Swing-based main window for ImageJ2. */
 public class MainFrame {
 
-	private String info = "Hello, ImageJ";
+	private JPanel mainPanel;
+	private StatusBar statusBar;
 
 	/** Creates a new ImageJ frame that runs as an application. */
 	public MainFrame() {
 		final JFrame frame = new JFrame("ImageJ");
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		createMenuBar(frame);
 		createContentPane(frame);
+		createMenuBar(frame);
 		frame.pack();
 		frame.setVisible(true);
 	}
@@ -36,15 +36,20 @@ public class MainFrame {
 		final JPanel pane = new JPanel();
 		frame.setContentPane(pane);
 		pane.setLayout(new BorderLayout());
-		pane.setBorder(new EmptyBorder(15, 15, 15, 15));
-		pane.add(new JLabel(info));
+
+		mainPanel = new JPanel();
+		mainPanel.setBorder(new EmptyBorder(15, 15, 15, 15));
+		pane.add(mainPanel, BorderLayout.CENTER);
+
+		statusBar = new StatusBar();
+		pane.add(statusBar, BorderLayout.SOUTH);
 	}
 
 	private void createMenuBar(JFrame frame) {
 		final List<PluginEntry> entries = PluginUtils.findPlugins();
-		info = "Discovered " + entries.size() + " plugins";
-		final JMenuBar menubar = new MenuBuilder().buildMenuBar(entries);
-		frame.setJMenuBar(menubar);
+		statusBar.setText("Discovered " + entries.size() + " plugins");
+		final JMenuBar menuBar = new MenuBuilder().buildMenuBar(entries);
+		frame.setJMenuBar(menuBar);
 	}
 
 	public static void main(String[] args) {
