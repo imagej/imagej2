2721ccd4ef3b9fc6cd0acab13281bb85d3915b32
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-01-19 19:16:25 +0000

    Fix import statements.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2040 be23795f-a60e-40d4-ba19-055798872e57

--- WorkFlowPipe/src/main/java/imagej/servlet/AjaxFeedPreviewServletProvider.java
+++ WorkFlowPipe/src/main/java/imagej/servlet/AjaxFeedPreviewServletProvider.java
@@ -7,10 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
-import pipes.FeedFind;
 import imagej.pipes.FeedPreview;
 
 public class AjaxFeedPreviewServletProvider extends HttpServlet {
