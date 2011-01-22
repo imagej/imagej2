aaf890435659854e6065f0ef5031e638ff71ee24
aivar <aivar@be23795f-a60e-40d4-ba19-055798872e57>
2011-01-22 04:37:38 +0000

    Keep track of items in debug mode.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2082 be23795f-a60e-40d4-ba19-055798872e57

---
+++ workflow/src/main/java/imagej/workflow/WorkflowDebugInfo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package imagej.workflow;
+
+import imagej.workflow.plugin.ItemWrapper;
+
+/**
+ *
+ * @author aivar
+ */
+public class WorkflowDebugInfo {
+    String desc;
+    ItemWrapper item;
+}
