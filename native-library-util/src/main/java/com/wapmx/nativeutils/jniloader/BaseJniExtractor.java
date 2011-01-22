4b1ad5abe1b29137a87736218709bcde80d719d0
Aivar Grislis <grislis@wisc.edu>
2011-01-21 22:53:45 -0600

    Utility for dealing with native libraries.
    This used to be revision r2083.

---
+++ native-library-util/src/main/java/com/wapmx/nativeutils/jniloader/BaseJniExtractor.java
@@ -0,0 +1,190 @@
+// $Id: BaseJniExtractor.java 325571 2009-08-19 15:35:24Z markjh $
+// Copyright 2006 MX Telecom Ltd
+
+package com.wapmx.nativeutils.jniloader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Enumeration;
+
+import com.wapmx.nativeutils.MxSysInfo;
+
+/**
+ * @author Richard van der Hoff <richardv@mxtelecom.com>
+ */
+public abstract class BaseJniExtractor implements JniExtractor {
+    private static boolean debug = false;
+    static {
+        // initialise the debug switch
+        String s = System.getProperty("java.library.debug");
+        if (s != null && (s.toLowerCase().startsWith("y") || s.startsWith("1")))
+            debug = true;
+    }
+    private Class libraryJarClass;
+
+    /**
+     * We use a resource path of the form META-INF/lib/${mx.sysinfo}/ This way native builds for multiple architectures
+     * can be packaged together without interfering with each other And by setting mx.sysinfo the jvm can pick the
+     * native libraries appropriate for itself.
+     */
+    private String[] nativeResourcePaths;
+
+    public BaseJniExtractor() throws IOException {
+        init(null);
+    }
+
+    public BaseJniExtractor(Class libraryJarClass) throws IOException {
+        init(libraryJarClass);
+    }
+
+    private void init(Class libraryJarClass) throws IOException {
+        this.libraryJarClass = libraryJarClass;
+
+        String mxSysInfo = MxSysInfo.getMxSysInfo();
+        
+        if (mxSysInfo != null) {
+            nativeResourcePaths = new String[] { "META-INF/lib/" + mxSysInfo + "/",
+                    "META-INF/lib/" };
+        }
+        else {
+            nativeResourcePaths = new String[] { "META-INF/lib/" };
+        }
+    }
+
+    /**
+     * this is where native dependencies are extracted to (e.g. tmplib/).
+     * 
+     * @return native working dir
+     */
+    public abstract File getNativeDir();
+
+    /**
+     *  this is where JNI libraries are extracted to (e.g. tmplib/classloaderName.1234567890000.0/).
+     * 
+     * @return jni working dir
+     */
+    public abstract File getJniDir();
+
+    /** {@inheritDoc} */
+    public File extractJni(String libname) throws IOException {
+        String mappedlib = System.mapLibraryName(libname);
+        System.out.println("mappedLib is " + mappedlib);
+        /*
+         * On Darwin, the default mapping is to .jnilib; but we use .dylibs so that library interdependencies are
+         * handled correctly. if we don't find a .jnilib, try .dylib instead.
+         */
+        URL lib = null;
+        
+        // if no class specified look for resources in the jar of this class
+        if (null == libraryJarClass) {
+            libraryJarClass = this.getClass();
+        }
+
+        for (int i = 0; i < nativeResourcePaths.length; i++) {
+            System.out.println("nativeResourcesPaths[" + i + "] is " + nativeResourcePaths[i]);
+            lib = libraryJarClass.getClassLoader().getResource(nativeResourcePaths[i] + mappedlib);
+            if (lib != null)
+                break;
+            if (mappedlib.endsWith(".jnilib")) {
+                lib = this.getClass().getClassLoader().getResource(
+                        nativeResourcePaths[i] + mappedlib.substring(0, mappedlib.length() - 7) + ".dylib");
+                if (lib != null) {
+                    mappedlib = mappedlib.substring(0, mappedlib.length() - 7) + ".dylib";
+                    break;
+                }
+            }
+        }
+
+        System.out.println("URL is " + lib.toString());
+        System.out.println("URL path is " + lib.getPath());
+        System.out.println("jni dir is " + getJniDir().toString());
+
+   // THis is a protected method:     System.out.println("ClassLoader.finLibrary returns " + this.getClass().getClassLoader().findLibrary(mappedlib));
+
+        if (lib != null) {
+            return extractResource(getJniDir(), lib, mappedlib);
+        }
+        else {
+            throw new IOException("Couldn't find jni library " + mappedlib + " on the classpath");
+        }
+    }
+
+    /** {@inheritDoc} */
+    public void extractRegistered() throws IOException {
+        if (debug) System.err.println("Extracting libraries registered in classloader " + this.getClass().getClassLoader());
+        for (int i = 0; i < nativeResourcePaths.length; i++) {
+            Enumeration<URL> resources = this.getClass().getClassLoader().getResources(
+                    nativeResourcePaths[i] + "AUTOEXTRACT.LIST");
+            while (resources.hasMoreElements()) {
+                URL res = resources.nextElement();
+                if (debug) System.err.println("Extracting libraries listed in " + res);
+                BufferedReader r = new BufferedReader(new InputStreamReader(res.openStream(), "UTF-8"));
+                String line;
+                while ((line = r.readLine()) != null) {
+                    URL lib = null;
+                    for (int j = 0; j < nativeResourcePaths.length; j++) {
+                        lib = this.getClass().getClassLoader().getResource(nativeResourcePaths[j] + line);
+                        if (lib != null)
+                            break;
+                    }
+                    if (lib != null) {
+                        extractResource(getNativeDir(), lib, line);
+                    }
+                    else {
+                        throw new IOException("Couldn't find native library " + line + "on the classpath");
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Extract a resource to the tmp dir (this entry point is used for unit testing)
+     * 
+     * @param dir the directory to extract the resource to
+     * @param resource the resource on the classpath
+     * @param outputname the filename to copy to (within the tmp dir)
+     * @return the extracted file
+     * @throws IOException
+     */
+    File extractResource(File dir, URL resource, String outputname) throws IOException {
+        InputStream in = resource.openStream(); //TODO there's also a getResourceAsStream
+        File outfile = new File(dir, outputname);
+        // Create a new file rather than writing into old file
+        File outfiletemp = File.createTempFile(outputname, null, getJniDir());
+        if (debug)
+            System.err.println("Extracting '" + resource + "' to '" + outfile.getAbsolutePath() + "'");
+        FileOutputStream out = new FileOutputStream(outfiletemp);
+        copy(in, out);
+        out.close();
+        in.close();
+        outfiletemp.renameTo(outfile);
+        outfile.deleteOnExit();
+        return outfile;
+    }
+
+    /**
+     * copy an InputStream to an OutputStream.
+     * 
+     * @param in InputStream to copy from
+     * @param out OutputStream to copy to
+     * @throws IOException if there's an error
+     */
+    static void copy(InputStream in, OutputStream out) throws IOException {
+        byte[] tmp = new byte[8192];
+        int len = 0;
+        while (true) {
+            len = in.read(tmp);
+            if (len <= 0) {
+                break;
+            }
+            out.write(tmp, 0, len);
+        }
+    }
+}
---
+++ native-library-util/src/main/java/imagej/nativelibrary/NativeLibraryUtil.java
@@ -0,0 +1,287 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package imagej.nativelibrary;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.IllegalAccessException;
+import java.lang.NoSuchFieldException;
+import java.lang.reflect.Field;
+
+import com.wapmx.nativeutils.MxSysInfo;
+import com.wapmx.nativeutils.jniloader.DefaultJniExtractor;
+import com.wapmx.nativeutils.jniloader.JniExtractor;
+
+/**
+ *
+ * @author Aivar Grislis
+ */
+public class NativeLibraryUtil {
+    private static final String USER_TMPDIR = "java.library.tmpdir";
+    private static final String JAVA_TMPDIR = "java.io.tmpdir";
+    private static final String JAVA_PATH = "java.library.path";
+    private static final String SUN_PATH = "sun.boot.library.path";
+    private static final String USER_PATHS = "usr_paths";
+    private static final String CURRENT_DIRECTORY = ".";
+    private static boolean s_skipHack = false;
+    private static String s_writableDirectory = null;
+
+    /**
+     * Loads the native library specified by the libname argument.
+     * Can be used in place of System.loadLibrary().
+     * Extracts
+     */
+    public static void loadLibrary(Class libraryJarClass, String libname) {
+        extractNativeLibraryToPath(libraryJarClass, libname);
+        System.loadLibrary(libname);
+    }
+
+    /**
+     * Extracts the native library specified by the libname argument from the
+     * resources of the jar file that contains the given libraryJarClass class.
+     * Puts it on the library path.
+     *
+     * @param libraryJarClass
+     * @param libname
+     * @return
+     */
+    public static boolean extractNativeLibraryToPath(Class libraryJarClass, String libname) {
+        boolean success = false;
+
+        try {
+            // get a temporary directory
+            boolean userSuppliedDirectory = true;
+            String directory = System.getProperty(USER_TMPDIR);
+            if (null == directory) {
+                userSuppliedDirectory = false;
+
+                directory = System.getProperty(JAVA_TMPDIR);
+            }
+
+            // if we should try the hack
+            if (!s_skipHack) {
+                // is it necessary? already on path?
+                if (!isOnLibraryPath(directory)) {
+
+                    // try the hack
+                    if (!addToLibraryPath(directory)) {
+                        // fails, don't try again
+                        s_skipHack = true;
+                    }
+                }
+            }
+
+            // if hack doesn't work
+            if (s_skipHack) {
+                // go with user supplied directory
+                if (!userSuppliedDirectory) {
+                    // otherwise, find a directory on the path to extract to
+                    directory = findWritableDirectoryOnPath();
+                }
+            }
+
+            // extract library to directory
+            if (null != directory) {
+                try {
+                    JniExtractor jniExtractor =
+                            new DefaultJniExtractor(libraryJarClass, directory);
+
+                    File extractedFile = jniExtractor.extractJni(libname);
+                }
+                catch (IOException e) {
+                    System.out.println("IOException creating DefaultJniExtractor " + e.getMessage());
+                }
+
+                success = true;
+            }
+        }
+        catch (SecurityException e) {
+            // a security manager exists and its checkPropertyAccess method
+            // doesn't allow access to the specified system property.
+        }
+
+        return success;
+    }
+
+    /**
+     * Is the given directory on java.library.path?
+     *
+     * @param directory
+     * @return whether or not on path
+     */
+    public static boolean isOnLibraryPath(String directory) {
+        return checkLibraryPath(JAVA_PATH, directory)
+                || checkLibraryPath(SUN_PATH, directory);
+    }
+
+    /**
+     * Helper routine, checks path for a given property name.
+     *
+     * @param propertyName
+     * @param directory
+     * @return whether or not on path
+     */
+    private static boolean checkLibraryPath(String propertyName, String directory) {
+        String paths[] = getPaths(propertyName);
+        for (String path : paths) {
+            if (directory.equals(path)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Helper routine, gets list of paths for a given property name.
+     *
+     * @param propertyName
+     * @return list of paths
+     */
+    private static String[] getPaths(String propertyName) {
+        String paths[] = null;
+        try {
+            paths = System.getProperty(propertyName).split(File.pathSeparator);
+        }
+        catch (SecurityException e) {
+            // unable to get list of paths
+            paths = new String[0];
+        }
+        return paths;
+    }
+
+    /**
+     * Adds a given folder to the java.library.path.
+     *
+     * From {@link http://nicklothian.com/blog/2008/11/19/modify-javalibrarypath-at-runtime/}
+     *
+     * "This enables the java.library.path to be modified at runtime. From a
+     * Sun engineer at http://forums.sun.com/thread.jspa?threadID=707176" (link
+     * is dead)
+     *
+     * See also {@link http://forums.java.net/node/703790}
+     *
+     * "So here's what I found. I decompiled the RV library, and used that to
+     * step into the Java classloader code. There I found two variables being
+     * used to look for native libraries: sys_paths and usr_paths. Usr_paths
+     * *appears* to be loaded from the environment variable 'java.library.path'
+     * and sys_paths *appears* to be loaded from the environment variable
+     * 'sun.boot.library.path'."
+     *
+     * See also {@link http://safcp.googlecode.com/svn/trunk/SAFCP/src/main/java/ufrj/safcp/util/JavaLibraryPath.java}
+     *
+     * Uses similar approach, GPL3 license: "Will not work if JVM security
+     * policy gets in the way (like in an applet). Will not work if Sun changes
+     * the private members. This really shouldn't be used at all."
+     *
+     * @param directory folder to add, should be absolute path
+     * @return whether successful
+     */
+    public static boolean addToLibraryPath(String directory) {
+        boolean success = false;
+
+        try {
+            // get user paths
+            Field field = ClassLoader.class.getDeclaredField(USER_PATHS);
+            field.setAccessible(true);
+            String[] paths = (String[])field.get(null);
+
+            // already in paths?
+            for (int i = 0; i < paths.length; i++) {
+                if (directory.equals(paths[i])) {
+                    return true;
+                }
+            }
+
+            // add to user paths
+            String[] tmp = new String[paths.length+1];
+            System.arraycopy(paths,0,tmp,0,paths.length);
+            tmp[paths.length] = directory;
+            field.set(null,tmp);
+
+            System.setProperty(JAVA_PATH,
+                    System.getProperty(JAVA_PATH) + File.pathSeparator + directory); //TODO why bother?
+
+            success = true;
+        }
+        catch (IllegalAccessException e) {
+            // Failed to get permissions to set library path
+        }
+        catch (NoSuchFieldException e) {
+            // Failed to get field handle to set library path
+	}
+        catch (Exception e) {
+            // play it safe
+        }
+
+        return success;
+    }
+
+    public static String findWritableDirectoryOnPath() {
+        // if we haven't found this already
+        if (null == s_writableDirectory) {
+            // try the current directory first
+            if (isOnLibraryPath(CURRENT_DIRECTORY)) {
+                // is on path, is it writable?
+               if (isWritableDirectory(CURRENT_DIRECTORY)) {
+                   // yes, use it
+                   s_writableDirectory = CURRENT_DIRECTORY;
+               }
+            }
+            // still looking?
+            if (null == s_writableDirectory) {
+                // look on java library path
+                s_writableDirectory = findWritableDirectory(JAVA_PATH);
+                // still looking?
+                if (null == s_writableDirectory) {
+                    // look on Sun library path
+                    s_writableDirectory = findWritableDirectory(SUN_PATH);
+                }
+            }
+        }
+        return s_writableDirectory;
+    }
+
+    /**
+     * Helper routine, checks path for a given property name.
+     *
+     * @param propertyName
+     * @param directory
+     * @return whether or not on path
+     */
+    private static String findWritableDirectory(String propertyName) {
+        String paths[] = getPaths(propertyName);
+        for (String path : paths) {
+            if (isWritableDirectory(path)) {
+                return path;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Do we have write access to the given directory?
+     *
+     * @param directory
+     * @return whether or not writable
+     */
+    public static boolean isWritableDirectory(String directory) {
+        boolean success = false;
+        try {
+            File tempFile = File.createTempFile("dummy", null, new File(directory));
+            tempFile.deleteOnExit();
+            success = true;
+        }
+        catch (IOException e) {
+            // file could not be created
+        }
+        catch (SecurityException e) {
+            // security manager exists and checkWrite method does not allow a file to be created
+
+        }
+        return success;
+    }
+}
+
