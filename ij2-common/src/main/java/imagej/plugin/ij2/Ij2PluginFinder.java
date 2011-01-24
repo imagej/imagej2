fd4a1aae3313534eb32c113a407b8685220da66f
Curtis Rueden <ctrueden@wisc.edu>
2011-01-24 16:01:23 -0600

    Add debug statement indicating initiation of plugin search.
    This used to be revision r2110.

--- ij2-common/src/main/java/imagej/plugin/ij2/Ij2PluginFinder.java
+++ ij2-common/src/main/java/imagej/plugin/ij2/Ij2PluginFinder.java
@@ -18,6 +18,7 @@ public class Ij2PluginFinder implements PluginFinder {
 
 	@Override
 	public void findPlugins(List<PluginEntry> plugins) {
+		Log.debug("Searching for IJ2 plugins...");
 		// use SezPoz to discover available IJ2 plugins
 		for (final IndexItem<Plugin, IPlugin> item :
 			Index.load(Plugin.class, IPlugin.class))
