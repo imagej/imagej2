8587a1988871b02f749785f811a2cc06d007be7e
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2011-01-27 17:58:18 +0000

    make variable names better
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2132 be23795f-a60e-40d4-ba19-055798872e57

--- ij2-common/src/main/java/imagej/function/unary/AddNoiseUnaryFunction.java
+++ ij2-common/src/main/java/imagej/function/unary/AddNoiseUnaryFunction.java
@@ -8,19 +8,19 @@ import java.util.Random;
 public class AddNoiseUnaryFunction implements UnaryFunction
 {
 	private boolean dataIsIntegral;
-	private double min;
-	private double max;
-	private double range;
-	private Random rnd;
+	private double typeRangeMin;
+	private double typeRangeMax;
+	private double spread;
+	private Random rng;
 	
-	public AddNoiseUnaryFunction(boolean isIntegral, double min, double max, double range)
+	public AddNoiseUnaryFunction(boolean isIntegral, double typeMin, double typeMax, double spread)
 	{
 		this.dataIsIntegral = isIntegral;
-		this.min = min;
-		this.max = max;
-		this.range = range;
-		this.rnd = new Random();
-		this.rnd.setSeed(System.currentTimeMillis());  // TODO - this line not present in original code (by design???)
+		this.typeRangeMin = typeMin;
+		this.typeRangeMax = typeMax;
+		this.spread = spread;
+		this.rng = new Random();
+		this.rng.setSeed(System.currentTimeMillis());  // TODO - this line not present in original code (by design???)
 	}
 	
 	public double compute(double input)
@@ -28,11 +28,11 @@ public class AddNoiseUnaryFunction implements UnaryFunction
 		double result, ran;
 		boolean inRange = false;
 		do {
-			ran = this.rnd.nextGaussian() * this.range;
+			ran = this.rng.nextGaussian() * this.spread;
 			if (this.dataIsIntegral)
 				ran = Math.round(ran);
 			result = input + ran;
-			inRange = DoubleRange.inside(this.min, this.max, result);
+			inRange = DoubleRange.inside(this.typeRangeMin, this.typeRangeMax, result);
 		} while (!inRange);
 		return result;
 	}
