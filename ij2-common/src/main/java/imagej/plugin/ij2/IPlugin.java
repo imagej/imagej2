5df5664076bf978c88ddd1cdeb224e810bbebc15
Curtis Rueden <ctrueden@wisc.edu>
2011-01-30 23:09:22 -0600

    Document empty blocks.
    This used to be revision r2148.

--- ij2-common/src/main/java/imagej/plugin/ij2/IPlugin.java
+++ ij2-common/src/main/java/imagej/plugin/ij2/IPlugin.java
@@ -1,3 +1,6 @@
 package imagej.plugin.ij2;
 
-public interface IPlugin extends Runnable { }
+public interface IPlugin extends Runnable {
+	// IPlugin trivially extends Runnable, so that the name of the interface
+	// unambiguously identifies an ImageJ plugin, rather than any Runnable object.
+}
--- ij2-common/src/main/java/imagej/plugin/ij2/PluginPreprocessor.java
+++ ij2-common/src/main/java/imagej/plugin/ij2/PluginPreprocessor.java
@@ -1,3 +1,6 @@
 package imagej.plugin.ij2;
 
-public interface PluginPreprocessor extends PluginProcessor { }
+public interface PluginPreprocessor extends PluginProcessor {
+	// PluginPreprocessor trivially extends PluginProcessor to differentiate
+	// preprocessors from postprocessors while sharing the same contract.
+}
