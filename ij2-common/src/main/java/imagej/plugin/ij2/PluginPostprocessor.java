3f6a510b1d3e7003b98f0e433e3ba543a8fd1776
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-01-31 05:09:22 +0000

    Document empty blocks.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2148 be23795f-a60e-40d4-ba19-055798872e57

--- ij2-common/src/main/java/imagej/plugin/ij2/PluginPostprocessor.java
+++ ij2-common/src/main/java/imagej/plugin/ij2/PluginPostprocessor.java
@@ -1,3 +1,6 @@
 package imagej.plugin.ij2;
 
-public interface PluginPostprocessor extends PluginProcessor { }
+public interface PluginPostprocessor extends PluginProcessor {
+	// PluginPostprocessor trivially extends PluginProcessor to differentiate
+	// preprocessors from postprocessors while sharing the same contract.
+}
