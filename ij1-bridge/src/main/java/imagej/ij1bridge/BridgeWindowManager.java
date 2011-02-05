f0e39da42bafb696b5daec244d7a59bac9cfe55f
Curtis Rueden <ctrueden@wisc.edu>
2011-02-04 19:04:26 -0600

    Use delegation in BridgeWindowManager for now, with debugging statements.
    This used to be revision r2165.

--- ij1-bridge/src/main/java/imagej/ij1bridge/BridgeWindowManager.java
+++ ij1-bridge/src/main/java/imagej/ij1bridge/BridgeWindowManager.java
@@ -1,181 +1,185 @@
 package imagej.ij1bridge;
 
-import ij.IWindowManager;
-import ij.ImagePlus;
-import ij.gui.ImageWindow;
-
 import java.awt.Frame;
 import java.awt.MenuItem;
 
-/** ImageJ window manager that bridges ImageWindow with Dataset. */
+import ij.DefaultWindowManager;
+import ij.IWindowManager;
+import ij.ImagePlus;
+import ij.gui.ImageWindow;
+import imagej.Log;
+
 public class BridgeWindowManager implements IWindowManager {
 
-	@Override
-	public void activateWindow(String arg0, MenuItem arg1) {
-		// TODO Auto-generated method stub
-		
-	}
+	private DefaultWindowManager dwm = new DefaultWindowManager();
 
 	@Override
-	public void addWindow(Frame arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void checkForDuplicateName(ImagePlus arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public boolean closeAllWindows() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public ImagePlus getCurrentImage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getCurrentIndex() {
-		// TODO Auto-generated method stub
-		return 0;
+	public void setCurrentWindow(ImageWindow win) {
+		Log.debug("BridgeWindowManager.setCurrentWindow: win=" + win);//TEMP
+		dwm.setCurrentWindow(win);
 	}
 
 	@Override
 	public ImageWindow getCurrentWindow() {
-		// TODO Auto-generated method stub
-		return null;
+		Log.debug("BridgeWindowManager.getCurrentWindow");//TEMP
+		return dwm.getCurrentWindow();
 	}
 
 	@Override
-	public Frame getFrame(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public int getCurrentIndex() {
+		Log.debug("BridgeWindowManager.getCurrentIndex");//TEMP
+		return dwm.getCurrentIndex();
 	}
 
 	@Override
-	public Frame getFrontWindow() {
-		// TODO Auto-generated method stub
-		return null;
+	public ImagePlus getCurrentImage() {
+		Log.debug("BridgeWindowManager.getCurrentImage");//TEMP
+		return dwm.getCurrentImage();
 	}
 
 	@Override
-	public int[] getIDList() {
-		// TODO Auto-generated method stub
-		return null;
+	public void setTempCurrentImage(ImagePlus img) {
+		Log.debug("setTempCurrentImage: img=" + img);//TEMP
+		dwm.setTempCurrentImage(img);
 	}
 
 	@Override
-	public ImagePlus getImage(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public ImagePlus getImage(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getImageCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Frame[] getNonImageWindows() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getNthImageID(int arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public ImagePlus getTempCurrentImage() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getUniqueName(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public void setTempCurrentImage(Thread thread, ImagePlus img) {
+		Log.debug("setTempCurrentImage(Thread thread, ImagePlus img)");//TEMP
+		dwm.setTempCurrentImage(thread, img);
 	}
 
 	@Override
 	public int getWindowCount() {
-		// TODO Auto-generated method stub
-		return 0;
+		Log.debug("getWindowCount()");//TEMP
+		return dwm.getWindowCount();
 	}
 
 	@Override
-	public boolean isDuplicateName(String arg0) {
-		// TODO Auto-generated method stub
-		return false;
+	public int getImageCount() {
+		Log.debug("getImageCount()");//TEMP
+		return dwm.getImageCount();
 	}
 
 	@Override
-	public String makeUniqueName(String arg0) {
-		// TODO Auto-generated method stub
-		return null;
+	public Frame getFrontWindow() {
+		Log.debug("getFrontWindow()");//TEMP
+		return dwm.getFrontWindow();
+	}
+
+	@Override
+	public int[] getIDList() {
+		Log.debug("getIDList()");//TEMP
+		return dwm.getIDList();
+	}
+
+	@Override
+	public Frame[] getNonImageWindows() {
+		Log.debug("getNonImageWindows()");//TEMP
+		return dwm.getNonImageWindows();
+	}
+
+	@Override
+	public ImagePlus getImage(int imageID) {
+		Log.debug("getImage(int imageID)");//TEMP
+		return dwm.getImage(imageID);
+	}
+
+	@Override
+	public int getNthImageID(int n) {
+		Log.debug("getNthImageID(int n)");//TEMP
+		return dwm.getNthImageID(n);
+	}
+
+	@Override
+	public ImagePlus getImage(String title) {
+		Log.debug("getImage(String title)");//TEMP
+		return dwm.getImage(title);
+	}
+
+	@Override
+	public void addWindow(Frame win) {
+		Log.debug("addWindow(Frame win)");//TEMP
+		dwm.addWindow(win);
+	}
+
+	@Override
+	public void checkForDuplicateName(ImagePlus imp) {
+		Log.debug("checkForDuplicateName(ImagePlus imp)");//TEMP
+		dwm.checkForDuplicateName(imp);
+
+	}
+
+	@Override
+	public boolean isDuplicateName(String name) {
+		Log.debug("isDuplicateName(String name)");//TEMP
+		return dwm.isDuplicateName(name);
+	}
+
+	@Override
+	public String getUniqueName(String name) {
+		Log.debug("getUniqueName(String name)");//TEMP
+		return dwm.getUniqueName(name);
+	}
+
+	@Override
+	public String makeUniqueName(String name) {
+		Log.debug("makeUniqueName(String name)");//TEMP
+		return dwm.makeUniqueName(name);
+	}
+
+	@Override
+	public void removeWindow(Frame win) {
+		Log.debug("removeWindow(Frame win)");//TEMP
+		dwm.removeWindow(win);
+	}
+
+	@Override
+	public void setWindow(Frame win) {
+		Log.debug("setWindow(Frame win)");//TEMP
+		dwm.setWindow(win);
+	}
+
+	@Override
+	public boolean closeAllWindows() {
+		Log.debug("closeAllWindows()");//TEMP
+		return dwm.closeAllWindows();
 	}
 
 	@Override
 	public void putBehind() {
-		// TODO Auto-generated method stub
-		
+		Log.debug("putBehind()");//TEMP
+		dwm.putBehind();
 	}
 
 	@Override
-	public void removeWindow(Frame arg0) {
-		// TODO Auto-generated method stub
-		
+	public ImagePlus getTempCurrentImage() {
+		Log.debug("getTempCurrentImage()");//TEMP
+		return dwm.getTempCurrentImage();
+	}
+
+	@Override
+	public Frame getFrame(String title) {
+		Log.debug("getFrame(String title)");//TEMP
+		return dwm.getFrame(title);
+	}
+
+	@Override
+	public void activateWindow(String menuItemLabel, MenuItem item) {
+		Log.debug("activateWindow(String menuItemLabel, MenuItem item)");//TEMP
+		dwm.activateWindow(menuItemLabel, item);
 	}
 
 	@Override
 	public void repaintImageWindows() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setCurrentWindow(ImageWindow arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setTempCurrentImage(ImagePlus arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setTempCurrentImage(Thread arg0, ImagePlus arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setWindow(Frame arg0) {
-		// TODO Auto-generated method stub
-		
+		Log.debug("repaintImageWindows()");//TEMP
+		dwm.repaintImageWindows();
 	}
 
 	@Override
 	public void showList() {
-		// TODO Auto-generated method stub
-		
+		Log.debug("showList()");//TEMP
+		dwm.showList();
 	}
 
 }
