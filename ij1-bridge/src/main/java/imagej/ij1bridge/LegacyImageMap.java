43e681a241caf0f6ea636a4235b2a4841994b824
Curtis Rueden <ctrueden@wisc.edu>
2011-02-08 08:56:37 -0600

    Split image translation into a separate class.
    This used to be revision r2181.

--- ij1-bridge/src/main/java/imagej/ij1bridge/LegacyImageMap.java
+++ ij1-bridge/src/main/java/imagej/ij1bridge/LegacyImageMap.java
@@ -2,17 +2,8 @@ package imagej.ij1bridge;
 
 import ij.ImagePlus;
 import ij.WindowManager;
-import imagej.AxisLabel;
-import imagej.MetaData;
-import imagej.data.FloatType;
-import imagej.data.Type;
-import imagej.data.UnsignedByteType;
-import imagej.data.UnsignedShortType;
-import imagej.dataset.CompositeDataset;
 import imagej.dataset.Dataset;
-import imagej.dataset.PlanarDataset;
 
-import java.util.ArrayList;
 import java.util.Map;
 import java.util.WeakHashMap;
 
@@ -21,6 +12,8 @@ public class LegacyImageMap {
 	// TODO - change Dataset to Img when imglib2 is ready
 	private Map<ImagePlus, Dataset> imageTable =
 		new WeakHashMap<ImagePlus, Dataset>();
+
+	private ImageTranslator imageTranslator = new ImageTranslator();
 
 	/**
 	 * Ensures that the given legacy image has a corresponding dataset.
@@ -32,7 +25,7 @@ public class LegacyImageMap {
 			Dataset dataset = imageTable.get(imp);
 			if (dataset == null) {
 				// mirror image window to dataset
-				dataset = createDatasetMirror(imp);
+				dataset = imageTranslator.createDataset(imp);
 				imageTable.put(imp, dataset);
 			}
 			return dataset;
@@ -57,64 +50,12 @@ public class LegacyImageMap {
 			}
 			if (imp == null) {
 				// mirror dataset to image window
-				imp = createLegacyImageMirror(dataset);
+				imp = imageTranslator.createLegacyImage(dataset);
 				imageTable.put(imp, dataset);
 			}
 		}
 		WindowManager.setTempCurrentImage(imp);
 		return imp;
-	}
-
-	private Dataset createDatasetMirror(final ImagePlus imp) {
-		final Type type;
-		switch (imp.getType()) {
-			case ImagePlus.GRAY8:
-				type = new UnsignedByteType();
-				break;
-			case ImagePlus.GRAY16:
-				type = new UnsignedShortType();
-				break;
-			case ImagePlus.GRAY32:
-				type = new FloatType();
-				break;
-			default:
-				throw new IllegalArgumentException(
-					"Only uint8, uint16 and float32 supported for now");
-		}
-		final int[] dims = imp.getDimensions();
-		final int[] cDims = {dims[0], dims[1]};
-		final int[] zDims = {dims[0], dims[1], dims[2]};
-		final int[] tDims = {dims[0], dims[1], dims[2], dims[3]};
-		int i = 0;
-		final ArrayList<Dataset> tDatasets = new ArrayList<Dataset>();
-		for (int t=0; t<dims[4]; t++) {
-			final ArrayList<Dataset> zDatasets = new ArrayList<Dataset>();
-			for (int z=0; z<dims[3]; z++) {
-				final ArrayList<Dataset> cDatasets = new ArrayList<Dataset>();
-				for (int c=0; c<dims[2]; c++) {
-					final Object plane = imp.getStack().getPixels(++i);
-					cDatasets.add(new PlanarDataset(cDims, type, plane));
-				}
-				zDatasets.add(new CompositeDataset(type, zDims, cDatasets));
-			}
-			tDatasets.add(new CompositeDataset(type, tDims, zDatasets));
-		}
-		final Dataset dataset = new CompositeDataset(type, dims, tDatasets);
-
-		final MetaData metadata = new MetaData();
-		metadata.setLabel(imp.getTitle());
-		final AxisLabel[] axisLabels = {
-			AxisLabel.X, AxisLabel.Y, AxisLabel.CHANNEL, AxisLabel.Z, AxisLabel.TIME
-		};
-		metadata.setAxisLabels(axisLabels);
-		dataset.setMetaData(metadata);
-
-		return dataset;
-	}
-
-	private ImagePlus createLegacyImageMirror(final Dataset dataset) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
