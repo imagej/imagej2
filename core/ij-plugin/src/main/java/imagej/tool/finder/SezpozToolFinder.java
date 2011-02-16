0bcf618df657399724e3199e670901b0f71c91d3
Rick Lentz <rlentz@gmail.com>
2011-02-16 13:49:44 -0600

    Started adding the ITool java files
    This used to be revision r2221.

---
+++ core/ij-plugin/src/main/java/imagej/tool/finder/SezpozToolFinder.java
@@ -0,0 +1,62 @@
+package imagej.tool.finder;
+
+import imagej.tool.ITool;
+import imagej.plugin.Menu;
+import imagej.plugin.Plugin;
+import imagej.plugin.api.MenuEntry;
+import imagej.plugin.api.PluginEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.java.sezpoz.Index;
+import net.java.sezpoz.IndexItem;
+
+public class SezpozToolFinder<T extends ITool>
+{
+	public void findPlugins(List<PluginEntry> plugins, Class<T> c)
+        {
+		// use SezPoz to discover available plugins
+		for ( final IndexItem<Plugin, T> item :
+			Index.load(Plugin.class, c))
+		{
+			final String pluginClass = item.className();
+			final List<MenuEntry> menuPath = new ArrayList<MenuEntry>();
+
+			// parse menu path from annotations
+			final Menu[] menu = item.annotation().menu();
+			if (menu.length > 0) {
+				parseMenuPath(menuPath, menu);
+			}
+			else {
+				// parse menuPath attribute
+				final String path = item.annotation().menuPath();
+				parseMenuPath(menuPath, path);
+			}
+
+			final PluginEntry pluginEntry = new PluginEntry( pluginClass, menuPath );
+			plugins.add(pluginEntry);
+		}
+	}
+
+	private void parseMenuPath( final List<MenuEntry> menuPath, final Menu[] menu )
+	{
+		for (int i = 0; i < menu.length; i++)
+                {
+			final String name = menu[i].label();
+			final double weight = menu[i].weight();
+			final char mnemonic = menu[i].mnemonic();
+			final String accelerator = menu[i].accelerator();
+			final String icon = menu[i].icon();				
+			menuPath.add(new MenuEntry(name, weight, mnemonic, accelerator, icon));
+		}
+	}
+
+	private void parseMenuPath(final List<MenuEntry> menuPath,
+		final String path)
+	{
+		final String[] menuPathTokens = path.split(">");
+		for (String token : menuPathTokens) menuPath.add(new MenuEntry(token));
+	}
+
+}
