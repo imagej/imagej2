0865de47db93d04751b7468f08bc944e9b45ae15
rlentz <rlentz@be23795f-a60e-40d4-ba19-055798872e57>
2011-02-16 22:04:43 +0000

    Initial tools commit
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2224 be23795f-a60e-40d4-ba19-055798872e57

---
+++ core/ij-core-plugins/src/main/java/imagej/core/tools/EraserTool.java
@@ -0,0 +1,70 @@
+package imagej.core.tools;
+
+import imagej.tool.ITool;
+import imagej.tool.Tool;
+import javax.swing.JFrame;
+
+/**
+ * @author rick
+ */
+@Tool
+public class EraserTool implements ITool {
+
+
+    public void setJFrame( JFrame jFrame ){
+         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean deactivate()
+    {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getCursor() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean onContextMenu(int x, int y) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onDblClick()
+    {
+         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onKeyDown(int keyCode, int shift)
+    {
+         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onKeyUp(int keyCode, int shift)
+    {
+       throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onMouseDown(int button, int shift, int x, int y)
+    {
+         throw new UnsupportedOperationException("Not supported yet.");
+
+    }
+
+    @Override
+    public void onMouseMove(int button, int shift, int x, int y)
+    {
+         throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void onMouseUp(int button, int shift, int x, int y)
+    {
+         throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
