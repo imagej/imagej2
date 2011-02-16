ae248c72cfb0f95e3e02a838bcf6e39860ff2868
rlentz <rlentz@be23795f-a60e-40d4-ba19-055798872e57>
2011-02-16 22:21:37 +0000

    Removed ProbeTool
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2226 be23795f-a60e-40d4-ba19-055798872e57

--- core/ij-plugin/src/main/java/imagej/tool/Tool.java
+++ core/ij-plugin/src/main/java/imagej/tool/Tool.java
@@ -10,8 +10,10 @@ import net.java.sezpoz.Indexable;
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.TYPE)
 @Indexable(type=ITool.class)
-public @interface Tool {
-
-	String toolbarIcon() default "";
-
+public @interface Tool
+{
+        String name() default "";
+	String label() default "";
+	String description() default "";
+	String iconPath() default "";
 }
