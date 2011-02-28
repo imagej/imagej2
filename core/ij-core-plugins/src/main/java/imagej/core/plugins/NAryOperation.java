f5dcd92224cc3f0bfcff42d68df623c03082702d
Barry DeZonia <bdezonia@wisc.edu>
2011-02-28 12:27:57 -0600

    change constructors from protected to public allowing better reuse plus did some minor internal changes
    This used to be revision r2287.

--- core/ij-core-plugins/src/main/java/imagej/core/plugins/NAryOperation.java
+++ core/ij-core-plugins/src/main/java/imagej/core/plugins/NAryOperation.java
@@ -10,10 +10,8 @@ import imglib.ops.operation.AssignOperation;
 import java.util.ArrayList;
 import java.util.List;
 
-import mpicbg.imglib.container.planar.PlanarContainerFactory;
 import mpicbg.imglib.cursor.Cursor;
 import mpicbg.imglib.image.Image;
-import mpicbg.imglib.image.ImageFactory;
 import mpicbg.imglib.type.numeric.RealType;
 import mpicbg.imglib.type.numeric.integer.UnsignedShortType;
 
@@ -25,8 +23,8 @@ import mpicbg.imglib.type.numeric.integer.UnsignedShortType;
  * @author Barry DeZonia
  * @author Curtis Rueden
  */
-public class NAryOperation<T extends RealType<T>> implements ImageJPlugin {
-
+public class NAryOperation<T extends RealType<T>> implements ImageJPlugin
+{
 	// TODO - SwingInputHarvester must prompt user for list of images
 	//  + need N-list widget with plus and minus buttons
 	//  + need Dataset support -- JComboBox
@@ -47,21 +45,21 @@ public class NAryOperation<T extends RealType<T>> implements ImageJPlugin {
 	// ***************  public/protected interface ***************************************************************
 
 	/** this constructor required for plugins that create a function that uses one of it's @Parameter values in construction */
-	protected NAryOperation()
+	public NAryOperation()
 	{
 		this.function = null;
 	}
 	
 	/** preferred constructor */
-	protected NAryOperation(RealFunction<T> function)
+	public NAryOperation(RealFunction<T> function)
 	{
-		setFunc(function);
+		this.function = function;
 	}
 
 	/** helper method that allows the function of an operation to be changed */
 	public void setFunction(RealFunction<T> function)
 	{
-		setFunc(function);
+		this.function = function;
 	}
 	
 	/** runs the plugin applying the operation's function to the input and assigning it to the output */
@@ -70,7 +68,7 @@ public class NAryOperation<T extends RealType<T>> implements ImageJPlugin {
 	{
 		if (in == null)  // TODO - temporary code to test these until IJ2 plugins can correctly fill a List<Dataset> @Parameter
 		{
-			Image<UnsignedShortType> junkImage = createImage(new UnsignedShortType(), new int[]{200,200});
+			Image<UnsignedShortType> junkImage = Dataset.createPlanarImage("", new UnsignedShortType(), new int[]{200,200});
 			Cursor<UnsignedShortType> cursor = junkImage.createCursor();
 			int index = 0;
 			for (UnsignedShortType pixRef : cursor)
@@ -105,20 +103,6 @@ public class NAryOperation<T extends RealType<T>> implements ImageJPlugin {
 
 	// ***************  private interface ***************************************************************
 
-	/** this exists so that anyone overriding setFunction() does not break NAryOperation(function) constructor */
-	private void setFunc(RealFunction<T> func)
-	{
-		this.function = func;
-	}
-	
-	/** create an image of given type and dimensions using specified container type */
-	private static <K extends RealType<K>> Image<K> createImage(RealType<K> type, int[] dimensions)
-	{
-		PlanarContainerFactory cFact = new PlanarContainerFactory();
-		ImageFactory<K> factory = new ImageFactory<K>((K)type, cFact);
-		return factory.createImage(dimensions);
-	}
-	
 	/** make an image that has same type and dimensions as Dataset (but in a planar container) */
 	private Image<T> imageFromDataset(Dataset dataset)
 	{
