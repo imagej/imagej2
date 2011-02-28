3cd32009e34bf6e9efcc520e36c836fee71c86d5
Barry DeZonia <bdezonia@wisc.edu>
2011-02-28 17:28:50 -0600

    initial implementation
    This used to be revision r2293.

---
+++ core/ij-core-plugins/src/main/java/imagej/core/plugins/AddNoiseToDataValues.java
@@ -0,0 +1,64 @@
+package imagej.core.plugins;
+
+import java.util.ArrayList;
+import imagej.model.Dataset;
+import imagej.plugin.Parameter;
+import imagej.plugin.Plugin;
+import imglib.ops.function.p1.UnaryOperatorFunction;
+import imglib.ops.operator.UnaryOperator;
+import imglib.ops.operator.unary.AddNoise;
+import mpicbg.imglib.cursor.Cursor;
+import mpicbg.imglib.image.Image;
+import mpicbg.imglib.type.numeric.RealType;
+import mpicbg.imglib.type.numeric.integer.UnsignedShortType;
+
+/**
+ * TODO
+ *
+ * @author Barry DeZonia
+ */
+@Plugin(
+	menuPath = "Process>Noise>Add Specified Noise"
+)
+public class AddNoiseToDataValues<T extends RealType<T>> extends NAryOperation<T>
+{
+	@Parameter(label="Enter standard deviation of range")
+	private double rangeStdDev;
+	
+	private double rangeMin, rangeMax;
+	
+	public AddNoiseToDataValues()
+	{
+	}
+
+	@Override
+	public void run()
+	{
+		if (in == null)  // TODO - temporary code to test these until IJ2 plugins can correctly fill a List<Dataset> @Parameter
+		{
+			Image<UnsignedShortType> junkImage = Dataset.createPlanarImage("", new UnsignedShortType(), new int[]{200,200});
+			Cursor<UnsignedShortType> cursor = junkImage.createCursor();
+			int index = 0;
+			for (UnsignedShortType pixRef : cursor)
+				pixRef.set(index++);
+			cursor.close();
+			in = new ArrayList<Dataset>();
+			in.add(new Dataset(junkImage));
+		}
+		
+		calcRangeMinAndMax();
+		UnaryOperator op = new AddNoise(rangeMin, rangeMax, rangeStdDev);
+		UnaryOperatorFunction<T> opFunc = new UnaryOperatorFunction<T>(op);
+		setFunction(opFunc);
+		super.run();
+	}
+
+	private void calcRangeMinAndMax()
+	{
+		Cursor<T> cursor = (Cursor<T>) in.get(0).getImage().createCursor();
+		rangeMin = cursor.getType().getMinValue();
+		rangeMax = cursor.getType().getMaxValue();
+		cursor.close();
+	}
+	
+}
