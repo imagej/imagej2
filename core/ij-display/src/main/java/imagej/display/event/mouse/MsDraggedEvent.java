47f37670cd1e36668438dcdb845fd5f46038f39f
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-03-02 17:51:02 +0000

    Add support for isPopupTrigger to mouse events.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2307 be23795f-a60e-40d4-ba19-055798872e57

--- core/ij-display/src/main/java/imagej/display/event/mouse/MsDraggedEvent.java
+++ core/ij-display/src/main/java/imagej/display/event/mouse/MsDraggedEvent.java
@@ -9,10 +9,10 @@ import imagej.display.Display;
  */
 public class MsDraggedEvent extends MsButtonEvent {
 
-	public MsDraggedEvent(final Display display,
-		final int x, final int y, final int button)
+	public MsDraggedEvent(final Display display, final int x, final int y,
+		final int button, final int numClicks, final boolean isPopupTrigger)
 	{
-		super(display, x, y, button, 1);
+		super(display, x, y, button, numClicks, isPopupTrigger);
 	}
 
 }
