dce2984d7d970e86e8d809a367ce976db820a39a
Curtis Rueden <ctrueden@wisc.edu>
2011-03-05 14:08:23 -0600

    Clean up licensing and add license blurbs.
    This used to be revision r2329.

--- extra/workflow/src/main/java/imagej/workflow/PluginModule.java
+++ extra/workflow/src/main/java/imagej/workflow/PluginModule.java
@@ -1,15 +1,36 @@
+//
+// PluginModule.java
+//
+
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-//TODO
-/*
- * Is the name a short name or the full path, i.e. loci.plugin.Whatever?  Why have a set name?
- * Need to instantiate the plugin.
- * Feeding in the image should work; how to set up listener?
- * How to do the linking?  IComponent could have chain() method, delegates to m_linkedPlugin.  How do WorkFlows get
- * chained?
- */
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
 
 package imagej.workflow;
 
@@ -32,6 +53,15 @@ import imagej.workflow.util.xmllight.XMLException;
 import imagej.workflow.util.xmllight.XMLParser;
 import imagej.workflow.util.xmllight.XMLTag;
 import imagej.workflow.util.xmllight.XMLWriter;
+
+//TODO
+/*
+ * Is the name a short name or the full path, i.e. loci.plugin.Whatever?  Why have a set name?
+ * Need to instantiate the plugin.
+ * Feeding in the image should work; how to set up listener?
+ * How to do the linking?  IComponent could have chain() method, delegates to m_linkedPlugin.  How do WorkFlows get
+ * chained?
+ */
 
 /**
  *
--- extra/workflow/src/main/java/imagej/workflow/plugin/ItemWrapper.java
+++ extra/workflow/src/main/java/imagej/workflow/plugin/ItemWrapper.java
@@ -3,21 +3,21 @@
 //
 
 /*
-Framework for chaining processors together; one input and one output.
+ImageJ software for multidimensional image processing and analysis.
 
-Copyright (c) 2010, UW-Madison LOCI
+Copyright (c) 2010, ImageJDev.org.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-  * Redistributions of source code must retain the above copyright
-    notice, this list of conditions and the following disclaimer.
-  * Redistributions in binary form must reproduce the above copyright
-    notice, this list of conditions and the following disclaimer in the
-    documentation and/or other materials provided with the distribution.
-  * Neither the name of the UW-Madison LOCI nor the
-    names of its contributors may be used to endorse or promote products
-    derived from this software without specific prior written permission.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
