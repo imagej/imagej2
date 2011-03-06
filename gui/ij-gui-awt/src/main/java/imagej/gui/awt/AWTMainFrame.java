a23f893e05b7acdf06b26a264181676ff02fb666
Curtis Rueden <ctrueden@wisc.edu>
2011-03-05 23:31:31 -0600

    Migrate GUI-related projects to separate subtree.
    Also begin work on AWT version of the user interface, as proof of concept.
    
    
    This used to be revision r2337.

---
+++ gui/ij-gui-awt/src/main/java/imagej/gui/awt/AWTMainFrame.java
@@ -0,0 +1,92 @@
+//
+// AWTMainFrame.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.gui.awt;
+
+import imagej.plugin.api.PluginEntry;
+import imagej.plugin.api.PluginUtils;
+import imagej.plugin.gui.ShadowMenu;
+import imagej.plugin.gui.awt.MenuBarCreator;
+import imagej.tool.ToolManager;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.MenuBar;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
+
+/**
+ * AWT-based main window for ImageJ.
+ *
+ * @author Curtis Rueden
+ */
+public class AWTMainFrame {
+
+	private final Frame frame;
+	private final AWTToolBar toolBar;
+	private final AWTStatusBar statusBar;
+
+	/** Creates a new ImageJ application frame. */
+	public AWTMainFrame() {
+		frame = new Frame("ImageJ");
+		toolBar = new AWTToolBar(new ToolManager());
+		statusBar = new AWTStatusBar();
+		createMenuBar();
+
+		frame.setLayout(new BorderLayout());
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(final WindowEvent e) {
+				System.exit(0);
+			}
+		});
+
+		frame.add(toolBar, BorderLayout.NORTH);
+		frame.add(statusBar, BorderLayout.SOUTH);
+
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+	private void createMenuBar() {
+		final List<PluginEntry<?>> entries = PluginUtils.findPlugins();
+		statusBar.setStatus("Discovered " + entries.size() + " plugins");
+		final ShadowMenu rootMenu = new ShadowMenu(entries);
+		final MenuBar menuBar = new MenuBar();
+		new MenuBarCreator().createMenus(rootMenu, menuBar);
+		frame.setMenuBar(menuBar);
+	}
+
+}
--- gui/imagej/src/main/java/imagej/launcher/swing/SwingLauncher.java
+++ gui/imagej/src/main/java/imagej/launcher/swing/SwingLauncher.java
@@ -1,5 +1,5 @@
 //
-// MainFrame.java
+// SwingLauncher.java
 //
 
 /*
@@ -32,63 +32,24 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 */
 
-package imagej.gui.swing;
+package imagej.launcher.swing;
 
-import imagej.plugin.api.PluginEntry;
-import imagej.plugin.api.PluginUtils;
-import imagej.plugin.gui.ShadowMenu;
-import imagej.plugin.gui.swing.JMenuBarCreator;
-
-import java.awt.BorderLayout;
-import java.util.List;
-
-import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import imagej.gui.swing.SwingMainFrame;
 
 /**
- * A simple and dumb Swing-based main window for ImageJ2.
+ * Launches the ImageJ Swing user interface.
  *
  * @author Curtis Rueden
  */
-public class MainFrame {
+public final class SwingLauncher {
 
-	private final JFrame frame;
-	private final ToolBar toolBar;
-	private final StatusBar statusBar;
-
-	/** Creates a new ImageJ frame that runs as an application. */
-	public MainFrame() {
-		System.setProperty("apple.laf.useScreenMenuBar", "true");
-		frame = new JFrame("ImageJ");
-		toolBar = new ToolBar();
-		statusBar = new StatusBar();
-		createMenuBar();
-
-		final JPanel pane = new JPanel();
-		frame.setContentPane(pane);
-		pane.setLayout(new BorderLayout());
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-		pane.add(toolBar, BorderLayout.NORTH);
-		pane.add(statusBar, BorderLayout.SOUTH);
-
-		frame.pack();
-		frame.setVisible(true);
-	}
-
-	private void createMenuBar() {
-		final List<PluginEntry<?>> entries = PluginUtils.findPlugins();
-		statusBar.setStatus("Discovered " + entries.size() + " plugins");
-		final ShadowMenu rootMenu = new ShadowMenu(entries);
-		final JMenuBar menuBar = new JMenuBar();
-		new JMenuBarCreator().createMenus(rootMenu, menuBar);
-		frame.setJMenuBar(menuBar);
+	private SwingLauncher() {
+		// prevent instantiation of utility class
 	}
 
 	public static void main(String[] args) {
-		new MainFrame();
+		System.setProperty("apple.laf.useScreenMenuBar", "true");
+		new SwingMainFrame();
 	}
 
 }
