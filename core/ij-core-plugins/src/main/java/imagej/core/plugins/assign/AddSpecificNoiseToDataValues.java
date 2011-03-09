934f096b76362a7604f42c2054dfa4d366b5c728
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2011-03-09 16:31:55 +0000

    reorganize core-plugins package structure
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2358 be23795f-a60e-40d4-ba19-055798872e57

---
+++ core/ij-core-plugins/src/main/java/imagej/core/plugins/assign/AddSpecificNoiseToDataValues.java
@@ -0,0 +1,75 @@
+//
+// AddSpecificNoiseToDataValues.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.core.plugins.assign;
+
+import imagej.model.Dataset;
+import imagej.plugin.ImageJPlugin;
+import imagej.plugin.Menu;
+import imagej.plugin.Parameter;
+import imagej.plugin.Plugin;
+
+/**
+ * Fills an output Dataset by applying a user calibrated amount of random noise
+ * to an input Dataset.
+ * 
+ * @author Barry DeZonia
+ */
+@Plugin(menu = {
+	@Menu(label = "Process", mnemonic = 'p'),
+	@Menu(label = "Noise", mnemonic = 'n'),
+	@Menu(label = "Add Specified Noise...", weight = 2) })
+public class AddSpecificNoiseToDataValues implements ImageJPlugin {
+
+	// -- instance variables that are Parameters --
+
+	@Parameter
+	Dataset input;
+
+	@Parameter(output = true)
+	Dataset output;
+
+	@Parameter(label = "Enter standard deviation of range")
+	double stdDev;
+
+	// -- public interface --
+
+	@Override
+	public void run() {
+		AddNoiseToDataValues noiseAdder = new AddNoiseToDataValues(input);
+		noiseAdder.setOutput(output);
+		noiseAdder.setStdDev(stdDev);
+		output = noiseAdder.run();
+	}
+}
---
+++ core/ij-core-plugins/src/main/java/imagej/core/plugins/assign/FillDataValues.java
@@ -0,0 +1,74 @@
+//
+// FillDataValues.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.core.plugins.assign;
+
+import imagej.model.Dataset;
+import imagej.plugin.ImageJPlugin;
+import imagej.plugin.Menu;
+import imagej.plugin.Parameter;
+import imagej.plugin.Plugin;
+import imglib.ops.operator.UnaryOperator;
+import imglib.ops.operator.unary.Constant;
+
+/**
+ * Fills an output Dataset with a user defined constant value.
+ * 
+ * @author Barry DeZonia
+ */
+@Plugin(menu = {
+	@Menu(label = "Edit", mnemonic = 'e'),
+	@Menu(label = "Fill", accelerator = "command f")})
+public class FillDataValues implements ImageJPlugin {
+
+	// -- instance variables that are Parameters --
+
+	@Parameter
+	private Dataset input; // TODO - does this make sense? used for dimensions
+	// only I think. Or do we want in place changing?
+
+	@Parameter(output = true)
+	private Dataset output;
+
+	@Parameter(label = "Enter value to fill each data value with")
+	private double constant;
+
+	// -- public interrface --
+
+	@Override
+	public void run() {
+		UnaryOperator op = new Constant(constant);
+		output = new UnaryTransformation(input, output, op).run();
+	}
+}
