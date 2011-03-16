ec31be21445df7dab7254298d3e6f1fa9f4ce402
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-03-16 20:17:18 +0000

    Add initial event classes for Mac platform issues.
    Not hooked up to anything yet.
    
    
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2407 be23795f-a60e-40d4-ba19-055798872e57

---
+++ gui/platform-macosx/src/main/java/imagej/platform/macosx/MacHandler.java
@@ -0,0 +1,189 @@
+//
+// MacHandler.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.platform.macosx;
+
+import com.apple.eawt.AboutHandler;
+import com.apple.eawt.AppEvent.AboutEvent;
+import com.apple.eawt.AppEvent.AppForegroundEvent;
+import com.apple.eawt.AppEvent.AppHiddenEvent;
+import com.apple.eawt.AppEvent.AppReOpenedEvent;
+import com.apple.eawt.AppEvent.PreferencesEvent;
+import com.apple.eawt.AppEvent.PrintFilesEvent;
+import com.apple.eawt.AppEvent.QuitEvent;
+import com.apple.eawt.AppEvent.ScreenSleepEvent;
+import com.apple.eawt.AppEvent.SystemSleepEvent;
+import com.apple.eawt.AppEvent.UserSessionEvent;
+import com.apple.eawt.AppForegroundListener;
+import com.apple.eawt.AppHiddenListener;
+import com.apple.eawt.AppReOpenedListener;
+import com.apple.eawt.Application;
+import com.apple.eawt.PreferencesHandler;
+import com.apple.eawt.PrintFilesHandler;
+import com.apple.eawt.QuitHandler;
+import com.apple.eawt.QuitResponse;
+import com.apple.eawt.ScreenSleepListener;
+import com.apple.eawt.SystemSleepListener;
+import com.apple.eawt.UserSessionListener;
+
+import imagej.event.Events;
+import imagej.event.app.AppAboutEvent;
+import imagej.event.app.AppFocusEvent;
+import imagej.event.app.AppPreferencesEvent;
+import imagej.event.app.AppPrintEvent;
+import imagej.event.app.AppQuitEvent;
+import imagej.event.app.AppReOpenEvent;
+import imagej.event.app.AppScreenSleepEvent;
+import imagej.event.app.AppSystemSleepEvent;
+import imagej.event.app.AppUserSessionEvent;
+import imagej.event.app.AppVisibleEvent;
+
+/**
+ * An adapter for handling Mac OS X application issues. Application events are
+ * rebroadcast as ImageJ events.
+ * 
+ * @author Curtis Rueden
+ */
+public class MacHandler implements AboutHandler, AppForegroundListener,
+	AppHiddenListener, AppReOpenedListener, PreferencesHandler,
+	PrintFilesHandler, QuitHandler, ScreenSleepListener, SystemSleepListener,
+	UserSessionListener
+{
+
+	/** Constructs a Mac OS X adapter. */
+	public MacHandler() {
+		final Application app = Application.getApplication();
+		app.setAboutHandler(this);
+		app.setPreferencesHandler(this);
+		app.setPrintFileHandler(this);
+		app.setQuitHandler(this);
+		app.addAppEventListener(this);
+	}
+
+	// -- AboutHandler methods --
+
+	@Override
+	public void handleAbout(final AboutEvent e) {
+		Events.publish(new AppAboutEvent());
+	}
+
+	// -- PreferencesHandler methods --
+
+	@Override
+	public void handlePreferences(final PreferencesEvent e) {
+		Events.publish(new AppPreferencesEvent());
+	}
+
+	// -- PrintFilesHandler --
+
+	@Override
+	public void printFiles(final PrintFilesEvent e) {
+		Events.publish(new AppPrintEvent());
+	}
+
+	// -- QuitHandler methods --
+
+	@Override
+	public void handleQuitRequestWith(final QuitEvent e, final QuitResponse r) {
+		Events.publish(new AppQuitEvent());
+	}
+
+	// -- UserSessionListener methods --
+
+	@Override
+	public void userSessionActivated(final UserSessionEvent e) {
+		Events.publish(new AppUserSessionEvent(true));
+	}
+
+	@Override
+	public void userSessionDeactivated(final UserSessionEvent e) {
+		Events.publish(new AppUserSessionEvent(false));
+	}
+
+	// -- SystemSleepListener methods --
+
+	@Override
+	public void systemAboutToSleep(final SystemSleepEvent e) {
+		Events.publish(new AppSystemSleepEvent(true));
+	}
+
+	@Override
+	public void systemAwoke(final SystemSleepEvent e) {
+		Events.publish(new AppSystemSleepEvent(false));
+	}
+
+	// -- ScreenSleepListener methods --
+
+	@Override
+	public void screenAboutToSleep(final ScreenSleepEvent e) {
+		Events.publish(new AppScreenSleepEvent(true));
+	}
+
+	@Override
+	public void screenAwoke(final ScreenSleepEvent e) {
+		Events.publish(new AppScreenSleepEvent(false));
+	}
+
+	// -- AppHiddenListener methods --
+
+	@Override
+	public void appHidden(final AppHiddenEvent e) {
+		Events.publish(new AppVisibleEvent(false));
+	}
+
+	@Override
+	public void appUnhidden(final AppHiddenEvent e) {
+		Events.publish(new AppVisibleEvent(true));
+	}
+
+	// -- AppForegroundListener methods --
+
+	@Override
+	public void appMovedToBackground(final AppForegroundEvent e) {
+		Events.publish(new AppFocusEvent(false));
+	}
+
+	@Override
+	public void appRaisedToForeground(final AppForegroundEvent e) {
+		Events.publish(new AppFocusEvent(true));
+	}
+
+	// -- AppReOpenedListener methods --
+
+	@Override
+	public void appReOpened(final AppReOpenedEvent e) {
+		Events.publish(new AppReOpenEvent());
+	}
+
+}
