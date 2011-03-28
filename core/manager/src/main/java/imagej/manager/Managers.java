7c3ce26338d4391d160f2d428adc869ed1dd9bc4
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-03-28 19:12:06 +0000

    Add simple code for ImageJ manager components.
    The idea is that the ImageJ software is made up of discoverable
    "managers" that encapsulate specific pieces of functionality. The main
    ImageJ program discovers all the available managers and initializes
    them. In this way, ImageJ is more extensible, at least in theory...
    
    
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2490 be23795f-a60e-40d4-ba19-055798872e57

---
+++ core/manager/src/main/java/imagej/manager/Managers.java
@@ -0,0 +1,72 @@
+//
+// Managers.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.manager;
+
+import imagej.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.java.sezpoz.Index;
+import net.java.sezpoz.IndexItem;
+
+/**
+ * Utility class for discovering available manager components.
+ *
+ * @author Curtis Rueden
+ */
+public final class Managers {
+
+	private Managers() {
+		// prevent instantiation of utility class
+	}
+
+	public static List<ManagerComponent> loadManagers() {
+		// use SezPoz to discover all user interfaces
+		final List<ManagerComponent> managers = new ArrayList<ManagerComponent>();
+		for (final IndexItem<Manager, ManagerComponent> item :
+			Index.load(Manager.class, ManagerComponent.class))
+		{
+			try {
+				managers.add(item.instance());
+			}
+			catch (InstantiationException e) {
+				Log.warn("Invalid manager component: " + item, e);
+			}
+		}
+		return managers;
+	}
+
+}
