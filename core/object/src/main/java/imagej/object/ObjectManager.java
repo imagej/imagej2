37389b76f1b42f38f95b043c8db2bdfb699fa257
Curtis Rueden <ctrueden@wisc.edu>
2011-03-29 16:02:34 -0500

    Avoid garbage collection of event subscribers.
    This used to be revision r2524.

--- core/object/src/main/java/imagej/object/ObjectManager.java
+++ core/object/src/main/java/imagej/object/ObjectManager.java
@@ -34,6 +34,7 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package imagej.object;
 
+import imagej.Log;
 import imagej.event.EventSubscriber;
 import imagej.event.Events;
 import imagej.manager.Manager;
@@ -63,16 +64,21 @@ public final class ObjectManager implements ManagerComponent {
 	 */
 	private Map<Class<?>, List<?>> hoard;
 
+	/** Maintain list of subscribers, to avoid garbage collection. */
+	private List<EventSubscriber<?>> subscribers;
+
 	public <T> List<T> getObjects(final Class<T> type) {
 		final List<T> list = getList(type);
 		return Collections.unmodifiableList(list);
 	}
 
 	public void addObject(final Object obj) {
+		Log.debug("=======> ADDING OBJECT: " + obj);//TEMP
 		addObject(obj, obj.getClass());
 	}
 
 	public void removeObject(final Object obj) {
+		Log.debug("=======> REMOVING OBJECT: " + obj);//TEMP
 		removeObject(obj, obj.getClass());
 	}
 
@@ -81,23 +87,7 @@ public final class ObjectManager implements ManagerComponent {
 	@Override
 	public void initialize() {
 		hoard = new ConcurrentHashMap<Class<?>, List<?>>();
-
-		Events.subscribe(ObjectCreatedEvent.class,
-			new EventSubscriber<ObjectCreatedEvent>()
-		{
-			@Override
-			public void onEvent(final ObjectCreatedEvent event) {
-				addObject(event.getObject());
-			}			
-		});
-		Events.subscribe(ObjectDeletedEvent.class,
-			new EventSubscriber<ObjectDeletedEvent>()
-		{
-			@Override
-			public void onEvent(final ObjectDeletedEvent event) {
-				removeObject(event.getObject());
-			}
-		});
+		subscribeToEvents();
 	}
 
 	// -- Helper methods --
@@ -133,6 +123,33 @@ public final class ObjectManager implements ManagerComponent {
 			hoard.put(type, list);
 		}
 		return list;
+	}
+
+
+	private void subscribeToEvents() {
+		subscribers = new ArrayList<EventSubscriber<?>>();
+
+		final EventSubscriber<ObjectCreatedEvent> objectCreatedSubscriber =
+			new EventSubscriber<ObjectCreatedEvent>()
+		{
+			@Override
+			public void onEvent(final ObjectCreatedEvent event) {
+				addObject(event.getObject());
+			}			
+		};
+		subscribers.add(objectCreatedSubscriber);
+		Events.subscribe(ObjectCreatedEvent.class, objectCreatedSubscriber);
+
+		final EventSubscriber<ObjectDeletedEvent> objectDeletedSubscriber =
+			new EventSubscriber<ObjectDeletedEvent>()
+		{
+			@Override
+			public void onEvent(final ObjectDeletedEvent event) {
+				removeObject(event.getObject());
+			}
+		};
+		subscribers.add(objectDeletedSubscriber);
+		Events.subscribe(ObjectDeletedEvent.class, objectDeletedSubscriber);
 	}
 
 }
