bb81f98961003b448d2619b46414e56c358093a5
Barry DeZonia <bdezonia@wisc.edu>
2011-04-01 09:56:56 -0500

    move Imglib oriented plugins to imagej.core.plugins.imglib
    This used to be revision r2579.

--- core/core-plugins/src/main/java/imagej/core/plugins/imglib/SaltAndPepper.java
+++ core/core-plugins/src/main/java/imagej/core/plugins/imglib/SaltAndPepper.java
@@ -32,7 +32,7 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 */
 
-package imagej.core.plugins;
+package imagej.core.plugins.imglib;
 
 import java.util.Random;
 
