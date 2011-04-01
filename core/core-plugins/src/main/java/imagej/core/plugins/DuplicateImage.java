a49e54ec23647b5bb17cb9c4c48e60c0d148479b
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2011-04-01 14:27:03 +0000

    fix this plugin so it works. cleanup UI. simplify it to use Imglib more directly.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2575 be23795f-a60e-40d4-ba19-055798872e57

---
+++ core/core-plugins/src/main/java/imagej/core/plugins/DuplicateImage.java
@@ -0,0 +1,77 @@
+//
+// DuplicateImage.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.core.plugins;
+
+import mpicbg.imglib.image.Image;
+import imagej.data.AxisLabel;
+import imagej.data.Dataset;
+import imagej.data.Metadata;
+import imagej.plugin.ImageJPlugin;
+import imagej.plugin.Menu;
+import imagej.plugin.Parameter;
+import imagej.plugin.Plugin;
+
+/**
+ * Fills an output Dataset with the contents of an input Dataset.
+ * 
+ * @author Barry DeZonia
+ */
+@Plugin(menu = {
+	@Menu(label = "Image", mnemonic = 'i'),
+	@Menu(label = "Duplicate...", accelerator="shift control D")})
+public class DuplicateImage implements ImageJPlugin {
+
+	// -- instance variables that are Parameters --
+
+	@Parameter
+	private Dataset input;
+
+	@Parameter(output = true)
+	private Dataset output;
+
+	@Parameter(label="Output filename")
+	private String outputFilename;
+
+	// -- public interface --
+
+	@Override
+	public void run() {
+		Image<?> image = input.getImage().clone();
+		Metadata metadata = new Metadata();
+		metadata.copyFrom(input.getMetadata());
+		metadata.setName(outputFilename);
+		output = new Dataset(image, metadata);
+	}
+}
