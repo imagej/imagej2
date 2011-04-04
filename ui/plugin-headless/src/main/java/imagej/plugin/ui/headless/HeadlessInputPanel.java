595a38be102c2377dc08807ee2a121671ff354cd
Curtis Rueden <ctrueden@wisc.edu>
2011-04-03 23:46:28 -0500

    Add initial version of headless UI.
    Uses the Java Scripting Framework, but doesn't work too well yet.
    
    
    This used to be revision r2605.

---
+++ ui/plugin-headless/src/main/java/imagej/plugin/ui/headless/HeadlessInputPanel.java
@@ -0,0 +1,97 @@
+//
+// HeadlessInputPanel.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.plugin.ui.headless;
+
+import imagej.plugin.ui.AbstractInputPanel;
+import imagej.plugin.ui.ParamDetails;
+
+/**
+ * TODO
+ * 
+ * @author Curtis Rueden
+ */
+public class HeadlessInputPanel extends AbstractInputPanel {
+
+	public HeadlessInputPanel() {
+		// TODO
+	}
+
+	// -- InputPanel methods --
+
+	@Override
+	public void addMessage(final String text) {
+		// TODO
+	}
+
+	@Override
+	public void addNumber(final ParamDetails details,
+		final Number min, final Number max, final Number stepSize)
+	{
+		// TODO
+	}
+
+	@Override
+	public void addToggle(final ParamDetails details) {
+		// TODO
+	}
+
+	@Override
+	public void addTextField(final ParamDetails details, final int columns) {
+		// TODO
+	}
+
+	@Override
+	public void addChoice(final ParamDetails details, final String[] items) {
+		// TODO
+	}
+
+	@Override
+	public void addFile(final ParamDetails details) {
+		// TODO
+	}
+
+	@Override
+	public void addObject(final ParamDetails details) {
+		// TODO
+	}
+
+	// -- InputWidget methods --
+
+	@Override
+	public void refresh() {
+		// NB: No action needed.
+	}
+
+}
