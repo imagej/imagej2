dda41c867d21d39cbf72047ba547d570668c2576
Barry DeZonia <bdezonia@wisc.edu>
2011-04-05 11:27:58 -0500

    Total rewrite of getImagePlane() to support all RealType pixel types currently in Imglib. This fix points out problems with the ProbeTool making assumptions about image plane data that will need to be addressed. This rewrite treats all data types as gray level measurements against the full range of the type. Future changes should rely on the range of pixel data actually present in a Dataset. Data is represetned by a 16-bit unsigned BufferedImage.
    This used to be revision r2618.

--- ui/awt-common/src/main/java/imagej/awt/AWTDisplayController.java
+++ ui/awt-common/src/main/java/imagej/awt/AWTDisplayController.java
@@ -41,9 +41,12 @@ import imagej.display.ImageDisplayWindow;
 import imagej.util.Index;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
 
-import loci.formats.gui.AWTImageTools;
+//import loci.formats.gui.AWTImageTools;
+import mpicbg.imglib.cursor.LocalizableByDimCursor;
 import mpicbg.imglib.image.Image;
+import mpicbg.imglib.type.numeric.RealType;
 
 /**
  * TODO
@@ -185,30 +188,104 @@ public class AWTDisplayController implements DisplayController {
 		final int no = Index.positionToRaster(planeDims, pos);
 		final Object plane = dataset.getPlane(no);
 		currentPlane = plane;
-		if (plane instanceof byte[]) {
-			return AWTImageTools.makeImage((byte[]) plane, dims[xIndex],
-				dims[yIndex], dataset.isSigned());
-		}
-		else if (plane instanceof short[]) {
-			return AWTImageTools.makeImage((short[]) plane, dims[xIndex],
-				dims[yIndex], dataset.isSigned());
-		}
-		else if (plane instanceof int[]) {
-			return AWTImageTools.makeImage((int[]) plane, dims[xIndex],
-				dims[yIndex], dataset.isSigned());
-		}
-		else if (plane instanceof float[]) {
-			return AWTImageTools.makeImage((float[]) plane, dims[xIndex],
-				dims[yIndex]);
-		}
-		else if (plane instanceof double[]) {
-			return AWTImageTools.makeImage((double[]) plane, dims[xIndex],
-				dims[yIndex]);
-		}
-		else {
-			throw new IllegalStateException("Unknown data type: " +
-				plane.getClass().getName());
-		}
+		return makeImage();
 	}
+	
+	/*
+	// TODO - make type info always available in Imglib and not repeatedly
+	// hatch cursor. good workaround would be to set for Dataset once.
+	// TODO - make numBits (which represents the number of bits of data
+	// this type is represented as in memory. For example, 12-bit data is
+	// stored in a big int[] packed 12 bits at a time. So need to know I have
+	// 12-bits of info storedin an int[]. But if we chose to put 12-bit ints
+	// into 16-bit shorts with no packing we'd want to be able to represent
+	// that. Notice that its still not genral enough. Image 3 bits per pixel
+	// stored in bytes. Either the 3rd pixel will straddle two bytes or they
+	// could only be encoded to use the first six bits of each byte.
+	private int getNumBits(Dataset ds) {
+		Cursor<? extends RealType<?>> cursor =
+			(Cursor<? extends RealType<?>>) ds.getImage().createCursor();
+		RealType<?> type = cursor.getType();
+		cursor.close();
+		// FIXME - temp hacky way to get this working for now
+		if (type instanceof BitType)
+			return 1;
+		else if (type instanceof ByteType)
+			return 8;
+		else if (type instanceof UnsignedByteType)
+			return 8;
+		else if (type instanceof Unsigned12BitType)
+			return 12;
+		else if (type instanceof ShortType)
+			return 16;
+		else if (type instanceof UnsignedShortType)
+			return 16;
+		else if (type instanceof IntType)
+			return 32;
+		else if (type instanceof UnsignedIntType)
+			return 32;
+		else if (type instanceof FloatType)
+			return 32;
+		else if (type instanceof LongType)
+			return 64;
+		else if (type instanceof DoubleType)
+			return 64;
+		
+		return 0;
+	}
+	*/
 
+	/** 
+	 * <p>
+	 * creates a BufferedImage of the plane of data referenced by the current
+	 * position index. For now it only creates BufferedImages of 16-bit gray
+	 * type. Represents data values graphically by scaling data to 16-bit
+	 * unsigned range.
+	 * </p>
+	 * 
+	 * <p>
+	 * rewritten to not use AWTTools::makeImage(). That code could not support
+	 * 1-bit, 12-bit, and 64-bit data. For 64-bit data AWT does not support it
+	 * internally as a type. For the other two AWTTools assume the input plane
+	 * is a primitive type and not an encoded type (i.e. 12-bit is represented
+	 * as 12-bit packings within an int[]). This assumption is bad.
+	 * </p>
+	 */
+	// TODO - notice that ProbeTool also makes assumption that plane of data does
+	// not have any encoded types like 1-bit or 12-bit etc. The whole concept of
+	// getting the current plane is broken.
+	private BufferedImage makeImage() {
+		int w = dataset.getImage().getDimension(xIndex);
+		int h = dataset.getImage().getDimension(yIndex);
+		BufferedImage bImage = new BufferedImage(w, h, BufferedImage.TYPE_USHORT_GRAY);
+		int[] dataInShortRangeEncodedAsInts = new int[w*h];
+		int[] position = dims.clone();
+		int p = 0;
+		for (int i = 0; i < position.length; i++) {
+			if ((i == xIndex) || (i == yIndex))
+				continue;
+			position[i] = pos[p++];
+		}
+		LocalizableByDimCursor<? extends RealType<?>> cursor =
+			(LocalizableByDimCursor<? extends RealType<?>>)
+			dataset.getImage().createLocalizableByDimCursor();
+		// create a grayscale image representation of data
+		// TODO - broken for float types - may need to range of actual pixel values
+		double rangeMin = cursor.getType().getMinValue();
+		double rangeMax = cursor.getType().getMaxValue();
+		for (int y = 0; y < h; y++) {
+			position[yIndex] = y;
+			for (int x = 0; x < w; x++) {
+				position[xIndex] = x;
+				cursor.setPosition(position);
+				double value = cursor.getType().getRealDouble();
+				int shortVal = (int) (0xffff * ((value - rangeMin) / (rangeMax - rangeMin)));
+				dataInShortRangeEncodedAsInts[y*w + x] = shortVal;
+			}
+		}
+		cursor.close();
+		WritableRaster raster = bImage.getRaster();
+		raster.setPixels(0, 0, w, h, dataInShortRangeEncodedAsInts);
+		return bImage;
+	}
 }
