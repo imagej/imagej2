baf006eda7c93a0db340d6a8f2fe5e182fcbd69a
gharris <gharris@be23795f-a60e-40d4-ba19-055798872e57>
2011-04-08 16:04:58 +0000

    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2640 be23795f-a60e-40d4-ba19-055798872e57

--- envisaje/PaintAPI/src/main/java/imagej/envisaje/spi/SelectionContextContributor.java
+++
@@ -1,9 +0,0 @@
-package imagej.envisaje.spi;
-
-import org.openide.util.Lookup;
-
-
-public interface SelectionContextContributor {
-    //XXX fixme - needs working out what we really need for support here
-    Lookup getLookup();
-}
