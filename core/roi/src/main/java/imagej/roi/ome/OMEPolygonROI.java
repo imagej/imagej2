3f2cc635dd5bee5d65bc6208fc102ce20bae7a13
afraser <afraser@be23795f-a60e-40d4-ba19-055798872e57>
2011-04-12 19:00:52 +0000

    Added OMEEllipseROI and OMEPolygonROI
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2662 be23795f-a60e-40d4-ba19-055798872e57

---
+++ core/roi/src/main/java/imagej/roi/ome/OMEPolygonROI.java
@@ -0,0 +1,39 @@
+package imagej.roi.ome;
+
+import imagej.roi.ImageJROI;
+import mpicbg.imglib.roi.AbstractIterableRegionOfInterest;
+import mpicbg.imglib.roi.RegionOfInterest;
+import ome.xml.model.Polyline;
+
+public class OMEPolygonROI extends OMEShapeROI<Polyline> implements ImageJROI {
+	
+	public OMEPolygonROI() {
+		omeShape = new Polyline();
+		omeShape.setClosed(true);
+	}
+	
+	class OPRegionOfInterest extends AbstractIterableRegionOfInterest {
+		protected OPRegionOfInterest() {
+			super(2);
+		}
+		
+		@Override
+		protected boolean isMember(double[] position) {
+			// TODO
+			return false;
+		}
+		
+		@Override
+		protected boolean nextRaster(long[] position, long[] end) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+	}
+	
+	@Override
+	public RegionOfInterest getRegionOfInterest() {
+		return new OPRegionOfInterest();
+	}
+	
+	
+}
