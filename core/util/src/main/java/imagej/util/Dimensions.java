394e649ddd814f2c759260c2b4992d6b05669660
Curtis Rueden <ctrueden@wisc.edu>
2011-04-21 13:01:08 -0500

    Fix ImgLib2-related compile errors in ij-util.
    This necessitated a change from int[] to long[] for all dimensional
    position arrays. Tests were also cleaned up and consolidated.
    
    
    git-svn-id: http://code.imagej.net/svn/imagej/branches/imglib2@2691 be23795f-a60e-40d4-ba19-055798872e57

--- core/util/src/main/java/imagej/util/Dimensions.java
+++ core/util/src/main/java/imagej/util/Dimensions.java
@@ -36,8 +36,9 @@ package imagej.util;
 
 /**
  * General purpose methods.
- *
+ * 
  * @author Barry DeZonia
+ * @author Curtis Rueden
  */
 public final class Dimensions {
 
@@ -45,93 +46,87 @@ public final class Dimensions {
 		// prevent instantiation of utility class
 	}
 
-	/** gets the last n-2 dimensions of a n-dimensional int array. */
-	public static int[] getDims3AndGreater(int[] fullDims)
-	{
-		if (fullDims.length < 2)
-			throw new IllegalArgumentException("Image must be at least 2-D");
+	/** Gets the last n-2 dimensions of a n-dimensional long array. */
+	public static long[] getDims3AndGreater(final long[] lengths) {
+		if (lengths.length < 2) throw new IllegalArgumentException(
+			"Image must be at least 2-D");
 
-		int[] extraDims = new int[fullDims.length-2];
+		final long[] extraDims = new long[lengths.length - 2];
 
 		for (int i = 0; i < extraDims.length; i++)
-			extraDims[i] = fullDims[i+2];
+			extraDims[i] = lengths[i + 2];
 
 		return extraDims;
 	}
 
-	/** returns, as a long, the total number of samples present in an image of given dimensions */
-	public static long getTotalSamples(int[] dimensions)
-	{
-		int numDims = dimensions.length;
-
-		if (numDims == 0)
-			return 0;
-
-		long totalSamples = 1;
-
-		for (int i = 0; i < numDims; i++)
-			totalSamples *= dimensions[i];
-
-		return totalSamples;
-	}
-
-	/** returns the number of planes present in an image of given dimensions. assumes the planes lie
-	 *  in the 1st two dimensions
+	/**
+	 * Gets the number of planes present in an image of given dimensions. Assumes
+	 * the planes lie in the 1st two dimensions.
 	 */
-	public static long getTotalPlanes(int[] dimensions)
-	{
-		int numDims = dimensions.length;
-
-		if (numDims < 2)
-			return 0;
-
-		if (numDims == 2)
-			return 1;
-
+	public static long getTotalPlanes(final long[] lengths) {
+		final int numDims = lengths.length;
+		if (numDims < 2) return 0;
+		if (numDims == 2) return 1;
 		// else numDims > 2
 
-		int[] sampleSpace = getDims3AndGreater(dimensions);
+		final long[] sampleSpace = getDims3AndGreater(lengths);
 
-		return getTotalSamples(sampleSpace);
+		return Index.getTotalLength(sampleSpace);
 	}
 
-	/** throws an exception if the combination of origins and spans is outside an image's dimensions */
-	public static void verifyDimensions(int[] imageDimensions, int[] origin, int[] span)
+	/**
+	 * Throws an exception if the combination of origins and spans is outside an
+	 * image's dimensions.
+	 */
+	public static void verifyDimensions(final long[] lengths,
+		final long[] origin, final long[] span)
 	{
 		// span dims should match origin dims
-		if (origin.length != span.length)
-			throw new IllegalArgumentException("verifyDimensions() : origin (dim="+origin.length+") and span (dim="+span.length+") arrays are of differing sizes");
+		if (origin.length != span.length) {
+			throw new IllegalArgumentException("origin (dim=" + origin.length +
+				") and span (dim=" + span.length + ") arrays are of differing sizes");
+		}
 
 		// origin/span dimensions should match image dimensions
-		if (origin.length != imageDimensions.length)
-			throw new IllegalArgumentException("verifyDimensions() : origin/span (dim="+origin.length+") different size than input image (dim="+imageDimensions.length+")");
+		if (origin.length != lengths.length) {
+			throw new IllegalArgumentException("origin/span (dim=" + origin.length +
+				") different size than input image (dim=" + lengths.length + ")");
+		}
 
-		// make sure origin in a valid range : within bounds of source image
-		for (int i = 0; i < origin.length; i++)
-			if ((origin[i] < 0) || (origin[i] >= imageDimensions[i]))
-				throw new IllegalArgumentException("verifyDimensions() : origin outside bounds of input image at index " + i);
+		// make sure origin in a valid range, within bounds of source image
+		for (int i = 0; i < origin.length; i++) {
+			if ((origin[i] < 0) || (origin[i] >= lengths[i])) {
+				throw new IllegalArgumentException(
+					"origin outside bounds of input image at index " + i);
+			}
+		}
 
-		// make sure span in a valid range : >= 1
-		for (int i = 0; i < span.length; i++)
-			if (span[i] < 1)
-				throw new IllegalArgumentException("verifyDimensions() : span size < 1 at index " + i);
+		// make sure span in a valid range, >= 1
+		for (int i = 0; i < span.length; i++) {
+			if (span[i] < 1) {
+				throw new IllegalArgumentException("span size < 1 at index " + i);
+			}
+		}
 
 		// make sure origin + span within the bounds of the input image
-		for (int i = 0; i < span.length; i++)
-			if ( (origin[i] + span[i]) > imageDimensions[i] )
-				throw new IllegalArgumentException("verifyDimensions() : span range (origin+span) beyond input image boundaries at index " + i);
+		for (int i = 0; i < span.length; i++) {
+			if ((origin[i] + span[i]) > lengths[i]) {
+				throw new IllegalArgumentException("span range " +
+					"(origin+span) beyond input image boundaries at index " + i);
+			}
+		}
 	}
 
-
-	/** given an array of dimensions this returns the number of axes whose dimension is > 1 */
-	public static int countNontrivialDimensions(int[] dimensions)
-	{
+	/**
+	 * Given an array of dimensions, this gets the number of axes whose dimension
+	 * is > 1
+	 */
+	public static long countNontrivialDimensions(final long[] dimensions) {
 		int numNonTrivial = 0;
-		
-		for (int dim : dimensions)
-			if (dim > 1)
-				numNonTrivial++;
-		
+
+		for (final long dim : dimensions)
+			if (dim > 1) numNonTrivial++;
+
 		return numNonTrivial;
 	}
 
