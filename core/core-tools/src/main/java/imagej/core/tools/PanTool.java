4db5647609829d3e9bad8bee379a36ce582e1a00
Curtis Rueden <ctrueden@wisc.edu>
2011-05-19 13:29:36 -0500

    Minor style tweak in pan tool.
    This used to be revision r2879.

--- core/core-tools/src/main/java/imagej/core/tools/PanTool.java
+++ core/core-tools/src/main/java/imagej/core/tools/PanTool.java
@@ -94,8 +94,9 @@ public class PanTool extends BaseTool {
 	@Override
 	public void onMouseDrag(final MsDraggedEvent evt) {
 		final Display display = evt.getDisplay();
-		display.getImageCanvas().pan(
-			new IntCoords(evt.getX() - lastX, evt.getY() - lastY));
+		final int xDelta = evt.getX() - lastX;
+		final int yDelta = evt.getY() - lastY;
+		display.getImageCanvas().pan(new IntCoords(xDelta, yDelta));
 		lastX = evt.getX();
 		lastY = evt.getY();
 	}
