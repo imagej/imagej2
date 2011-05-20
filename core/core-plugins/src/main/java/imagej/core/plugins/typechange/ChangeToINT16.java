9e737510f71801f1d9b54c1f63333652a9ba87f1
Curtis Rueden <ctrueden@wisc.edu>
2011-05-20 11:40:38 -0500

    Clean up headers.
    This used to be revision r2904.

--- core/core-plugins/src/main/java/imagej/core/plugins/typechange/ChangeToINT16.java
+++ core/core-plugins/src/main/java/imagej/core/plugins/typechange/ChangeToINT16.java
@@ -10,14 +10,14 @@ All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
- * Neither the names of the ImageJDev.org developers nor the
-names of its contributors may be used to endorse or promote products
-derived from this software without specific prior written permission.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -30,7 +30,7 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
- */
+*/
 
 package imagej.core.plugins.typechange;
 
@@ -41,26 +41,23 @@ import imagej.plugin.Parameter;
 import imagej.plugin.Plugin;
 import net.imglib2.type.numeric.integer.ShortType;
 
-
-/** changes an input Dataset's underlying imglib data to be of
- * 16-bit signed type. copies the original data as best it can.
- * it does not do any range clamping though this could be easily
- * added if desired. if no then narrowing conversions can have
- * unexpected values since imglib does the simplest translation
- * it can for efficiency reasons.
- *  
+/**
+ * Changes an input Dataset's underlying imglib data to be of 16-bit signed
+ * type. copies the original data as best it can. it does not do any range
+ * clamping though this could be easily added if desired. if no then narrowing
+ * conversions can have unexpected values since imglib does the simplest
+ * translation it can for efficiency reasons.
+ * 
  * @author Barry DeZonia
- *
  */
-@Plugin(menu = {
-	@Menu(label = "Image", mnemonic = 'i'),
+@Plugin(menu = { @Menu(label = "Image", mnemonic = 'i'),
 	@Menu(label = "Type", mnemonic = 't'),
 	@Menu(label = "16-bit signed", weight = 9) })
 public class ChangeToINT16 implements ImageJPlugin {
 
 	@Parameter
 	Dataset input;
-	
+
 	@Override
 	public void run() {
 		TypeChanger.changeType(input, new ShortType());
