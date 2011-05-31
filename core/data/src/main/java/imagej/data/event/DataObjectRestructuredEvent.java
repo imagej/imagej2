0a4642e86d6a7678ed9bf3f248a22dce46286f85
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2011-05-31 17:40:53 +0000

    make DataObjectRestructuredEvent and DataObjectUpdatedEvent abstract
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@2999 be23795f-a60e-40d4-ba19-055798872e57

--- core/data/src/main/java/imagej/data/event/DataObjectRestructuredEvent.java
+++ core/data/src/main/java/imagej/data/event/DataObjectRestructuredEvent.java
@@ -42,7 +42,7 @@ import imagej.data.DataObject;
  * 
  * @author Curtis Rueden
  */
-public class DataObjectRestructuredEvent extends DataObjectChangedEvent {
+public abstract class DataObjectRestructuredEvent extends DataObjectChangedEvent {
 
 	public DataObjectRestructuredEvent(final DataObject dataObject) {
 		super(dataObject);
