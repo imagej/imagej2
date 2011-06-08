0f92bc81e4a0a73861339b0f1b57cc068e8b164e
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-06-08 17:52:50 +0000

    Simply Swing input harvester to use new utility methods.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3103 be23795f-a60e-40d4-ba19-055798872e57

--- ui/plugin-swing/src/main/java/imagej/plugin/ui/swing/SwingInputHarvester.java
+++ ui/plugin-swing/src/main/java/imagej/plugin/ui/swing/SwingInputHarvester.java
@@ -34,20 +34,12 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package imagej.plugin.ui.swing;
 
-import imagej.plugin.MenuEntry;
 import imagej.plugin.Plugin;
 import imagej.plugin.PluginModule;
-import imagej.plugin.PluginModuleInfo;
 import imagej.plugin.process.PluginPreprocessor;
 import imagej.plugin.ui.AbstractInputHarvester;
 import imagej.plugin.ui.InputPanel;
-import imagej.util.awt.AWTWindows;
 
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.util.List;
-
-import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -72,65 +64,20 @@ public class SwingInputHarvester extends AbstractInputHarvester {
 	public boolean harvestInputs(final InputPanel inputPanel,
 		final PluginModule<?> module)
 	{
-		final JOptionPane optionPane = new JOptionPane(null);
-		optionPane.setOptionType(JOptionPane.OK_CANCEL_OPTION);
-		final JDialog dialog =
-			optionPane.createDialog(module.getInfo().getLabel());
-		final String title = getTitle(module);
-		dialog.setTitle(title);
-		final JPanel mainPane = (JPanel) optionPane.getComponent(0);
-		final JPanel widgetPane = (JPanel) mainPane.getComponent(0);
-		// TODO - use JScrollPane in case there are many widgets
-		widgetPane.add(((SwingInputPanel) inputPanel).getPanel());
-		dialog.setModal(true);
-		dialog.pack();
-		AWTWindows.centerWindow(dialog);
-		ensureDialogSizeReasonable(dialog);
-		// TODO - open IJ2 from within Eclipse. Give any app focus. Then close
-		// IJ2 app via red close box in its left corner. Quit dialog comes
-		// up behind the other app. Is there a window event we should track
-		// in IJ2 that could pull dialog in front of the other app?
-		// #1 attempted fix of visibility/focus issue
-		// dialog.setAlwaysOnTop(true);
-		dialog.setVisible(true);
-		// #2 attempted fix of visibility/focus issue
-		// if (!dialog.hasFocus())
-		// dialog.requestFocus();
-		final Integer rval = (Integer) optionPane.getValue();
-		return rval != null && rval == JOptionPane.OK_OPTION;
-	}
+		// convert input panel to Swing component with scroll bars
+		final JPanel pane = ((SwingInputPanel) inputPanel).getPanel();
 
-	// -- Helper methods --
+		// display input panel in a dialog
+		final String title = module.toString();
+		// TODO: check module for optionType value
+		final int optionType = JOptionPane.OK_CANCEL_OPTION;
+		// TODO: set messageType based on inputPanel.isMessageOnly()
+		final int messageType = JOptionPane.PLAIN_MESSAGE;
+		final int rval =
+			SwingUtils.showDialog(null, pane, title, optionType, messageType);
 
-	private String getTitle(final PluginModule<?> module) {
-		final PluginModuleInfo<?> moduleInfo = module.getInfo();
-		final String label = moduleInfo.getLabel();
-		if (label != null && !label.isEmpty()) return label;
-		final List<MenuEntry> menuPath = moduleInfo.getPluginEntry().getMenuPath();
-		if (menuPath != null && menuPath.size() > 0) {
-			final MenuEntry menuEntry = menuPath.get(menuPath.size() - 1);
-			final String menuName = menuEntry.getName();
-			if (menuName != null && !menuName.isEmpty()) return menuName;
-		}
-		final String name = moduleInfo.getName();
-		if (name != null && !name.isEmpty()) return name;
-		return "Choose Parameters";
-	}
-
-	private void ensureDialogSizeReasonable(final JDialog dialog) {
-		final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		final Dimension dialogSize = dialog.getSize();
-
-		int newWidth = dialogSize.width;
-		int newHeight = dialogSize.height;
-
-		final int maxWidth = 3 * screenSize.width / 4;
-		final int maxHeight = 3 * screenSize.height / 4;
-
-		if (newWidth > maxWidth) newWidth = maxWidth;
-		if (newHeight > maxHeight) newHeight = maxHeight;
-
-		dialog.setSize(newWidth, newHeight);
+		// verify return value of dialog
+		return rval == JOptionPane.OK_OPTION;
 	}
 
 }
