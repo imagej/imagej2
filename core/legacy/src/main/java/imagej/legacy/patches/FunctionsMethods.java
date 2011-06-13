907c0d6e7bd76e08b77aed317f53b15142aa1b62
Barry DeZonia <bdezonia@wisc.edu>
2011-06-13 16:09:46 -0500

    put hooks in legacy manager support so that during the run of a batch process drawMethods are ignored. Fixes bug #554.
    This used to be revision r3156.

---
+++ core/legacy/src/main/java/imagej/legacy/patches/FunctionsMethods.java
@@ -0,0 +1,14 @@
+package imagej.legacy.patches;
+
+public class FunctionsMethods {
+	
+	public static int InsideBatchDrawing = 0;
+	
+	public static void beforeBatchDraw() {
+		InsideBatchDrawing++;
+	}
+
+	public static void afterBatchDraw() {
+		InsideBatchDrawing--;
+	}
+}
