a52bcc76ab5991fe02593a2e3cd86001ec39237b
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-06-23 17:20:57 +0000

    Add priority mechanism to control notification order.
    Uses EventBus's Prioritized interface.
    
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3227 be23795f-a60e-40d4-ba19-055798872e57

--- core/event/src/main/java/imagej/event/EventSubscriber.java
+++ core/event/src/main/java/imagej/event/EventSubscriber.java
@@ -35,14 +35,14 @@ POSSIBILITY OF SUCH DAMAGE.
 package imagej.event;
 
 /**
- * Interface for subscribers that wish to be notified of ImageJ events.
- *
+ * Interface for subscribers that wish to be notified of ImageJ events (in no
+ * particular order).
+ * 
  * @author Curtis Rueden
- *
  * @param <E> Type of event for which to listen
  */
-public interface EventSubscriber<E extends ImageJEvent>
-	extends org.bushe.swing.event.EventSubscriber<E>
+public interface EventSubscriber<E extends ImageJEvent> extends
+	org.bushe.swing.event.EventSubscriber<E>
 {
 
 	@Override
