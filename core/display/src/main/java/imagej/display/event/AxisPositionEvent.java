9793a7c37c31c371033833d7958c722cf808f65f
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-07-05 21:20:49 +0000

    Fix style and add javadoc for AxisPositionEvent.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3262 be23795f-a60e-40d4-ba19-055798872e57

--- core/display/src/main/java/imagej/display/event/AxisPositionEvent.java
+++ core/display/src/main/java/imagej/display/event/AxisPositionEvent.java
@@ -1,16 +1,56 @@
+//
+// AxisPositionEvent.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package imagej.display.event;
 
-import net.imglib2.img.Axis;
 import imagej.display.Display;
+import net.imglib2.img.Axis;
 
+/**
+ * An event indicating that a display's dimensional position has changed.
+ * 
+ * @author Barry DeZonia
+ */
 public class AxisPositionEvent extends DisplayEvent {
 
-	private Axis axis;
-	private long value;
-	private long max;
-	private boolean relative;
-	
-	public AxisPositionEvent(Display display, Axis axis, long value, long max, boolean relative)
+	private final Axis axis;
+	private final long value;
+	private final long max;
+	private final boolean relative;
+
+	public AxisPositionEvent(final Display display, final Axis axis,
+		final long value, final long max, final boolean relative)
 	{
 		super(display);
 		this.axis = axis;
@@ -19,11 +59,20 @@ public class AxisPositionEvent extends DisplayEvent {
 		this.relative = relative;
 	}
 
-	public Axis getAxis() { return axis; }
-	
-	public long getValue() { return value; }
+	public Axis getAxis() {
+		return axis;
+	}
 
-	public long getMax() { return max; }
-	
-	public boolean isRelative() { return relative; }
+	public long getValue() {
+		return value;
+	}
+
+	public long getMax() {
+		return max;
+	}
+
+	public boolean isRelative() {
+		return relative;
+	}
+
 }
