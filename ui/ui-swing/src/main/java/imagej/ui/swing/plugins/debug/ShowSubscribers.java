50be6fa21fc23fadeb555128a8707a008967e70c
Grant Harris <gharris@mbl.edu>
2011-07-06 15:41:21 -0400

    Added ShowSubscribers diagnostic - lists subscribers to certain event types.
    This used to be revision r3275.

---
+++ ui/ui-swing/src/main/java/imagej/ui/swing/plugins/debug/ShowSubscribers.java
@@ -0,0 +1,47 @@
+package imagej.ui.swing.plugins.debug;
+
+import imagej.display.event.DisplayActivatedEvent;
+import imagej.event.EventSubscriber;
+import imagej.event.Events;
+import imagej.object.event.ObjectCreatedEvent;
+import imagej.object.event.ObjectDeletedEvent;
+import imagej.object.event.ObjectsUpdatedEvent;
+import imagej.plugin.ImageJPlugin;
+import imagej.plugin.Plugin;
+import imagej.ui.swing.StaticSwingUtils;
+import imagej.ui.swing.SwingOutputWindow;
+import java.util.List;
+
+/**
+ * For EventBus diagnostics...
+ * Shows what is subscribed to a event types...  
+ * 
+ * Change/Add event types as necessary...
+ * 
+ * @author GBH
+ */
+@Plugin(menuPath = "Plugins>Subscribers")
+public class ShowSubscribers implements ImageJPlugin {
+
+	private static SwingOutputWindow window;
+	private List<EventSubscriber<?>> subscribers;
+
+	@Override
+	public void run() {
+		window = new SwingOutputWindow("Subscribers");
+		StaticSwingUtils.locateLowerRight(window);
+		listSubs(ObjectsUpdatedEvent.class);
+		listSubs(ObjectCreatedEvent.class);
+		listSubs(ObjectDeletedEvent.class);
+		listSubs(DisplayActivatedEvent.class);
+		
+	}
+
+	private void listSubs(Class clazz) {
+		subscribers = Events.getSubscribers(clazz);
+		window.append(clazz.getSimpleName() + "...\n");
+		for (EventSubscriber<?> subscriber : subscribers) {
+			window.append(subscriber.toString() + "\n");
+		}
+	}
+}
