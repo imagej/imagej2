90caa5356bc215f4ab8b717b314f3c7b7fb7a846
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-07-15 21:53:13 +0000

    Add more default methods to AbstractModuleInfo.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3350 be23795f-a60e-40d4-ba19-055798872e57

--- core/module/src/main/java/imagej/module/AbstractModuleInfo.java
+++ core/module/src/main/java/imagej/module/AbstractModuleInfo.java
@@ -37,6 +37,7 @@ package imagej.module;
 import imagej.event.Events;
 import imagej.module.event.ModuleInfoUpdatedEvent;
 
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -45,6 +46,13 @@ import java.util.Map;
 
 /**
  * Abstract superclass of {@link ModuleInfo} implementations.
+ * <p>
+ * By default, {@link ModuleItem}s are stored in {@link HashMap}s and
+ * {@link List}s, internally. The {@link Module} {@link Class} given in the
+ * constructor is given as the delegate class name for
+ * {@link #getDelegateClassName()}, and instantiated using a constructor that
+ * takes a single {@link ModuleInfo} parameter.
+ * </p>
  * 
  * @author Curtis Rueden
  */
@@ -66,6 +74,38 @@ public abstract class AbstractModuleInfo extends AbstractUIDetails implements
 	/** Ordered list of output items. */
 	private final List<ModuleItem<?>> outputList =
 		new ArrayList<ModuleItem<?>>();
+
+	private final Class<? extends Module> moduleClass;
+
+	public AbstractModuleInfo(final Class<? extends Module> moduleClass) {
+		this.moduleClass = moduleClass;
+	}
+
+	// -- AbstractModuleInfo methods --
+
+	/** Adds an input to the list. */
+	public void addInput(final ModuleItem<?> input) {
+		inputMap.put(input.getName(), input);
+		inputList.add(input);
+	}
+
+	/** Adds an output to the list. */
+	public void addOutput(final ModuleItem<?> output) {
+		outputMap.put(output.getName(), output);
+		outputList.add(output);
+	}
+
+	/** Removes an input from the list. */
+	public void removeInput(final ModuleItem<?> input) {
+		inputMap.remove(input.getName());
+		inputList.remove(input);
+	}
+
+	/** Removes an output from the list. */
+	public void removeOutput(final ModuleItem<?> output) {
+		outputMap.remove(output.getName());
+		outputList.remove(output);
+	}
 
 	// -- ModuleInfo methods --
 
@@ -90,6 +130,24 @@ public abstract class AbstractModuleInfo extends AbstractUIDetails implements
 	}
 
 	@Override
+	public String getDelegateClassName() {
+		return moduleClass.getName();
+	}
+
+	@Override
+	public Module createModule() throws ModuleException {
+		try {
+			final Constructor<? extends Module> ctor =
+				moduleClass.getConstructor(ModuleInfo.class);
+			return ctor.newInstance(this);
+		}
+		catch (final Exception e) {
+			// NB: Several types of exceptions; simpler to handle them all the same.
+			throw new ModuleException(e);
+		}
+	}
+
+	@Override
 	public boolean canPreview() {
 		return false;
 	}
@@ -97,18 +155,6 @@ public abstract class AbstractModuleInfo extends AbstractUIDetails implements
 	@Override
 	public void update() {
 		Events.publish(new ModuleInfoUpdatedEvent(this));
-	}
-
-	// -- Internal AbstractModuleInfo methods --
-
-	protected void addInput(final ModuleItem<?> input) {
-		inputMap.put(input.getName(), input);
-		inputList.add(input);
-	}
-
-	protected void addOutput(final ModuleItem<?> output) {
-		outputMap.put(output.getName(), output);
-		outputList.add(output);
 	}
 
 }
