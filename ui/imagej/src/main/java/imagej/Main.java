3e8b4d49843610255cc88804f74e714f9c9b5e6e
Barry DeZonia <bdezonia@wisc.edu>
2011-07-29 11:26:51 -0500

    remove manageSettings() stub code. This was in place to set IJ1 options values from IJ2 after everything was loaded. Now the LegacyService sets IJ1 settings after ij.ImageJ is initialized
    This used to be revision r3539.

--- ui/imagej/src/main/java/imagej/Main.java
+++ ui/imagej/src/main/java/imagej/Main.java
@@ -48,18 +48,8 @@ public final class Main {
 	}
 
 	public static void main(String[] args) {
-		manageSettings();
 		final ImageJ context = ImageJ.createContext();
 		context.getService(UIService.class).processArgs(args);
-	}
-
-	private static void manageSettings() {
-		// TODO
-		// if (settings don't exist on disk) {
-		//   someone.initSettingsToDefault();  // who should own this code?
-		//   maybe write to disk or let persistence mechanism auto save on exit
-		//   also optionsSynchronizer.update() after IJ1 initialized
-		// }
 	}
 
 }
