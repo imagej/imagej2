8df54cccd4ae5702797c1e55ee45dbf7950e6a6b
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-07-29 19:30:24 +0000

    Add script for checking source code headers.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3550 be23795f-a60e-40d4-ba19-055798872e57

---
+++ scripts/check-headers.pl
@@ -0,0 +1,153 @@
+#!/usr/bin/perl
+use strict;
+
+# check-headers.pl - Verifies existence and correctness of source code headers.
+
+# parse command line arguments
+my @args;
+if (scalar @ARGV == 0) {
+  @args = ('.');
+}
+else {
+  @args = @ARGV;
+}
+
+# read copyright file
+my @copyright = readFile("scripts/copyright.txt");
+
+# find source files
+my $cmd = "find @args -name '*.java'";
+my @src = `$cmd`;
+
+# process files
+for my $file (@src) {
+  chop $file;
+  process($file);
+}
+
+sub readFile {
+  my ($file) = @_;
+  open FILE, "$file" or die "$file: $!";
+  my @data = <FILE>;
+  close(FILE);
+  for my $line (@data) {
+    chop $line;
+  }
+  return @data;
+}
+
+sub process {
+  my ($file) = @_;
+
+  my $dir = `dirname "$file"`;
+  chop $dir;
+  my $base = `basename "$file"`;
+  chop $base;
+  my $class = substr($file, 0, length($file) - 5);
+
+  # read in source file
+  my @data = readFile($file);
+
+  # check header comment
+  my $i = 0;
+  my @header = ('//', "// $base", '//', '');
+  if (!match(\@header, \@data, $i)) {
+    print "$file: invalid header\n";
+    return;
+  }
+  $i += @header;
+
+  # check copyright statement
+  if (!match(\@copyright, \@data, $i)) {
+    print "$file: invalid copyright\n";
+    return;
+  }
+  $i += @copyright;
+
+  # check package statement
+  if ($data[$i++] !~ /^package .*;$/) {
+    print "$file: invalid package\n";
+    return;
+  }
+
+  # check import statements
+  my $blank = 0;
+  while (1) {
+    my $line = trim($data[$i++]);
+    if ($line =~ /^$/) {
+      if ($blank) {
+        print "$file: duplicate blank lines at line #$i\n";
+        return;
+      }
+      $blank = 1;
+    }
+    else {
+      $blank = 0;
+      if ($line eq '/**') {
+        last;
+      }
+      elsif ($line !~ /^import /) {
+        print "$file: unexpected text at line #$i\n";
+        return;
+      }
+    }
+  }
+
+  if ($data[$i] !~ /^ \* [^\s]/) {
+    print "$file: malformed class comment at line #$i\n";
+    return;
+  }
+
+  # check class comment
+  my $author = 0;
+  while (1) {
+    my $line = $data[$i++];
+    if ($line eq ' */') {
+      last;
+    }
+    if ($line !~ /^ \* ?/) {
+      print "$file: malformed class comment at line #$i\n";
+      return;
+    }
+    if ($line =~ /^ \* \@author /) {
+      $author = 1;
+    }
+  }
+
+  if (!$author) {
+    print "$file: missing author tag\n";
+    return;
+  }
+
+  # check type declaration
+  if ($data[$i++] !~
+    /^public ((abstract)|(final) )*(class)|(interface) $class .* {$/)
+  {
+    print "$file: invalid type declaration at line #$i\n";
+    return;
+  }
+}
+
+sub match {
+  my ($tRef, $dRef, $index) = @_;
+  my @template = @$tRef;
+  my @data = @$dRef;
+
+  my $i = $index;
+  my $result = 1;
+  for my $expected (@template) {
+    my $actual = $data[$i++];
+    if ($actual ne $expected) {
+      $result = 0;
+      last;
+    }
+  }
+  return $result;
+}
+
+sub trim {
+  my ($line) = @_;
+  $line =~ s/^\s*//;
+  $line =~ s/\s*$//;
+  return $line;
+}
