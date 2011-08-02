644d4bdedf7bf9113cf12e2be84ec348bfda24d1
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-08-02 19:56:14 +0000

    Make the pixel probe a global tool.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3603 be23795f-a60e-40d4-ba19-055798872e57

--- core/core-tools/src/main/java/imagej/core/tools/ZoomTool.java
+++ core/core-tools/src/main/java/imagej/core/tools/ZoomTool.java
@@ -56,7 +56,7 @@ import imagej.util.IntCoords;
 	iconPath = "/icons/tools/zoom.png", priority = ZoomTool.PRIORITY)
 public class ZoomTool extends AbstractTool {
 
-	public static final int PRIORITY = ProbeTool.PRIORITY + 1;
+	public static final int PRIORITY = 200;
 
 	private static final int DRAG_THRESHOLD = 8;
 
