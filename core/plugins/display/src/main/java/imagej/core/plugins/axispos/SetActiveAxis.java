6ff144c2f5d2044066a269d8abdd4ed2013c13a3
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-08-02 20:45:09 +0000

    Fix style & headers for ij-plugins-display.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3611 be23795f-a60e-40d4-ba19-055798872e57

--- core/plugins/display/src/main/java/imagej/core/plugins/axispos/SetActiveAxis.java
+++ core/plugins/display/src/main/java/imagej/core/plugins/axispos/SetActiveAxis.java
@@ -1,7 +1,5 @@
-package imagej.core.plugins.axispos;
-
 //
-//SetActiveAxis.java
+// SetActiveAxis.java
 //
 
 /*
@@ -12,14 +10,14 @@ All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-* Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
-* Redistributions in binary form must reproduce the above copyright
- notice, this list of conditions and the following disclaimer in the
- documentation and/or other materials provided with the distribution.
-* Neither the names of the ImageJDev.org developers nor the
- names of its contributors may be used to endorse or promote products
- derived from this software without specific prior written permission.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -34,10 +32,7 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+package imagej.core.plugins.axispos;
 
 import imagej.ImageJ;
 import imagej.data.Dataset;
@@ -47,53 +42,58 @@ import imagej.ext.module.DefaultModuleItem;
 import imagej.ext.plugin.DynamicPlugin;
 import imagej.ext.plugin.Menu;
 import imagej.ext.plugin.Plugin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import net.imglib2.img.Axes;
 import net.imglib2.img.Axis;
 
-
 /**
-* Changes the axis to move along to user specified value. This axis of movement
-* is used in the move axis position forward/backward plugins.
-* 
-* @author Barry DeZonia
-*/
-@Plugin(menu = {
-@Menu(label = "Image", mnemonic = 'i'),
-@Menu(label = "Stacks", mnemonic = 's'),
-@Menu(label = "Set Active Axis...") })
+ * Changes the axis to move along to user specified value. This axis of movement
+ * is used in the move axis position forward/backward plugins.
+ * 
+ * @author Barry DeZonia
+ */
+@Plugin(
+	menu = { @Menu(label = "Image", mnemonic = 'i'),
+		@Menu(label = "Stacks", mnemonic = 's'),
+		@Menu(label = "Set Active Axis...") })
 public class SetActiveAxis extends DynamicPlugin {
 
 	private static final String NAME_KEY = "Axis";
 
 	Display display;
-	
+
 	String axisName;
-	
+
 	public SetActiveAxis() {
 		final DisplayService displayService = ImageJ.get(DisplayService.class);
 		display = displayService.getActiveDisplay();
 		if (display == null) return;
-		Dataset dataset = ImageJ.get(DisplayService.class).getActiveDataset(display);
+		final Dataset dataset =
+			ImageJ.get(DisplayService.class).getActiveDataset(display);
 		final DefaultModuleItem<String> name =
 			new DefaultModuleItem<String>(this, NAME_KEY, String.class);
-		List<Axis> datasetAxes = Arrays.asList(dataset.getAxes());
-		ArrayList<String> choices = new ArrayList<String>();
-		for (Axis candidateAxis : Axes.values()) {
+		final List<Axis> datasetAxes = Arrays.asList(dataset.getAxes());
+		final ArrayList<String> choices = new ArrayList<String>();
+		for (final Axis candidateAxis : Axes.values()) {
 			// TODO - remove someday when we allow X or Y sliders
 			if ((candidateAxis == Axes.X) || (candidateAxis == Axes.Y)) continue;
-			if (datasetAxes.contains(candidateAxis))
-				choices.add(candidateAxis.getLabel());
+			if (datasetAxes.contains(candidateAxis)) choices.add(candidateAxis
+				.getLabel());
 		}
 		name.setChoices(choices);
 		addInput(name);
 	}
-	
+
 	@Override
 	public void run() {
 		final Map<String, Object> inputs = getInputs();
 		axisName = (String) inputs.get(NAME_KEY);
-		Axis newActiveAxis = Axes.get(axisName);
-		if (newActiveAxis != null)
-			display.setActiveAxis(newActiveAxis);
+		final Axis newActiveAxis = Axes.get(axisName);
+		if (newActiveAxis != null) display.setActiveAxis(newActiveAxis);
 	}
 }
