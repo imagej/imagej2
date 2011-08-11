43123daa24a16285496cfdac6e18993f14829cd3
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-08-11 13:58:35 +0000

    Tweak script to not delete META-INF folder.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3680 be23795f-a60e-40d4-ba19-055798872e57

--- scripts/gen-jar-with-deps.sh
+++ scripts/gen-jar-with-deps.sh
@@ -11,4 +11,3 @@ mkdir -p src/main/assembly/all/META-INF/annotations
 java -cp 'target/test-classes:target/classes:target/dependency/*' \
   imagej.util.CombineAnnotations
 mvn -P deps,swing package
-rm -rf src/main/assembly/all/META-INF
