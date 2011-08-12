f2cb58497895bb7601332056841a4046db800241
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-08-12 20:11:26 +0000

    Add a simple ThreadService.
    Uses the java.util.concurrent package.
    
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3682 be23795f-a60e-40d4-ba19-055798872e57

---
+++ core/core/src/main/java/imagej/thread/ThreadService.java
@@ -0,0 +1,95 @@
+//
+// ThreadService.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.thread;
+
+import imagej.AbstractService;
+import imagej.ImageJ;
+import imagej.Service;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Service for managing active ImageJ threads.
+ * 
+ * @author Curtis Rueden
+ */
+@Service
+public final class ThreadService extends AbstractService implements
+	ThreadFactory
+{
+
+	private ExecutorService executor;
+
+	private int nextThread = 0;
+
+	// -- Constructors --
+
+	public ThreadService() {
+		// NB: Required by SezPoz.
+		super(null);
+		throw new UnsupportedOperationException();
+	}
+
+	public ThreadService(final ImageJ context) {
+		super(context);
+	}
+
+	// -- ThreadService methods --
+
+	public <V> Future<V> run(final Callable<V> code) {
+		return executor.submit(code);
+	}
+
+	// -- IService methods --
+
+	@Override
+	public void initialize() {
+		executor = Executors.newCachedThreadPool(this);
+	}
+
+	// -- ThreadFactory methods --
+
+	@Override
+	public Thread newThread(final Runnable r) {
+		final String threadName =
+			"ImageJ-" + getContext().getID() + "-Thread-" + nextThread++;
+		return new Thread(r, threadName);
+	}
+
+}
