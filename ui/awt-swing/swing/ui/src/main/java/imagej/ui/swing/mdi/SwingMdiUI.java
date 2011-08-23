b46499d1de277da5f16666ca001c9880d70a7fcb
Grant Harris <gharris@mbl.edu>
2011-08-23 18:43:30 -0400

    Adjustments to MDI, full desktop bounds.
    This used to be revision r3760.

--- ui/awt-swing/swing/ui/src/main/java/imagej/ui/swing/mdi/SwingMdiUI.java
+++ ui/awt-swing/swing/ui/src/main/java/imagej/ui/swing/mdi/SwingMdiUI.java
@@ -10,14 +10,14 @@ All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the names of the ImageJDev.org developers nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+ * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+ * Neither the names of the ImageJDev.org developers nor the
+names of its contributors may be used to endorse or promote products
+derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -30,8 +30,7 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
-*/
-
+ */
 package imagej.ui.swing.mdi;
 
 import imagej.ImageJ;
@@ -64,7 +63,10 @@ import imagej.util.Log;
 import imagej.util.Prefs;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.DataInputStream;
@@ -99,7 +101,7 @@ public class SwingMdiUI implements UserInterface {
 	private UIService uiService;
 	private SwingApplicationFrame parentFrame;
 	private JScrollPane scrollPane;
-    private JMDIDesktopPane desktopPane;
+	private JMDIDesktopPane desktopPane;
 	private SwingToolBar toolBar;
 	private SwingStatusBar statusBar;
 	private ArrayList<EventSubscriber<?>> subscribers;
@@ -114,11 +116,11 @@ public class SwingMdiUI implements UserInterface {
 		createMenus();
 		// Create MDI Desktop 
 		parentFrame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-		parentFrame.setPreferredSize(new Dimension(800, 600));
 		desktopPane = new JMDIDesktopPane();
 		// TODO desktopPane.setTransferHandler(new DropFileTransferHandler());
 		scrollPane = new JScrollPane();
 		scrollPane.setViewportView(desktopPane);
+		desktopPane.setBackground(new Color(200,200,255));
 		parentFrame.getContentPane().add(scrollPane);
 		//PreferencesUtil.installFramePrefsHandler(prefs, "parentFrame", parentFrame);
 		parentFrame.addWindowListener(new WindowAdapter() {
@@ -133,12 +135,18 @@ public class SwingMdiUI implements UserInterface {
 		parentFrame.getContentPane().add(toolBar, BorderLayout.NORTH);
 		parentFrame.getContentPane().add(statusBar, BorderLayout.SOUTH);
 
-		parentFrame.pack();
+		//parentFrame.pack();
+		parentFrame.setBounds(getWorkSpaceBounds());
+		//parentFrame.setPreferredSize(new Dimension(800, 600));
 		parentFrame.setVisible(true);
 
 		subscribeToEvents();
 
 		//displayReadme();
+	}
+
+	public Rectangle getWorkSpaceBounds() {
+		return GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
 	}
 
 	@Override
@@ -371,6 +379,5 @@ public class SwingMdiUI implements UserInterface {
 			final MessageType msg, final OptionType option) {
 		return new SwingMdiDialogPrompt(message, title, msg, option);
 	}
-
 
 }
