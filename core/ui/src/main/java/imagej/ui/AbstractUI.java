91616db0c51c418d717508ff40392612c63c46bf
Curtis Rueden <ctrueden@wisc.edu>
2011-08-29 15:17:57 -0500

    Eliminate copy/pasted code between Swing UIs.
    This used to be revision r3774.

---
+++ core/ui/src/main/java/imagej/ui/AbstractUI.java
@@ -0,0 +1,200 @@
+//
+// AbstractUI.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.ui;
+
+import imagej.ImageJ;
+import imagej.util.Log;
+import imagej.util.Prefs;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+/**
+ * Abstract superclass for {@link UserInterface} implementations.
+ * 
+ * @author Curtis Rueden
+ */
+public abstract class AbstractUI implements UserInterface {
+
+	private static final String README_FILE = "README.txt";
+	private static final String PREF_FIRST_RUN = "firstRun-" + ImageJ.VERSION;
+
+	private UIService uiService;
+
+	// -- UserInterface methods --
+
+	@Override
+	public void initialize(final UIService service) {
+		uiService = service;
+		createUI();
+		displayReadme();
+	}
+
+	@Override
+	public UIService getUIService() {
+		return uiService;
+	}
+
+	@Override
+	public void processArgs(final String[] args) {
+		// TODO
+	}
+
+	@Override
+	public Desktop getDesktop() {
+		return null;
+	}
+
+	@Override
+	public ApplicationFrame getApplicationFrame() {
+		return null;
+	}
+
+	@Override
+	public ToolBar getToolBar() {
+		return null;
+	}
+
+	@Override
+	public StatusBar getStatusBar() {
+		return null;
+	}
+
+	// -- Internal methods --
+
+	protected void createUI() {
+		// does nothing by default
+	}
+
+	// -- Helper methods --
+
+	private void displayReadme() {
+		final OutputWindow out =
+			newOutputWindow("ImageJ v" + ImageJ.VERSION + " - " + README_FILE);
+
+		final String firstRun = Prefs.get(getClass(), PREF_FIRST_RUN);
+		if (firstRun != null) return;
+		Prefs.put(getClass(), PREF_FIRST_RUN, false);
+
+		final String readmeText = loadReadmeFile();
+		out.append(readmeText);
+		out.setVisible(true);
+	}
+
+	private String loadReadmeFile() {
+		final File baseDir = getBaseDirectory();
+		final File readmeFile = new File(baseDir, README_FILE);
+
+		try {
+			final DataInputStream in =
+				new DataInputStream(new FileInputStream(readmeFile));
+			final int len = (int) readmeFile.length();
+			final byte[] bytes = new byte[len];
+			in.readFully(bytes);
+			in.close();
+			return new String(bytes);
+		}
+		catch (final FileNotFoundException e) {
+			throw new IllegalArgumentException(README_FILE + " not found at " +
+				baseDir.getAbsolutePath());
+		}
+		catch (final IOException e) {
+			throw new IllegalStateException(e.getMessage());
+		}
+	}
+
+	private File getBaseDirectory() {
+		final File pathToClass = getPathToClass();
+		final String path = pathToClass.getPath();
+
+		final File baseDir;
+		if (path.endsWith(".class")) {
+			// assume class is in a subfolder of Maven target
+			File dir = pathToClass;
+			while (dir != null && !dir.getName().equals("target")) {
+				dir = up(dir);
+			}
+			// NB: Base directory is 5 levels up from ui/awt-swing/swing/ui/target.
+			baseDir = up(up(up(up(up(dir)))));
+		}
+		else if (path.endsWith(".jar")) {
+			// assume class is in a library folder of the distribution
+			final File dir = pathToClass.getParentFile();
+			baseDir = up(dir);
+		}
+		else baseDir = null;
+
+		// return current working directory if not found
+		return baseDir == null ? new File(".") : baseDir;
+	}
+
+	/**
+	 * Gets the file on disk containing this class.
+	 * <p>
+	 * This could be a jar archive, or a standalone class file.
+	 * </p>
+	 */
+	private File getPathToClass() {
+		final Class<?> c = getClass();
+		final String className = c.getSimpleName();
+		String path = getClass().getResource(className + ".class").toString();
+		path = path.replaceAll("^jar:", "");
+		path = path.replaceAll("^file:", "");
+		path = path.replaceAll("^/*/", "/");
+		path = path.replaceAll("^/([A-Z]:)", "$1");
+		path = path.replaceAll("!.*", "");
+		try {
+			path = URLDecoder.decode(path, "UTF-8");
+		}
+		catch (final UnsupportedEncodingException e) {
+			Log.warn("Cannot parse class: " + className, e);
+		}
+		String slash = File.separator;
+		if (slash.equals("\\")) slash = "\\\\";
+		path = path.replaceAll("/", slash);
+		return new File(path);
+	}
+
+	private File up(final File file) {
+		if (file == null) return null;
+		return file.getParentFile();
+	}
+
+}
---
+++ ui/awt-swing/swing/ui-base/src/main/java/imagej/ui/swing/AbstractSwingUI.java
@@ -0,0 +1,212 @@
+//
+// AbstractSwingUI.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.ui.swing;
+
+import imagej.ImageJ;
+import imagej.display.Display;
+import imagej.display.DisplayPanel;
+import imagej.display.event.DisplayCreatedEvent;
+import imagej.display.event.DisplayDeletedEvent;
+import imagej.event.EventService;
+import imagej.event.EventSubscriber;
+import imagej.ext.menu.MenuService;
+import imagej.ext.menu.ShadowMenu;
+import imagej.ext.ui.swing.SwingJMenuBarCreator;
+import imagej.platform.event.AppMenusCreatedEvent;
+import imagej.platform.event.AppQuitEvent;
+import imagej.ui.AbstractUI;
+import imagej.ui.OutputWindow;
+import imagej.ui.UIService;
+import imagej.ui.swing.display.SwingDisplayPanel;
+import imagej.ui.swing.display.SwingDisplayWindow;
+
+import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+/**
+ * Abstract superclass for Swing-based user interfaces.
+ * 
+ * @author Curtis Rueden
+ * @author Barry DeZonia
+ * @author Grant Harris
+ */
+public abstract class AbstractSwingUI extends AbstractUI {
+
+	private SwingApplicationFrame appFrame;
+	private SwingToolBar toolBar;
+	private SwingStatusBar statusBar;
+
+	private ArrayList<EventSubscriber<?>> subscribers;
+
+	// -- UserInterface methods --
+
+	@Override
+	public SwingApplicationFrame getApplicationFrame() {
+		return appFrame;
+	}
+
+	@Override
+	public SwingToolBar getToolBar() {
+		return toolBar;
+	}
+
+	@Override
+	public SwingStatusBar getStatusBar() {
+		return statusBar;
+	}
+
+	@Override
+	public void createMenus() {
+		final JMenuBar menuBar = createMenuBar(appFrame);
+		getUIService().getEventService().publish(new AppMenusCreatedEvent(menuBar));
+	}
+
+	@Override
+	public OutputWindow newOutputWindow(final String title) {
+		return new SwingOutputWindow(title);
+	}
+
+	// -- Internal methods --
+
+	@Override
+	protected void createUI() {
+		appFrame = new SwingApplicationFrame("ImageJ");
+		toolBar = new SwingToolBar();
+		statusBar = new SwingStatusBar();
+		createMenus();
+
+		setupAppFrame();
+
+		appFrame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		appFrame.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(final WindowEvent evt) {
+				getUIService().getEventService().publish(new AppQuitEvent());
+			}
+		});
+
+		appFrame.getContentPane().add(toolBar, BorderLayout.NORTH);
+		appFrame.getContentPane().add(statusBar, BorderLayout.SOUTH);
+
+		appFrame.pack();
+		appFrame.setVisible(true);
+
+		subscribeToEvents();
+	}
+
+	protected abstract void setupAppFrame();
+
+	/**
+	 * Creates a {@link JMenuBar} from the master {@link ShadowMenu} structure,
+	 * and adds it to the given {@link JFrame}.
+	 */
+	protected JMenuBar createMenuBar(final JFrame f) {
+		final MenuService menuService = ImageJ.get(MenuService.class);
+		final JMenuBar menuBar =
+			menuService.createMenus(new SwingJMenuBarCreator(), new JMenuBar());
+		f.setJMenuBar(menuBar);
+		f.validate();
+		return menuBar;
+	}
+
+	protected void deleteMenuBar(final JFrame f) {
+		f.setJMenuBar(null);
+		// HACK - w/o this next call the JMenuBars do not get garbage collected.
+		// At least its true on the Mac. This might be a Java bug. Update:
+		// I hunted on web and have found multiple people with the same problem.
+		// The Apple ScreenMenus don't GC when a Frame disposes. Their workaround
+		// was exactly the same. I have not found any official documentation of
+		// this issue.
+		f.setMenuBar(null);
+	}
+
+	// -- Helper methods --
+
+	private void subscribeToEvents() {
+		final UIService uiService = getUIService();
+		final EventService eventService = uiService.getEventService();
+		subscribers = new ArrayList<EventSubscriber<?>>();
+
+		if (uiService.getPlatformService().isMenuBarDuplicated()) {
+			// NB: If menu bars are supposed to be duplicated across all window
+			// frames, listen for display creations and deletions and clone the menu
+			// bar accordingly.
+
+			final EventSubscriber<DisplayCreatedEvent> createSubscriber =
+				new EventSubscriber<DisplayCreatedEvent>() {
+
+					@Override
+					public void onEvent(final DisplayCreatedEvent event) {
+						final Display display = event.getObject();
+						final DisplayPanel panel = display.getDisplayPanel();
+						if (!(panel instanceof SwingDisplayPanel)) return;
+						final SwingDisplayPanel swingPanel = (SwingDisplayPanel) panel;
+						final SwingDisplayWindow swingWindow =
+							(SwingDisplayWindow) SwingUtilities.getWindowAncestor(swingPanel);
+						// add a copy of the JMenuBar to the new display
+						if (swingWindow.getJMenuBar() == null) createMenuBar(swingWindow);
+					}
+				};
+			subscribers.add(createSubscriber);
+			eventService.subscribe(DisplayCreatedEvent.class, createSubscriber);
+
+			final EventSubscriber<DisplayDeletedEvent> deleteSubscriber =
+				new EventSubscriber<DisplayDeletedEvent>() {
+
+					@Override
+					public void onEvent(final DisplayDeletedEvent event) {
+						final Display display = event.getObject();
+						final DisplayPanel panel = display.getDisplayPanel();
+						if (!(panel instanceof SwingDisplayPanel)) return;
+						final SwingDisplayPanel swingPanel = (SwingDisplayPanel) panel;
+						final SwingDisplayWindow swingWindow =
+							(SwingDisplayWindow) SwingUtilities.getWindowAncestor(swingPanel);
+						deleteMenuBar(swingWindow);
+					}
+				};
+			subscribers.add(deleteSubscriber);
+			eventService.subscribe(DisplayDeletedEvent.class, deleteSubscriber);
+		}
+	}
+
+}
