40bcfd0a1474de340d705f994d54c8cdcfa117fe
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-09-01 19:37:06 +0000

    Many bug fixes and improvements to ShadowMenu:
      * Add several more unit tests and fix bugs thus exposed.
        In particular, remove and removeAll now work properly.
      * Implement toString and retainAll methods.
      * Only link ModuleInfo to its associated leaf node, and not to any
        parent nodes. Before, the e.g. "File" node was linked to whichever
        ModuleInfo was the first to belong under the File structure.
        Now, a linked ModuleInfo of null is indicative of a non-leaf node.
      * Fix size method to return only the number of ModuleInfos,
        not the total number of ShadowMenu nodes in the tree.
      * Similarly, fix the iterator to only iterate over ModuleInfos
        linked to leaf nodes. In total, ShadowMenu now behaves much better
        as a collection of ModuleInfo objects, with the tree structure
        largely irrelevant to the behavior of the collection methods.
      * Declare that ShadowMenu implements Runnable.
      * More thorough documentation.
    
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@3804 be23795f-a60e-40d4-ba19-055798872e57

--- core/ext/src/main/java/imagej/ext/menu/ShadowMenu.java
+++ core/ext/src/main/java/imagej/ext/menu/ShadowMenu.java
@@ -40,6 +40,7 @@ import imagej.ext.menu.event.MenusAddedEvent;
 import imagej.ext.menu.event.MenusRemovedEvent;
 import imagej.ext.menu.event.MenusUpdatedEvent;
 import imagej.ext.module.ModuleInfo;
+import imagej.ext.plugin.PluginService;
 import imagej.util.ClassUtils;
 import imagej.util.Log;
 
@@ -54,27 +55,48 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * A tree representing the menu structure independent of any particular user
- * interface. Each menu item is linked to a corresponding {@link ModuleInfo}.
+ * A tree representing a menu structure independent of any particular user
+ * interface.
+ * <p>
+ * The class is implemented as a {@link Collection} of modules (i.e.,
+ * {@link ModuleInfo} objects), with the tree structure generated from the
+ * modules' menu paths (see {@link ModuleInfo#getMenuPath()}). The class also
+ * implements {@link Runnable}, with each leaf node retaining a link to its
+ * corresponding {@link ModuleInfo}, and executing that module when
+ * {@link #run()} is called.
+ * </p>
  * 
  * @author Curtis Rueden
  * @see MenuCreator
+ * @see MenuPath
+ * @see MenuEntry
  */
 public class ShadowMenu implements Comparable<ShadowMenu>,
-	Collection<ModuleInfo>
+	Collection<ModuleInfo>, Runnable
 {
 
+	/** Icon to use for leaf entries by default, if no icon is specified. */
 	private static final String DEFAULT_ICON_PATH = "/icons/plugin.png";
 
+	/** The service to use for executing modules and publishing menu events. */
 	private final MenuService menuService;
 
+	/** The module linked to this node. Always null for non-leaf nodes. */
 	private final ModuleInfo moduleInfo;
 
+	/** The menu entry corresponding to this node. */
+	private final MenuEntry menuEntry;
+
+	/** How deep into the menu structure this node is. */
 	private final int menuDepth;
 
+	/** Table of child nodes, keyed by name. */
 	private final Map<String, ShadowMenu> children;
 
-	/** Constructs a root menu node populated with all available modules. */
+	/**
+	 * Constructs a root menu node populated with all available modules obtained
+	 * from the {@link PluginService}.
+	 */
 	public ShadowMenu(final MenuService menuService) {
 		this(menuService, menuService.getPluginService().getModuleService()
 			.getModules());
@@ -88,76 +110,96 @@ public class ShadowMenu implements Comparable<ShadowMenu>,
 		addAll(modules);
 	}
 
-	private ShadowMenu(final MenuService menuService, final ModuleInfo info,
-		final int menuDepth)
+	private ShadowMenu(final MenuService menuService,
+		final ModuleInfo moduleInfo, final int menuDepth)
 	{
 		this.menuService = menuService;
-		this.moduleInfo = info;
+		if (moduleInfo == null) {
+			this.moduleInfo = null;
+			menuEntry = null;
+		}
+		else {
+			final MenuPath menuPath = moduleInfo.getMenuPath();
+			// preserve moduleInfo reference only for leaf items
+			final boolean leaf = menuDepth == menuPath.size() - 1;
+			this.moduleInfo = leaf ? moduleInfo : null;
+			menuEntry = menuPath.get(menuDepth);
+		}
 		this.menuDepth = menuDepth;
 		children = new HashMap<String, ShadowMenu>();
 	}
 
 	// -- ShadowMenu methods --
 
-	/** Gets the {@link MenuService} associated with this menu. */
+	/** Gets the {@link MenuService} associated with this node. */
 	public MenuService getMenuService() {
 		return menuService;
 	}
 
-	/** Gets the module linked to this menu. */
+	/** Gets the module linked to this node, or null if node is not a leaf. */
 	public ModuleInfo getModuleInfo() {
 		return moduleInfo;
 	}
 
-	/** Gets this menu's index into its linked module's {@link MenuEntry}. */
+	/**
+	 * Gets the menu entry corresponding to this node. May be a non-leaf menu
+	 * (e.g., "File") or a leaf item (e.g., "Exit").
+	 */
+	public MenuEntry getMenuEntry() {
+		return menuEntry;
+	}
+
+	/**
+	 * Gets how deep into the menu structure this node is. For example, "File"
+	 * would be at depth 1, whereas "Exit" (of "File>Exit") would be at depth 2.
+	 */
 	public int getMenuDepth() {
 		return menuDepth;
 	}
 
-	/** Gets this menu's children, sorted by weight. */
-	public List<ShadowMenu> getChildren() {
-		final List<ShadowMenu> childMenus =
-			new ArrayList<ShadowMenu>(children.values());
-		Collections.sort(childMenus);
-		return childMenus;
+	/** Gets the name of the menu. */
+	public String getName() {
+		return menuEntry == null ? null : menuEntry.getName();
 	}
 
-	/** Returns true if this menu has no children. */
+	/** Gets this node's children, sorted by weight. */
+	public List<ShadowMenu> getChildren() {
+		// copy the children table into an ordered list
+		final List<ShadowMenu> childList =
+			new ArrayList<ShadowMenu>(children.values());
+		// sort the list by weight then alphabetically
+		Collections.sort(childList);
+		return childList;
+	}
+
+	/** Returns true if this node has no children. */
 	public boolean isLeaf() {
 		return children.isEmpty();
 	}
 
-	/** Returns true if this menu is selectable (checkbox or radio button). */
+	/** Returns true if this node is selectable (checkbox or radio button). */
 	public boolean isToggle() {
 		if (moduleInfo == null) return false;
 		return moduleInfo.isSelectable();
 	}
 
-	/** Returns true if this menu is a checkbox. */
+	/** Returns true if this node is a checkbox. */
 	public boolean isCheckBox() {
 		if (!isToggle()) return false;
 		final String selectionGroup = moduleInfo.getSelectionGroup();
 		return selectionGroup == null || selectionGroup.isEmpty();
 	}
 
-	/** Returns true if this menu is a radio button. */
+	/** Returns true if this node is a radio button. */
 	public boolean isRadioButton() {
 		if (!isToggle()) return false;
 		final String selectionGroup = moduleInfo.getSelectionGroup();
 		return selectionGroup != null && !selectionGroup.isEmpty();
 	}
 
-	/** Gets the {@link MenuEntry} associated with this menu. */
-	public MenuEntry getMenuEntry() {
-		if (moduleInfo == null) return null;
-		final MenuPath menuPath = moduleInfo.getMenuPath();
-		if (menuPath == null) return null;
-		return menuPath.get(menuDepth);
-	}
-
-	/** Gets the URL of the icon associated with this menu's {@link MenuEntry}. */
+	/** Gets the URL of the icon associated with this node's {@link MenuEntry}. */
 	public URL getIconURL() {
-		final MenuEntry menuEntry = getMenuEntry();
+		// See also: imagej.ext.IndexItemInfo#getIconURL()
 		if (menuEntry == null) return null;
 		String iconPath = menuEntry.getIconPath();
 		if (iconPath == null || iconPath.isEmpty()) {
@@ -183,9 +225,9 @@ public class ShadowMenu implements Comparable<ShadowMenu>,
 	public boolean update(final ModuleInfo module) {
 		final ShadowMenu removed = removeInternal(module);
 		if (removed == null) return false; // was not in menu structure
-		final ShadowMenu menu = addInternal(module);
-		if (menu == null) return false;
-		menuService.getEventService().publish(new MenusUpdatedEvent(menu));
+		final ShadowMenu node = addInternal(module);
+		if (node == null) return false;
+		menuService.getEventService().publish(new MenusUpdatedEvent(node));
 		return true;
 	}
 
@@ -197,64 +239,76 @@ public class ShadowMenu implements Comparable<ShadowMenu>,
 	 * @return true if at least one module was successfully updated
 	 */
 	public boolean updateAll(final Collection<? extends ModuleInfo> c) {
-		final HashSet<ShadowMenu> menus = new HashSet<ShadowMenu>();
+		final HashSet<ShadowMenu> nodes = new HashSet<ShadowMenu>();
 		for (final ModuleInfo info : c) {
-			System.out.println("==> ShadowMenu.updateAll: info=" + info);//TEMP
 			final ShadowMenu removed = removeInternal(info);
 			if (removed == null) continue; // was not in menu structure
-			System.out.println("==> ShadowMenu.updateAll: removed item: " + removed);//TEMP
-			final ShadowMenu menu = addInternal(info);
-			System.out.println("==> ShadowMenu.updateAll: added item: " + menu);//TEMP
-			if (menu != null) menus.add(menu);
+			final ShadowMenu node = addInternal(info);
+			if (node != null) nodes.add(node);
 		}
-		if (menus.isEmpty()) return false;
-		menuService.getEventService().publish(new MenusUpdatedEvent(menus));
+		if (nodes.isEmpty()) return false;
+		menuService.getEventService().publish(new MenusUpdatedEvent(nodes));
 		return true;
-	}
-
-	/** Executes the module linked to this menu. */
-	public void run() {
-		menuService.getPluginService().run(moduleInfo);
 	}
 
 	// -- Object methods --
 
 	@Override
 	public int compareTo(final ShadowMenu c) {
-		final double w1 = getMenuEntry().getWeight();
-		final double w2 = c.getMenuEntry().getWeight();
+		if (menuEntry == null || c.menuEntry == null) return 0;
+		// compare weights
+		final double w1 = menuEntry.getWeight();
+		final double w2 = c.menuEntry.getWeight();
 		if (w1 < w2) return -1;
 		if (w1 > w2) return 1;
 		// if weights are equal, sort alphabetically
-		final String n1 = getMenuEntry().getName();
-		final String n2 = c.getMenuEntry().getName();
+		final String n1 = menuEntry.getName();
+		final String n2 = c.menuEntry.getName();
 		return n1.compareTo(n2);
 	}
 
 	@Override
 	public String toString() {
-		return getMenuEntry().getName();
+		final StringBuilder sb = new StringBuilder();
+		for (int i=0; i<=menuDepth; i++) sb.append("\t");
+		final String name = getName();
+		sb.append(name == null ? "[-]" : name);
+		for (final ShadowMenu child : getChildren()) {
+			sb.append("\n" + child.toString());
+		}
+		return sb.toString();
+	}
+
+	// -- Runnable methods --
+
+	/**
+	 * Executes the module linked to this node. Does nothing for non-leaf nodes.
+	 */
+	@Override
+	public void run() {
+		if (moduleInfo == null) return; // no module to run
+		menuService.getPluginService().run(moduleInfo);
 	}
 
 	// -- Collection methods --
 
 	@Override
 	public boolean add(final ModuleInfo o) {
-		final ShadowMenu menu = addInternal(o);
-		if (menu == null) return false;
-		menuService.getEventService().publish(new MenusAddedEvent(menu));
+		final ShadowMenu node = addInternal(o);
+		if (node == null) return false;
+		menuService.getEventService().publish(new MenusAddedEvent(node));
 		return true;
 	}
 
 	@Override
 	public boolean addAll(final Collection<? extends ModuleInfo> c) {
-		final HashSet<ShadowMenu> menus = new HashSet<ShadowMenu>();
+		final HashSet<ShadowMenu> nodes = new HashSet<ShadowMenu>();
 		for (final ModuleInfo info : c) {
-			final ShadowMenu menu = addInternal(info);
-			if (menu != null) menus.add(menu);
+			final ShadowMenu node = addInternal(info);
+			if (node != null) nodes.add(node);
 		}
-		if (menus.isEmpty()) return false;
-		menuService.getEventService().publish(new MenusAddedEvent(menus));
+		if (nodes.isEmpty()) return false;
+		menuService.getEventService().publish(new MenusAddedEvent(nodes));
 		return true;
 	}
 
@@ -294,34 +348,38 @@ public class ShadowMenu implements Comparable<ShadowMenu>,
 	public boolean remove(final Object o) {
 		if (!(o instanceof ModuleInfo)) return false;
 		final ModuleInfo info = (ModuleInfo) o;
-		final ShadowMenu menu = removeInternal(info);
-		if (menu == null) return false;
-		menuService.getEventService().publish(new MenusRemovedEvent(menu));
+		final ShadowMenu node = removeInternal(info);
+		if (node == null) return false;
+		menuService.getEventService().publish(new MenusRemovedEvent(node));
 		return true;
 	}
 
 	@Override
 	public boolean removeAll(final Collection<?> c) {
-		final HashSet<ShadowMenu> menus = new HashSet<ShadowMenu>();
+		final HashSet<ShadowMenu> nodes = new HashSet<ShadowMenu>();
 		for (final Object o : c) {
 			if (!(o instanceof ModuleInfo)) continue;
 			final ModuleInfo info = (ModuleInfo) o;
-			final ShadowMenu menu = removeInternal(info);
-			if (menu != null) menus.add(menu);
+			final ShadowMenu node = removeInternal(info);
+			if (node != null) nodes.add(node);
 		}
-		if (menus.isEmpty()) return false;
-		menuService.getEventService().publish(new MenusRemovedEvent(menus));
+		if (nodes.isEmpty()) return false;
+		menuService.getEventService().publish(new MenusRemovedEvent(nodes));
 		return true;
 	}
 
 	@Override
 	public boolean retainAll(final Collection<?> c) {
-		throw new UnsupportedOperationException();
+		final ArrayList<Object> toRemove = new ArrayList<Object>();
+		for (final ModuleInfo info : this) {
+			if (!c.contains(info)) toRemove.add(info);
+		}
+		return removeAll(toRemove);
 	}
 
 	@Override
 	public int size() {
-		int sum = 1;
+		int sum = moduleInfo == null ? 0 : 1;
 		for (final ShadowMenu child : children.values()) {
 			sum += child.size();
 		}
@@ -378,53 +436,60 @@ public class ShadowMenu implements Comparable<ShadowMenu>,
 
 	private ShadowMenu removeInternal(final ModuleInfo o) {
 		for (final String menuName : children.keySet()) {
-			final ShadowMenu menu = children.get(menuName);
+			final ShadowMenu child = children.get(menuName);
 			// remove directly from children
-			if (menu.getModuleInfo() == o) {
+			if (child.getModuleInfo() == o) {
 				children.remove(menuName);
-				return menu;
+				return child;
 			}
 
 			// recursively remove from descendants
-			final ShadowMenu removed = menu.removeInternal(o);
-			if (removed != null) return removed;
+			final ShadowMenu removed = child.removeInternal(o);
+			if (removed != null) {
+				if (child.isLeaf() && child.getModuleInfo() == null) {
+					// prune empty non-leaf node
+					children.remove(menuName);
+				}
+				return removed;
+			}
 		}
 
 		return null;
 	}
 
-	private ShadowMenu addChild(final ModuleInfo child, final int depth) {
-		// retrieve existing child
-		final MenuEntry menuEntry = child.getMenuPath().get(depth);
-		final ShadowMenu existingChild = getChild(menuEntry);
+	private ShadowMenu addChild(final ModuleInfo info, final int depth) {
+		final MenuPath menuPath = info.getMenuPath();
+		final MenuEntry entry = menuPath.get(depth);
+		final boolean leaf = isLeaf(depth, menuPath);
 
-		final ShadowMenu childMenu;
+		// retrieve existing child
+		final ShadowMenu existingChild = children.get(entry.getName());
+
+		final ShadowMenu child;
 		if (existingChild == null) {
 			// create new child and add to table
-			final String menuName = menuEntry.getName();
-			final ShadowMenu newChild = new ShadowMenu(menuService, child, depth);
+			final String menuName = entry.getName();
+			final ShadowMenu newChild = new ShadowMenu(menuService, info, depth);
 			children.put(menuName, newChild);
-			childMenu = newChild;
+			child = newChild;
 		}
 		else {
 			// fill in any missing menu properties of existing child
 			final MenuEntry childMenuEntry = existingChild.getMenuEntry();
-			childMenuEntry.assignProperties(menuEntry);
-			childMenu = existingChild;
+			childMenuEntry.assignProperties(entry);
+			child = existingChild;
 		}
 
 		// recursively add remaining child menus
-		if (depth + 1 < child.getMenuPath().size()) {
-			childMenu.addChild(child, depth + 1);
-		}
+		if (!leaf) child.addChild(info, depth + 1);
 		else if (existingChild != null) {
 			Log.warn("ShadowMenu: leaf item already exists: " + existingChild);
 		}
-		return childMenu;
+		return child;
 	}
 
-	private ShadowMenu getChild(final MenuEntry menuEntry) {
-		return children.get(menuEntry.getName());
+	private boolean isLeaf(final int depth, final MenuPath path) {
+		return depth == path.size() - 1;
 	}
 
 }
