b95ac5e93edbdae594c23e3c4b7165877333d63f
Lee Kamentsky <leek@broadinstitute.org>
2011-09-06 11:13:54 -0400

    Fixed close() so that it calls this.window.dispose() instead of trying to figure out what window might be the SwingDisplayWindow
    This used to be revision r3811.

--- ui/awt-swing/swing/ui-base/src/main/java/imagej/ui/swing/display/SwingDisplayPanel.java
+++ ui/awt-swing/swing/ui-base/src/main/java/imagej/ui/swing/display/SwingDisplayPanel.java
@@ -85,7 +85,7 @@ public class SwingDisplayPanel extends AbstractSwingDisplayPanel {
 	private final ImageDisplay display;
 	private final JLabel imageLabel;
 	private final JPanel sliders;
-	private final DisplayWindow window;
+	private final SwingDisplayWindow window;
 
 	protected final Map<Axis, Integer> axisPositions =
 		new HashMap<Axis, Integer>();
@@ -104,7 +104,7 @@ public class SwingDisplayPanel extends AbstractSwingDisplayPanel {
 		final DisplayWindow window)
 	{
 		this.display = display;
-		this.window = window;
+		this.window = (SwingDisplayWindow) window;
 
 		imageLabel = new JLabel(" ");
 		final int prefHeight = imageLabel.getPreferredSize().height;
@@ -453,7 +453,7 @@ public class SwingDisplayPanel extends AbstractSwingDisplayPanel {
 	@Override
 	public void close() {
 		closeHelper();
-		((SwingDisplayWindow) this.getParent()).dispose();
+		this.window.dispose();
 	}
 
 }
