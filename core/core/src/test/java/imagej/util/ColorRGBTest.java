f443640f255bbe5b7c002899a6e60733833a7f00
Curtis Rueden <ctrueden@wisc.edu>
2011-09-08 14:07:57 -0500

    Header and style fixes.
    This used to be revision r3837.

--- core/core/src/test/java/imagej/util/ColorRGBTest.java
+++ core/core/src/test/java/imagej/util/ColorRGBTest.java
@@ -1,21 +1,63 @@
+//
+// ColorRGBTest.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package imagej.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+/**
+ * Tests {@link ColorRGB}.
+ * 
+ * @author Lee Kamentsky
+ */
 public class ColorRGBTest {
 
 	@Test
 	public void testHashCode() {
-		assertNotSame(new ColorRGB(1,2,3).hashCode(), new ColorRGB(3,2,1));
+		assertNotSame(new ColorRGB(1, 2, 3).hashCode(), new ColorRGB(3, 2, 1));
 	}
 
 	@Test
 	public void testColorRGBIntIntInt() {
-		assertEquals(1, new ColorRGB(1,2,3).getRed());
-		assertEquals(2, new ColorRGB(1,2,3).getGreen());
-		assertEquals(3, new ColorRGB(1,2,3).getBlue());
+		assertEquals(1, new ColorRGB(1, 2, 3).getRed());
+		assertEquals(2, new ColorRGB(1, 2, 3).getGreen());
+		assertEquals(3, new ColorRGB(1, 2, 3).getBlue());
 	}
 
 	@Test
@@ -42,18 +84,19 @@ public class ColorRGBTest {
 
 	@Test
 	public void testGetAlpha() {
-		assertEquals(255, new ColorRGB(1,2,3).getAlpha());
+		assertEquals(255, new ColorRGB(1, 2, 3).getAlpha());
 	}
 
 	@Test
 	public void testGetARGB() {
-		assertEquals(255*256*256*256 + 256*256 + 2 * 256 + 3, new ColorRGB(1,2,3).getARGB());
+		assertEquals(255 * 256 * 256 * 256 + 256 * 256 + 2 * 256 + 3, new ColorRGB(
+			1, 2, 3).getARGB());
 	}
 
 	@Test
 	public void testToHTMLColor() {
-		assertEquals("#010203", new ColorRGB(1,2,3).toHTMLColor());
-		assertEquals("red", new ColorRGB(255,0,0).toHTMLColor());
+		assertEquals("#010203", new ColorRGB(1, 2, 3).toHTMLColor());
+		assertEquals("red", new ColorRGB(255, 0, 0).toHTMLColor());
 	}
 
 	@Test
@@ -63,14 +106,14 @@ public class ColorRGBTest {
 
 	@Test
 	public void testEqualsObject() {
-		assertTrue(new ColorRGB(1,2,3).equals(new ColorRGB(1,2,3)));
-		assertFalse(new ColorRGB(1,2,3).equals(new ColorRGB(3,2,1)));
+		assertTrue(new ColorRGB(1, 2, 3).equals(new ColorRGB(1, 2, 3)));
+		assertFalse(new ColorRGB(1, 2, 3).equals(new ColorRGB(3, 2, 1)));
 	}
 
 	@Test
 	public void testFromHTMLColor() {
-		assertEquals(new ColorRGB(1,2,3), ColorRGB.fromHTMLColor("#010203"));
-		assertEquals(new ColorRGB(255,0,0), ColorRGB.fromHTMLColor("red"));
+		assertEquals(new ColorRGB(1, 2, 3), ColorRGB.fromHTMLColor("#010203"));
+		assertEquals(new ColorRGB(255, 0, 0), ColorRGB.fromHTMLColor("red"));
 	}
 
 }
