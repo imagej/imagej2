455da38697b636165c0efa99da9528ecf5e02332
Johannes Schindelin <johannes.schindelin@gmx.de>
2011-09-12 15:55:54 -0500

    Initial version of the ScriptService
    This service's purpose is to discover script language implementations and
    provide some convenience functions to work with them.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ core/core/src/main/java/imagej/script/DefaultScriptService.java
@@ -0,0 +1,140 @@
+//
+// DefaultScriptService.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.script;
+
+import imagej.InstantiableException;
+import imagej.command.CommandInfo;
+import imagej.log.LogService;
+import imagej.plugin.Parameter;
+import imagej.plugin.Plugin;
+import imagej.plugin.PluginInfo;
+import imagej.plugin.PluginService;
+import imagej.service.AbstractService;
+import imagej.service.Service;
+import imagej.util.FileUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.script.ScriptEngineFactory;
+import javax.script.ScriptException;
+
+/**
+ * A service discovering all available script languages and convenience methods
+ * to interact with them
+ * 
+ * @author Johannes Schindelin
+ */
+@Plugin(type = Service.class)
+public class DefaultScriptService extends AbstractService implements ScriptService {
+
+	@Parameter
+	private PluginService pluginService;
+
+	@Parameter
+	private LogService log;
+
+	/** Index of registered script languages. */
+	private final ScriptLanguageIndex scriptLanguageIndex =
+		new ScriptLanguageIndex();
+
+	@Override
+	public void initialize() {
+		reloadScriptLanguages();
+
+		final ArrayList<CommandInfo<?>> plugins = new ArrayList<CommandInfo<?>>();
+		new ScriptFinder().findPlugins(plugins);
+		pluginService.addPlugins(plugins);
+
+	}
+
+	// -- ScriptService methods --
+
+	@Override
+	public PluginService getPluginService() {
+		return pluginService;
+	}
+
+	/** Gets the index of available script languages. */
+	@Override
+	public ScriptLanguageIndex getIndex() {
+		return scriptLanguageIndex;
+	}
+
+	@Override
+	public List<ScriptEngineFactory> getLanguages() {
+		return new ArrayList<ScriptEngineFactory>(scriptLanguageIndex);
+	}
+
+	@Override
+	public ScriptEngineFactory getByFileExtension(final String fileExtension) {
+		return scriptLanguageIndex.getByFileExtension(fileExtension);
+	}
+
+	@Override
+	public ScriptEngineFactory getByName(final String name) {
+		return scriptLanguageIndex.getByName(name);
+	}
+
+	@Override
+	public Object eval(final File file) throws FileNotFoundException,
+		ScriptException
+	{
+		final String fileExtension = FileUtils.getExtension(file);
+		final ScriptEngineFactory language = getByFileExtension(fileExtension);
+		if (language == null) {
+			throw new UnsupportedOperationException(
+				"Could not determine language for file extension " + fileExtension);
+		}
+		return language.getScriptEngine().eval(new FileReader(file));
+	}
+
+	public void reloadScriptLanguages() {
+		scriptLanguageIndex.clear();
+		for (final PluginInfo<? extends ScriptLanguage> item : pluginService.getPluginsOfType(ScriptLanguage.class))
+		{
+			try {
+				final ScriptEngineFactory language = item.createInstance();
+				scriptLanguageIndex.add(language, false);
+			} catch (InstantiableException e) {
+				log.error("Invalid script language: " + item, e);
+			}
+		}
+	}
+
+}
---
+++ core/core/src/main/java/imagej/script/ScriptLanguageIndex.java
@@ -0,0 +1,104 @@
+//
+// ScriptLanguageIndex.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.script;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import javax.script.ScriptEngineFactory;
+
+/**
+ * Data structure for managing registered script languages.
+ * 
+ * @author Johannes Schindelin
+ */
+public class ScriptLanguageIndex extends HashSet<ScriptEngineFactory> {
+
+	private static final long serialVersionUID = 1L;
+
+	private final Map<String, ScriptEngineFactory> byFileExtension =
+		new HashMap<String, ScriptEngineFactory>();
+
+	private final Map<String, ScriptEngineFactory> byName =
+		new HashMap<String, ScriptEngineFactory>();
+
+	@Override
+	public boolean add(final ScriptEngineFactory language) {
+		return add(language, false);
+	}
+
+	public boolean add(final ScriptEngineFactory language, final boolean gently) {
+		for (final String fileExtension : language.getExtensions()) {
+			if ("".equals(fileExtension)) continue;
+			if (byFileExtension.containsKey(fileExtension)) {
+				if (gently) continue;
+				final String previous =
+					byFileExtension.get(fileExtension).getEngineName();
+				throw new IllegalArgumentException(
+					"Duplicate script languages for file extension " + fileExtension +
+						": " + language.getEngineName() + " vs " + previous);
+			}
+			byFileExtension.put(fileExtension, language);
+		}
+
+		final String name = language.getLanguageName();
+		if (byName.containsKey(name)) {
+			if (gently) return false;
+			final String previous = byName.get(name).getEngineName();
+			throw new IllegalArgumentException(
+				"Duplicate script languages for name " + name + ": " +
+					language.getEngineName() + " vs " + previous);
+		}
+		byName.put(name, language);
+
+		return super.add(language);
+	}
+
+	public ScriptEngineFactory getByFileExtension(final String fileExtension) {
+		return byFileExtension.get(fileExtension);
+	}
+
+	public ScriptEngineFactory getByName(final String name) {
+		return byName.get(name);
+	}
+
+	public String[] getFileExtensions(final ScriptEngineFactory language) {
+		final List<String> extensions = language.getExtensions();
+		return extensions.toArray(new String[extensions.size()]);
+	}
+
+}
