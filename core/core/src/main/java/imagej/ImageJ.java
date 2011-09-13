8f379c3275c785c8d1cc8f078c24556dcd8c139e
Curtis Rueden <ctrueden@wisc.edu>
2011-09-13 16:23:58 -0500

    Update version number to 2.0.0-alpha5.
    This used to be revision r3871.

--- core/core/src/main/java/imagej/ImageJ.java
+++ core/core/src/main/java/imagej/ImageJ.java
@@ -52,7 +52,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ImageJ {
 
 	/** Version of the ImageJ software. */
-	public static final String VERSION = "2.0.0-alpha4";
+	public static final String VERSION = "2.0.0-alpha5";
 
 	/** Table of ImageJ application contexts. */
 	private static Map<Integer, ImageJ> contexts =
