027f46dd2b4ed85a8709fdd81cf0864e6ed21476
Curtis Rueden <ctrueden@wisc.edu>
2011-09-22 22:50:42 -0500

    Simplify pixel probe implementation.
    No working variables are kept, so references cannot go stale or cause
    memory leaks. Hence, tracking dataset events is no longer required.
    
    The probe is still off by one though, due to the fact that JHotDraw adds
    a one-pixel border around its image figures.
    
    This used to be revision r3974.

--- core/tools/src/main/java/imagej/core/tools/global/PixelProbe.java
+++ core/tools/src/main/java/imagej/core/tools/global/PixelProbe.java
@@ -34,29 +34,21 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package imagej.core.tools.global;
 
+import imagej.ImageJ;
 import imagej.data.Dataset;
 import imagej.data.Position;
 import imagej.data.display.DataView;
 import imagej.data.display.ImageCanvas;
 import imagej.data.display.ImageDisplay;
 import imagej.data.display.ImageDisplayService;
-import imagej.data.event.DatasetDeletedEvent;
-import imagej.data.event.DatasetRestructuredEvent;
-import imagej.data.event.DatasetUpdatedEvent;
 import imagej.event.EventService;
-import imagej.event.EventSubscriber;
-import imagej.event.Events;
 import imagej.event.StatusEvent;
 import imagej.ext.display.Display;
-import imagej.ext.display.MouseCursor;
 import imagej.ext.display.event.mouse.MsMovedEvent;
 import imagej.ext.tool.AbstractTool;
 import imagej.ext.tool.Tool;
 import imagej.util.IntCoords;
 import imagej.util.RealCoords;
-
-import java.util.ArrayList;
-
 import net.imglib2.RandomAccess;
 import net.imglib2.img.Axes;
 import net.imglib2.img.Img;
@@ -73,28 +65,14 @@ import net.imglib2.type.numeric.RealType;
 @Tool(name = "Probe", global = true)
 public class PixelProbe extends AbstractTool {
 
-	// -- private instance variables --
-
-	private Dataset dataset;
-	private RandomAccess<? extends RealType<?>> randomAccess;
-	private long[] position;
-	private ArrayList<EventSubscriber<?>> subscribers;
-	private int xAxis, yAxis;
-
-	// -- constructor --
-
-	public PixelProbe() {
-		subscribeToEvents();
-	}
-
 	// -- ITool methods --
 
 	@Override
 	public void onMouseMove(final MsMovedEvent evt) {
+		final ImageJ context = evt.getContext();
 		final ImageDisplayService imageDisplayService =
-			evt.getContext().getService(ImageDisplayService.class);
-		final EventService eventService =
-			evt.getContext().getService(EventService.class);
+			context.getService(ImageDisplayService.class);
+		final EventService eventService = context.getService(EventService.class);
 
 		final Display<?> display = evt.getDisplay();
 		if (!(display instanceof ImageDisplay)) return;
@@ -102,135 +80,54 @@ public class PixelProbe extends AbstractTool {
 
 		final ImageCanvas canvas = imageDisplay.getImageCanvas();
 		final IntCoords mousePos = new IntCoords(evt.getX(), evt.getY());
-		// mouse not in image ?
 		if (!canvas.isInImage(mousePos)) {
-			clearWorkingVariables();
-			eventService.publish(new StatusEvent(""));
+			eventService.publish(new StatusEvent(null));
+			return;
 		}
-		else { // mouse is over image
-			// CTR TODO - update tool to probe more than just the active view
-			final DataView activeView = imageDisplay.getActiveView();
-			final Dataset d = imageDisplayService.getActiveDataset(imageDisplay);
-			setWorkingVariables(d);
-			final RealCoords coords = canvas.panelToImageCoords(mousePos);
-			// Re: bug #639
-			// note - can't use getIntX() and getIntY() since they round and can
-			// take the integer coords out of the image bounds. Exceptions happen
-			// final int cx = (int) Math.floor(coords.x);
-			// final int cy = (int) Math.floor(coords.y);
-			// The previous attempt did not fix things. There is a scaling issue
-			// in the canvas that needs to be figured out.
-			final int cx = coords.getIntX();
-			final int cy = coords.getIntY();
-			final Position planePos = activeView.getPlanePosition();
-			fillCurrentPosition(position, cx, cy, planePos);
-			randomAccess.setPosition(position);
-			double doubleValue = 0;
-			// try {
-			doubleValue = randomAccess.get().getRealDouble();
-			// Re: bug #639
-			// } catch (Exception e) {
-			// System.out.println("Exception happened with position "+
-			// position[xAxis]+","+position[yAxis]);
-			// }
-			final String statusMessage;
-			if (dataset.isInteger()) {
-				statusMessage =
-					String.format("x=%d, y=%d, value=%d", cx, cy, (long) doubleValue);
-			}
-			else {
-				statusMessage =
-					String.format("x=%d, y=%d, value=%f", cx, cy, doubleValue);
-			}
-			eventService.publish(new StatusEvent(statusMessage));
-		}
-	}
 
-	@Override
-	public MouseCursor getCursor() {
-		return MouseCursor.CROSSHAIR;
+		// mouse is over image
+
+		// TODO - update tool to probe more than just the active view
+		final DataView activeView = imageDisplay.getActiveView();
+		final Dataset dataset = imageDisplayService.getActiveDataset(imageDisplay);
+
+		final RealCoords coords = canvas.panelToImageCoords(mousePos);
+		final int cx = coords.getIntX();
+		final int cy = coords.getIntY();
+
+		final Position planePos = activeView.getPlanePosition();
+
+		final Img<? extends RealType<?>> image = dataset.getImgPlus();
+		final RandomAccess<? extends RealType<?>> randomAccess =
+			image.randomAccess();
+		final int xAxis = dataset.getAxisIndex(Axes.X);
+		final int yAxis = dataset.getAxisIndex(Axes.Y);
+
+		setPosition(randomAccess, cx, cy, planePos, xAxis, yAxis);
+
+		final double value = randomAccess.get().getRealDouble();
+		final String message;
+		if (dataset.isInteger()) {
+			message = String.format("x=%d, y=%d, value=%d", cx, cy, (long) value);
+		}
+		else {
+			message = String.format("x=%d, y=%d, value=%f", cx, cy, value);
+		}
+		eventService.publish(new StatusEvent(message));
 	}
 
 	// -- private interface --
 
-	private void clearWorkingVariables() {
-		position = null;
-		randomAccess = null;
-		dataset = null;
-		xAxis = Integer.MIN_VALUE;
-		yAxis = Integer.MIN_VALUE;
-	}
-
-	private void setWorkingVariables(final Dataset d) {
-		if (d != dataset) {
-			clearWorkingVariables();
-			dataset = d;
-			final Img<? extends RealType<?>> image = d.getImgPlus();
-			randomAccess = image.randomAccess();
-			position = new long[image.numDimensions()];
-			randomAccess.localize(position);
-			xAxis = dataset.getAxisIndex(Axes.X);
-			yAxis = dataset.getAxisIndex(Axes.Y);
-		}
-	}
-
-	private void fillCurrentPosition(final long[] pos, final long x,
-		final long y, final Position planePos)
+	private void setPosition(
+		final RandomAccess<? extends RealType<?>> randomAccess, final int cx,
+		final int cy, final Position planePos, final int xAxis, final int yAxis)
 	{
-		int d = 0;
-		for (int i = 0; i < pos.length; i++) {
-			if (i == xAxis) pos[i] = x;
-			else if (i == yAxis) pos[i] = y;
-			else pos[i] = planePos.getLongPosition(d++);
+		int i = 0;
+		for (int d = 0; d < randomAccess.numDimensions(); d++) {
+			if (d == xAxis) randomAccess.setPosition(cx, d);
+			else if (d == yAxis) randomAccess.setPosition(cy, d);
+			else randomAccess.setPosition(planePos.dimension(i++), d);
 		}
-	}
-
-	@SuppressWarnings("synthetic-access")
-	private void subscribeToEvents() {
-
-		subscribers = new ArrayList<EventSubscriber<?>>();
-
-		// it is possible that underlying data is changed in such a way that
-		// probe gets out of sync. force a resync
-
-		final EventSubscriber<DatasetUpdatedEvent> updateSubscriber =
-			new EventSubscriber<DatasetUpdatedEvent>() {
-
-				@Override
-				public void onEvent(final DatasetUpdatedEvent event) {
-					if (event.getObject() == dataset) {
-						clearWorkingVariables();
-					}
-				}
-			};
-		subscribers.add(updateSubscriber);
-		Events.subscribe(DatasetUpdatedEvent.class, updateSubscriber);
-
-		final EventSubscriber<DatasetDeletedEvent> deleteSubscriber =
-			new EventSubscriber<DatasetDeletedEvent>() {
-
-				@Override
-				public void onEvent(final DatasetDeletedEvent event) {
-					if (event.getObject() == dataset) {
-						clearWorkingVariables();
-					}
-				}
-			};
-		subscribers.add(deleteSubscriber);
-		Events.subscribe(DatasetDeletedEvent.class, deleteSubscriber);
-
-		final EventSubscriber<DatasetRestructuredEvent> restructureSubscriber =
-			new EventSubscriber<DatasetRestructuredEvent>() {
-
-				@Override
-				public void onEvent(final DatasetRestructuredEvent event) {
-					if (event.getObject() == dataset) {
-						clearWorkingVariables();
-					}
-				}
-			};
-		subscribers.add(restructureSubscriber);
-		Events.subscribe(DatasetRestructuredEvent.class, restructureSubscriber);
 	}
 
 }
