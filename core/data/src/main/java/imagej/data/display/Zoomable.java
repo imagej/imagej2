e1d8ab4d38820079979cbec268b4b2031b905aa9
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-10-05 22:21:16 +0000

    Style and javadoc.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@4089 be23795f-a60e-40d4-ba19-055798872e57

--- core/data/src/main/java/imagej/data/display/Zoomable.java
+++ core/data/src/main/java/imagej/data/display/Zoomable.java
@@ -34,12 +34,10 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package imagej.data.display;
 
-import imagej.ext.display.Display;
 import imagej.util.IntCoords;
 
 /**
- * Defines methods needed to adjust the zoom of a {@link Display} or
- * {@link ImageCanvas}.
+ * Defines methods needed to adjust the zoom of an {@link ImageCanvas}.
  * 
  * @author Barry DeZonia
  * @author Grant Harris
