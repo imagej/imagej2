3026613f3d649ac3192f2d6558655b238affc580
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2011-10-26 16:50:20 +0000

    implemented the ability to drop files on the application window which are then opened in their own windows
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@4216 be23795f-a60e-40d4-ba19-055798872e57

--- ui/awt-swing/swing/ui-base/src/main/java/imagej/ui/swing/SwingDropListener.java
+++ ui/awt-swing/swing/ui-base/src/main/java/imagej/ui/swing/SwingDropListener.java
@@ -34,11 +34,34 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package imagej.ui.swing;
 
+import imagej.ImageJ;
+import imagej.event.EventService;
+import imagej.event.StatusEvent;
+import imagej.ext.plugin.PluginService;
+import imagej.io.plugins.OpenImage;
+
 import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
 import java.awt.dnd.DropTargetDragEvent;
 import java.awt.dnd.DropTargetDropEvent;
 import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+// NOTE - this code adapted from IJ1's DragAndDrop class
+//   Original contributor: Wayne Rasband
+//   Extensions for linux support: Albert Cardona 2006
 
 /**
  * SwingDropListener implements the drag and drop functionality of IJ2 that is
@@ -48,33 +71,153 @@ import java.awt.dnd.DropTargetListener;
  */
 public class SwingDropListener implements DropTargetListener {
 
+	// -- instance variables --
+	
+	private EventService eventService;
+
+	// -- constructor --
+	
+	public SwingDropListener() {
+		eventService = ImageJ.get(EventService.class);
+	}
+	
+	// -- public interface --
+	
 	@Override
 	public void dragEnter(DropTargetDragEvent dtde) {
-		System.out.println("drag enter");
+		eventService.publish(new StatusEvent("< <Drag and Drop> >"));
+		dtde.acceptDrag(DnDConstants.ACTION_COPY);
 	}
 
 	@Override
 	public void dragExit(DropTargetEvent dte) {
-		System.out.println("drag exit");
+		eventService.publish(new StatusEvent(""));
 	}
 
 	@Override
 	public void dragOver(DropTargetDragEvent dtde) {
-		System.out.println("drag over");
-	}
-
-	@Override
-	public void drop(DropTargetDropEvent dtde) {
-		System.out.println("drop");
-		System.out.println("  source = "+dtde.getSource());
-		for (DataFlavor flavor : dtde.getCurrentDataFlavors()) {
-			System.out.println("  flavor: "+flavor.getPrimaryType()+":"+flavor.getSubType()+" - "+flavor.getDefaultRepresentationClassAsString());
-		}
+		// do nothing
 	}
 
 	@Override
 	public void dropActionChanged(DropTargetDragEvent dtde) {
-		System.out.println("drop action change");
+		// do nothing
 	}
 	
+	@Override
+	public void drop(DropTargetDropEvent dtde) {
+		dtde.acceptDrop(DnDConstants.ACTION_COPY);
+		doDrop(dtde);
+	}
+
+	// -- private helpers --
+	
+	private void doDrop(DropTargetDropEvent dtde) {
+		try {
+			List<Object> inputs = buildInputs(dtde);
+			for (int i = 0; i < inputs.size(); i++) {
+				final Object input = inputs.get(i);
+				Thread thread = new Thread(new Runnable() {
+
+					@SuppressWarnings("synthetic-access")
+					@Override
+					public void run() {
+							if (input instanceof String)
+								openFile((String)input);
+							if (input instanceof File)
+								openFile((File)input);
+					}
+				});
+				thread.setPriority(Math.max(thread.getPriority()-1, Thread.MIN_PRIORITY));
+				thread.start();
+			}
+		}
+		catch(Exception e)  {
+			dtde.dropComplete(false);
+			return;
+		}
+		dtde.dropComplete(true);
+	}
+	
+	private List<Object> buildInputs(DropTargetDropEvent dtde) {
+		List<Object> inputs = new ArrayList<Object>();
+		Transferable t = dtde.getTransferable();
+		try {
+			for (DataFlavor flavor : t.getTransferDataFlavors()) {
+				if (flavor.isFlavorJavaFileListType()) {
+					Object data = t.getTransferData(DataFlavor.javaFileListFlavor);
+					@SuppressWarnings("unchecked")
+					List<Object> fileList = (List<Object>) data;
+					inputs.addAll(fileList);
+					break;
+				}
+				else if (flavor.isFlavorTextType()) {
+					Object obj = t.getTransferData(flavor);
+					if (!(obj instanceof String)) continue;
+					String s = obj.toString().trim();
+					if (isLinux() && (s.length()>1) && (s.charAt(1)==0))
+						s = fixLinuxString(s);
+					if (s.indexOf("href=\"")!=-1 || s.indexOf("src=\"")!=-1) {
+						s = parseHTML(s);
+						inputs.add(s);
+						break;
+					}
+					BufferedReader br = new BufferedReader(new StringReader(s));
+					String tmp;
+					while (null != (tmp = br.readLine())) {
+						tmp = java.net.URLDecoder.decode(tmp.replaceAll("\\+","%2b"), "UTF-8");
+						if (tmp.startsWith("file://")) tmp = tmp.substring(7);
+						if (tmp.startsWith("http://"))
+							inputs.add(s);
+						else
+							inputs.add(tmp);
+					}
+					break;
+				}
+			}
+		}
+		catch (UnsupportedFlavorException e) { /*do nothing */ }
+		catch (UnsupportedEncodingException e) { /*do nothing */ }
+		catch (IOException e) { /*do nothing */ }
+		
+		return inputs;
+	}
+	
+  private void openFile(File f) {
+  	Map<String,Object> params = new HashMap<String,Object>();
+  	params.put("inputFile",f);
+ 		ImageJ.get(PluginService.class).run(OpenImage.class, params);
+  }
+  
+  private void openFile(String filename) {
+  	File file = new File(filename);
+  	openFile(file);
+  }
+
+  private boolean isLinux() {
+		return System.getProperty("os.name").startsWith("Linux");
+  }
+  
+  private String fixLinuxString(String s) {
+  	StringBuffer sb = new StringBuffer(200);
+  	for (int i=0; i<s.length(); i+=2)
+  		sb.append(s.charAt(i));
+  	return new String(sb);
+  }
+
+  private String parseHTML(String s) {
+  	int index1 = s.indexOf("src=\"");
+  	if (index1>=0) {
+  		int index2 = s.indexOf("\"", index1+5);
+  		if (index2>0)
+  			return s.substring(index1+5, index2);
+  	}
+  	index1 = s.indexOf("href=\"");
+  	if (index1>=0) {
+  		int index2 = s.indexOf("\"", index1+6);
+  		if (index2>0)
+  			return s.substring(index1+6, index2);
+  	}
+  	return s;
+  }
 }
