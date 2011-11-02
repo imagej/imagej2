edfc1ebb299203d84c920033d283bcf328d56653
bdezonia <bdezonia@be23795f-a60e-40d4-ba19-055798872e57>
2011-11-02 16:52:54 +0000

    Commit last state of transition code towards more fully supported drop support. these changes were very rudimentary and the plan was to move them elsewhere. shelved as part of the hatching of ticket #860. note that the change to pom.xml is adding a dependency on ij-io that allows images to be loaded. the code currently tries to open all dropped files as images. there is tested inactive code that handles opening as text images and inactive partially tested code for handling opening LUT files. TODO - remove ij-io dependency from pom.xml when refactor to support #860 gets done.
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@4249 be23795f-a60e-40d4-ba19-055798872e57

--- ui/awt-swing/swing/ui-base/src/main/java/imagej/ui/swing/SwingDropListener.java
+++ ui/awt-swing/swing/ui-base/src/main/java/imagej/ui/swing/SwingDropListener.java
@@ -35,10 +35,16 @@ POSSIBILITY OF SUCH DAMAGE.
 package imagej.ui.swing;
 
 import imagej.ImageJ;
+import imagej.data.display.DatasetView;
+import imagej.data.display.ImageDisplay;
+import imagej.data.display.ImageDisplayService;
 import imagej.event.EventService;
 import imagej.event.StatusEvent;
 import imagej.ext.plugin.PluginService;
+import imagej.io.plugins.NewImage;
 import imagej.io.plugins.OpenImage;
+import imagej.ui.OutputWindow;
+import imagej.ui.UIService;
 
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
@@ -50,18 +56,21 @@ import java.awt.dnd.DropTargetEvent;
 import java.awt.dnd.DropTargetListener;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
+import net.imglib2.display.ColorTable8;
 
 
 // NOTE - this code adapted from IJ1's DragAndDrop class
 //   Original contributor: Wayne Rasband
-//   Extensions for linux support: Albert Cardona 2006
 
 /**
  * SwingDropListener implements the drag and drop functionality of IJ2 that is
@@ -74,11 +83,13 @@ public class SwingDropListener implements DropTargetListener {
 	// -- instance variables --
 	
 	private EventService eventService;
+	private DropHandler dropHandler;
 
 	// -- constructor --
 	
 	public SwingDropListener() {
 		eventService = ImageJ.get(EventService.class);
+		dropHandler = new DropHandler();
 	}
 	
 	// -- public interface --
@@ -122,10 +133,7 @@ public class SwingDropListener implements DropTargetListener {
 					@SuppressWarnings("synthetic-access")
 					@Override
 					public void run() {
-							if (input instanceof String)
-								openFile((String)input);
-							if (input instanceof File)
-								openFile((File)input);
+						dropHandler.handleInput(input);
 					}
 				});
 				thread.setPriority(Math.max(thread.getPriority()-1, Thread.MIN_PRIORITY));
@@ -180,19 +188,8 @@ public class SwingDropListener implements DropTargetListener {
 		
 		return inputs;
 	}
-	
-  private void openFile(File f) {
-  	Map<String,Object> params = new HashMap<String,Object>();
-  	params.put("inputFile",f);
- 		ImageJ.get(PluginService.class).run(OpenImage.class, params);
-  }
-  
-  private void openFile(String filename) {
-  	File file = new File(filename);
-  	openFile(file);
-  }
 
-  private String parseHTML(String s) {
+	private String parseHTML(String s) {
   	int index1 = s.indexOf("src=\"");
   	if (index1>=0) {
   		int index2 = s.indexOf("\"", index1+5);
@@ -207,4 +204,149 @@ public class SwingDropListener implements DropTargetListener {
   	}
   	return s;
   }
+
+	// TODO -- RELOCATE THE FOLLOWING CODE TO VARIOUS DropHandler implementations
+	// later. They should be sezpoz discoverable. This code is a partial HACK
+	// that allows beta1's drag and drop to actually do something. See ticket
+	// #860.
+	
+	private class DropHandler {
+		
+		public DropHandler() {
+			// nothing to do
+		}
+		
+		public void handleInput(Object input) {
+			if (input instanceof String)
+				handleInputAsFileName((String) input);
+			if (input instanceof File)
+				handleInputAsFile((File) input);
+		}
+		
+	  private void handleInputAsFileName(String filename) {
+	  	// is it a LUT?
+	  	if (filename.toLowerCase().endsWith(".lut")) {
+	  		importLut(filename);
+	  		return;
+	  	}
+	  	
+	  	// default case
+	  	openAsTextFile(filename);
+	  }
+
+	  private void handleInputAsFile(File file) {
+			String filename = file.getAbsolutePath();
+			if (isKnownImageType(filename))
+				loadImage(file);
+			else {
+				handleInputAsFileName(filename);
+			}
+	  }
+	  
+	  private boolean isKnownImageType(String filename) {
+	  	/*
+	  	 * way that relies on BioFormats : avoid
+	  	 * 
+	  	IFormatReader reader = null;
+	  	try {
+	  		reader = ImgOpener.createReader(filename, false);
+	  	} catch (FormatException e) {
+	  		// fall through
+	  	} catch (IOException e) {
+	  		// fall through
+	  	}
+	  	return reader != null;
+	  	 *
+	  	 */
+	  	// TODO - actually do something
+	  	return true;  // always open as image
+	  	// return false;  // always open as LUT or TEXT
+	  }
+	  
+	  private void loadImage(File f) {
+	  	Map<String,Object> params = new HashMap<String,Object>();
+	  	params.put("inputFile",f);
+	 		ImageJ.get(PluginService.class).run(OpenImage.class, params);
+	  }
+
+	  private void openAsTextFile(String filename) {
+	  	String title = shortName(filename);
+			List<String> fileContents = loadFileContents(filename);
+			OutputWindow window = ImageJ.get(UIService.class).createOutputWindow(title);
+			for (String line : fileContents)
+				window.append(line + '\n');
+			window.setVisible(true);
+	  }
+	  
+		private void importLut(String filename) {
+			List<ImageDisplay> imageDisplays =
+					ImageJ.get(ImageDisplayService.class).getImageDisplays();
+			if (imageDisplays.size() == 0) {
+				createSmallRampedImage(filename);
+				// TODO TEMP HACK pause long enough so active image is set
+				try {
+					Thread.sleep(2500);
+				} catch (Exception e) {/**/}
+			}
+			applyLutToActiveImage(filename);
+		}
+
+		private void applyLutToActiveImage(String filename) {
+			ImageDisplay display = ImageJ.get(ImageDisplayService.class).getActiveImageDisplay();
+			ColorTable8 colorTable = loadColorTable(filename);
+			DatasetView view = (DatasetView) display.getActiveView();
+			// TODO - broken - THESE NEXT TWO LINES SEEM TO HAVE NO EFFECT
+			view.setColorTable(colorTable, 0);
+			view.update();
+		}
+		
+		private List<String> loadFileContents(String filename) {
+			List<String> contents = new LinkedList<String>();
+			try {
+				FileReader fileReader = new FileReader(filename);
+				BufferedReader reader = new BufferedReader(fileReader);
+				while (reader.ready()) {
+					contents.add(reader.readLine());
+				}
+			}
+			catch (Exception e) {
+				// do nothing
+			}
+			return contents;
+		}
+		
+		private ColorTable8 loadColorTable(String filename) {
+			// TODO do something sensible by loading ColorTable from
+			// .lut file
+			byte[] reds = new byte[256];
+			byte[] greens = new byte[256];
+			byte[] blues = new byte[256];
+			for (int i = 0; i < 256; i++) {
+				reds[i] = (byte) i;
+				greens[i] = (byte)(255 - i/2);
+				blues[i] = (byte) (i * 0.8);
+			}
+			return new ColorTable8(reds,greens,blues);
+		}
+		
+		private void createSmallRampedImage(String filename) {
+	  	Map<String,Object> params = new HashMap<String,Object>();
+	  	params.put("name",shortName(filename));
+	  	params.put("bitDepth",NewImage.DEPTH8);
+	  	params.put("signed",false);
+	  	params.put("floating",false);
+	  	params.put("fillType",NewImage.RAMP);
+	  	params.put("width",256L);
+	  	params.put("height",50L);
+	 		ImageJ.get(PluginService.class).run(NewImage.class, params);
+		}
+		
+	  private String shortName(String filename) {
+	  	String shortname = filename;
+	  	int lastSlash = filename.lastIndexOf(File.separatorChar);
+	  	if (lastSlash >= 0)
+	  		shortname = filename.substring(lastSlash+1);
+	  	return shortname;
+	  }
+	}
 }
