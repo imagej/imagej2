ec2a5ca98f22eb59fc3ac444d8b8313f0e8e27c8
Curtis Rueden <ctrueden@wisc.edu>
2011-11-11 13:48:31 -0600

    Add more convenient ways to use the EventService:
      * There are now subscribe and unsubscribe methods that do not require
        the (redundant) Class object, but only the relevant EventSubscriber.
      * There are now subscribe and unsubscribe methods for processing a
        list of EventSubscribers (of unknown type) en masse.
      * There is now a subscribe method that takes any Object, and
        creates EventSubscriber instances on the fly based on that Object's
        methods annotated with a new EventHandler annotation. This allows a
        class to more conveniently implement event handlers for several
        types of events.
    
    This used to be revision r4364.

--- core/core/src/main/java/imagej/event/EventService.java
+++ core/core/src/main/java/imagej/event/EventService.java
@@ -37,7 +37,11 @@ package imagej.event;
 import imagej.AbstractService;
 import imagej.ImageJ;
 import imagej.Service;
+import imagej.util.Log;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bushe.swing.event.SwingEventService;
@@ -72,6 +76,27 @@ public final class EventService extends AbstractService {
 		eventBus.publish(e);
 	}
 
+	public <E extends ImageJEvent> void subscribe(
+		final EventSubscriber<E> subscriber)
+	{
+		final Class<E> c = getEventClass(subscriber);
+		subscribe(c, subscriber);
+	}
+
+	public <E extends ImageJEvent> void subscribeStrongly(
+		final EventSubscriber<E> subscriber)
+	{
+		final Class<E> c = getEventClass(subscriber);
+		subscribeStrongly(c, subscriber);
+	}
+
+	public <E extends ImageJEvent> void unsubscribe(
+		final EventSubscriber<E> subscriber)
+	{
+		final Class<E> c = getEventClass(subscriber);
+		unsubscribe(c, subscriber);
+	}
+
 	public <E extends ImageJEvent> void subscribe(final Class<E> c,
 		final EventSubscriber<E> subscriber)
 	{
@@ -88,6 +113,55 @@ public final class EventService extends AbstractService {
 		final EventSubscriber<E> subscriber)
 	{
 		eventBus.unsubscribe(c, subscriber);
+	}
+
+	public void subscribe(final List<EventSubscriber<?>> subscribers) {
+		for (final EventSubscriber<?> subscriber : subscribers) {
+			subscribe(subscriber);
+		}
+	}
+
+	public void subscribeStrongly(final List<EventSubscriber<?>> subscribers) {
+		for (final EventSubscriber<?> subscriber : subscribers) {
+			subscribeStrongly(subscriber);
+		}
+	}
+
+	public void unsubscribe(final List<EventSubscriber<?>> subscribers) {
+		for (final EventSubscriber<?> subscriber : subscribers) {
+			unsubscribe(subscriber);
+		}
+	}
+
+	/**
+	 * Subscribes all of the given object's @{@link EventHandler} annotated
+	 * methods. This allows a single class to subscribe to multiple types of
+	 * events by implementing multiple event handling methods and annotating each
+	 * one with @{@link EventHandler}.
+	 * 
+	 * @return The list of newly created {@link EventSubscriber}s, weakly
+	 *         subscribed to the event service. These objects must not be allowed
+	 *         to fall out of scope or they will be garbage collected (in which
+	 *         case events will not be delivered to them!).
+	 */
+	public List<EventSubscriber<?>> subscribe(final Object o) {
+		final List<EventSubscriber<?>> subscribers =
+			new ArrayList<EventSubscriber<?>>();
+
+		for (final Method m : o.getClass().getDeclaredMethods()) {
+			final EventHandler ann = m.getAnnotation(EventHandler.class);
+			if (ann == null) continue; // not an event handler method
+
+			final Class<? extends ImageJEvent> eventClass = getEventClass(m);
+			if (eventClass == null) {
+				Log.warn("Invalid EventHandler method: " + m);
+				continue;
+			}
+
+			subscribers.add(subscribe(eventClass, o, m));
+		}
+
+		return subscribers;
 	}
 
 	public <E extends ImageJEvent> List<EventSubscriber<E>> getSubscribers(
@@ -114,6 +188,81 @@ public final class EventService extends AbstractService {
 		// JHotDraw partially repaints images before they are done being processed.
 		// See ticket #719: http://dev.imagejdev.org/trac/imagej/ticket/719
 		eventBus = new SwingEventService();
+	}
+
+	// -- Helper methods --
+
+	private <E extends ImageJEvent> EventSubscriber<E> subscribe(
+		final Class<E> eventClass, final Object o, final Method m)
+	{
+		final EventMethodSubscriber<E> subscriber =
+			new EventMethodSubscriber<E>(o, m);
+		subscribe(eventClass, subscriber);
+		return subscriber;
+	}
+
+	/**
+	 * Gets the event class for the given {@link EventSubscriber}.
+	 * <p>
+	 * This method works by scanning for an appropriate <code>onEvent</code>
+	 * method via reflection, and extracting the {@link Class} of the argument.
+	 * </p>
+	 */
+	private <E extends ImageJEvent> Class<E> getEventClass(
+		final EventSubscriber<E> subscriber)
+	{
+		for (final Method m : subscriber.getClass().getDeclaredMethods()) {
+			if (!m.getName().equals("onEvent")) continue;
+			final Class<? extends ImageJEvent> eventClass = getEventClass(m);
+			@SuppressWarnings("unchecked")
+			final Class<E> typedClass = (Class<E>) eventClass;
+			return typedClass;
+		}
+		return null;
+	}
+
+	/** Gets the event class parameter of the given method. */
+	private Class<? extends ImageJEvent> getEventClass(final Method m) {
+		final Class<?>[] c = m.getParameterTypes();
+		if (c == null || c.length != 1) return null; // wrong number of args
+		if (!ImageJEvent.class.isAssignableFrom(c[0])) return null; // wrong class
+
+		@SuppressWarnings("unchecked")
+		final Class<? extends ImageJEvent> eventClass =
+			(Class<? extends ImageJEvent>) c[0];
+		return eventClass;
+	}
+
+	// -- Helper classes --
+
+	/** Helper class used by {@link #subscribe(Object)}. */
+	private class EventMethodSubscriber<E extends ImageJEvent> implements
+		EventSubscriber<E>
+	{
+
+		private final Object o;
+		private final Method m;
+
+		public EventMethodSubscriber(final Object o, final Method m) {
+			this.o = o;
+			this.m = m;
+		}
+
+		@Override
+		public void onEvent(final E event) {
+			try {
+				m.invoke(o, event);
+			}
+			catch (final IllegalArgumentException e) {
+				Log.error("Event handler threw exception", e);
+			}
+			catch (final IllegalAccessException e) {
+				Log.error("Event handler threw exception", e);
+			}
+			catch (final InvocationTargetException e) {
+				Log.error("Event handler threw exception", e);
+			}
+		}
 	}
 
 }
