2e46023a16ec93f712ee84278908aed5283308d7
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2011-11-23 21:30:21 +0000

    Tweak javadoc
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@4483 be23795f-a60e-40d4-ba19-055798872e57

--- core/tools/src/main/java/imagej/core/tools/AcceleratorHandler.java
+++ core/tools/src/main/java/imagej/core/tools/AcceleratorHandler.java
@@ -45,7 +45,7 @@ import imagej.ext.tool.AbstractTool;
 import imagej.ext.tool.Tool;
 
 /**
- * Handles keyboard accelerator combinations.
+ * Handles keyboard accelerator combinations that launch modules.
  * <p>
  * Specifically, we want to handle key presses even if the current UI's built-in
  * event handler would pass them up. For example, with the Swing UI, the menu
