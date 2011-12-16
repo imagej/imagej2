ab051d55c08c735489dd08faed6ba8cbe33c2088
Curtis Rueden <ctrueden@wisc.edu>
2011-12-16 15:13:49 -0600

    Update domain name in scripts
    This used to be revision r4638.

--- scripts/jenkins-git-job.sh
+++ scripts/jenkins-git-job.sh
@@ -7,12 +7,12 @@ test -d .git || {
 	git init &&
 	git config core.bare true &&
 	git remote add origin \
-		git@dev.imagejdev.org:imagej.git &&
+		git@code.imagej.net:imagej.git &&
 	git config remote.origin.fetch 'refs/heads/svn/*:refs/remotes/*' &&
 	git config remote.origin.push 'refs/remotes/*:refs/heads/svn/*' &&
 	git config --add remote.origin.push refs/remotes/trunk:refs/heads/master &&
 	git fetch
 }
-test -d .git/svn || git svn init -s http://dev.imagejdev.org/svn/imagej
+test -d .git/svn || git svn init -s http://code.imagej.net/svn/imagej
 git svn fetch
 git push
