ee41b101a43edc9655ecade978b1e6c121e31a66
Curtis Rueden <ctrueden@wisc.edu>
2011-12-21 15:32:06 -0600

    Add source code to combined imagej.jar
    At the moment this is done in a poor way, by complexifying the shell
    script. Ideally it could be done as part of the Maven assembly.
    
    This used to be revision r4660.

--- scripts/gen-jar-with-deps.sh
+++ scripts/gen-jar-with-deps.sh
@@ -8,9 +8,32 @@
 set -e
 
 ROOT=`cd "$(dirname $0)/.." ; pwd`
-cd $ROOT/ui/app
+EXTRA='src/main/assembly/all'
+
+cd "$ROOT/ui/app"
+
+# build individual JARs and copy dependencies
 mvn package dependency:copy-dependencies
-mkdir -p src/main/assembly/all/META-INF/annotations
+
+# combine SezPoz annotations
+mkdir -p "$EXTRA/META-INF/annotations"
 java -cp 'target/test-classes:target/classes:target/dependency/*' \
   imagej.util.CombineAnnotations
+
+# add source code
+# NB: This is a lame HACK because I am too stupid to figure out how to write a
+# proper Maven assembly descriptor that includes the source code of all IJ2
+# modules in the toplevel directory structure alongside the class files.
+cp -r \
+  ../../core/*/src/main/java/* \
+  ../../core/*/*/src/main/java/* \
+  ../../ui/*/src/main/java/* \
+  ../../ui/*/*/src/main/java/* \
+  "$EXTRA"
+
+# generate combined JAR file
 mvn -P deps,swing package
+
+# clean up
+rm $EXTRA/META-INF/annotations/imagej.*
+rm -rf "$EXTRA/imagej"
