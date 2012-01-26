1de84adf69f920b47d195eca01e916b91ec536a9
dscho <dscho@be23795f-a60e-40d4-ba19-055798872e57>
2012-01-26 21:49:45 +0000

    Jenkins-job: also push to GitHub and fiji.sc
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@4701 be23795f-a60e-40d4-ba19-055798872e57

--- scripts/jenkins-git-job.sh
+++ scripts/jenkins-git-job.sh
@@ -7,12 +7,23 @@ test -d .git || {
 	git init &&
 	git config core.bare true &&
 	git remote add origin \
-		git@code.imagej.net:imagej.git &&
-	git config remote.origin.fetch 'refs/heads/svn/*:refs/remotes/*' &&
-	git config remote.origin.push 'refs/remotes/*:refs/heads/svn/*' &&
-	git config --add remote.origin.push refs/remotes/trunk:refs/heads/master &&
-	git fetch
+	git@code.imagej.net:imagej.git &&
+	git remote add github imagej-github:imagej/imagej &&
+	git remote add fiji.sc hudson-imagej@fiji.sc:/srv/git/imagej2/.git &&
+	for remote in origin github fiji.sc
+	do
+		git config remote.$remote.fetch \
+			'refs/heads/svn/*:refs/remotes/*' &&
+		 git config remote.$remote.push \
+			 'refs/remotes/*:refs/heads/svn/*' &&
+		 git config --add remote.$remote.push \
+			 refs/remotes/trunk:refs/heads/master
+	 done &&
+	 git fetch
 }
 test -d .git/svn || git svn init -s http://code.imagej.net/svn/imagej
 git svn fetch
-git push
+for remote in origin github fiji.sc
+do
+        git push $remote
+done
