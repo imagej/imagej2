c192904b851bcddbdb26893ead8dfc4a42f8ce7a
dscho <dscho@be23795f-a60e-40d4-ba19-055798872e57>
2012-01-30 20:23:39 +0000

    Updater: Fix Checksummer.java's header
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@4739 be23795f-a60e-40d4-ba19-055798872e57

--- core/updater/src/main/java/imagej/updater/core/Checksummer.java
+++ core/updater/src/main/java/imagej/updater/core/Checksummer.java
@@ -1,3 +1,36 @@
+//
+// Checksummer.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
 
 package imagej.updater.core;
 
@@ -22,9 +55,8 @@ import java.util.Set;
 import java.util.TreeMap;
 import java.util.zip.ZipException;
 
-/*
- * Checksummer's overall role is to be in charge of building of a plugin
- * list for interface usage.
+/**
+ * A class to checksum all the files shown in the Updater's UI.
  *
  * 1st step: Get information of local plugins (checksums and version)
  * 2nd step: Given XML file, get information of latest updatable files (checksums
@@ -33,6 +65,9 @@ import java.util.zip.ZipException;
  *
  * digests and dates hold checksums and versions of local plugins respectively
  * latestDigests and latestDates hold checksums and versions of latest file versions
+ * 
+ * @author Johannes Schindelin
+ * @author Yap Chin Kiet
  */
 public class Checksummer extends Progressable {
 
