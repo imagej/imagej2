cfd4f1a7ef900d3c472d032783c7f75dc4776f6d
dscho <dscho@be23795f-a60e-40d4-ba19-055798872e57>
2012-01-30 20:25:24 +0000

    Updater: move the conflict resolution logic into updater-core
    Resolving issues is very critical before updating/uploading files. So
    it really needs to live in the core, the UI-specific part only should
    take care to display the conflicts properly.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@4822 be23795f-a60e-40d4-ba19-055798872e57

--- ui/awt-swing/swing/updater/src/main/java/imagej/updater/gui/ResolveDependencies.java
+++ ui/awt-swing/swing/updater/src/main/java/imagej/updater/gui/ResolveDependencies.java
@@ -34,21 +34,16 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package imagej.updater.gui;
 
-import imagej.updater.core.Dependency;
-import imagej.updater.core.FileObject;
-import imagej.updater.core.FileObject.Action;
+import imagej.updater.core.Conflicts;
+import imagej.updater.core.Conflicts.Conflict;
+import imagej.updater.core.Conflicts.Resolution;
 import imagej.updater.core.FilesCollection;
-import imagej.updater.core.FilesCollection.DependencyMap;
-import imagej.updater.util.Util;
 
 import java.awt.Color;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -62,17 +57,14 @@ import javax.swing.text.StyleConstants;
 @SuppressWarnings("serial")
 public class ResolveDependencies extends JDialog implements ActionListener {
 
-	UpdaterFrame updaterFrame;
-	JPanel rootPanel;
+	protected UpdaterFrame updaterFrame;
+	protected JPanel rootPanel;
 	public JTextPane panel; // this is public for debugging purposes
-	SimpleAttributeSet bold, indented, italic, normal, red;
-	JButton ok, cancel;
+	protected SimpleAttributeSet bold, indented, italic, normal, red;
+	protected JButton ok, cancel;
 
-	FilesCollection files;
-	DependencyMap toInstall, obsoleted;
-	Collection<FileObject> automatic, ignore;
-	int conflicts;
-	boolean forUpload, wasCanceled;
+	protected Conflicts conflicts;
+	protected boolean forUpload, wasCanceled;
 
 	public ResolveDependencies(final UpdaterFrame owner,
 		final FilesCollection files)
@@ -87,7 +79,7 @@ public class ResolveDependencies extends JDialog implements ActionListener {
 
 		updaterFrame = owner;
 		this.forUpload = forUpload;
-		this.files = files;
+		conflicts = new Conflicts(files);
 
 		rootPanel = SwingTools.verticalPanel();
 		setContentPane(rootPanel);
@@ -122,8 +114,6 @@ public class ResolveDependencies extends JDialog implements ActionListener {
 		SwingTools.addAccelerator(cancel, rootPanel, this, KeyEvent.VK_ESCAPE, 0);
 		SwingTools.addAccelerator(cancel, rootPanel, this, KeyEvent.VK_W, ctrl);
 		SwingTools.addAccelerator(ok, rootPanel, this, KeyEvent.VK_ENTER, 0);
-
-		ignore = new HashSet<FileObject>();
 	}
 
 	@Override
@@ -134,16 +124,8 @@ public class ResolveDependencies extends JDialog implements ActionListener {
 		}
 		else if (e.getSource() == ok) {
 			if (!ok.isEnabled()) return;
-			for (final FileObject file : automatic)
-				file.setFirstValidAction(files, new Action[] { Action.INSTALL,
-					Action.UPDATE, Action.UNINSTALL });
 			dispose();
 		}
-	}
-
-	void enableOKIfValid() {
-		ok.setEnabled(conflicts == 0);
-		if (ok.isEnabled()) ok.requestFocus();
 	}
 
 	public boolean resolve() {
@@ -153,15 +135,34 @@ public class ResolveDependencies extends JDialog implements ActionListener {
 		return !wasCanceled;
 	}
 
-	void listIssues() {
-		conflicts = 0;
+	protected void listIssues() {
 		panel.setText("");
 
-		automatic = new ArrayList<FileObject>();
-		if (forUpload) listUploadIssues();
-		else listUpdateIssues();
+		int count = 0;
+		for (final Conflict conflict : conflicts.getConflicts(forUpload)) {
+			count++;
+			maybeAddSeparator();
+			newText(conflict.isError() ? "Conflict: " : "Warning: ", conflict
+				.isError() ||
+				conflict.isCritical() ? red : normal);
+			addText(conflict.getFilename(), bold);
+			addText(conflict.getConflict());
+			addText("\n    ");
+			for (final Resolution resolution : conflict.getResolutions()) {
+				addButton(resolution.getDescription(), new ActionListener() {
 
-		enableOKIfValid();
+					@Override
+					public void actionPerformed(final ActionEvent e) {
+						resolution.resolve();
+						listIssues();
+					}
+				});
+			}
+		}
+
+		ok.setEnabled(count == 0);
+		if (ok.isEnabled()) ok.requestFocus();
+
 		if (isShowing()) {
 			if (panel.getStyledDocument().getLength() == 0) addText(
 				"No more issues to be resolved!", italic);
@@ -170,241 +171,31 @@ public class ResolveDependencies extends JDialog implements ActionListener {
 		}
 	}
 
-	void listUpdateIssues() {
-		toInstall = files.getDependencies(false);
-		obsoleted = files.getDependencies(true);
-
-		for (final FileObject file : toInstall.keySet())
-			if (obsoleted.get(file) != null) bothInstallAndUninstall(file);
-			else if (!file.willBeUpToDate()) {
-				if (file.isLocallyModified()) locallyModified(file);
-				else automatic.add(file);
-			}
-
-		for (final FileObject file : obsoleted.keySet())
-			if (toInstall.get(file) != null && // handled above
-				!file.willNotBeInstalled()) needUninstall(file);
-
-		if (automatic.size() > 0) {
-			maybeAddSeparator();
-			newText("These components will be updated/"
-				+ "installed automatically: \n\n");
-			addList(automatic);
-		}
-	}
-
-	void bothInstallAndUninstall(final FileObject file) {
-		maybeAddSeparator();
-		final FilesCollection reasons = FilesCollection.clone(toInstall.get(file));
-		reasons.addAll(obsoleted.get(file));
-		newText("Conflict: ", red);
-		addText(file.getFilename(), bold);
-		addText(" is required by\n\n");
-		addList(toInstall.get(file));
-		addText("\nbut made obsolete by\n\n");
-		addList(obsoleted.get(file));
-		addText("\n    ");
-		addIgnoreButton("Ignore this issue", file);
-		addText("    ");
-		addButton("Do not update " + reasons, reasons, null);
-	}
-
-	void needUninstall(final FileObject file) {
-		maybeAddSeparator();
-		final FilesCollection reasons = obsoleted.get(file);
-		newText("Conflict: ", red);
-		addText(file.getFilename(), bold);
-		addText(" is locally modified but made obsolete by\n\n");
-		addList(reasons);
-		addText("\n    ");
-		addButton("Uninstall " + file, file, Action.UNINSTALL);
-		addText("    ");
-		addButton("Do not update " + reasons, reasons, null);
-	}
-
-	void locallyModified(final FileObject file) {
-		if (ignore.contains(file)) return;
-		maybeAddSeparator();
-		newText("Warning: ");
-		addText(file.getFilename(), bold);
-		addText(" is locally modified and the Updater cannot determine its "
-			+ "status. A newer version might be required by\n\n");
-		addList(toInstall.get(file));
-		addText("\n    ");
-		addIgnoreButton("Keep the local version", file);
-		addText("    ");
-		final boolean toInstall = file.getStatus().isValid(Action.INSTALL);
-		addButton((toInstall ? "Install" : "Update") + " " + file, file, toInstall
-			? Action.INSTALL : Action.UPDATE);
-	}
-
-	void listUploadIssues() {
-		toInstall = new FilesCollection.DependencyMap();
-		for (final FileObject file : files.toUpload())
-			for (final Dependency dependency : file.getDependencies()) {
-				final FileObject dep = files.getFile(dependency.filename);
-				if (dep == null || ignore.contains(dep)) continue;
-				if (dep.isInstallable() ||
-					(dep.isLocalOnly() && dep.getAction() != Action.UPLOAD) ||
-					dep.isObsolete() ||
-					(dep.getStatus().isValid(Action.UPLOAD) && dep.getAction() != Action.UPLOAD)) toInstall
-					.add(dep, file);
-			}
-		for (final FileObject file : toInstall.keySet())
-			needUpload(file);
-
-		// Replace dependencies on to-be-removed files
-		for (final FileObject file : files.managedFiles()) {
-			if (file.getAction() == Action.REMOVE) continue;
-			for (final Dependency dependency : file.getDependencies())
-				if (files.getFile(dependency.filename) == null) dependencyNotUploaded(
-					file, dependency.filename);
-				else if (files.getFile(dependency.filename).getAction() == Action.REMOVE) dependencyRemoved(
-					file, dependency.filename);
-		}
-	}
-
-	void needUpload(final FileObject file) {
-		final boolean localOnly = file.isLocalOnly();
-		final boolean notInstalled = file.isInstallable();
-		final boolean obsolete = file.isObsolete();
-		final FilesCollection reasons = toInstall.get(file);
-		maybeAddSeparator();
-		newText("Warning: ", localOnly || obsolete ? red : normal);
-		addText(file.getFilename(), bold);
-		addText((localOnly ? "was not uploaded yet" : "is " +
-			(notInstalled ? "not installed" : (obsolete ? "marked obsolete"
-				: "locally modified"))) +
-			" but a dependency of\n\n");
-		addList(reasons);
-		addText("\n    ");
-		if (!localOnly && !obsolete) {
-			addIgnoreButton("Do not upload " + file, file);
-			addText("    ");
-		}
-		if (!notInstalled) {
-			addButton("Upload " + file + (obsolete ? " again" : ""), file,
-				Action.UPLOAD);
-			addText("    ");
-		}
-		addButton("Break the dependency", new ActionListener() {
-
-			@Override
-			public void actionPerformed(final ActionEvent e) {
-				for (final FileObject other : reasons)
-					other.removeDependency(file.getFilename());
-				listIssues();
-			}
-		});
-	}
-
-	void dependencyNotUploaded(final FileObject file, final String dependency) {
-		maybeAddSeparator();
-		newText("Error: ", normal);
-		addText(file.getFilename(), bold);
-		addText(" depends on " + dependency + " which was not uploaded.\n\n");
-		addDependencyButton("Break the dependency", file, dependency, null);
-	}
-
-	void dependencyRemoved(final FileObject file, final String dependency) {
-		maybeAddSeparator();
-		newText("Warning: ", normal);
-		addText(file.getFilename(), bold);
-		addText(" depends on " + dependency + " which is about to be removed.\n\n");
-		addDependencyButton("Break the dependency", file, dependency, null);
-		for (final FileObject toUpload : files.toUpload()) {
-			if (file.hasDependency(toUpload.getFilename())) continue;
-			addText("    ");
-			addDependencyButton("Replace with dependency to " + toUpload, file,
-				dependency, toUpload.getFilename());
-			addText("    ");
-			addDependencyButton("Replace all dependencies on " + dependency +
-				" with dependencies to " + toUpload, null, dependency, toUpload
-				.getFilename());
-		}
-	}
-
-	void addIgnoreButton(final String label, final FileObject file) {
-		addButton(label, new ActionListener() {
-
-			@Override
-			public void actionPerformed(final ActionEvent e) {
-				ignore.add(file);
-				listIssues();
-			}
-		});
-	}
-
-	void addDependencyButton(final String label, final FileObject file,
-		final String removeDependency, final String addDependency)
-	{
-		addButton(label, new ActionListener() {
-
-			void replaceDependency(final FileObject file) {
-				file.removeDependency(removeDependency);
-				if (addDependency != null) file.addDependency(addDependency, files
-					.prefix(addDependency));
-			}
-
-			@Override
-			public void actionPerformed(final ActionEvent e) {
-				if (file != null) replaceDependency(file);
-				else for (final FileObject file : files)
-					if (file.hasDependency(removeDependency)) replaceDependency(file);
-				listIssues();
-			}
-		});
-	}
-
-	void
-		addButton(final String label, final FileObject file, final Action action)
-	{
-		final Collection<FileObject> one = new ArrayList<FileObject>();
-		one.add(file);
-		addButton(label, one, action);
-	}
-
-	void addButton(final String label, final Collection<FileObject> files,
-		final Action action)
-	{
-		addButton(label, new ActionListener() {
-
-			@Override
-			public void actionPerformed(final ActionEvent e) {
-				for (final FileObject file : files)
-					if (action == null) file.setNoAction();
-					else file.setAction(ResolveDependencies.this.files, action);
-				listIssues();
-			}
-		});
-	}
-
-	void addButton(final String label, final ActionListener listener) {
-		conflicts++;
+	protected void addButton(final String label, final ActionListener listener) {
 		final JButton button = SwingTools.button(label, null, listener, null);
 		selectEnd();
 		panel.insertComponent(button);
 	}
 
-	void selectEnd() {
+	protected void selectEnd() {
 		final int end = panel.getStyledDocument().getLength();
 		panel.select(end, end);
 	}
 
-	void newText(final String message) {
+	protected void newText(final String message) {
 		newText(message, normal);
 	}
 
-	void newText(final String message, final SimpleAttributeSet style) {
+	protected void newText(final String message, final SimpleAttributeSet style) {
 		if (panel.getStyledDocument().getLength() > 0) addText("\n\n");
 		addText(message, style);
 	}
 
-	void addText(final String message) {
+	protected void addText(final String message) {
 		addText(message, normal);
 	}
 
-	void addText(final String message, final SimpleAttributeSet style) {
+	protected void addText(final String message, final SimpleAttributeSet style) {
 		final int end = panel.getStyledDocument().getLength();
 		try {
 			panel.getStyledDocument().insertString(end, message, style);
@@ -412,13 +203,6 @@ public class ResolveDependencies extends JDialog implements ActionListener {
 		catch (final BadLocationException e) {
 			e.printStackTrace();
 		}
-	}
-
-	void addList(final Collection<FileObject> list) {
-		addText(Util.join(", ", list) + "\n", bold);
-		final int end = panel.getStyledDocument().getLength();
-		panel.select(end - 1, end - 1);
-		panel.setParagraphAttributes(indented, true);
 	}
 
 	protected void maybeAddSeparator() {
