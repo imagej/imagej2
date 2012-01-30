8f39a0cd5c24c72e0b779f27b47f831eae83f70e
dscho <dscho@be23795f-a60e-40d4-ba19-055798872e57>
2012-01-30 20:25:34 +0000

    Updater: a file that is installed is not uploadable
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@4829 be23795f-a60e-40d4-ba19-055798872e57

--- core/updater/core/src/main/java/imagej/updater/core/FileObject.java
+++ core/updater/core/src/main/java/imagej/updater/core/FileObject.java
@@ -461,6 +461,7 @@ public class FileObject {
 	 * not check whether the file is locally modified.
 	 */
 	public boolean isUploadable(final FilesCollection files) {
+		if (getStatus() == Status.INSTALLED) return false;
 		if (updateSite == null) return files.hasUploadableSites();
 		final FilesCollection.UpdateSite updateSite =
 			files.getUpdateSite(this.updateSite);
