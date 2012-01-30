a9fdd337790800395eb101afd366ffc7fb1c3323
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-01-30 14:24:20 -0600

    Updater (platform check): normalize platform names a bit
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    This used to be revision r4775.

--- core/updater/core/src/main/java/imagej/updater/util/Util.java
+++ core/updater/core/src/main/java/imagej/updater/util/Util.java
@@ -125,6 +125,8 @@ public class Util {
 			if (name.startsWith("ImageJ-") || name.startsWith("fiji-")) {
 				name = name.substring(name.indexOf('-') + 1);
 				if (name.endsWith(".exe")) name = name.substring(0, name.length() - 4);
+				if (name.equals("tiger") || name.equals("panther")) name = "macosx";
+				else if (name.equals("linux")) name = "linux32";
 				updateablePlatforms.add(name);
 			}
 	}
