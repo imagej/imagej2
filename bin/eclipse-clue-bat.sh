bac3024c5fae6ded6121ecd4fc5b3d94865b2dc4
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-02-24 16:45:51 -0600

    eclipse-clue-bat: also write .settings/org.eclipse.jdt.apt.core.prefs
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    This used to be revision r4979.

--- bin/eclipse-clue-bat.sh
+++ bin/eclipse-clue-bat.sh
@@ -36,4 +36,15 @@ org.eclipse.jdt.core.compiler.source=1.6
 EOF
 		echo "Do not forget to commit $prefs even if it is ignored by Git"
 	}
+	prefs2=$directory/.settings/org.eclipse.jdt.apt.core.prefs
+	test -f $prefs2 || {
+		cat > $prefs2 << EOF
+#$(date)
+eclipse.preferences.version=1
+org.eclipse.jdt.apt.aptEnabled=true
+org.eclipse.jdt.apt.genSrcDir=target/classes
+org.eclipse.jdt.apt.reconcileEnabled=false
+EOF
+		echo "Do not forget to commit $prefs2 even if it is ignored by Git"
+	}
 done
