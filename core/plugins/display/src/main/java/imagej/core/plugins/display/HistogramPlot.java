e674a706a49a6cb35f4385d899d4479df84b36c3
curtis <curtis@be23795f-a60e-40d4-ba19-055798872e57>
2012-03-01 21:23:54 +0000

    Fix header of HistogramPlot
    git-svn-id: http://code.imagej.net/svn/imagej/trunk@5009 be23795f-a60e-40d4-ba19-055798872e57

--- core/plugins/display/src/main/java/imagej/core/plugins/display/HistogramPlot.java
+++ core/plugins/display/src/main/java/imagej/core/plugins/display/HistogramPlot.java
@@ -1,23 +1,23 @@
- //
+//
 // HistogramPlot.java
 //
 
 /*
 ImageJ software for multidimensional image processing and analysis.
 
-Copyright (c) 2011, ImageJDev.org.
+Copyright (c) 2010, ImageJDev.org.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
- * Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
- * Neither the names of the ImageJDev.org developers nor the
-names of its contributors may be used to endorse or promote products
-derived from this software without specific prior written permission.
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -30,7 +30,8 @@ INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
- */
+*/
+
 package imagej.core.plugins.display;
 
 import imagej.data.Dataset;
@@ -49,7 +50,6 @@ import imagej.ext.plugin.Plugin;
 import imagej.ui.DialogPrompt;
 import imagej.ui.UIService;
 import imagej.ui.UserInterface;
-
 
 import imagej.util.RealRect;
 import java.awt.BasicStroke;
@@ -78,24 +78,22 @@ import org.jfree.chart.renderer.xy.XYBarRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
+// Only operates on IntegerTypes
+//
+// TODO
+// + Selection of axes to include in histogram computation
+//
+// TODO Add these features from IJ1
+// [++] The horizontal LUT bar below the X-axis is scaled to reflect the display range of the image.
+// [++] The total pixel Count is also calculated and displayed, as well as the 
+// Mean, standard deviation (StdDev), minimum (Min), maximum (Max) and modal (Mode) gray value.
+//
+// This relies on Larry's Histogram...
+
 /**
  * Histogram plotter.
-
- * <p>
- * Only operates on IntegerTypes
- * </p>
  * 
- *  TODO
- *	+ Selection of axes to include in histogram computation
- * 
- * 	TODO Add these features from IJ1
- * [++] The horizontal LUT bar below the X-axis is scaled to reflect the display range of the image.
- * [++] The total pixel Count is also calculated and displayed, as well as the 
- * Mean, standard deviation (StdDev), minimum (Min), maximum (Max) and modal (Mode) gray value.
-
  * @author Grant Harris
- * 
- * This relies on Larry's Histogram...
  */
 @Plugin(menu = {
 	@Menu(label = "Analyze"),
