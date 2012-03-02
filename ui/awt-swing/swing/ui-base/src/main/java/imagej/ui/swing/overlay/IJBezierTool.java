e0137ad53bc4d887d9443ba36b4dd6550c2b5c84
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-03-02 17:48:37 -0600

    Add an adapter for JHotDraw's BezierTool
    We will use this for constructing polygon selections.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    This used to be revision r5037.

---
+++ ui/awt-swing/swing/ui-base/src/main/java/imagej/ui/swing/overlay/IJBezierTool.java
@@ -0,0 +1,114 @@
+//
+// IJBezierTool.java
+//
+
+/*
+ImageJ software for multidimensional image processing and analysis.
+
+Copyright (c) 2010, ImageJDev.org.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the names of the ImageJDev.org developers nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package imagej.ui.swing.overlay;
+
+import imagej.data.display.ImageDisplay;
+import imagej.data.display.OverlayView;
+import imagej.data.overlay.Overlay;
+import imagej.ui.swing.display.SwingOverlayView;
+
+import javax.swing.event.EventListenerList;
+
+import org.jhotdraw.draw.BezierFigure;
+import org.jhotdraw.draw.DrawingView;
+import org.jhotdraw.draw.Figure;
+import org.jhotdraw.draw.tool.BezierTool;
+
+/**
+ * TODO
+ * 
+ * @author Lee Kamentsky, Johannes Schindelin
+ */
+public class IJBezierTool extends BezierTool implements JHotDrawTool {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final ImageDisplay display;
+	private final IJHotDrawOverlayAdapter adapter;
+	private final EventListenerList listeners = new EventListenerList();
+
+	public IJBezierTool(final ImageDisplay display,
+		final IJHotDrawOverlayAdapter adapter,
+		final OverlayCreatedListener... listeners)
+	{
+		super((BezierFigure) adapter.createDefaultFigure());
+		this.display = display;
+		this.adapter = adapter;
+		for (final OverlayCreatedListener listener : listeners) {
+			addOverlayCreatedListener(listener);
+		}
+	}
+
+	public void addOverlayCreatedListener(final OverlayCreatedListener listener) {
+		listeners.add(OverlayCreatedListener.class, listener);
+	}
+
+	public void
+		removeOverlayCreatedListener(final OverlayCreatedListener listener)
+	{
+		listeners.remove(OverlayCreatedListener.class, listener);
+	}
+
+	protected void fireOverlayCreatedEvent(final OverlayView overlay,
+		final Figure figure)
+	{
+		final FigureCreatedEvent e = new FigureCreatedEvent(overlay, figure);
+		for (final OverlayCreatedListener listener : listeners
+			.getListeners(OverlayCreatedListener.class))
+		{
+			listener.overlayCreated(e);
+		}
+	}
+
+	@Override
+	protected BezierFigure createFigure() {
+		return (BezierFigure) adapter.createDefaultFigure();
+	}
+
+	@Override
+	protected void finishCreation(final BezierFigure figure,
+		final DrawingView drawingView)
+	{
+		super.finishCreation(figure, drawingView);
+		final Overlay overlay = adapter.createNewOverlay();
+		final SwingOverlayView view =
+			new SwingOverlayView(display, overlay, figure);
+		adapter.updateOverlay(figure, view);
+		fireOverlayCreatedEvent(view, figure);
+	}
+
+}
