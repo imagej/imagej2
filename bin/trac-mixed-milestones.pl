6f67a58479c8d10de3af73e30c8d323b28a4e283
Curtis Rueden <ctrueden@wisc.edu>
2012-03-05 14:28:55 -0600

    Show only mixed milestones in invalid combinations
    It is invalid for a ticket with one milestone to be blocked by a ticket
    from a later milestone, but not vice versa.
    
    Other improvements:
      * The script prints the first offending ticket related to a bad ticket
      * The script has a zero exit code only if all tickets are good
    
    This used to be revision r5046.

--- bin/trac-mixed-milestones.pl
+++ bin/trac-mixed-milestones.pl
@@ -28,59 +28,79 @@ unless (-e $db_path) {
   exit 2;
 }
 
+my $tickets = {};
+my $due = {};
+
 # query DB for ticket milestones
 my $milestone_script = 'select id, milestone from ticket;';
 my @milestones = `echo "$milestone_script" | sqlite3 "$db_path"`;
-
-# query DB for ticket blockedby list
-my $blockedby_script = "select ticket, value " .
-  "from ticket_custom where name = 'blockedby';";
-my @blockedby = `echo "$blockedby_script" | sqlite3 "$db_path"`;
-
-# pack results from the DB into a nice hash
-my $tickets = {};
 for my $token (@milestones) {
   chop $token;
   (my $id, my $milestone) = split(/\|/, $token);
   $tickets->{$id} = {};
   $tickets->{$id}->{'milestone'} = $milestone;
 }
+
+# query DB for ticket blockedby list
+my $blockedby_script = "select ticket, value " .
+  "from ticket_custom where name = 'blockedby';";
+my @blockedby = `echo "$blockedby_script" | sqlite3 "$db_path"`;
 for my $token (@blockedby) {
   chop $token;
   (my $id, my $blockedby) = split(/\|/, $token);
   $tickets->{$id}->{'blockedby'} = $blockedby;
 }
 
+# query DB for milestone due dates
+my $due_date_script = 'select name, due from milestone;';
+my @due_dates = `echo "$due_date_script" | sqlite3 "$db_path"`;
+for my $token (@due_dates) {
+  chop $token;
+  (my $milestone, my $due_date) = split(/\|/, $token);
+  $due->{$milestone} = $due_date;
+}
+
 # check tickets for mixed milestones
 my $good_ticket_count = 0;
 foreach my $id (sort {$a<=>$b} keys %$tickets) {
-  if (check($tickets, $id)) {
+  my $bid = check($tickets, $due, $id);
+  if ($bid == 0) {
     $good_ticket_count++;
   }
   else {
-    print "$id\n";
+    print "$id: $bid\n";
   }
 }
 my $total_ticket_count = keys %$tickets;
-print "\nGood tickets: $good_ticket_count of $total_ticket_count\n";
+if ($good_ticket_count < $total_ticket_count) {
+  print "\nGood tickets: $good_ticket_count of $total_ticket_count\n";
+  exit $good_ticket_count;
+}
+exit 0;
 
-sub check($$) {
+sub check($$$) {
   my $tickets = shift;
+  my $due = shift;
   my $id = shift;
 
   my $ticket = $tickets->{$id};
   my $milestone = $ticket->{'milestone'};
+  my $due_date = $due->{$milestone};
   my @blockedby = split(/, /, $ticket->{'blockedby'});
   for my $bid (@blockedby) {
-    if ($milestone ne $tickets->{$bid}->{'milestone'}) {
-      # milestone does not match child
-      return 0;
+    my $bid_milestone = $tickets->{$bid}->{'milestone'};
+    my $bid_due_date = $due->{$bid_milestone};
+    if ($due_date < $bid_due_date) {
+      # parent milestone precedes child milestone
+      return $bid;
     }
-    if (not check($tickets, $bid)) {
+    my $result = check($tickets, $due, $bid);
+    if ($result != 0) {
       # child has mixed milestones
-      return 0;
+      return $result;
     }
   }
 
-  return 1;
+  # all OK!
+  return 0;
 }
