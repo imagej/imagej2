c1757338efefc332d1d6ac2b65f4a24bef5646cc
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-03-04 23:54:46 -0600

    Factor out PointFigure into its own file
    While at it, rename it to 'SwingPointFigure' to point out that it relies
    on Swing.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ui/swing/tools/src/main/java/imagej/ui/swing/tools/overlay/SwingPointTool.java
+++ ui/swing/tools/src/main/java/imagej/ui/swing/tools/overlay/SwingPointTool.java
@@ -36,10 +36,8 @@
 package imagej.ui.swing.tools.overlay;
 
 import imagej.data.display.ImageDisplay;
-import imagej.data.display.OverlayService;
 import imagej.data.display.OverlayView;
 import imagej.data.overlay.Overlay;
-import imagej.data.overlay.OverlaySettings;
 import imagej.data.overlay.PointOverlay;
 import imagej.input.MouseCursor;
 import imagej.plugin.Plugin;
@@ -48,24 +46,8 @@ import imagej.ui.swing.overlay.IJCreationTool;
 import imagej.ui.swing.overlay.JHotDrawAdapter;
 import imagej.ui.swing.overlay.JHotDrawTool;
 import imagej.util.ColorRGB;
-import imagej.util.awt.AWTColors;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Point;
-import java.awt.Rectangle;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Point2D;
-import java.awt.geom.Rectangle2D;
-import java.util.Arrays;
-import java.util.List;
-
-import org.jhotdraw.draw.AbstractAttributedFigure;
-import org.jhotdraw.draw.AttributeKeys;
 import org.jhotdraw.draw.Figure;
-import org.jhotdraw.draw.handle.AbstractHandle;
-import org.jhotdraw.draw.handle.Handle;
-import org.jhotdraw.geom.Geom;
 
 /**
  * Swing/JHotDraw implementation of point tool.
@@ -84,7 +66,7 @@ public class SwingPointTool extends AbstractJHotDrawAdapter<PointOverlay> {
 	@Override
 	public boolean supports(final Overlay overlay, final Figure figure) {
 		if (!(overlay instanceof PointOverlay)) return false;
-		return figure == null || figure instanceof PointFigure;
+		return figure == null || figure instanceof SwingPointFigure;
 	}
 
 	@Override
@@ -94,7 +76,7 @@ public class SwingPointTool extends AbstractJHotDrawAdapter<PointOverlay> {
 
 	@Override
 	public Figure createDefaultFigure() {
-		final PointFigure figure = new PointFigure();
+		final SwingPointFigure figure = new SwingPointFigure(this);
 		initDefaultSettings(figure);
 		return figure;
 	}
@@ -102,8 +84,8 @@ public class SwingPointTool extends AbstractJHotDrawAdapter<PointOverlay> {
 	@Override
 	public void updateFigure(final OverlayView view, final Figure figure) {
 		super.updateFigure(view, figure);
-		assert figure instanceof PointFigure;
-		final PointFigure pointFigure = (PointFigure) figure;
+		assert figure instanceof SwingPointFigure;
+		final SwingPointFigure pointFigure = (SwingPointFigure) figure;
 		final Overlay overlay = view.getData();
 		assert overlay instanceof PointOverlay;
 		final PointOverlay pointOverlay = (PointOverlay) overlay;
@@ -114,8 +96,8 @@ public class SwingPointTool extends AbstractJHotDrawAdapter<PointOverlay> {
 
 	@Override
 	public void updateOverlay(final Figure figure, final OverlayView view) {
-		assert figure instanceof PointFigure;
-		final PointFigure point = (PointFigure) figure;
+		assert figure instanceof SwingPointFigure;
+		final SwingPointFigure point = (SwingPointFigure) figure;
 		final Overlay overlay = view.getData();
 		assert overlay instanceof PointOverlay;
 		final PointOverlay pointOverlay = (PointOverlay) overlay;
@@ -145,254 +127,6 @@ public class SwingPointTool extends AbstractJHotDrawAdapter<PointOverlay> {
 	@Override
 	public JHotDrawTool getCreationTool(final ImageDisplay display) {
 		return new IJCreationTool(display, this);
-	}
-
-	// -- Helper classes --
-
-	private class PointFigure extends AbstractAttributedFigure {
-
-		protected Rectangle2D.Double bounds;
-		private final Rectangle2D.Double rect;
-		private Color fillColor = Color.yellow;
-		private Color lineColor = Color.white;
-
-		/** Creates a new instance. */
-		public PointFigure() {
-			this(0, 0);
-		}
-
-		public PointFigure(final double x, final double y) {
-			bounds = new Rectangle2D.Double(x, y, 1, 1);
-			rect = new Rectangle2D.Double();
-		}
-
-		public void setPoint(final double x, final double y) {
-			bounds.x = x;
-			bounds.y = y;
-		}
-
-		public void setFillColor(final ColorRGB c) {
-			if (c == null) {
-				final OverlayService srv =
-					getContext().getService(OverlayService.class);
-				final OverlaySettings settings = srv.getDefaultSettings();
-				fillColor = AWTColors.getColor(settings.getFillColor());
-			}
-			else fillColor = AWTColors.getColor(c);
-		}
-
-		public void setLineColor(final ColorRGB c) {
-			if (c == null) {
-				final OverlayService srv =
-					getContext().getService(OverlayService.class);
-				final OverlaySettings settings = srv.getDefaultSettings();
-				lineColor = AWTColors.getColor(settings.getLineColor());
-			}
-			else lineColor = AWTColors.getColor(c);
-		}
-
-		public double getX() {
-			return bounds.x;
-		}
-
-		public double getY() {
-			return bounds.y;
-		}
-
-		// DRAWING
-		@Override
-		protected void drawFill(final Graphics2D g) {
-			final Rectangle2D.Double r = (Rectangle2D.Double) bounds.clone();
-			final double grow = AttributeKeys.getPerpendicularFillGrowth(this);
-			Geom.grow(r, grow, grow);
-			g.fill(r);
-		}
-
-		@Override
-		protected void drawStroke(final Graphics2D g) {
-			final Rectangle2D.Double r = (Rectangle2D.Double) bounds.clone();
-			final double grow = AttributeKeys.getPerpendicularDrawGrowth(this);
-			Geom.grow(r, grow, grow);
-			g.draw(r);
-		}
-
-		// SHAPE AND BOUNDS
-		@Override
-		public Rectangle2D.Double getBounds() {
-			final Rectangle2D.Double b = (Rectangle2D.Double) bounds.clone();
-			return b;
-		}
-
-		@Override
-		public Rectangle2D.Double getDrawingArea() {
-			final Rectangle2D.Double r = (Rectangle2D.Double) bounds.clone();
-			final double grow = AttributeKeys.getPerpendicularHitGrowth(this) + 1d;
-			Geom.grow(r, grow, grow);
-			return r;
-		}
-
-		/**
-		 * Checks if a Point2D.Double is inside the figure.
-		 */
-		@Override
-		public boolean contains(final Point2D.Double p) {
-			final Rectangle2D.Double r = (Rectangle2D.Double) bounds.clone();
-			final double grow = AttributeKeys.getPerpendicularHitGrowth(this) + 1d;
-			Geom.grow(r, grow, grow);
-			return r.contains(p);
-		}
-
-		@Override
-		public void
-			setBounds(final Point2D.Double anchor, final Point2D.Double lead)
-		{
-			bounds.x = anchor.x;
-			bounds.y = anchor.y;
-			bounds.width = 1;
-			bounds.height = 1;
-		}
-
-		/**
-		 * Moves the Figure to a new location.
-		 * 
-		 * @param tx the transformation matrix.
-		 */
-		@Override
-		public void transform(final AffineTransform tx) {
-			final Point2D.Double anchor = new Point2D.Double(bounds.x, bounds.y);
-			tx.transform(anchor, anchor);
-			setBounds(anchor, anchor);
-		}
-
-		@Override
-		public void restoreTransformTo(final Object geometry) {
-			bounds.setRect((Rectangle2D.Double) geometry);
-		}
-
-		@Override
-		public Object getTransformRestoreData() {
-			return bounds.clone();
-		}
-
-		@Override
-		public PointFigure clone() {
-			final PointFigure that = (PointFigure) super.clone();
-			that.bounds = (Rectangle2D.Double) this.bounds.clone();
-			return that;
-		}
-
-		@Override
-		public List<Handle> createHandles(final int detailLevel) {
-			final Handle handle = new PointHandle(this);
-			return Arrays.asList(handle);
-		}
-
-		/* scale invariant version but would be nice if we could avoid scale code
-		 * and work in pixels. */
-		@Override
-		public void draw(final Graphics2D g) {
-			final Color origC = g.getColor();
-			final double sx = g.getTransform().getScaleX();
-			final double sy = g.getTransform().getScaleY();
-			final double ctrX = getX();
-			final double ctrY = getY();
-
-			g.setColor(Color.black);
-
-			// black outline around center region
-			rect.x = ctrX - 2 / sx;
-			rect.y = ctrY - 2 / sy;
-			rect.width = 5 / sx;
-			rect.height = 5 / sy;
-			g.fill(rect);
-
-			g.setColor(fillColor);
-
-			// center region
-			rect.x = ctrX - 1 / sx;
-			rect.y = ctrY - 1 / sy;
-			rect.width = 3 / sx;
-			rect.height = 3 / sy;
-			g.fill(rect);
-
-			g.setColor(lineColor);
-
-			// tick mark line # 1
-			rect.x = ctrX + 3 / sx;
-			rect.y = ctrY;
-			rect.width = 4 / sx;
-			rect.height = 1 / sy;
-			g.fill(rect);
-
-			// tick mark line # 2
-			rect.x = ctrX - 6 / sx;
-			rect.y = ctrY;
-			rect.width = 4 / sx;
-			rect.height = 1 / sy;
-			g.fill(rect);
-
-			// tick mark line # 3
-			rect.x = ctrX;
-			rect.y = ctrY - 6 / sy;
-			rect.width = 1 / sx;
-			rect.height = 4 / sy;
-			g.fill(rect);
-
-			// tick mark line # 4
-			rect.x = ctrX;
-			rect.y = ctrY + 3 / sy;
-			rect.width = 1 / sx;
-			rect.height = 4 / sy;
-			g.fill(rect);
-
-			g.setColor(origC);
-		}
-
-	}
-
-	private class PointHandle extends AbstractHandle {
-
-		private final PointFigure figure;
-
-		private PointHandle(final PointFigure fig) {
-			super(fig);
-			figure = fig;
-		}
-
-		@Override
-		public void trackEnd(final Point anchor, final Point lead,
-			final int modifiers)
-		{
-			final double currX = figure.getX();
-			final double currY = figure.getY();
-			final double dx = lead.x - anchor.x;
-			final double dy = lead.y - anchor.y;
-			figure.setPoint(currX + dx, currY + dy);
-		}
-
-		@Override
-		public void trackStart(final Point anchor, final int modifiers) {
-			// do nothing
-		}
-
-		@Override
-		public void trackStep(final Point anchor, final Point lead,
-			final int modifiers)
-		{
-			// do nothing
-		}
-
-		@Override
-		protected Rectangle basicGetBounds() {
-			final Rectangle rect = new Rectangle();
-			final Rectangle2D.Double bounds = figure.getBounds();
-			rect.x = (int) bounds.x;
-			rect.y = (int) bounds.y;
-			rect.width = (int) bounds.width;
-			rect.height = (int) bounds.height;
-			return rect;
-		}
-
 	}
 
 }
