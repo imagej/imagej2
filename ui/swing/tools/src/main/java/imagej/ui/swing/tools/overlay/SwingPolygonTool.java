263c57612446f818dd59880f467f7d57f7a85ee8
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-03-04 23:55:33 -0600

    Factor out PolygonFigure into its own file
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- ui/swing/tools/src/main/java/imagej/ui/swing/tools/overlay/SwingPolygonTool.java
+++ ui/swing/tools/src/main/java/imagej/ui/swing/tools/overlay/SwingPolygonTool.java
@@ -46,21 +46,13 @@ import imagej.ui.swing.overlay.IJBezierTool;
 import imagej.ui.swing.overlay.JHotDrawAdapter;
 import imagej.ui.swing.overlay.JHotDrawTool;
 
-import java.awt.Point;
-import java.awt.event.InputEvent;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.LinkedList;
-
 import net.imglib2.RealLocalizable;
 import net.imglib2.RealPoint;
 import net.imglib2.roi.PolygonRegionOfInterest;
 
 import org.jhotdraw.draw.BezierFigure;
 import org.jhotdraw.draw.Figure;
-import org.jhotdraw.draw.handle.BezierNodeHandle;
-import org.jhotdraw.draw.handle.BezierOutlineHandle;
-import org.jhotdraw.draw.handle.Handle;
 import org.jhotdraw.geom.BezierPath.Node;
 
 /**
@@ -90,7 +82,7 @@ public class SwingPolygonTool extends AbstractJHotDrawAdapter<PolygonOverlay> {
 
 	@Override
 	public boolean supports(final Overlay overlay, final Figure figure) {
-		if (figure != null && !(figure instanceof PolygonFigure)) return false;
+		if (figure != null && !(figure instanceof SwingPolygonFigure)) return false;
 		return overlay instanceof PolygonOverlay;
 	}
 
@@ -102,7 +94,7 @@ public class SwingPolygonTool extends AbstractJHotDrawAdapter<PolygonOverlay> {
 
 	@Override
 	public Figure createDefaultFigure() {
-		final BezierFigure figure = new PolygonFigure();
+		final BezierFigure figure = new SwingPolygonFigure();
 		initDefaultSettings(figure);
 		return figure;
 	}
@@ -170,61 +162,6 @@ public class SwingPolygonTool extends AbstractJHotDrawAdapter<PolygonOverlay> {
 	@Override
 	public JHotDrawTool getCreationTool(final ImageDisplay display) {
 		return new IJBezierTool(display, this);
-	}
-
-	// -- Helper classes --
-
-	/**
-	 * The BezierFigure uses a BezierNodeHandle which can change the curve
-	 * connecting vertices from a line to a Bezier curve. We subclass both the
-	 * figure and the node handle to defeat this.
-	 */
-	public static class PolygonNodeHandle extends BezierNodeHandle {
-
-		public PolygonNodeHandle(final BezierFigure owner, final int index,
-			final Figure transformOwner)
-		{
-			super(owner, index, transformOwner);
-		}
-
-		public PolygonNodeHandle(final BezierFigure owner, final int index) {
-			super(owner, index);
-		}
-
-		@Override
-		public void trackEnd(final Point anchor, final Point lead,
-			final int modifiersEx)
-		{
-			// Remove the behavior associated with the shift keys
-			super.trackEnd(anchor, lead, modifiersEx &
-				~(InputEvent.META_DOWN_MASK | InputEvent.CTRL_DOWN_MASK |
-					InputEvent.ALT_DOWN_MASK | InputEvent.SHIFT_DOWN_MASK));
-		}
-
-	}
-
-	public static class PolygonFigure extends BezierFigure {
-
-		public PolygonFigure() {
-			// The constructor makes the BezierFigure a closed figure.
-			super(true);
-		}
-
-		@Override
-		public Collection<Handle> createHandles(final int detailLevel) {
-			final LinkedList<Handle> handles = new LinkedList<Handle>();
-			if (detailLevel != 0) {
-				return super.createHandles(detailLevel);
-			}
-			handles.add(new BezierOutlineHandle(this));
-			for (int i = 0, n = path.size(); i < n; i++) {
-				handles.add(new PolygonNodeHandle(this, i));
-			}
-			return handles;
-		}
-
-		private static final long serialVersionUID = 1L;
-
 	}
 
 }
