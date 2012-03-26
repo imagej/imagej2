1997978287c8fb2880964574b8a2b6816c6e46d9
Barry DeZonia <bdezonia@wisc.edu>
2012-03-26 16:28:04 -0500

    Create ConfigFileParameters class
    Breaking out private class from OptionsMemoryAndThreads
    
    This used to be revision r5135.

--- core/options/src/main/java/imagej/options/plugins/OptionsMemoryAndThreads.java
+++ core/options/src/main/java/imagej/options/plugins/OptionsMemoryAndThreads.java
@@ -34,28 +34,12 @@ POSSIBILITY OF SUCH DAMAGE.
 
 package imagej.options.plugins;
 
-import imagej.ImageJ;
+import imagej.config.ConfigFileParameters;
 import imagej.ext.menu.MenuConstants;
 import imagej.ext.plugin.Menu;
 import imagej.ext.plugin.Parameter;
 import imagej.ext.plugin.Plugin;
 import imagej.options.OptionsPlugin;
-import imagej.util.FileUtils;
-import imagej.util.Log;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * Runs the Edit::Options::Memory &amp; Threads dialog.
@@ -83,13 +67,9 @@ public class OptionsMemoryAndThreads extends OptionsPlugin {
 	@Parameter(label = "Run garbage collector on status bar click")
 	private boolean runGcOnClick = true;
 
-	// -- private static variables --
-
-	private static final String CONFIG_FILE = "ImageJ.cfg";
-	
 	// -- private instance variables --
 
-	private LauncherParams params = null;
+	private ConfigFileParameters params = new ConfigFileParameters();
 	
 	// -- OptionsMemoryAndThreads methods --
 
@@ -179,8 +159,6 @@ public class OptionsMemoryAndThreads extends OptionsPlugin {
 
 	/** loads the maxMemory instance variable from persistent storage */
 	private void loadMaxMemory() {
-		if (params == null)
-			params = new LauncherParams(getCfgFileName());
 		maxMemory = params.getMemoryInMB();
 	}
 	
@@ -188,250 +166,5 @@ public class OptionsMemoryAndThreads extends OptionsPlugin {
 	private void saveMaxMemory() {
 		if (maxMemory != params.getMemoryInMB())
 			params.setMemoryInMB(maxMemory);
-	}
-	
-	/** Finds the name/location of the launcher config file */
-	private String getCfgFileName() {
-		final File directory = FileUtils.getImageJDirectory();
-		return new File(directory, CONFIG_FILE).getAbsolutePath();
-	}
-
-	// TODO - break out private class into a publicly accessible one somewhere
-	
-	/**
-	 * This class reads launcher configuration parameters from a file and allows
-	 * them to be maintained.
-	 *  
-	 * @author Barry DeZonia
-	 */
-	private class LauncherParams {
-
-		// -- constants --
-		
-		private final Integer MINIMUM_MEMORY = 256;  // in megabytes
-		private static final String SENTINEL = "ImageJ startup properties";
-		private static final String MEMORY_KEY = "maxheap.mb";
-		private static final String JVMARGS_KEY = "jvmargs";
-
-		// -- private instance variables --
-		
-		private final Map<String,String> dataMap;
-		private final String filename;
-		
-		// -- constructor --
-
-		/** Constructs a LauncherParams object. Uses filename for loading/saving
-		 * it's values.
-		 */
-		public LauncherParams(String filename) {
-			this.dataMap = new HashMap<String,String>();
-			this.filename = filename;
-			initialize();
-		}
-		
-		// -- public interface --
-
-		/** Returns the value of the number of megabytes of ram to allocate that is
-		 * specified in the launcher config file. Will never return less than a
-		 * minimum number (currently 256).
-		 */
-		public int getMemoryInMB() {
-			final String memVal = dataMap.get(MEMORY_KEY);
-			Integer val = 0;
-			try {
-				val = Integer.parseInt(memVal);
-			} catch (NumberFormatException e) {
-				Log.warn("Launcher configuration file " + filename + " has key " + MEMORY_KEY +
-									" that is not in an integer format");
-			}
-			if (val < MINIMUM_MEMORY) val = MINIMUM_MEMORY;
-			return val;
-		}
-		
-		/** Sets the value of the number of megabytes of ram to allocate. Saves this
-		 * value in the launcher config file. Will not allow values less than a
-		 * minimum number (currently 256).
-		 */
-		public void setMemoryInMB(int numMegabytes) {
-			Integer memory = numMegabytes;
-			if (memory < MINIMUM_MEMORY) {
-				memory = MINIMUM_MEMORY;
-				Log.warn("Max Java heap size can be no smaller than "+MINIMUM_MEMORY+
-									" megabytes.");
-			}
-			dataMap.put(MEMORY_KEY, memory.toString());
-			save();
-		}
-
-		/** Initializes launcher config values. If possible loads values from
-		 * launcher config file. If launcher config file does not exist or is
-		 * outdated or faulty this method will save a valid set of parameters in
-		 * the launcher config file.
-		 */
-		public void initialize() {
-			setDefaultValues(dataMap);
-			final boolean needSave;
-			if (isLegacyConfigFile(filename)) {
-				loadLegacyConfigValues(dataMap, filename);
-				needSave = true;
-			}
-			else
-				needSave = !loadModernConfigValues(dataMap, filename);
-			if (needSave) save();
-		}
-
-		/** Saves current values to the launcher config file */
-		public void save() {
-			saveConfigValues(dataMap, filename);
-		}
-
-		// -- private helpers --
-
-		/** initializes launcher config file values to valid defaults */
-		private void setDefaultValues(Map<String,String> map) {
-			map.clear();
-			map.put(MEMORY_KEY, MINIMUM_MEMORY.toString());
-			map.put(JVMARGS_KEY, "");
-		}
-		
-		/** returns true if specified config file is an old legacy style launcher
-		 * config file */
-		private boolean isLegacyConfigFile(String fname) {
-			try {
-				final FileInputStream fstream = new FileInputStream(fname);
-			  final DataInputStream din = new DataInputStream(fstream);
-			  final InputStreamReader in = new InputStreamReader(din);
-			  final BufferedReader br = new BufferedReader(in);
-				final String firstLine = br.readLine();
-				in.close();
-				return !firstLine.contains(SENTINEL);
-			} catch (Exception e) {
-				return false;
-			}
-		}
-
-		/** loads launcher config file values from an old legacy style file */
-		private boolean loadLegacyConfigValues(Map<String,String> map, String fname) {
-			try {
-				final FileInputStream fstream = new FileInputStream(fname);
-			  final DataInputStream din = new DataInputStream(fstream);
-			  final InputStreamReader in = new InputStreamReader(din);
-			  final BufferedReader br = new BufferedReader(in);
-			  // ignore first line: a path ... something like "."
-				br.readLine();
-			  // ignore second line: path to java.exe
-				br.readLine();
-				// everything we want is on third line
-				final String argString = br.readLine();
-				in.close();
-				final Integer memSize = memorySize(argString);
-				final String jvmArgs = jvmArgs(argString);
-				map.put(MEMORY_KEY, memSize.toString());
-				map.put(JVMARGS_KEY, jvmArgs);
-				return true;
-			}
-			catch (Exception e) {
-				Log.warn("Could not load legacy launcher config file "+fname);
-				return false;
-			}
-		}
-		
-		/** loads launcher config file values from a modern IJ2 style file */
-		private boolean loadModernConfigValues(Map<String,String> map, String fname) {
-			try {
-				final FileInputStream fstream = new FileInputStream(fname);
-			  final DataInputStream din = new DataInputStream(fstream);
-			  final InputStreamReader in = new InputStreamReader(din);
-			  final BufferedReader br = new BufferedReader(in);
-				final Pattern keyValuePairPattern = Pattern.compile("\\s*(.*)\\s*=\\s*(.*)");
-			  // skip first line : sentinel
-			  br.readLine();
-			  while (br.ready()) {
-			  	final String s = br.readLine();
-			  	if (s.trim().startsWith("#")) continue;
-					final Matcher matcher = keyValuePairPattern.matcher(s);
-					if (matcher.matches()) {
-						final String key = matcher.group(1).trim();
-						final String value = matcher.group(2).trim();
-						map.put(key, value);
-					}
-					
-			  }
-				return true;
-			}
-			catch (IOException e) {
-				Log.warn("Could not load launcher config file "+fname);
-				return false;
-			}
-		}
-		
-		/** writes launcher config values to an IJ2 style launcher config file */
-		private void saveConfigValues(Map<String,String> map, String fname) {
-			try {
-			  final FileOutputStream fos = new FileOutputStream(fname);
-			  final OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF8");
-			  final BufferedWriter out = new BufferedWriter(osw);
-			  out.write("#"+SENTINEL + " ("+ImageJ.VERSION+")");
-		  	out.newLine();
-			  for (String key : map.keySet()) {
-			  	String value = map.get(key);
-			  	// make sure we don't write out something that breaks file structure
-			  	value = value.replaceAll("\n", "");
-			  	value = value.replaceAll("\r", "");
-			  	out.write(key + " = " + map.get(key));
-			  	out.newLine();
-			  }
-				out.close();
-			} catch (IOException e) {
-				Log.warn("Could not save launcher config file values to "+fname);
-			}
-		}
-
-		/** returns the number of megabytes specified in a text line from a legacy
-		 * launcher config file (3rd line).
-		 */
-		private int memorySize(String argList) {
-			// NB - In old IJ1 cfg files heap use is encoded as a single argument:
-			// 	"-XmxNNNNNNm" where N are numbers. 
-			// Find this argument and parse it
-			final String[] args = argList.split("\\s+");
-			for (String arg : args) {
-				if (arg.startsWith("-Xmx")) {
-					String numString = arg.substring(4);
-					numString = numString.substring(0,numString.length()-1);
-					try {
-						return Integer.parseInt(numString);
-					} catch (NumberFormatException e) {
-						return MINIMUM_MEMORY;
-					}
-				}
-			}
-			return MINIMUM_MEMORY;
-		}
-
-		/** returns a string containing all the command line arguments from
-		 * a legacy launcher config file (3rd line). Ignores memory specification
-		 * as that is handled by memorySize().
-		 */
-		private String jvmArgs(String argList) {
-			
-			// NB - From old IJ1 cfg we return the list of arguments except the heap
-			// specification (which we've handled elsewhere) and the name of the
-			// main class to launch (since ours will be different). 
-
-			String value = "";
-			final String[] args = argList.split("\\s+");
-			for (String arg : args) {
-				if (arg.startsWith("-Xmx")) continue;  // skip heap size specification
-				if (arg.equals("ij.ImageJ")) continue; // skip name of main class
-				// if here then we are interested in this parameter
-				if (value.length() > 0)
-					value += " ";
-				value += arg;
-				// TODO This last addition could include class path info. Is that a
-				// problem? We may not want the legacy file's class path info.
-			}
-			return value;
-		}
 	}
 }
