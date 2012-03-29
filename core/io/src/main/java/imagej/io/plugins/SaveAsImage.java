01844d903371f88df4963fe0e06e8562523c87c2
Mark Hiner <hiner@wisc.edu>
2012-03-29 16:43:03 -0500

    Added StatusUpdates to SaveAsImage
    The SaveAsImage plugin will now report status messages, following the
    same conventions as the OpenImage plugin.
    
    Note that SaveImage does not report statuses as it always delegates to
    SaveAsImage.
    
    This used to be revision r5180.

--- core/io/src/main/java/imagej/io/plugins/SaveAsImage.java
+++ core/io/src/main/java/imagej/io/plugins/SaveAsImage.java
@@ -36,6 +36,8 @@
 package imagej.io.plugins;
 
 import imagej.data.Dataset;
+import imagej.event.EventService;
+import imagej.event.StatusEvent;
 import imagej.ext.display.Display;
 import imagej.ext.menu.MenuConstants;
 import imagej.ext.module.ui.WidgetStyle;
@@ -43,6 +45,7 @@ import imagej.ext.plugin.ImageJPlugin;
 import imagej.ext.plugin.Menu;
 import imagej.ext.plugin.Parameter;
 import imagej.ext.plugin.Plugin;
+import imagej.io.event.FileSavedEvent;
 import imagej.ui.DialogPrompt;
 import imagej.ui.DialogPrompt.Result;
 import imagej.ui.UIService;
@@ -50,6 +53,7 @@ import imagej.util.Log;
 
 import java.io.File;
 
+import loci.common.StatusListener;
 import net.imglib2.exception.IncompatibleTypeException;
 import net.imglib2.img.ImgPlus;
 import ome.scifio.img.ImgIOException;
@@ -61,66 +65,90 @@ import ome.scifio.img.ImgSaver;
  * @author Mark Hiner
  */
 @Plugin(menu = {
-	@Menu(label = MenuConstants.FILE_LABEL, weight = MenuConstants.FILE_WEIGHT,
-		mnemonic = MenuConstants.FILE_MNEMONIC),
-	@Menu(label = "Save As...", weight = 21) })
-public class SaveAsImage implements ImageJPlugin {
+  @Menu(label = MenuConstants.FILE_LABEL, weight = MenuConstants.FILE_WEIGHT,
+    mnemonic = MenuConstants.FILE_MNEMONIC),
+    @Menu(label = "Save As...", weight = 21) })
+    public class SaveAsImage implements ImageJPlugin, StatusListener {
 
-	@Parameter(persist = false)
-	private UIService uiService;
+  @Parameter(persist = false)
+  private UIService uiService;
 
-	@Parameter(style = WidgetStyle.FILE_SAVE)
-	private File outputFile;
+  @Parameter(persist = false)
+  private EventService eventService;
 
-	@Parameter
-	private Dataset dataset;
+  @Parameter(style = WidgetStyle.FILE_SAVE)
+  private File outputFile;
 
-	@Parameter
-	private Display<?> display;
+  @Parameter
+  private Dataset dataset;
 
-	@Override
-	public void run() {
-		@SuppressWarnings("rawtypes")
-		final ImgPlus img = dataset.getImgPlus();
-		boolean overwrite = true;
+  @Parameter
+  private Display<?> display;
 
-		// TODO prompts the user if the file is dirty or being saved to a new
-		// location. Could remove the isDirty check to always overwrite the current
-		// file
-		if (outputFile.exists() &&
-			(dataset.isDirty() || !outputFile.getAbsolutePath().equals(
-				img.getSource())))
-		{
-			final Result result =
-				uiService.showDialog("\"" + outputFile.getName() +
-					"\" already exists. Do you want to replace it?", "Save [IJ2]",
-					DialogPrompt.MessageType.QUESTION_MESSAGE,
-					DialogPrompt.OptionType.YES_NO_OPTION);
-			overwrite = result == DialogPrompt.Result.YES_OPTION;
-		}
+  @Override
+  public void run() {
+    @SuppressWarnings("rawtypes")
+    final ImgPlus img = dataset.getImgPlus();
+    boolean overwrite = true;
 
-		if (overwrite) {
-			final ImgSaver imageSaver = new ImgSaver();
-			try {
-				imageSaver.saveImg(outputFile.getAbsolutePath(), img);
-			}
-			catch (final ImgIOException e) {
-				Log.error(e);
-				return;
-			}
-			catch (final IncompatibleTypeException e) {
-				Log.error(e);
-				return;
-			}
-			dataset.setName(outputFile.getName());
-			dataset.setDirty(false);
+    // TODO prompts the user if the file is dirty or being saved to a new
+    // location. Could remove the isDirty check to always overwrite the current
+    // file
+    if (outputFile.exists() &&
+      (dataset.isDirty() || !outputFile.getAbsolutePath().equals(
+        img.getSource())))
+    {
+      final Result result =
+        uiService.showDialog("\"" + outputFile.getName() +
+          "\" already exists. Do you want to replace it?", "Save [IJ2]",
+          DialogPrompt.MessageType.QUESTION_MESSAGE,
+          DialogPrompt.OptionType.YES_NO_OPTION);
+      overwrite = result == DialogPrompt.Result.YES_OPTION;
+    }
 
-			// TODO -- HACK -- setName() + update() currently doesn't work.
-			// Pending #995
-			display.getPanel().getWindow().setTitle(outputFile.getName());
-			display.setName(outputFile.getName());
-			display.update();
-		}
-	}
+    if (overwrite) {
+      final ImgSaver imageSaver = new ImgSaver();
+      try {
+        imageSaver.addStatusListener(this);
+        imageSaver.saveImg(outputFile.getAbsolutePath(), img);
+        eventService.publish(new FileSavedEvent(img.getSource()));
+      }
+      catch (final ImgIOException e) {
+        Log.error(e);
+        return;
+      }
+      catch (final IncompatibleTypeException e) {
+        Log.error(e);
+        return;
+      }
+      dataset.setName(outputFile.getName());
+      dataset.setDirty(false);
+
+      // TODO -- HACK -- setName() + update() currently doesn't work.
+      // Pending #995
+      display.getPanel().getWindow().setTitle(outputFile.getName());
+      display.setName(outputFile.getName());
+      display.update();
+    }
+  }
+
+  private long lastTime;
+
+  @Override
+  public void statusUpdated(final loci.common.StatusEvent e) {
+    final long time = System.currentTimeMillis();
+    final int progress = e.getProgressValue();
+    final int maximum = e.getProgressMaximum();
+    final String message = e.getStatusMessage();
+    final boolean warn = e.isWarning();
+
+    // don't update more than 20 times/sec
+    if (time - lastTime < 50 && progress > 0 && progress < maximum && !warn) {
+      return;
+    }
+    lastTime = time;
+
+    eventService.publish(new StatusEvent(progress, maximum, message, warn));
+  }
 
 }
