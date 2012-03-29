e2eace0f45ba9278418fce6905905cf007115527
Curtis Rueden <ctrueden@wisc.edu>
2012-03-29 16:22:34 -0500

    Apply Eclipse formatting rules
    This used to be revision r5176.

--- core/plugins/app/src/main/java/imagej/core/plugins/app/AboutImageJ.java
+++ core/plugins/app/src/main/java/imagej/core/plugins/app/AboutImageJ.java
@@ -35,6 +35,22 @@
 
 package imagej.core.plugins.app;
 
+import imagej.ImageJ;
+import imagej.data.ChannelCollection;
+import imagej.data.Dataset;
+import imagej.data.DatasetService;
+import imagej.data.DrawingTool;
+import imagej.data.DrawingTool.TextJustification;
+import imagej.ext.display.DisplayService;
+import imagej.ext.menu.MenuConstants;
+import imagej.ext.plugin.ImageJPlugin;
+import imagej.ext.plugin.Menu;
+import imagej.ext.plugin.Parameter;
+import imagej.ext.plugin.Plugin;
+import imagej.util.ColorRGB;
+import imagej.util.Colors;
+import imagej.util.Log;
+
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
@@ -53,21 +69,6 @@ import net.imglib2.meta.Axes;
 import net.imglib2.meta.AxisType;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
-import imagej.ImageJ;
-import imagej.data.ChannelCollection;
-import imagej.data.Dataset;
-import imagej.data.DatasetService;
-import imagej.data.DrawingTool;
-import imagej.data.DrawingTool.TextJustification;
-import imagej.ext.display.DisplayService;
-import imagej.ext.menu.MenuConstants;
-import imagej.ext.plugin.ImageJPlugin;
-import imagej.ext.plugin.Menu;
-import imagej.ext.plugin.Parameter;
-import imagej.ext.plugin.Plugin;
-import imagej.util.ColorRGB;
-import imagej.util.Colors;
-import imagej.util.Log;
 
 // TODO
 //   Have imageX.tif files and ImageX.metadata.txt files
@@ -79,8 +80,8 @@ import imagej.util.Log;
 //     attribution text
 
 /**
- * Display information and credits about the ImageJ2 software. Note that some
- * of this code was adapted from code written by Wayne Rasband for ImageJ.
+ * Display information and credits about the ImageJ2 software. Note that some of
+ * this code was adapted from code written by Wayne Rasband for ImageJ.
  * 
  * @author Barry DeZonia
  */
@@ -88,14 +89,15 @@ import imagej.util.Log;
 	@Menu(label = MenuConstants.HELP_LABEL, weight = MenuConstants.HELP_WEIGHT,
 		mnemonic = MenuConstants.HELP_MNEMONIC),
 	@Menu(label = "About ImageJ...", weight = 43) }, headless = true)
-public class AboutImageJ<T extends RealType<T> & NativeType<T>>
-	implements ImageJPlugin
+public class AboutImageJ<T extends RealType<T> & NativeType<T>> implements
+	ImageJPlugin
 {
+
 	// -- constants --
-	
+
 	private static final long ONE_K_BYTES = 1024;
 	private static final long ONE_M_BYTES = ONE_K_BYTES * ONE_K_BYTES;
-	
+
 	// -- parameters --
 
 	@Parameter
@@ -105,15 +107,16 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 	private DisplayService dispSrv;
 
 	// -- instance variables that are not parameters --
-	private List<String> attributionStrings = new LinkedList<String>();
+
+	private final List<String> attributionStrings = new LinkedList<String>();
 	private ColorRGB textColor = Colors.YELLOW;
-	private ColorRGB outlineColor = Colors.BLACK;
+	private final ColorRGB outlineColor = Colors.BLACK;
 	private int largestFontSize = 35;
 	private ChannelCollection textChannels = null;
 	private ChannelCollection outlineChannels = null;
-	
+
 	// -- public interface --
-	
+
 	@Override
 	public void run() {
 		final Dataset dataset = getDataset();
@@ -127,21 +130,19 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 	 * Returns a merged color Dataset as a backdrop.
 	 */
 	private Dataset getDataset() {
-		
+
 		final URL imageURL = getImgPlusURL();
-		
+
 		final String filename;
-		if (imageURL == null)
-			filename = "";
-		else
-			filename = imageURL.toString().substring(5); // strip off "file:"
-		
+		if (imageURL == null) filename = "";
+		else filename = imageURL.toString().substring(5); // strip off "file:"
+
 		final ImgPlus<T> img = getImgPlus(filename);
-		
+
 		final String title = "About ImageJ " + ImageJ.VERSION;
 
 		Dataset ds = null;
-		
+
 		// did we successfully load a background image?
 		if (img != null) {
 			// yes we did - inspect it
@@ -149,7 +150,7 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 			boolean validImage = true;
 			validImage &= (ds.numDimensions() == 3);
 			// Too restrictive? Ran into images where 3rd axis is mislabeled
-			//validImage &= (ds.getAxisIndex(Axes.CHANNEL) == 2);
+			// validImage &= (ds.getAxisIndex(Axes.CHANNEL) == 2);
 			validImage &= (ds.getImgPlus().firstElement().getBitsPerPixel() == 8);
 			validImage &= (ds.isInteger());
 			validImage &= (!ds.isSigned());
@@ -165,14 +166,14 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 		if (ds == null) {
 			Log.warn("Could not load a 3 channel unsigned 8 bit image as backdrop");
 			// make a black 3 channel 8-bit unsigned background image.
-			ds = dataSrv.create(
-				new long[]{500,500,3} , title,
-				new AxisType[]{Axes.X,Axes.Y,Axes.CHANNEL}, 8, false, false);
+			ds =
+				dataSrv.create(new long[] { 500, 500, 3 }, title, new AxisType[] {
+					Axes.X, Axes.Y, Axes.CHANNEL }, 8, false, false);
 		}
-		
+
 		ds.setName(title);
 		ds.setRGBMerged(true);
-		
+
 		return ds;
 	}
 
@@ -183,7 +184,7 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 	private URL getImgPlusURL() {
 		final List<URL> fileURLs = new LinkedList<URL>();
 		for (int i = 0; i < 100; i++) {
-			final URL url = getClass().getResource("/images/about"+i+".tif");
+			final URL url = getClass().getResource("/images/about" + i + ".tif");
 			if (url != null) {
 				fileURLs.add(url);
 			}
@@ -200,14 +201,15 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 	/**
 	 * Loads an ImgPlus from a filename.
 	 */
-	private ImgPlus<T> getImgPlus(String filename) {
+	private ImgPlus<T> getImgPlus(final String filename) {
 		if (filename == null) return null;
 		try {
 			final ImgOpener opener = new ImgOpener();
 			// TODO - ImgOpener should be extended to handle URLs
 			// Hack for now to get local file name
 			return opener.openImg(filename);
-		} catch (Exception e) {
+		}
+		catch (final Exception e) {
 			return null;
 		}
 	}
@@ -215,7 +217,7 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 	/**
 	 * Draws the textual information over a given merged color Dataset.
 	 */
-	private void drawTextOverImage(Dataset ds) {
+	private void drawTextOverImage(final Dataset ds) {
 		textChannels = new ChannelCollection(textColor);
 		outlineChannels = new ChannelCollection(outlineColor);
 		final DrawingTool tool = new DrawingTool(ds);
@@ -225,45 +227,45 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 		final long x = width / 2;
 		long y = 50;
 		tool.setTextAntialiasing(true);
-		//tool.setTextOutlineWidth(5);
+		// tool.setTextOutlineWidth(5);
 		tool.setFontSize(largestFontSize);
-		drawOutlinedText(tool, x, y, "ImageJ2 "+ImageJ.VERSION,
+		drawOutlinedText(tool, x, y, "ImageJ2 " + ImageJ.VERSION,
 			TextJustification.CENTER, textChannels, outlineChannels);
-		y += 5*tool.getFontSize()/4;
-		tool.setFontSize((int)Math.round(0.6 * largestFontSize));
+		y += 5 * tool.getFontSize() / 4;
+		tool.setFontSize((int) Math.round(0.6 * largestFontSize));
 		for (final String line : getTextBlock()) {
 			drawOutlinedText(tool, x, y, line, TextJustification.CENTER,
 				textChannels, outlineChannels);
-			y += 5*tool.getFontSize()/4;
+			y += 5 * tool.getFontSize() / 4;
 		}
 	}
 
 	/**
 	 * Draws a text string and outline in two different sets of fill values.
 	 */
-	private void drawOutlinedText(DrawingTool tool, long x, long y, String text,
-		TextJustification just, ChannelCollection textValues,
-		ChannelCollection outlineValues)
+	private void drawOutlinedText(final DrawingTool tool, final long x,
+		final long y, final String text, final TextJustification just,
+		final ChannelCollection textValues, final ChannelCollection outlineValues)
 	{
 		tool.setChannels(outlineValues);
 		for (int dx = -1; dx <= 1; dx++) {
 			for (int dy = -1; dy <= 1; dy++) {
 				if (dx == 0 && dy == 0) continue;
-				tool.drawText(x+dx, y+dy, text, just);
+				tool.drawText(x + dx, y + dy, text, just);
 			}
 		}
 		tool.setChannels(textValues);
 		tool.drawText(x, y, text, just);
 	}
-	
+
 	/**
-	 * Returns the paragraph of textual information to display over the
-	 * backdrop image.
+	 * Returns the paragraph of textual information to display over the backdrop
+	 * image.
 	 */
 	private List<String> getTextBlock() {
-		
+
 		final LinkedList<String> stringList = new LinkedList<String>();
-		
+
 		stringList.add("Open source image processing software");
 		stringList.add("Copyright 2010, 2011, 2012");
 		stringList.add("http://developer.imagej.net/");
@@ -273,13 +275,13 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 
 		return stringList;
 	}
-	
+
 	/**
 	 * Returns a string showing java platform and version information.
 	 */
 	private String javaInfo() {
-		return "Java "+System.getProperty("java.version") +
-				(is64Bit() ? " (64-bit)" : " (32-bit)");
+		return "Java " + System.getProperty("java.version") +
+			(is64Bit() ? " (64-bit)" : " (32-bit)");
 	}
 
 	// NB - this code used to mirror IJ1 more closely. It figured max memory from
@@ -291,7 +293,7 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 	// ij-options that this class and SwingStatusBar use. Then we can decide
 	// whether to emulate IJ1's memory option constraint. Or if we'll instead
 	// rely on launcher to constrain memory. Either way the two will be consistent
-	
+
 	/**
 	 * Returns a string showing used and available memory information.
 	 */
@@ -302,42 +304,46 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 		final long usedMem = totalMem - freeMem;
 		final long maxMB = maxMem / ONE_M_BYTES;
 		String inUseStr =
-				(usedMem < 10000*ONE_K_BYTES) ?
-					(usedMem/ONE_K_BYTES + "K") : (usedMem/ONE_M_BYTES + "MB");
-		if (maxMem>0L) {
+			(usedMem < 10000 * ONE_K_BYTES) ? (usedMem / ONE_K_BYTES + "K")
+				: (usedMem / ONE_M_BYTES + "MB");
+		if (maxMem > 0L) {
 			final long percent = usedMem * 100 / maxMem;
-			inUseStr += " of "+maxMB+"MB ("+(percent<1 ? "<1" : percent) + "%)";
+			inUseStr +=
+				" of " + maxMB + "MB (" + (percent < 1 ? "<1" : percent) + "%)";
 		}
 		return inUseStr;
 	}
-	
+
 	/**
 	 * Returns true if ImageJ2 is running a 64-bit version of Java.
 	 */
 	private boolean is64Bit() {
 		final String osarch = System.getProperty("os.arch");
-		return osarch!=null && osarch.indexOf("64")!=-1;
+		return osarch != null && osarch.indexOf("64") != -1;
 	}
 
 	/**
 	 * Given an image file name (i.e. filename.ext) loads associated attributes
-	 * from a text file (filename.ext.txt) if possible.  
+	 * from a text file (filename.ext.txt) if possible.
 	 */
-	private void loadAttributes(String baseFileName) {
+	private void loadAttributes(final String baseFileName) {
 		final String fileName = baseFileName + ".txt";
 		final File file = new File(fileName);
 		if (file.exists()) {
 			final Pattern attributionPattern = Pattern.compile("attribution\\s+(.*)");
-			final Pattern colorPattern = Pattern.compile("color\\s+([0-9]+)\\s+([0-9]+)\\s+([0-9]+)");
-			final Pattern fontsizePattern = Pattern.compile("fontsize\\s+([1-9][0-9]*)");
+			final Pattern colorPattern =
+				Pattern.compile("color\\s+([0-9]+)\\s+([0-9]+)\\s+([0-9]+)");
+			final Pattern fontsizePattern =
+				Pattern.compile("fontsize\\s+([1-9][0-9]*)");
 			try {
 				final FileInputStream fstream = new FileInputStream(file);
 				final DataInputStream in = new DataInputStream(fstream);
 				final BufferedReader br = new BufferedReader(new InputStreamReader(in));
 				String strLine;
-				//Read File Line By Line
+				// Read File Line By Line
 				while ((strLine = br.readLine()) != null) {
-					final Matcher attributionMatcher = attributionPattern.matcher(strLine);
+					final Matcher attributionMatcher =
+						attributionPattern.matcher(strLine);
 					if (attributionMatcher.matches()) {
 						attributionStrings.add(attributionMatcher.group(1).trim());
 					}
@@ -347,8 +353,9 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 							final int r = Integer.parseInt(colorMatcher.group(1));
 							final int g = Integer.parseInt(colorMatcher.group(2));
 							final int b = Integer.parseInt(colorMatcher.group(3));
-							textColor = new ColorRGB(r,g,b);
-						} catch (Exception e) {
+							textColor = new ColorRGB(r, g, b);
+						}
+						catch (final Exception e) {
 							// do nothing
 						}
 					}
@@ -356,18 +363,20 @@ public class AboutImageJ<T extends RealType<T> & NativeType<T>>
 					if (fontsizeMatcher.matches()) {
 						try {
 							largestFontSize = Integer.parseInt(fontsizeMatcher.group(1));
-						} catch (Exception e) {
+						}
+						catch (final Exception e) {
 							// do nothing
 						}
 					}
 				}
-				//Close the input stream
+				// Close the input stream
 				in.close();
-			   
+
 			}
-			catch (Exception e) {
+			catch (final Exception e) {
 				// do nothing
 			}
 		}
 	}
+
 }
