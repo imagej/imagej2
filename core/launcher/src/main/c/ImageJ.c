d3f9d6c34f9f4000830dbfa6e68945bd276ce158
Curtis Rueden <ctrueden@wisc.edu>
2012-04-01 12:48:37 -0500

    Add license blurbs to ImageJ launcher files
    Also fleshed out the explanation of the ImageJ launcher a bit in
    ImageJ.c, explaining how it was derived from the Fiji launcher.
    
    This used to be revision r5215.

--- core/launcher/src/main/c/ImageJ.c
+++ core/launcher/src/main/c/ImageJ.c
@@ -1,15 +1,55 @@
 /*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2012 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+/*
  * This is the ImageJ launcher, a small native program to handle the
- * startup of Java and Fiji.
+ * startup of Java and ImageJ.
  *
- * Copyright 2007-2011 Johannes Schindelin, Mark Longair, Albert Cardona
- * Benjamin Schmid, Erwin Frise and Gregory Jefferis
+ * This program was originally developed as the Fiji launcher
+ * (http://fiji.sc/), but has been adapted and improved for use with ImageJ
+ * core.
  *
- * The source is distributed under the BSD license.
+ * The Fiji launcher is copyright 2007 - 2011 Johannes Schindelin, Mark
+ * Longair, Albert Cardona, Benjamin Schmid, Erwin Frise and Gregory Jefferis.
  *
  * Clarification: the license of the ImageJ launcher has no effect on
  * the Java Runtime, ImageJ or any plugins, since they are not derivatives.
+ *
+ * @author Johannes Schindelin
  */
+
 #define _BSD_SOURCE
 #include <stdlib.h>
 #include "jni.h"
--- core/launcher/src/main/java/imagej/ClassLoaderPlus.java
+++ core/launcher/src/main/java/imagej/ClassLoaderPlus.java
@@ -1,8 +1,39 @@
-package imagej;
-
-/**
- * A classloader whose classpath can be augmented after instantiation
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2012 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
  */
+
+package imagej;
 
 import java.io.File;
 
@@ -13,6 +44,11 @@ import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A classloader whose classpath can be augmented after instantiation.
+ *
+ * @author Johannes Schindelin
+ */
 public class ClassLoaderPlus extends URLClassLoader {
 	// A frozen ClassLoaderPlus will add only to the urls array
 	protected boolean frozen;
