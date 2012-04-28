f489e37fb2967a309817db5ffbb8583dae65e2b2
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-04-27 23:10:46 -0500

    Add the jEdit syntax highlighting to the Script Editor
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ ui/swing/script-editor/src/main/java/org/jeditsyntax/XMLTokenMarker.java
@@ -0,0 +1,24 @@
+/*
+ * XMLTokenMarker.java - XML token marker
+ * Copyright (C) 1999 Slava Pestov
+ *
+ * You may use and modify this package for any purpose. Redistribution is
+ * permitted, in both source and binary form, provided that this notice
+ * remains intact in all source distributions of this package.
+ */
+
+package org.jeditsyntax;
+
+/**
+ * XML token marker.
+ *
+ * @author Slava Pestov
+ * @version $Id: XMLTokenMarker.java,v 1.2 1999/12/13 03:40:30 sp Exp $
+ */
+public class XMLTokenMarker extends HTMLTokenMarker
+{
+	public XMLTokenMarker()
+	{
+		super(false);
+	}
+}
