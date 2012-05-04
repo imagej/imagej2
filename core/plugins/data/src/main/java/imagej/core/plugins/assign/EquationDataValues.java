e8a73649ab2c82d29780322914f3aefcb7ff851d
Barry DeZonia <bdezonia@wisc.edu>
2012-05-04 13:06:27 -0500

    Mark display as ItemIO.BOTH
    Remove explicit call to dataset.update()
    
    This used to be revision r5413.

--- core/plugins/data/src/main/java/imagej/core/plugins/assign/EquationDataValues.java
+++ core/plugins/data/src/main/java/imagej/core/plugins/assign/EquationDataValues.java
@@ -43,6 +43,7 @@ import imagej.data.display.ImageDisplayService;
 import imagej.data.display.OverlayService;
 import imagej.data.overlay.Overlay;
 import imagej.ext.menu.MenuConstants;
+import imagej.ext.module.ItemIO;
 import imagej.ext.module.ItemVisibility;
 import imagej.ext.plugin.ImageJPlugin;
 import imagej.ext.plugin.Menu;
@@ -86,7 +87,7 @@ public class EquationDataValues<T extends RealType<T>> implements ImageJPlugin {
 	@Parameter(persist = false)
 	UIService uiService;
 	
-	@Parameter(persist = false)
+	@Parameter(type=ItemIO.BOTH, persist = false)
 	private ImageDisplay display;
 
 	@Parameter(label = "Format examples", visibility=ItemVisibility.MESSAGE)
@@ -134,7 +135,6 @@ public class EquationDataValues<T extends RealType<T>> implements ImageJPlugin {
 						(Img<T>)dataset.getImgPlus(), origin, span,
 						function, condition, factory);
 		assigner.assign();
-		dataset.update();
 	}
 
 	public ImageDisplay getDisplay() {
