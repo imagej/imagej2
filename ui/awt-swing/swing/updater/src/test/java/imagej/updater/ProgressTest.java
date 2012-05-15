d2158d4ba40f5c36a8ffab5dce2c7ac7b77deb7e
Johannes Schindelin <johannes.schindelin@gmx.de>
2012-05-15 13:44:54 -0400

    Add an interactive GUI test for Swing Updater's progress dialog
    This test class lives in src/test/ but is not annotated as @Test because
    it makes no sense to run it in an automated fashion.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ ui/awt-swing/swing/updater/src/test/java/imagej/updater/ProgressTest.java
@@ -0,0 +1,51 @@
+package imagej.updater;
+
+import imagej.updater.gui.ProgressDialog;
+import imagej.util.MersenneTwisterFast;
+
+public class ProgressTest {
+	public static void main(String[] args) {
+		int count = 35;
+		int minSize = 8192;
+		int maxSize = 65536;
+		int minChunk = 256;
+		int maxChunk = 16384;
+
+		MersenneTwisterFast random = new MersenneTwisterFast();
+
+		ProgressDialog progress = new ProgressDialog(null);
+
+		progress.setTitle("Hello");
+
+		int totalSize = 0;
+		int totalCurrent = 0;
+		int[] sizes = new int[count];
+		for (int i = 0; i < count; i++) {
+			sizes[i] = minSize + random.nextInt(maxSize - minSize);
+			totalSize += sizes[i];
+		}
+
+		for (int i = 0; i < count; i++) {
+			int current = 0;
+			String item = "Item " + i + "/" + sizes[i];
+			progress.addItem(item);
+			while (current < sizes[i]) {
+				int byteCount = minChunk + random.nextInt(maxChunk - minChunk);
+				current += byteCount;
+				progress.setItemCount(current, sizes[i]);
+				totalCurrent += byteCount;
+				progress.setCount(totalCurrent, totalSize);
+				int millis = random.nextInt(500);
+				if (millis > 0) try {
+					Thread.sleep(millis);
+				} catch (InterruptedException e) {
+					// we've been asked to stop
+					progress.done();
+					return;
+				}
+			}
+			progress.itemDone(item);
+		}
+		progress.done();
+	}
+}
