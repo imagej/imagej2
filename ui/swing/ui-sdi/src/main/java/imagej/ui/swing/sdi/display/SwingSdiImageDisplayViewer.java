ef56ef79b77dff8e15e04f1251d4f8c15400a709
Curtis Rueden <ctrueden@wisc.edu>
2012-07-03 16:24:15 -0500

    SwingDisplayWindow: remove unnecessary methods

--- ui/swing/ui-sdi/src/main/java/imagej/ui/swing/sdi/display/SwingSdiImageDisplayViewer.java
+++ ui/swing/ui-sdi/src/main/java/imagej/ui/swing/sdi/display/SwingSdiImageDisplayViewer.java
@@ -62,7 +62,7 @@ public class SwingSdiImageDisplayViewer extends AbstractSwingImageDisplayViewer
 	public void view(final DisplayWindow w, final Display<?> d) {
 		super.view(w, d);
 		final SwingDisplayWindow sdiWindow = (SwingDisplayWindow) w;
-		sdiWindow.addEventDispatcher(dispatcher);
+		dispatcher.register(sdiWindow);
 	}
 
 }
