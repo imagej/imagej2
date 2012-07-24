imagej/imagej2

commit 34d01ebe9f45172bcd16822a2124ae79a2dc22ec

Updater: prefer the update site's current versions to locally-cached ones
A rather embarrassing bug entered the ImageJ Updater which prevents
it from updating components because local db.xml.gz's versions are
preferred over those from the update site.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

core/updater/core/src/main/java/imagej/updater/core/XMLFileReader.java
@@ -43,6 +43,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.zip.GZIPInputStream;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -67,6 +69,7 @@ public class XMLFileReader extends DefaultHandler {
 	// this is the name of the update site (null means we read the local
 	// db.xml.gz)
 	protected String updateSite;
+	protected Set<FileObject> filesFromThisSite = new HashSet<FileObject>();
 
 	// every file newer than this was not seen by the user yet
 	protected long newTimestamp;
@@ -201,19 +204,23 @@ public class XMLFileReader extends DefaultHandler {
 			FileObject file = files.get(current.filename);
 			if (updateSite == null && current.updateSite != null &&
 				files.getUpdateSite(current.updateSite) == null) ; // ignore file with invalid update site
-			else if (file == null) files.add(current);
+			else if (file == null) {
+				files.add(current);
+				filesFromThisSite.add(current);
+			}
 			else {
 				// Be nice to old-style update sites where Jama-1.0.2.jar and Jama.jar were different file objects
-				if ((updateSite != null && updateSite.equals(file.updateSite)) || (updateSite == null && file.updateSite == null)) {
+				if (filesFromThisSite.contains(file)) {
 					if (file.isObsolete()) {
 						files.remove(file.filename);
 						final FileObject swap = file;
 						file = current;
 						current = swap;
 						files.add(file);
+						filesFromThisSite.add(file);
 					}
 					if (current.current != null) {
-						current.addPreviousVersion(current.current.checksum, current.current.timestamp, current.filename);
+						current.addPreviousVersion(file.current.checksum, file.current.timestamp, file.filename);
 					}
 					for (final FileObject.Version version : current.previous) {
 						if (version.filename == null) version.filename = current.filename;
@@ -225,15 +232,20 @@ public class XMLFileReader extends DefaultHandler {
 						current.addPreviousVersion(version.checksum, version.timestamp, version.filename);
 					}
 					files.add(current);
+					filesFromThisSite.add(current);
 				} else if (current.isObsolete()) {
 					for (final FileObject.Version version : current.previous) {
 						if (version.filename == null) version.filename = current.filename;
 						file.addPreviousVersion(version.checksum, version.timestamp, version.filename);
 					}
 				} else if (getRank(files, updateSite) >= getRank(files, file.updateSite)) {
-					current.overriddenUpdateSites.addAll(file.overriddenUpdateSites);
-					if (file.updateSite != null && !file.updateSite.equals(updateSite)) {
-						current.overriddenUpdateSites.add(file.updateSite);
+					if ((updateSite != null && updateSite.equals(file.updateSite)) || (updateSite == null && file.updateSite == null)) {
+						; // simply update the object
+					} else {
+						current.overriddenUpdateSites.addAll(file.overriddenUpdateSites);
+						if (file.updateSite != null && !file.updateSite.equals(updateSite)) {
+							current.overriddenUpdateSites.add(file.updateSite);
+						}
 					}
 					if (file.localFilename != null) {
 						current.localFilename = file.localFilename;
@@ -241,6 +253,7 @@ public class XMLFileReader extends DefaultHandler {
 					// do not forget metadata
 					current.completeMetadataFrom(file);
 					files.add(current);
+					filesFromThisSite.add(current);
 					if (this.updateSite != null && file.updateSite != null && getRank(files, this.updateSite) > getRank(files, file.updateSite))
 						warnings.append("Warning: '" + current.filename
 								+ "' from update site '" + current.updateSite

core/updater/core/src/test/java/imagej/updater/core/UpdaterTest.java
@@ -704,6 +704,59 @@ public class UpdaterTest {
 	}
 
 	@Test
+	public void testUpdateable() throws Exception {
+		initializeUpdateSite("jars/hello.jar");
+		FilesCollection files = readDb(true, true);
+		assertStatus(Status.INSTALLED, files.get("jars/hello.jar"));
+		String origChecksum = files.get("jars/hello.jar").getChecksum();
+		assertEquals(origChecksum, Util.getJarDigest(new File(ijRoot, "jars/hello.jar")));
+
+		assertTrue(new File(ijRoot, ".checksums").delete());
+		assertTrue(new File(ijRoot, "jars/hello.jar").delete());
+		writeJar("jars/hello-2.0.jar", "new-file", "empty");
+		new Checksummer(files, progress).updateFromLocal();
+		String newChecksum = files.get("jars/hello.jar").localChecksum;
+		assertEquals(newChecksum, Util.getJarDigest(new File(ijRoot, "jars/hello-2.0.jar")));
+
+		assertNotEqual(origChecksum, newChecksum);
+
+		// upload that version
+		files.get("jars/hello.jar").stageForUpload(files, FilesCollection.DEFAULT_UPDATE_SITE);
+		assertEquals(newChecksum, files.get("jars/hello.jar").localChecksum);
+		upload(files);
+
+		FilesCollection files2 = new FilesCollection(new File(ijRoot, "invalid"));
+		files2.getUpdateSite(FilesCollection.DEFAULT_UPDATE_SITE).url = webRoot.toURI().toURL().toString();
+		XMLFileDownloader xmlLoader = new XMLFileDownloader(files2);
+		xmlLoader.start();
+		String newChecksum2 = files2.get("jars/hello.jar").current.checksum;
+		assertEquals(newChecksum, newChecksum2);
+
+		// re-write the original version
+		assertTrue(new File(ijRoot, ".checksums").delete());
+		writeFile("jars/hello.jar");
+		assertTrue(new File(ijRoot, "jars/hello-2.0.jar").delete());
+		files = readDb(true, true);
+		String origChecksum2 = files.get("jars/hello.jar").localChecksum;
+		assertEquals(origChecksum, origChecksum2);
+		assertEquals(origChecksum, Util.getJarDigest(new File(ijRoot, "jars/hello.jar")));
+
+		assertTrue(new File(ijRoot, "db.xml.gz").delete());
+		files = readDb(false, true);
+		assertEquals(newChecksum, files.get("jars/hello.jar").current.checksum);
+
+		assertStatus(Status.UPDATEABLE, files.get("jars/hello.jar"));
+		files.get("jars/hello.jar").setAction(files, Action.UPDATE);
+		Installer installer = new Installer(files, progress);
+		installer.start();
+		assertEquals(newChecksum, Util.getJarDigest(new File(ijRoot, "update/jars/hello-2.0.jar")));
+		installer.moveUpdatedIntoPlace();
+
+		assertStatus(Status.INSTALLED, files.get("jars/hello.jar"));
+		assertEquals(newChecksum, Util.getJarDigest(new File(ijRoot, "jars/hello-2.0.jar")));
+	}
+
+	@Test
 	public void testReReadFiles() throws Exception {
 		initializeUpdateSite("macros/macro.ijm");
 		FilesCollection files = readDb(true, true);

commit 9f2f138d347be797aad669d163f0d901d3d02d87

Updater: Brown paper bag fix
This fixes a bug introduced in 34d01ebe(Updater: prefer the update site's
current versions to locally-cached ones). Basically, it would prevent any
shadowing (update sites overriding other update sites' files).

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

core/updater/core/src/main/java/imagej/updater/core/XMLFileReader.java
@@ -117,6 +117,7 @@ public class XMLFileReader extends DefaultHandler {
 		SAXException
 	{
 		this.updateSite = updateSite;
+		filesFromThisSite.clear();
 		newTimestamp = timestamp;
 
 		final InputSource inputSource = new InputSource(in);