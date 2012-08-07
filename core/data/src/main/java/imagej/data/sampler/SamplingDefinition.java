4fe040422ec69a5e584a650cfd9428b7a01a0f77
Barry DeZonia <bdezonia@wisc.edu>
2012-08-07 16:42:03 -0500

    Improve Javadoc

--- core/data/src/main/java/imagej/data/sampler/SamplingDefinition.java
+++ core/data/src/main/java/imagej/data/sampler/SamplingDefinition.java
@@ -49,12 +49,20 @@ import net.imglib2.meta.AxisType;
 
 
 /**
+ * SamplingDefinitions define regions of space and are used by the
+ * SamplerService to pull data out of existing ImageDisplays.
+ * 
  * @author Barry DeZonia
  */
 public class SamplingDefinition {
+	
+	// -- instance variables --
+	
 	private ImageDisplay display;
 	private Map<AxisType,AxisSubrange> axisSubranges;
 	private String err;
+	
+	// -- private base class constructor --
 	
 	private SamplingDefinition(ImageDisplay display) {
 		this.display = display;
@@ -62,15 +70,28 @@ public class SamplingDefinition {
 		this.err = null;
 	}
 	
+	// -- public interface --
+
+	/** Returns the input ImageDisplay of the SamplingDefinition */ 
 	public ImageDisplay getDisplay() { return display; }
 	
+	/** Returns the current value of the error string of the SamplingDefinition */ 
 	public String getError() { return err; }
 
 
+	/** Returns the axes that are present in the input data. */
 	public AxisType[] getInputAxes() {
 		return display.getAxes();
 	}
 	
+	/**
+	 * Returns a multidimensional set of input axis values generated from the
+	 * input data of this SamplingDefinition.
+	 * <p>
+	 * For example, if the sampling definition has two axes defined as "1-4" and
+	 * "1-3" calling this routine would return something like this:
+	 * [[1,2,3,4] , [1,2,3]] 
+	 */
 	public List<List<Long>> getInputRanges() {
 		AxisType[] axes = display.getAxes();
 		List<List<Long>> axesDefs = new ArrayList<List<Long>>();
@@ -82,6 +103,8 @@ public class SamplingDefinition {
 		return Collections.unmodifiableList(axesDefs);
 	}
 	
+	/** Returns the axes that will be present in the output data. Those input axes
+	 * whose size is 1 are automatically collapsed. */
 	public AxisType[] getOutputAxes() {
 		AxisType[] inputAxes = getInputAxes();
 		List<List<Long>> inputRanges = getInputRanges();
@@ -97,6 +120,8 @@ public class SamplingDefinition {
 		return outputAxes;
 	}
 	
+	/** Returns the dimensions that will be present in the output data. Those
+	 * input dimensions whose size is 1 are automatically collapsed. */
 	public long[] getOutputDims() {
 		List<List<Long>> inputRanges = getInputRanges();
 		int dimCount = 0;
@@ -112,6 +137,46 @@ public class SamplingDefinition {
 		return outputDims;
 	}
 	
+	/**
+	 * Replaces the current constraining definition of a given axis within the
+	 * current SamplingDefinition with a given subrange. 
+	 * 
+	 * @param axis The axis to associate the constraint with
+	 * @param subrange The new subrange defining the constraint
+	 * @return True if the contraint is well defined. False otherwise (and the
+	 * existing constraint for the axis is unchanged).
+	 */
+	public boolean constrain(AxisType axis, AxisSubrange subrange) {
+		if (subrange.getError() != null) {
+			err = subrange.getError();
+			return false;
+		}
+		Data data = display.getActiveView().getData();
+		int axisIndex = data.getAxisIndex(axis);
+		if (axisIndex < 0) {
+			err = "Undefined axis " + axis + " for display " + display.getName();
+			return false;
+		}
+		List<Long> indices = subrange.getIndices();
+		if (data.dimension(axisIndex) < indices.get(0)) {
+			err = "Axis range fully beyond dimensions of display " +
+					display.getName() + " for axis " + axis;
+			return false;
+		}
+		axisSubranges.put(axis,  subrange);
+		return true;
+	}
+
+	// -- public static construction methods --
+	
+	/** A convenience method for defining a SamplingDefinition that returns a
+	 * single UV plane of an ImageDisplay. U and V are defined by the user.
+	 *  
+	 * @param display The ImageDisplay to sample
+	 * @param uAxis The U axis of the sample space
+	 * @param vAxis The V axis of the sample space
+	 * @return The specified SamplingDefinition
+	 */
 	public static SamplingDefinition sampleUVPlane(
 		ImageDisplay display,	AxisType uAxis, AxisType vAxis)
 	{
@@ -134,10 +199,25 @@ public class SamplingDefinition {
 		return definition;
 	}
 	
+	/** A convenience method for defining a SamplingDefinition that returns a
+	 * single XY plane of an ImageDisplay.
+	 *  
+	 * @param display The ImageDisplay to sample
+	 * @return The specified SamplingDefinition
+	 */
 	public static SamplingDefinition sampleXYPlane(ImageDisplay display) {
 		return sampleUVPlane(display, Axes.X, Axes.Y);
 	}
 	
+	/** A convenience method for defining a SamplingDefinition that returns a
+	 * composite (multichannel) UV plane of an ImageDisplay. U and V are defined
+	 * by the user.
+	 *  
+	 * @param display The ImageDisplay to sample
+	 * @param uAxis The U axis of the sample space
+	 * @param vAxis The V axis of the sample space
+	 * @return The specified SamplingDefinition
+	 */
 	public static SamplingDefinition sampleCompositeUVPlane(
 		ImageDisplay display, AxisType uAxis, AxisType vAxis)
 	{
@@ -163,10 +243,22 @@ public class SamplingDefinition {
 		return definition;
 	}
 
+	/** A convenience method for defining a SamplingDefinition that returns a
+	 * composite (multichannel) XY plane of an ImageDisplay.
+	 *  
+	 * @param display The ImageDisplay to sample
+	 * @return The specified SamplingDefinition
+	 */
 	public static SamplingDefinition sampleCompositeXYPlane(ImageDisplay display){
 		return sampleCompositeUVPlane(display, Axes.X, Axes.Y);
 	}
 	
+	/** A convenience method for defining a SamplingDefinition that returns a
+	 * complete copy of an ImageDisplay.
+	 *  
+	 * @param display The ImageDisplay to sample
+	 * @return The specified SamplingDefinition
+	 */
 	public static SamplingDefinition sampleAllPlanes(ImageDisplay display) {
 		SamplingDefinition definition = new SamplingDefinition(display);
 		AxisType[] axes = display.getAxes();
@@ -178,26 +270,6 @@ public class SamplingDefinition {
 		}
 		return definition;
 	}
-	
-	public boolean constrain(AxisType axis, AxisSubrange subrange) {
-		if (subrange.getError() != null) {
-			err = subrange.getError();
-			return false;
-		}
-		Data data = display.getActiveView().getData();
-		int axisIndex = data.getAxisIndex(axis);
-		if (axisIndex < 0) {
-			err = "Undefined axis " + axis + " for display " + display.getName();
-			return false;
-		}
-		List<Long> indices = subrange.getIndices();
-		if (data.dimension(axisIndex) < indices.get(0)) {
-			err = "Axis range fully beyond dimensions of display " +
-					display.getName() + " for axis " + axis;
-			return false;
-		}
-		axisSubranges.put(axis,  subrange);
-		return true;
-	}
+
 }
 
