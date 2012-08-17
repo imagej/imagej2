188f3232976305153ba56cf2b82377181ffe80c7
Barry DeZonia <bdezonia@wisc.edu>
2012-08-17 17:18:20 -0500

    Add a pure IJ2 version of Capture Screen plugin

---
+++ core/plugins/display/src/main/java/imagej/core/plugins/display/CaptureScreen.java
@@ -0,0 +1,84 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2012 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.core.plugins.display;
+
+import imagej.data.Dataset;
+import imagej.data.display.ScreenCaptureService;
+import imagej.ext.menu.MenuConstants;
+import imagej.ext.module.ItemIO;
+import imagej.ext.plugin.RunnablePlugin;
+import imagej.ext.plugin.Menu;
+import imagej.ext.plugin.Parameter;
+import imagej.ext.plugin.Plugin;
+
+
+/**
+ * Captures the entire screen to a {@link Dataset}.
+ * 
+ * @author Barry DeZonia
+ */
+@Plugin(menu = {
+	@Menu(label = MenuConstants.PLUGINS_LABEL,
+			weight = MenuConstants.PLUGINS_WEIGHT,
+			mnemonic = MenuConstants.PLUGINS_MNEMONIC),
+	@Menu(label = "Utilities"),
+	@Menu(label = "Capture Screen", weight = 19, 
+				accelerator = "shift control G")})
+public class CaptureScreen implements RunnablePlugin {
+
+	// -- Parameters --
+	
+	@Parameter
+	private ScreenCaptureService captureService;
+	
+	@Parameter(type=ItemIO.OUTPUT)
+	private Dataset output;
+
+	// -- accessors --
+	
+	public Dataset getOutput() {
+		return output;
+	}
+	
+	// -- run() method --
+	
+	@Override
+	public void run() {
+		output = captureService.captureScreen();
+		output.setName("Screen capture");
+	}
+
+}
