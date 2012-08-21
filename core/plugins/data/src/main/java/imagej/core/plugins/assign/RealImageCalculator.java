a40d8919c6ff5e6a88ebafdc1da9b67c6afef05e
Barry DeZonia <bdezonia@wisc.edu>
2012-08-21 17:10:10 -0500

    Add some error checking code

--- core/plugins/data/src/main/java/imagej/core/plugins/assign/RealImageCalculator.java
+++ core/plugins/data/src/main/java/imagej/core/plugins/assign/RealImageCalculator.java
@@ -70,9 +70,6 @@ import net.imglib2.ops.pointset.HyperVolumePointSet;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.DoubleType;
 
-// TODO - pull code from OPS' ImageCreator class. This class should shrink
-//   considerably.
-
 /**
  * Fills an output Dataset with a combination of two input Datasets. The
  * combination is specified by the user (such as Add, Min, Average, etc.).
@@ -158,13 +155,19 @@ public class RealImageCalculator<T extends RealType<T>> implements
 	 */
 	@Override
 	public void run() {
-		ImageCombiner computer = new ImageCombiner();
-		Img<T> img1 = (Img<T>) input1.getImgPlus();
-		Img<T> img2 = (Img<T>) input2.getImgPlus();
-		// TODO - limited by ArrayImg size constraints
-		Img<DoubleType> img =
+		Img<DoubleType> img = null;
+		try {
+			ImageCombiner computer = new ImageCombiner();
+			Img<T> img1 = (Img<T>) input1.getImgPlus();
+			Img<T> img2 = (Img<T>) input2.getImgPlus();
+			// TODO - limited by ArrayImg size constraints
+			img =
 				computer.applyOp(operators.get(opName), img1, img2, 
 													new ArrayImgFactory<DoubleType>(), new DoubleType());
+		} catch (IllegalArgumentException e) {
+			cancelReason = e.toString();
+			return;
+		}
 		long[] span = new long[img.numDimensions()];
 		img.dimensions(span);
 
