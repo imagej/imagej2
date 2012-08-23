7807869a1fbf0b7a57daeb13a60b6446b97bb1b9
Curtis Rueden <ctrueden@wisc.edu>
2012-08-23 14:17:15 -0500

    Add preprocessor that auto-fills UserInterfaces

---
+++ core/ui/src/main/java/imagej/ui/UIPreprocessor.java
@@ -0,0 +1,74 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2012 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.ui;
+
+import imagej.Priority;
+import imagej.ext.plugin.Plugin;
+import imagej.module.Module;
+import imagej.module.ModuleItem;
+import imagej.plugin.AbstractPreprocessorPlugin;
+import imagej.plugin.PreprocessorPlugin;
+
+/**
+ * The UI preprocessor automatically populates module {@link UserInterface}
+ * inputs with the {@link UIService}'s default UI instance, if compatible.
+ * 
+ * @author Curtis Rueden
+ */
+@Plugin(type = PreprocessorPlugin.class, priority = Priority.VERY_HIGH_PRIORITY)
+public class UIPreprocessor extends AbstractPreprocessorPlugin {
+
+	// -- ModuleProcessor methods --
+
+	@Override
+	public void process(final Module module) {
+		final UIService uiService = getContext().getService(UIService.class);
+		if (uiService == null) return; // no UI service available
+
+		final UserInterface ui = uiService.getDefaultUI();
+		for (final ModuleItem<?> input : module.getInfo().inputs()) {
+			if (!input.isAutoFill()) continue; // cannot auto-fill this input
+			final Class<?> type = input.getType();
+			if (type.isAssignableFrom(ui.getClass())) {
+				// input is a compatible UI
+				final String name = input.getName();
+				module.setInput(name, ui);
+				module.setResolved(name, true);
+			}
+		}
+	}
+
+}
