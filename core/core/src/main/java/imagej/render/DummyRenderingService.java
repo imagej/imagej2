ff8d719a41434dad29b940052c037d48117eee55
Barry DeZonia <bdezonia@wisc.edu>
2012-08-29 14:37:45 -0500

    Add dummy RenderingService implementations
    This allows any UI that does not provide a RenderingService to initialize
    correctly. The DummyRenderingService owns a DummyTextRenderer. If the
    DummyTextRenderer is actually used UnsupportedOperation exceptions are
    thrown.

---
+++ core/core/src/main/java/imagej/render/DummyRenderingService.java
@@ -0,0 +1,75 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2012 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.render;
+
+import imagej.ImageJ;
+import imagej.Priority;
+import imagej.ext.plugin.Plugin;
+import imagej.service.AbstractService;
+
+
+/**
+ * A dummy implementation of the RenderingService interface. Necessary to allow
+ * any UI that does not support rendering to still initialize correctly.
+
+ * @author Barry DeZonia
+ *
+ */
+@Plugin(priority=Priority.LOW_PRIORITY)
+public class DummyRenderingService
+	extends AbstractService
+	implements RenderingService
+{
+	private final TextRenderer textRenderer;
+	
+	public DummyRenderingService() {
+		// NB - needed by Sezpoz
+		super(null);
+		throw new UnsupportedOperationException(
+			"this constructor not meant to be used");
+	}
+
+	public DummyRenderingService(ImageJ context) {
+		super(context);
+		textRenderer = new DummyTextRenderer();
+	}
+	
+	@Override
+	public TextRenderer getTextRenderer() {
+		return textRenderer;
+	}
+
+}
---
+++ core/core/src/main/java/imagej/render/DummyTextRenderer.java
@@ -0,0 +1,120 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2012 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.render;
+
+
+/**
+ * A dummy implementation of the TextRenderer interface. Necessary to allow
+ * any UI that does not support text rendering to still initialize correctly.
+
+ * @author Barry DeZonia
+ *
+ */
+public class DummyTextRenderer implements TextRenderer {
+
+	@Override
+	public void renderText(String text) {
+		throw new UnsupportedOperationException(
+			"Headless text renderer is not functional");
+	}
+
+	@Override
+	public int getPixelsWidth() {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public int getPixelsHeight() {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public int[] getPixels() {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public void setFontFamily(FontFamily family) {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public FontFamily getFontFamily() {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public void setFontStyle(FontStyle style) {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public FontStyle getFontStyle() {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public void setFontSize(int size) {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public int getFontSize() {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public void setAntialiasing(boolean val) {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+
+	@Override
+	public boolean getAntialiasing() {
+		throw new UnsupportedOperationException(
+				"Headless text renderer is not functional");
+	}
+	
+}
