cb226c8f815217ed8474e5cc9c2947e8498c4f7a
Barry DeZonia <bdezonia@wisc.edu>
2012-08-30 09:40:51 -0500

    Add a DummyScreenCaptureService

---
+++ core/data/src/main/java/imagej/data/display/DummyScreenCaptureService.java
@@ -0,0 +1,102 @@
+
+package imagej.data.display;
+
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2012 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+import imagej.ImageJ;
+import imagej.Priority;
+import imagej.data.Dataset;
+import imagej.ext.plugin.Plugin;
+import imagej.service.AbstractService;
+import imagej.service.Service;
+
+/**
+ * A No-op implementation of the ScreenCaptureService interface. Ensures one
+ * always exists for initialization purposes.
+ * 
+ * @author Barry DeZonia
+ */
+@Plugin(type = Service.class, priority = Priority.LOW_PRIORITY)
+public class DummyScreenCaptureService extends AbstractService implements
+	ScreenCaptureService
+{
+
+	// -- instance variables --
+
+	private static final String MSG =
+		"Dummy screen capture service is not functional";
+
+	// -- constructor --
+
+	/**
+	 * Hobbled constructor. Needed for Sezpoz compatiblity and is not to be used.
+	 */
+	public DummyScreenCaptureService() {
+		// NB - needed by Sezpoz
+		super(null);
+		throw new UnsupportedOperationException(
+			"this constructor not meant to be used");
+	}
+
+	/**
+	 * Default constructor: creates a DummyScreenCaptureService from an ImageJ
+	 * context.
+	 */
+	public DummyScreenCaptureService(final ImageJ context) {
+		super(context);
+	}
+
+	// -- public api --
+
+	/**
+	 * No-op implementation.
+	 */
+	@Override
+	public Dataset captureScreenRegion(final int x, final int y, final int width,
+		final int height)
+	{
+		throw new UnsupportedOperationException(MSG);
+	}
+
+	/**
+	 * No-op implementation.
+	 */
+	@Override
+	public Dataset captureScreen() {
+		throw new UnsupportedOperationException(MSG);
+	}
+
+}
