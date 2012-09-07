LeeKamentsky/imagej

Only use fully-qualified IJHotDrawOverlayAdapter references
... except for the sezpoz discovery, which cannot qualify in full (there
is no way to specify IJHotDrawOverlayAdapter<?>.class).

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

ui/awt-swing/swing/ui-base/src/main/java/imagej/ui/swing/overlay/IJCreationTool.java
@@ -49,18 +49,18 @@ import org.jhotdraw.draw.tool.CreationTool;
  * 
  * @author Lee Kamentsky
  */
-public class IJCreationTool extends CreationTool implements JHotDrawTool {
+public class IJCreationTool<F extends Figure> extends CreationTool implements JHotDrawTool {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private final ImageDisplay display;
-	private final IJHotDrawOverlayAdapter adapter;
+	private final IJHotDrawOverlayAdapter<F> adapter;
 	private final EventListenerList listeners = new EventListenerList();
 
 	public IJCreationTool(final ImageDisplay display,
-		final IJHotDrawOverlayAdapter adapter,
+		final IJHotDrawOverlayAdapter<F> adapter,
 		final OverlayCreatedListener... listeners)
 	{
 		super(adapter.createDefaultFigure());
@@ -97,13 +97,14 @@ public class IJCreationTool extends CreationTool implements JHotDrawTool {
 		return adapter.createDefaultFigure();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void creationFinished(final Figure figure) {
 		super.creationFinished(figure);
 		final Overlay overlay = adapter.createNewOverlay();
 		final SwingOverlayView view =
 			new SwingOverlayView(display, overlay, figure);
-		adapter.updateOverlay(figure, view);
+		adapter.updateOverlay((F)figure, view);
 		fireOverlayCreatedEvent(view, figure);
 	}
 

imagej/imagej2

ui/swing/base/src/main/java/imagej/ui/swing/overlay/IJCreationTool.java
@@ -47,13 +47,13 @@ import org.jhotdraw.draw.tool.CreationTool;
  * 
  * @author Lee Kamentsky
  */
-public class IJCreationTool extends CreationTool implements JHotDrawTool {
+public class IJCreationTool<F extends Figure> extends CreationTool implements JHotDrawTool {
 
 	private final ImageDisplay display;
-	private final JHotDrawAdapter adapter;
+	private final JHotDrawAdapter<F> adapter;
 
 	public IJCreationTool(final ImageDisplay display,
-		final JHotDrawAdapter adapter)
+		final JHotDrawAdapter<F> adapter)
 	{
 		super(adapter.createDefaultFigure());
 		this.display = display;
@@ -67,12 +67,13 @@ public class IJCreationTool extends CreationTool implements JHotDrawTool {
 		return getAdapter().createDefaultFigure();
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void creationFinished(final Figure figure) {
 		super.creationFinished(figure);
 		final JHotDrawService jHotDrawService =
 			getDisplay().getContext().getService(JHotDrawService.class);
-		jHotDrawService.linkOverlay(figure, getAdapter(), getDisplay());
+		jHotDrawService.linkOverlay((F)figure, getAdapter(), getDisplay());
 	}
 
 	// -- JHotDrawTool methods --
@@ -83,7 +84,7 @@ public class IJCreationTool extends CreationTool implements JHotDrawTool {
 	}
 
 	@Override
-	public JHotDrawAdapter getAdapter() {
+	public JHotDrawAdapter<F> getAdapter() {
 		return adapter;
 	}
 