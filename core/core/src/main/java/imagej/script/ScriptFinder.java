melissalinkert/imagej

ScriptFinder: log how many scripts were found
Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

core/ext/src/main/java/imagej/ext/script/ScriptFinder.java
@@ -39,6 +39,7 @@ import imagej.ext.MenuPath;
 import imagej.ext.plugin.ImageJPlugin;
 import imagej.ext.plugin.PluginInfo;
 import imagej.ext.plugin.PluginModuleInfo;
+import imagej.util.Log;
 
 import java.io.File;
 import java.util.HashMap;
@@ -66,6 +67,8 @@ public class ScriptFinder {
 		this.scriptService = scriptService;
 	}
 
+	private int scriptCount;
+
 	/**
 	 * Discover the scripts
 	 * 
@@ -80,7 +83,9 @@ public class ScriptFinder {
 			final File pluginsDir = new File(directory, "plugins");
 			if (pluginsDir.isDirectory()) directory = pluginsDir;
 		}
+		scriptCount = 0;
 		discoverScripts(plugins, directory, null);
+		Log.info("Found " + scriptCount + " scripts");
 	}
 
 	/**
@@ -108,8 +113,10 @@ public class ScriptFinder {
 				else discoverScripts(plugins, file, subMenuPath(menuPath, file
 					.getName()));
 			}
-			else if (scriptService.isFileHandled(file)) plugins.add(createEntry(file,
-				subMenuPath(menuPath, file.getName())));
+			else if (scriptService.isFileHandled(file)) {
+				plugins.add(createEntry(file, subMenuPath(menuPath, file.getName())));
+				scriptCount++;
+			}
 	}
 
 	private MenuPath

imagej/imagej2

core/core/src/main/java/imagej/script/ScriptFinder.java
@@ -38,6 +38,7 @@ import imagej.MenuEntry;
 import imagej.MenuPath;
 import imagej.command.Command;
 import imagej.command.CommandInfo;
+import imagej.log.LogService;
 
 import java.io.File;
 import java.util.HashMap;
@@ -60,10 +61,14 @@ public class ScriptFinder {
 	private static final String SPECIAL_SUBDIRECTORY = "Scripts";
 
 	private final ScriptService scriptService;
+	private final LogService log;
 
 	public ScriptFinder(final ScriptService scriptService) {
 		this.scriptService = scriptService;
+		log = scriptService.getLogService();
 	}
+
+	private int scriptCount;
 
 	/**
 	 * Discover the scripts
@@ -79,7 +84,9 @@ public class ScriptFinder {
 			final File pluginsDir = new File(directory, "plugins");
 			if (pluginsDir.isDirectory()) directory = pluginsDir;
 		}
+		scriptCount = 0;
 		discoverScripts(plugins, directory, null);
+		log.info("Found " + scriptCount + " scripts");
 	}
 
 	/**
@@ -107,8 +114,10 @@ public class ScriptFinder {
 				else discoverScripts(plugins, file, subMenuPath(path, file
 					.getName()));
 			}
-			else if (scriptService.canHandleFile(file)) plugins.add(createEntry(file,
-				subMenuPath(path, file.getName())));
+			else if (scriptService.canHandleFile(file)) {
+				plugins.add(createEntry(file, subMenuPath(path, file.getName())));
+				scriptCount++;
+			}
 	}
 
 	private MenuPath