melissalinkert/imagej

Script Editor: rename the plugin to ScriptEditor
ScriptEditorPlugin is a much more intuitive name for a plugin _for_
the Script Editor...

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

ui/awt-swing/swing/script-editor/src/main/java/imagej/script/editor/ScriptEditor.java
@@ -1,5 +1,5 @@
 //
-// ScriptEditorPlugin.java
+// ScriptEditor.java
 //
 
 /*
@@ -47,7 +47,7 @@ import imagej.ext.script.ScriptService;
  */
 @Plugin(menu = { @Menu(label = "File"), @Menu(label = "New"),
 	@Menu(label = "Script...", accelerator = "open_bracket") })
-public class ScriptEditorPlugin implements ImageJPlugin {
+public class ScriptEditor implements ImageJPlugin {
 
 	@Parameter
 	private ScriptService scriptService;

imagej/imagej2

ui/swing/script-editor/src/main/java/imagej/script/editor/ScriptEditor.java
@@ -1,5 +1,5 @@
 //
-// ScriptEditorPlugin.java
+// ScriptEditor.java
 //
 
 /*
@@ -47,7 +47,7 @@ import imagej.script.ScriptService;
  */
 @Plugin(menu = { @Menu(label = "File"), @Menu(label = "New"),
 	@Menu(label = "Script...", accelerator = "open_bracket") })
-public class ScriptEditorPlugin implements Command {
+public class ScriptEditor implements Command {
 
 	@Parameter
 	private ScriptService scriptService;