5408d2654848a3b1e1817668d6c18e829a53807e
Curtis Rueden <ctrueden@wisc.edu>
2012-11-02 17:25:09 -0500

    Results table: improve javadoc
    The javadoc for the List methods is now clearer about the fact that a
    table is specifically a list of columns (as opposed to  rows or cells).
    
    The method parameters have also been similarly tweaked, for clarity.

--- core/data/src/main/java/imagej/data/table/Table.java
+++ core/data/src/main/java/imagej/data/table/Table.java
@@ -35,7 +35,10 @@
 
 package imagej.data.table;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 /**
  * A table of values.
@@ -52,7 +55,7 @@ public interface Table<C extends Column<T>, T> extends List<C> {
 	/** Sets the number of columns in the table. */
 	void setColumnCount(int colCount);
 
-	/** Adds a column with the given header to the table. */
+	/** Appends a column with the given header to the table. */
 	Column<T> addColumn(String header);
 
 	/** Gets the number of rows in the table. */
@@ -61,10 +64,10 @@ public interface Table<C extends Column<T>, T> extends List<C> {
 	/** Sets the number of rows in the table. */
 	void setRowCount(int rowCount);
 
-	/** Adds a row (with no header) to the table. */
+	/** Appends a row (with no header) to the table. */
 	void addRow();
-	
-	/** Adds a row with the given header to the table. */
+
+	/** Appends a row with the given header to the table. */
 	void addRow(String header);
 
 	/** Sets the number of columns and rows in the table. */
@@ -91,6 +94,203 @@ public interface Table<C extends Column<T>, T> extends List<C> {
 	/** Gets the table value at the given column and row. */
 	T get(int col, int row);
 
+	/** Removes the row at the given position from the table. */
 	void removeRow(int rowNum);
+
+	// -- List methods --
+
+	/** Gets the number of columns in the table. */
+	@Override
+	int size();
+
+	/** Gets whether the table is empty. */
+	@Override
+	boolean isEmpty();
+
+	/**
+	 * Gets whether the table contains the given column.
+	 * 
+	 * @param column The {@link Column} whose presence in the table is to be
+	 *          tested.
+	 */
+	@Override
+	boolean contains(Object column);
+
+	/** Returns an iterator over the columns in the table in proper sequence. */
+	@Override
+	Iterator<C> iterator();
+
+	/**
+	 * Returns an array containing all of the columns in the table in proper
+	 * sequence (from first to last column).
+	 */
+	@Override
+	Object[] toArray();
+
+	/**
+	 * Returns an array containing all of the column in the table in proper
+	 * sequence (from first to last column); the runtime type of the returned
+	 * array is that of the specified array. If the list of columns fits in the
+	 * specified array, it is returned therein. Otherwise, a new array is
+	 * allocated with the runtime type of the specified array and the size of this
+	 * list of columns.
+	 */
+	@Override
+	<A> A[] toArray(A[] a);
+
+	/**
+	 * Appends the specified column to the end of the table.
+	 * <p>
+	 * No checking is done to ensure the new column has the same number of rows as
+	 * the other existing columns.
+	 * </p>
+	 */
+	@Override
+	boolean add(C column);
+
+	/**
+	 * Removes the first occurrence of the specified column from the table, if it
+	 * is present.
+	 * 
+	 * @return <tt>true</tt> if the table contained the specified column
+	 */
+	@Override
+	boolean remove(Object column);
+
+	/**
+	 * Returns <tt>true</tt> if the table contains all of the columns of the
+	 * specified collection.
+	 */
+	@Override
+	boolean containsAll(Collection<?> c);
+
+	/**
+	 * Appends all of the columns in the specified collection to the end of the
+	 * table, in the order that they are returned by the specified collection's
+	 * iterator.
+	 * <p>
+	 * No checking is done to ensure the new columns have the same number of rows
+	 * as the other existing columns.
+	 * </p>
+	 * 
+	 * @return <tt>true</tt> if the table changed as a result of the call
+	 */
+	@Override
+	boolean addAll(Collection<? extends C> c);
+
+	/**
+	 * Inserts all of the columns in the specified collection into this list at
+	 * the specified position.
+	 * <p>
+	 * No checking is done to ensure the new columns have the same number of rows
+	 * as the other existing columns.
+	 * </p>
+	 * 
+	 * @return <tt>true</tt> if the table changed as a result of the call
+	 */
+	@Override
+	boolean addAll(int col, Collection<? extends C> c);
+
+	/**
+	 * Removes from the table all of its columns that are contained in the
+	 * specified collection.
+	 * 
+	 * @return <tt>true</tt> if the table changed as a result of the call
+	 */
+	@Override
+	boolean removeAll(Collection<?> c);
+
+	/**
+	 * Retains only the columns in the table that are contained in the specified
+	 * collection. In other words, removes from the table all the columns that are
+	 * not contained in the specified collection.
+	 * 
+	 * @return <tt>true</tt> if the table changed as a result of the call
+	 */
+	@Override
+	boolean retainAll(Collection<?> c);
+
+	/**
+	 * Removes all data (including row and column headers) from the table. The
+	 * table will be empty after this call returns.
+	 * <p>
+	 * If you want to retain the column headers, call {@link #setRowCount(int)}
+	 * with a value of 0. If you want to retain the row headers, call
+	 * {@link #setColumnCount(int)} with a value of 0.
+	 * </p>
+	 */
+	@Override
+	void clear();
+
+	/** Returns the column at the specified position in the table. */
+	@Override
+	C get(int col);
+
+	/**
+	 * Replaces the column at the specified position in the table with the
+	 * specified column.
+	 * <p>
+	 * No checking is done to ensure the new column has the same number of rows as
+	 * the other existing columns.
+	 * </p>
+	 * 
+	 * @return the column previously at the specified position
+	 */
+	@Override
+	C set(int col, C column);
+
+	/**
+	 * Inserts the specified column at the specified position in the table.
+	 * <p>
+	 * No checking is done to ensure the new column has the same number of rows as
+	 * the other existing columns.
+	 * </p>
+	 */
+	@Override
+	void add(int col, C column);
+
+	/**
+	 * Removes the column at the specified position in the table.
+	 * 
+	 * @return the column previously at the specified position
+	 */
+	@Override
+	C remove(int col);
+
+	/**
+	 * Returns the index of the first occurrence of the specified column in the
+	 * table, or -1 if the table does not contain the column.
+	 */
+	@Override
+	int indexOf(Object column);
+
+	/**
+	 * Returns the index of the last occurrence of the specified column in the
+	 * table, or -1 if the table does not contain the column.
+	 */
+	@Override
+	int lastIndexOf(Object column);
+
+	/**
+	 * Returns a list iterator over the columns in the table (in proper sequence).
+	 */
+	@Override
+	ListIterator<C> listIterator();
+
+	/**
+	 * Returns a list iterator of the columns in the table (in proper sequence),
+	 * starting at the specified position in the table.
+	 */
+	@Override
+	ListIterator<C> listIterator(int col);
+
+	/**
+	 * Returns a view of the portion of the table between the specified
+	 * <tt>fromIndex</tt>, inclusive, and <tt>toIndex</tt>, exclusive. The
+	 * returned list is backed by the table, so non-structural changes in the
+	 * returned list are reflected in the table, and vice-versa.
+	 */
+	@Override
+	List<C> subList(int fromCol, int toCol);
 
 }
