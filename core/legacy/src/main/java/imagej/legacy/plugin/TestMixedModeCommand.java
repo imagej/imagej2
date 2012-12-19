62c86b4ee3d77467ce3e048aa35a57bebf75558d
Curtis Rueden <ctrueden@wisc.edu>
2012-12-19 13:21:32 -0600

    MixedWorldCommand: clean up style

--- core/legacy/src/main/java/imagej/legacy/plugin/TestMixedModeCommand.java
+++ core/legacy/src/main/java/imagej/legacy/plugin/TestMixedModeCommand.java
@@ -41,11 +41,10 @@ import imagej.command.Command;
 import imagej.legacy.LegacyOutputTracker;
 import imagej.plugin.Plugin;
 
-// Test to see this can work
-// a command that mixes legacy and modern code without dealing with the legacy
-// layer
-
 /**
+ * A command that mixes legacy and modern code without dealing with the legacy
+ * layer.
+ * 
  * @author Barry DeZonia
  */
 @Plugin(menuPath = "Plugins>Sandbox>Mixed World Command")
@@ -55,32 +54,40 @@ public class TestMixedModeCommand implements Command {
 	public void run() {
 		final String title = "junkolaTheFourth";
 		final int size = 100;
-		ByteProcessor proc = new ByteProcessor(size, size);
-		ImagePlus imp = new ImagePlus(title, proc);
+		final ByteProcessor proc = new ByteProcessor(size, size);
+		final ImagePlus imp = new ImagePlus(title, proc);
 		if (imp.getWidth() != size) System.out.println("Error: size wrong");
 		if (imp.getHeight() != size) System.out.println("Error: size wrong");
 		if (imp.getBitDepth() != 8) System.out.println("Error: bit depth wrong");
 		if (!imp.getTitle().equals(title)) System.out.println("Error: title wrong");
 		imp.show();
-		if (LegacyOutputTracker.containsOutput(imp)) System.out
-			.println("Error: output imp is in output list");
-		if (LegacyOutputTracker.containsClosed(imp)) System.out
-			.println("Error: output imp is in closed list");
+		if (LegacyOutputTracker.containsOutput(imp)) {
+			System.out.println("Error: output imp is in output list");
+		}
+		if (LegacyOutputTracker.containsClosed(imp)) {
+			System.out.println("Error: output imp is in closed list");
+		}
 		imp.repaintWindow();
-		if (LegacyOutputTracker.containsOutput(imp)) System.out
-			.println("Error: output imp is in output list");
-		if (LegacyOutputTracker.containsClosed(imp)) System.out
-			.println("Error: output imp is in closed list");
+		if (LegacyOutputTracker.containsOutput(imp)) {
+			System.out.println("Error: output imp is in output list");
+		}
+		if (LegacyOutputTracker.containsClosed(imp)) {
+			System.out.println("Error: output imp is in closed list");
+		}
 		imp.hide();
-		if (LegacyOutputTracker.containsOutput(imp)) System.out
-			.println("Error: output imp is in output list");
-		if (LegacyOutputTracker.containsClosed(imp)) System.out
-			.println("Error: output imp is in closed list");
+		if (LegacyOutputTracker.containsOutput(imp)) {
+			System.out.println("Error: output imp is in output list");
+		}
+		if (LegacyOutputTracker.containsClosed(imp)) {
+			System.out.println("Error: output imp is in closed list");
+		}
 		imp.close();
-		if (LegacyOutputTracker.containsOutput(imp)) System.out
-			.println("Error: output imp is in output list");
-		if (LegacyOutputTracker.containsClosed(imp)) System.out
-			.println("Error: output imp is in closed list");
+		if (LegacyOutputTracker.containsOutput(imp)) {
+			System.out.println("Error: output imp is in output list");
+		}
+		if (LegacyOutputTracker.containsClosed(imp)) {
+			System.out.println("Error: output imp is in closed list");
+		}
 		System.out
 			.println("If no messages before this then terminated successfully");
 	}
