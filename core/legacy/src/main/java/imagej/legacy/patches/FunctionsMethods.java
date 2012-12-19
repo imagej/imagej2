smorsi/imagej

CodeHacker: pass LegacyService instance to patched functions
We really need the instance of the legacy service in those functions...

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

core/legacy/src/main/java/imagej/legacy/patches/FunctionsMethods.java
@@ -37,6 +37,7 @@ package imagej.legacy.patches;
 
 import ij.ImagePlus;
 import ij.macro.Functions;
+import imagej.legacy.DefaultLegacyService;
 
 /**
  * Overrides {@link Functions} methods.
@@ -61,7 +62,7 @@ public class FunctionsMethods {
 	}
 
 	/** Prepends {@link ij.macro.Functions#displayBatchModeImage(ImagePlus)}. */
-	public static void displayBatchModeImageBefore(
+	public static void displayBatchModeImageBefore(@SuppressWarnings("unused") final DefaultLegacyService legacyService,
 		@SuppressWarnings("unused") final ImagePlus imp2)
 	{
 		// NOTE - BDZ - removing for now - see if any problems rear their head.
@@ -70,7 +71,7 @@ public class FunctionsMethods {
 	}
 
 	/** Appends {@link ij.macro.Functions#displayBatchModeImage(ImagePlus)}. */
-	public static void displayBatchModeImageAfter(
+	public static void displayBatchModeImageAfter(@SuppressWarnings("unused") final DefaultLegacyService legacyService,
 		@SuppressWarnings("unused") final ImagePlus imp2)
 	{
 		// NOTE - BDZ - removing for now - see if any problems rear their head.

core/legacy/src/main/java/imagej/legacy/patches/IJMethods.java
@@ -38,7 +38,7 @@ package imagej.legacy.patches;
 import ij.IJ;
 import imagej.ImageJ;
 import imagej.event.StatusService;
-import imagej.legacy.LegacyService;
+import imagej.legacy.DefaultLegacyService;
 import imagej.util.Log;
 
 /**
@@ -52,16 +52,16 @@ public class IJMethods {
 	private static final int PROGRESS_GRANULARITY = 1000;
 
 	/** Appends {@link IJ#showProgress(double)}. */
-	public static void showProgress(final double progress) {
+	public static void showProgress(final DefaultLegacyService legacyService, final double progress) {
 		// approximate progress as int ratio
 		final int currentIndex = (int) (PROGRESS_GRANULARITY * progress);
 		final int finalIndex = PROGRESS_GRANULARITY;
-		showProgress(currentIndex, finalIndex);
+		showProgress(legacyService, currentIndex, finalIndex);
 	}
 
 	/** Appends {@link IJ#showProgress(int, int)}. */
 	public static void
-		showProgress(final int currentIndex, final int finalIndex)
+		showProgress(final DefaultLegacyService legacyService, final int currentIndex, final int finalIndex)
 	{
 		Log.debug("showProgress: " + currentIndex + "/" + finalIndex);
 		// report progress through global event mechanism
@@ -71,9 +71,8 @@ public class IJMethods {
 	}
 
 	/** Appends {@link IJ#showStatus(String)}. */
-	public static void showStatus(final String s) {
+	public static void showStatus(final DefaultLegacyService legacyService, final String s) {
 		Log.debug("showStatus: " + s);
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		if (legacyService == null || !legacyService.isInitialized()) {
 			// suppress ImageJ1 bootup messages
 			return;

core/legacy/src/main/java/imagej/legacy/patches/ImageJMethods.java
@@ -36,6 +36,7 @@
 package imagej.legacy.patches;
 
 import ij.ImageJ;
+import imagej.legacy.DefaultLegacyService;
 import imagej.util.Log;
 
 import java.awt.Point;
@@ -49,6 +50,7 @@ public class ImageJMethods {
 
 	/** Replaces {@link ImageJ#getLocationOnScreen()}. */
 	public static Point getLocationOnScreen(
+		@SuppressWarnings("unused") final DefaultLegacyService legacyService,
 		@SuppressWarnings("unused") final ImageJ obj)
 	{
 		Log.debug("getLocationOnScreen");

core/legacy/src/main/java/imagej/legacy/patches/ImagePlusMethods.java
@@ -37,10 +37,9 @@ package imagej.legacy.patches;
 
 import ij.ImagePlus;
 import ij.WindowManager;
-import imagej.ImageJ;
 import imagej.data.display.ImageDisplay;
+import imagej.legacy.DefaultLegacyService;
 import imagej.legacy.LegacyOutputTracker;
-import imagej.legacy.LegacyService;
 import imagej.util.Log;
 
 /**
@@ -56,41 +55,38 @@ public final class ImagePlusMethods {
 	}
 
 	/** Appends {@link ImagePlus#updateAndDraw()}. */
-	public static void updateAndDraw(final ImagePlus obj) {
+	public static void updateAndDraw(final DefaultLegacyService legacyService, final ImagePlus obj) {
 		if (obj == null) return;
 		if (!obj.isProcessor()) return;
 		if (obj.getWindow() == null) return;
 		Log.debug("ImagePlus.updateAndDraw(): " + obj);
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		legacyService.legacyImageChanged(obj);
 		// TODO - add here too?
 		//WindowManager.setCurrentWindow(obj.getWindow());
 	}
 
 	/** Appends {@link ImagePlus#repaintWindow()}. */
-	public static void repaintWindow(final ImagePlus obj) {
+	public static void repaintWindow(final DefaultLegacyService legacyService, final ImagePlus obj) {
 		if (obj == null) return;
 		if (obj.getWindow() == null) return;
 		Log.debug("ImagePlus.repaintWindow(): " + obj);
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		legacyService.legacyImageChanged(obj);
 		// TODO - add here too?
 		//WindowManager.setCurrentWindow(obj.getWindow());
 	}
 
 	/** Appends {@link ImagePlus#show(String message)}. */
-	public static void show(final ImagePlus obj,
+	public static void show(final DefaultLegacyService legacyService, final ImagePlus obj,
 		@SuppressWarnings("unused") final String message)
 	{
 		if (obj == null) return;
 		Log.debug("ImagePlus.show(): " + obj);
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		legacyService.legacyImageChanged(obj);
 		WindowManager.setCurrentWindow(obj.getWindow());
 	}
 
 	/** Appends {@link ImagePlus#hide()}. */
-	public static void hide(final ImagePlus obj) {
+	public static void hide(final DefaultLegacyService legacyService, final ImagePlus obj) {
 		if (obj == null) return;
 		Log.debug("ImagePlus.hide(): " + obj);
 		LegacyOutputTracker.getOutputImps().remove(obj);
@@ -98,7 +94,6 @@ public final class ImagePlusMethods {
 		//LegacyOutputTracker.getClosedImps().add(obj);
 		// Alternate method
 		// begin alternate
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		ImageDisplay disp = legacyService.getImageMap().lookupDisplay(obj);
 		if (disp == null) {
 			legacyService.getImageMap().unregisterLegacyImage(obj);
@@ -110,7 +105,8 @@ public final class ImagePlusMethods {
 	}
 
 	/** Appends {@link ImagePlus#close()}. */
-	public static void close(final ImagePlus obj) {
+	// TODO: LegacyOutputTracker should not be a singleton
+	public static void close(@SuppressWarnings("unused") final DefaultLegacyService legacyService, final ImagePlus obj) {
 		if (obj == null) return;
 		// TODO - what is correct here?????
 		//else if (obj.getWindow() == null)

core/legacy/src/main/java/imagej/legacy/patches/ImageWindowMethods.java
@@ -38,9 +38,8 @@ package imagej.legacy.patches;
 import ij.ImagePlus;
 import ij.WindowManager;
 import ij.gui.ImageWindow;
-import imagej.ImageJ;
+import imagej.legacy.DefaultLegacyService;
 import imagej.legacy.LegacyOutputTracker;
-import imagej.legacy.LegacyService;
 import imagej.util.Log;
 
 /**
@@ -56,10 +55,9 @@ public final class ImageWindowMethods {
 	}
 
 	/** Replaces {@link ImageWindow#setVisible(boolean)}. */
-	public static void setVisible(final ImageWindow obj, final boolean visible) {
+	public static void setVisible(final DefaultLegacyService legacyService, final ImageWindow obj, final boolean visible) {
 		Log.debug("ImageWindow.setVisible(" + visible + "): " + obj);
 		if (!visible) return;
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		legacyService.legacyImageChanged(obj.getImagePlus());
 		// TODO - not sure this is correct. Does setVisible(true) imply that it
 		// becomes the current window? This arose in fixing a bug with 3d Project
@@ -68,12 +66,13 @@ public final class ImageWindowMethods {
 	}
 
 	/** Replaces {@link ImageWindow#show()}. */
-	public static void show(final ImageWindow obj) {
-		setVisible(obj, true);
+	public static void show(final DefaultLegacyService legacyService, final ImageWindow obj) {
+		setVisible(legacyService, obj, true);
 	}
 
 	/** Appends {@link ImageWindow#close()}. */
-	public static void close(final ImageWindow obj) {
+	// TODO: LegacyOutputTracker should not be a singleton
+	public static void close(@SuppressWarnings("unused") final DefaultLegacyService legacyService, final ImageWindow obj) {
 		final ImagePlus imp = obj.getImagePlus();
 		if ((imp != null) && (!LegacyOutputTracker.isBeingClosedbyIJ2(imp))) {
 			LegacyOutputTracker.getClosedImps().add(imp);

core/legacy/src/main/java/imagej/legacy/patches/PluginClassLoaderMethods.java
@@ -36,6 +36,7 @@
 package imagej.legacy.patches;
 
 import ij.io.PluginClassLoader;
+import imagej.legacy.DefaultLegacyService;
 import imagej.util.Log;
 
 import java.io.File;
@@ -72,23 +73,23 @@ public final class PluginClassLoaderMethods {
 	}
 
 	/** Appends {@link PluginClassLoader#init(String)}. */
-	public static void init(final PluginClassLoader obj, final String path) {
+	public static void init(@SuppressWarnings("unused") final DefaultLegacyService legacyService, final PluginClassLoader obj, final String path) {
 		final File pluginsDirectory = new File(path);
 		if (!pluginsDirectory.getName().equals("plugins")) return;
 		final File ij1Directory = pluginsDirectory.getParentFile();
-		if (ij1Directory != null) addJars(obj, new File(ij1Directory, "jars"));
+		if (ij1Directory != null) addJars(legacyService, obj, new File(ij1Directory, "jars"));
 	}
 
-	protected static void addJars(final PluginClassLoader obj, final File directory) {
+	protected static void addJars(@SuppressWarnings("unused") final DefaultLegacyService legacyService, final PluginClassLoader obj, final File directory) {
 		final File[] list = directory.listFiles();
 		if (list == null) return;
 		for (final File file : list) {
-			if (file.isDirectory()) addJars(obj, file);
-			else if (file.getName().endsWith(".jar")) addJar(obj, file);
+			if (file.isDirectory()) addJars(legacyService, obj, file);
+			else if (file.getName().endsWith(".jar")) addJar(legacyService, obj, file);
 		}
 	}
 
-	protected static void addJar(final PluginClassLoader obj, final File jar) {
+	protected static void addJar(@SuppressWarnings("unused") final DefaultLegacyService legacyService, final PluginClassLoader obj, final File jar) {
 		try {
 			addURL.invoke(obj, jar.toURI().toURL());
 		} catch (IllegalArgumentException e) {

imagej/imagej2

core/legacy/src/main/java/imagej/legacy/patches/FunctionsMethods.java
@@ -37,6 +37,7 @@ package imagej.legacy.patches;
 
 import ij.ImagePlus;
 import ij.macro.Functions;
+import imagej.legacy.LegacyService;
 
 /**
  * Overrides {@link Functions} methods.
@@ -61,7 +62,7 @@ public class FunctionsMethods {
 	}
 
 	/** Prepends {@link ij.macro.Functions#displayBatchModeImage(ImagePlus)}. */
-	public static void displayBatchModeImageBefore(
+	public static void displayBatchModeImageBefore(@SuppressWarnings("unused") final LegacyService legacyService,
 		@SuppressWarnings("unused") final ImagePlus imp2)
 	{
 		// NOTE - BDZ - removing for now - see if any problems rear their head.
@@ -70,7 +71,7 @@ public class FunctionsMethods {
 	}
 
 	/** Appends {@link ij.macro.Functions#displayBatchModeImage(ImagePlus)}. */
-	public static void displayBatchModeImageAfter(
+	public static void displayBatchModeImageAfter(@SuppressWarnings("unused") final LegacyService legacyService,
 		@SuppressWarnings("unused") final ImagePlus imp2)
 	{
 		// NOTE - BDZ - removing for now - see if any problems rear their head.

core/legacy/src/main/java/imagej/legacy/patches/IJMethods.java
@@ -52,16 +52,16 @@ public class IJMethods {
 	private static final int PROGRESS_GRANULARITY = 1000;
 
 	/** Appends {@link IJ#showProgress(double)}. */
-	public static void showProgress(final double progress) {
+	public static void showProgress(final LegacyService legacyService, final double progress) {
 		// approximate progress as int ratio
 		final int currentIndex = (int) (PROGRESS_GRANULARITY * progress);
 		final int finalIndex = PROGRESS_GRANULARITY;
-		showProgress(currentIndex, finalIndex);
+		showProgress(legacyService, currentIndex, finalIndex);
 	}
 
 	/** Appends {@link IJ#showProgress(int, int)}. */
 	public static void
-		showProgress(final int currentIndex, final int finalIndex)
+		showProgress(final LegacyService legacyService, final int currentIndex, final int finalIndex)
 	{
 		Log.debug("showProgress: " + currentIndex + "/" + finalIndex);
 		// report progress through global event mechanism
@@ -71,9 +71,8 @@ public class IJMethods {
 	}
 
 	/** Appends {@link IJ#showStatus(String)}. */
-	public static void showStatus(final String s) {
+	public static void showStatus(final LegacyService legacyService, final String s) {
 		Log.debug("showStatus: " + s);
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		if (legacyService == null || !legacyService.isInitialized()) {
 			// suppress ImageJ1 bootup messages
 			return;

core/legacy/src/main/java/imagej/legacy/patches/ImageJMethods.java
@@ -36,6 +36,7 @@
 package imagej.legacy.patches;
 
 import ij.ImageJ;
+import imagej.legacy.LegacyService;
 import imagej.util.Log;
 
 import java.awt.Point;
@@ -49,6 +50,7 @@ public class ImageJMethods {
 
 	/** Replaces {@link ImageJ#getLocationOnScreen()}. */
 	public static Point getLocationOnScreen(
+		@SuppressWarnings("unused") final LegacyService legacyService,
 		@SuppressWarnings("unused") final ImageJ obj)
 	{
 		Log.debug("getLocationOnScreen");

core/legacy/src/main/java/imagej/legacy/patches/ImagePlusMethods.java
@@ -37,7 +37,6 @@ package imagej.legacy.patches;
 
 import ij.ImagePlus;
 import ij.WindowManager;
-import imagej.ImageJ;
 import imagej.data.display.ImageDisplay;
 import imagej.legacy.LegacyOutputTracker;
 import imagej.legacy.LegacyService;
@@ -57,44 +56,41 @@ public final class ImagePlusMethods {
 	}
 
 	/** Appends {@link ImagePlus#updateAndDraw()}. */
-	public static void updateAndDraw(final ImagePlus obj) {
+	public static void updateAndDraw(final LegacyService legacyService, final ImagePlus obj) {
 		if (obj == null) return;
 		if (!obj.isProcessor()) return;
 		if (obj.getWindow() == null) return;
 		if (!Utils.isLegacyThread(Thread.currentThread())) return;
 		Log.debug("ImagePlus.updateAndDraw(): " + obj);
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		legacyService.legacyImageChanged(obj);
 		// TODO - add here too?
 		//WindowManager.setCurrentWindow(obj.getWindow());
 	}
 
 	/** Appends {@link ImagePlus#repaintWindow()}. */
-	public static void repaintWindow(final ImagePlus obj) {
+	public static void repaintWindow(final LegacyService legacyService, final ImagePlus obj) {
 		if (obj == null) return;
 		if (obj.getWindow() == null) return;
 		if (!Utils.isLegacyThread(Thread.currentThread())) return;
 		Log.debug("ImagePlus.repaintWindow(): " + obj);
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		legacyService.legacyImageChanged(obj);
 		// TODO - add here too?
 		//WindowManager.setCurrentWindow(obj.getWindow());
 	}
 
 	/** Appends {@link ImagePlus#show(String message)}. */
-	public static void show(final ImagePlus obj,
+	public static void show(final LegacyService legacyService, final ImagePlus obj,
 		@SuppressWarnings("unused") final String message)
 	{
 		if (obj == null) return;
 		if (!Utils.isLegacyThread(Thread.currentThread())) return;
 		Log.debug("ImagePlus.show(): " + obj);
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		legacyService.legacyImageChanged(obj);
 		WindowManager.setCurrentWindow(obj.getWindow());
 	}
 
 	/** Appends {@link ImagePlus#hide()}. */
-	public static void hide(final ImagePlus obj) {
+	public static void hide(final LegacyService legacyService, final ImagePlus obj) {
 		if (obj == null) return;
 		if (!Utils.isLegacyThread(Thread.currentThread())) return;
 		Log.debug("ImagePlus.hide(): " + obj);
@@ -103,7 +99,6 @@ public final class ImagePlusMethods {
 		//LegacyOutputTracker.getClosedImps().add(obj);
 		// Alternate method
 		// begin alternate
-		final LegacyService legacyService = ImageJ.get(LegacyService.class);
 		ImageDisplay disp = legacyService.getImageMap().lookupDisplay(obj);
 		if (disp == null) {
 			legacyService.getImageMap().unregisterLegacyImage(obj);
@@ -115,7 +110,8 @@ public final class ImagePlusMethods {
 	}
 
 	/** Appends {@link ImagePlus#close()}. */
-	public static void close(final ImagePlus obj) {
+	// TODO: LegacyOutputTracker should not be a singleton
+	public static void close(@SuppressWarnings("unused") final LegacyService legacyService, final ImagePlus obj) {
 		if (obj == null) return;
 		if (!Utils.isLegacyThread(Thread.currentThread())) return;
 		LegacyOutputTracker.addClosed(obj);

core/legacy/src/main/java/imagej/legacy/patches/ImageWindowMethods.java
@@ -38,7 +38,6 @@ package imagej.legacy.patches;
 import ij.ImagePlus;
 import ij.WindowManager;
 import ij.gui.ImageWindow;
-import imagej.ImageJ;
 import imagej.legacy.LegacyOutputTracker;
 import imagej.legacy.LegacyService;
 import imagej.legacy.Utils;
@@ -57,11 +56,10 @@ public final class ImageWindowMethods {
 	}
 
 	/** Replaces {@link ImageWindow#setVisible(boolean)}. */
-	public static void setVisible(final ImageWindow obj, final boolean visible) {
+	public static void setVisible(final LegacyService legacyService, final ImageWindow obj, final boolean visible) {
 		Log.debug("ImageWindow.setVisible(" + visible + "): " + obj);
 		if (!visible) return;
 		if (Utils.isLegacyThread(Thread.currentThread())) {
-			final LegacyService legacyService = ImageJ.get(LegacyService.class);
 			legacyService.legacyImageChanged(obj.getImagePlus());
 		}
 		// TODO - not sure this is correct. Does setVisible(true) imply that it
@@ -71,12 +69,12 @@ public final class ImageWindowMethods {
 	}
 
 	/** Replaces {@link ImageWindow#show()}. */
-	public static void show(final ImageWindow obj) {
-		setVisible(obj, true);
+	public static void show(final LegacyService legacyService, final ImageWindow obj) {
+		setVisible(legacyService, obj, true);
 	}
 
 	/** Prepends {@link ImageWindow#close()}. */
-	public static void close(final ImageWindow obj) {
+	public static void close(@SuppressWarnings("unused") final LegacyService legacyService, final ImageWindow obj) {
 		if (!Utils.isLegacyThread(Thread.currentThread())) return;
 		final ImagePlus imp = obj.getImagePlus();
 		if (imp == null) return;

core/legacy/src/main/java/imagej/legacy/patches/PluginClassLoaderMethods.java
@@ -36,6 +36,7 @@
 package imagej.legacy.patches;
 
 import ij.io.PluginClassLoader;
+import imagej.legacy.LegacyService;
 import imagej.util.Log;
 
 import java.io.File;
@@ -72,23 +73,23 @@ public final class PluginClassLoaderMethods {
 	}
 
 	/** Appends {@link PluginClassLoader#init(String)}. */
-	public static void init(final PluginClassLoader obj, final String path) {
+	public static void init(@SuppressWarnings("unused") final LegacyService legacyService, final PluginClassLoader obj, final String path) {
 		final File pluginsDirectory = new File(path);
 		if (!pluginsDirectory.getName().equals("plugins")) return;
 		final File ij1Directory = pluginsDirectory.getParentFile();
-		if (ij1Directory != null) addJars(obj, new File(ij1Directory, "jars"));
+		if (ij1Directory != null) addJars(legacyService, obj, new File(ij1Directory, "jars"));
 	}
 
-	protected static void addJars(final PluginClassLoader obj, final File directory) {
+	protected static void addJars(@SuppressWarnings("unused") final LegacyService legacyService, final PluginClassLoader obj, final File directory) {
 		final File[] list = directory.listFiles();
 		if (list == null) return;
 		for (final File file : list) {
-			if (file.isDirectory()) addJars(obj, file);
-			else if (file.getName().endsWith(".jar")) addJar(obj, file);
+			if (file.isDirectory()) addJars(legacyService, obj, file);
+			else if (file.getName().endsWith(".jar")) addJar(legacyService, obj, file);
 		}
 	}
 
-	protected static void addJar(final PluginClassLoader obj, final File jar) {
+	protected static void addJar(@SuppressWarnings("unused") final LegacyService legacyService, final PluginClassLoader obj, final File jar) {
 		try {
 			addURL.invoke(obj, jar.toURI().toURL());
 		} catch (IllegalArgumentException e) {