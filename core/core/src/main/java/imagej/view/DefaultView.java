2021a7b32fa5e5df3a7a8c2216aa4b53df1eae36
Curtis Rueden <ctrueden@wisc.edu>
2013-02-05 16:42:39 -0600

    Create new generic View classes

---
+++ core/core/src/main/java/imagej/view/DefaultView.java
@@ -0,0 +1,82 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2013 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.view;
+
+import org.scijava.Priority;
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.SortablePlugin;
+
+/**
+ * Default implementation for {@link View}s.
+ * 
+ * @author Curtis Rueden
+ */
+@Plugin(type = View.class, priority = Priority.VERY_LOW_PRIORITY)
+public class DefaultView<D> extends SortablePlugin implements View<D> {
+
+	/** Class of linked data objects. */
+	private final Class<D> dataClass;
+
+	/** Linked data object. */
+	private D data;
+
+	public DefaultView(final Class<D> dataClass) {
+		this.dataClass = dataClass;
+	}
+
+	// -- View methods --
+
+	@Override
+	public Class<D> getDataClass() {
+		return dataClass;
+	}
+
+	@Override
+	public boolean isCompatible(final Object object) {
+		return dataClass.isInstance(data);
+	}
+
+	@Override
+	public void setData(final D data) {
+		this.data = data;
+	}
+
+	@Override
+	public D getData() {
+		return data;
+	}
+
+}
