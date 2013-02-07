7865cc6b48e241c83b35fd8ecbbdb4ca50c60b3b
Barry DeZonia <bdezonia@wisc.edu>
2013-02-07 13:22:41 -0600

    Make threshold dialog interactive and start adding functionality

--- core/commands/display/src/main/java/imagej/core/commands/display/interactive/Threshold.java
+++ core/commands/display/src/main/java/imagej/core/commands/display/interactive/Threshold.java
@@ -35,17 +35,22 @@
 
 package imagej.core.commands.display.interactive;
 
-import imagej.command.ContextCommand;
 import imagej.data.display.ImageDisplay;
+import imagej.data.display.ImageDisplayService;
 import imagej.data.overlay.ThresholdOverlay;
 import imagej.data.overlay.ThresholdService;
 import imagej.menu.MenuConstants;
+import imagej.module.DefaultModuleItem;
 import imagej.module.ItemIO;
 import imagej.options.OptionsService;
 import imagej.plugin.Menu;
 import imagej.plugin.Parameter;
 import imagej.plugin.Plugin;
-import imagej.util.ColorRGB;
+import imagej.util.Colors;
+import imagej.widget.Button;
+import net.imglib2.algorithm.stats.ComputeMinMax;
+import net.imglib2.img.Img;
+import net.imglib2.type.numeric.RealType;
 
 /**
  * @author Barry DeZonia
@@ -55,27 +60,50 @@ import imagej.util.ColorRGB;
 		mnemonic = MenuConstants.IMAGE_MNEMONIC), @Menu(label = "Adjust"),
 	@Menu(label = "Threshold...", accelerator = "control shift T") },
 	initializer = "initValues")
-public class Threshold extends ContextCommand {
+public class Threshold extends InteractiveCommand {
 
 	// -- Parameters --
-
-	@Parameter(type = ItemIO.BOTH)
-	private ImageDisplay display;
-
-	@Parameter(label = "Minimum", persist = false, callback = "rangeChanged")
-	private double min;
-
-	@Parameter(label = "Maximum", persist = false, callback = "rangeChanged")
-	private double max;
-
-	@Parameter(label = "Color", persist = false, callback = "colorChanged")
-	private ColorRGB color;
 
 	@Parameter
 	private ThresholdService threshSrv;
 
 	@Parameter
 	private OptionsService optionsSrv;
+
+	@Parameter
+	private ImageDisplayService imgDispSrv;
+
+	@Parameter(type = ItemIO.BOTH)
+	private ImageDisplay display;
+
+	@Parameter(label = "Method",
+		choices = { "Method 1", "Method 2", "Method 3" }, persist = false)
+	private String method;
+
+	@Parameter(label = "Display type", choices = { "Red", "B&W", "Over/Under" },
+		callback = "displayTypeChanged", persist = false)
+	private String displayType;
+
+	@Parameter(label = "Auto", callback = "autoThreshold")
+	private Button auto;
+
+	@Parameter(label = "Apply", callback = "changePixels")
+	private Button apply;
+
+	@Parameter(label = "Reset", callback = "deleteThreshold")
+	private Button reset;
+
+	@Parameter(label = "Dark Background", callback = "backgroundChange",
+		persist = false)
+	private boolean darkBackground;
+
+	@Parameter(label = "Stack Histogram", callback = "stackHistogram",
+		persist = false)
+	private boolean stackHistogram;
+
+	// -- instance variables --
+
+	private double dataMin, dataMax;
 
 	// -- accessors --
 
@@ -91,42 +119,99 @@ public class Threshold extends ContextCommand {
 
 	@Override
 	public void run() {
-		// TODO - we used to cancel() if min > max. What now? Callbacks keep them
-		// legal?
-		threshSrv.getThreshold(display).setRange(min, max);
+		// nothing to do
 	}
-
 
 	// -- callbacks --
 
+	@SuppressWarnings("unchecked")
 	protected void initValues() {
 
-		// TODO : cannot call service.getThreshold() all the time. Otherwise one can
-		// create a thresh that doesn't get thrown away if you cancel this dialog
+		computeDataMinMax(getImg());
 
-		if (threshSrv.hasThreshold(display)) {
-			ThresholdOverlay overlay = threshSrv.getThreshold(display);
-			min = overlay.getRangeMin();
-			max = overlay.getRangeMax();
-			color = overlay.getFillColor();
-		}
-		else { // no thresh exists: get defaults
-			min = threshSrv.getDefaultRangeMin();
-			max = threshSrv.getDefaultRangeMax();
-			color = threshSrv.getDefaultColor();
-		}
+		ThresholdOverlay overlay = threshSrv.getThreshold(display);
+
+		// color = overlay.getFillColor();
+
+		DefaultModuleItem<Double> minItem =
+			new DefaultModuleItem<Double>(getInfo(), "Minimum", Double.class);
+		minItem.setCallback("rangeChanged");
+		minItem.setMinimumValue(dataMin);
+		minItem.setMaximumValue(dataMax);
+		minItem.setValue(this, overlay.getRangeMin());
+		minItem.setPersisted(false);
+		getInfo().addInput(minItem);
+
+		DefaultModuleItem<Double> maxItem =
+			new DefaultModuleItem<Double>(getInfo(), "Maximum", Double.class);
+		maxItem.setCallback("rangeChanged");
+		maxItem.setMinimumValue(dataMin);
+		maxItem.setMaximumValue(dataMax);
+		maxItem.setValue(this, overlay.getRangeMax());
+		maxItem.setPersisted(false);
+		getInfo().addInput(maxItem);
 	}
 
 	protected void rangeChanged() {
+		double min = (Double) getInput("Minimum");
+		double max = (Double) getInput("Maximum");
 		ThresholdOverlay overlay = threshSrv.getThreshold(display);
 		overlay.setRange(min, max);
 		overlay.update();
 	}
 
-	protected void colorChanged() {
+	protected void displayTypeChanged() {
 		ThresholdOverlay overlay = threshSrv.getThreshold(display);
-		overlay.setFillColor(color);
+		overlay.setFillColor(Colors.RED); // TODO - temp hack
 		overlay.update();
 	}
 
+	protected void autoThreshold() {
+		// TODO
+		System.out.println("UNIMPLEMENTED");
+	}
+
+	protected void deleteThreshold() {
+		threshSrv.removeThreshold(display);
+	}
+
+	protected void changePixels() {
+		// TODO
+		System.out.println("UNIMPLEMENTED");
+	}
+
+	protected void backgroundChange() {
+		ThresholdOverlay overlay = threshSrv.getThreshold(display);
+		double min, max;
+		// if (darkBackground) {
+		min = dataMax - overlay.getRangeMax() + dataMin;
+		max = dataMax - overlay.getRangeMin() + dataMin;
+		// }
+		// else { // light background
+		// min = ;
+		// max = ;
+		// }
+		overlay.setRange(min, max);
+		overlay.update();
+	}
+
+	protected void stackHistogram() {
+		// TODO
+		System.out.println("UNIMPLEMENTED");
+	}
+
+	// -- helpers --
+
+	private <T extends RealType<T>> void computeDataMinMax(final Img<T> img) {
+		final ComputeMinMax<T> computeMinMax = new ComputeMinMax<T>(img);
+		computeMinMax.process();
+		dataMin = computeMinMax.getMin().getRealDouble();
+		dataMax = computeMinMax.getMax().getRealDouble();
+		log.debug("computeDataMinMax: dataMin=" + dataMin + ", dataMax=" + dataMax);
+	}
+
+	@SuppressWarnings("rawtypes")
+	private Img getImg() {
+		return imgDispSrv.getActiveDataset(display).getImgPlus();
+	}
 }
