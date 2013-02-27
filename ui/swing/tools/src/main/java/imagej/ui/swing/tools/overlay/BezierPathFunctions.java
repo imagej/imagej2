db1ee3ee180e55b8e10f32615894100b7d7509d0
Curtis Rueden <ctrueden@wisc.edu>
2013-02-27 09:51:23 -0600

    BezierPathFunctions: clean up style & add javadoc

--- ui/swing/tools/src/main/java/imagej/ui/swing/tools/overlay/BezierPathFunctions.java
+++ ui/swing/tools/src/main/java/imagej/ui/swing/tools/overlay/BezierPathFunctions.java
@@ -52,93 +52,119 @@ import net.imglib2.roi.GeneralPathRegionOfInterest;
 import org.jhotdraw.geom.BezierPath;
 
 /**
- * TODO
+ * Utility functions for {@link GeneralPathJHotDrawAdapter}.
  * 
  * @author Johannes Schindelin
  */
 public class BezierPathFunctions {
+
 	public static BezierPath toBezierPath(final PathIterator iterator) {
 		final BezierPath path = new BezierPath();
-		double[] segment = new double[6];
+		final double[] segment = new double[6];
 		for (; !iterator.isDone(); iterator.next()) {
-			int type = iterator.currentSegment(segment);
+			final int type = iterator.currentSegment(segment);
 			switch (type) {
-			case PathIterator.SEG_MOVETO:
-				path.moveTo(segment[0], segment[1]);
-				break;
-			case PathIterator.SEG_LINETO:
-				path.lineTo(segment[0], segment[1]);
-				break;
-			case PathIterator.SEG_QUADTO:
-				path.quadTo(segment[0], segment[1], segment[2], segment[3]);
-				break;
-			case PathIterator.SEG_CUBICTO:
-				path.curveTo(segment[0], segment[1], segment[2], segment[3], segment[4], segment[5]);
-				break;
-			case PathIterator.SEG_CLOSE:
-				path.setClosed(true);
-				break;
+				case PathIterator.SEG_MOVETO:
+					path.moveTo(segment[0], segment[1]);
+					break;
+				case PathIterator.SEG_LINETO:
+					path.lineTo(segment[0], segment[1]);
+					break;
+				case PathIterator.SEG_QUADTO:
+					path.quadTo(segment[0], segment[1], segment[2], segment[3]);
+					break;
+				case PathIterator.SEG_CUBICTO:
+					path.curveTo(segment[0], segment[1], segment[2], segment[3],
+						segment[4], segment[5]);
+					break;
+				case PathIterator.SEG_CLOSE:
+					path.setClosed(true);
+					break;
 			}
 		}
 		return path;
 	}
 
-	public static void addToRegionOfInterest(final BezierPath path, final GeneralPathRegionOfInterest roi) {
+	public static void addToRegionOfInterest(final BezierPath path,
+		final GeneralPathRegionOfInterest roi)
+	{
 		addToRegionOfInterest(path.toGeneralPath().getPathIterator(null), roi);
 	}
 
-	public static void addToRegionOfInterest(final PathIterator iterator, final GeneralPathRegionOfInterest roi) {
+	public static void addToRegionOfInterest(final PathIterator iterator,
+		final GeneralPathRegionOfInterest roi)
+	{
 		final double[] segment = new double[6];
 		for (; !iterator.isDone(); iterator.next()) {
-			int type = iterator.currentSegment(segment);
+			final int type = iterator.currentSegment(segment);
 			switch (type) {
-			case PathIterator.SEG_MOVETO:
-				roi.moveTo(segment[0], segment[1]);
-				break;
-			case PathIterator.SEG_LINETO:
-				roi.lineTo(segment[0], segment[1]);
-				break;
-			case PathIterator.SEG_QUADTO:
-				roi.quadTo(segment[0], segment[1], segment[2], segment[3]);
-				break;
-			case PathIterator.SEG_CUBICTO:
-				roi.cubicTo(segment[0], segment[1], segment[2], segment[3], segment[4], segment[5]);
-				break;
-			case PathIterator.SEG_CLOSE:
-				roi.close();
-				break;
-			default:
-				throw new UnsupportedOperationException();
+				case PathIterator.SEG_MOVETO:
+					roi.moveTo(segment[0], segment[1]);
+					break;
+				case PathIterator.SEG_LINETO:
+					roi.lineTo(segment[0], segment[1]);
+					break;
+				case PathIterator.SEG_QUADTO:
+					roi.quadTo(segment[0], segment[1], segment[2], segment[3]);
+					break;
+				case PathIterator.SEG_CUBICTO:
+					roi.cubicTo(segment[0], segment[1], segment[2], segment[3],
+						segment[4], segment[5]);
+					break;
+				case PathIterator.SEG_CLOSE:
+					roi.close();
+					break;
+				default:
+					throw new UnsupportedOperationException();
 			}
 		}
 	}
 
-	public enum OP { ADD, XOR, INTERSECT, SUBTRACT }
+	public enum OP {
+		ADD, XOR, INTERSECT, SUBTRACT
+	}
 
-	public static BezierPath add(final BezierPath path1, final BezierPath path2) {
+	public static BezierPath add(final BezierPath path1, final BezierPath path2)
+	{
 		return op(path1, path2, OP.ADD);
 	}
 
-	public static BezierPath exclusiveOr(final BezierPath path1, final BezierPath path2) {
+	public static BezierPath exclusiveOr(final BezierPath path1,
+		final BezierPath path2)
+	{
 		return op(path1, path2, OP.XOR);
 	}
 
-	public static BezierPath intersect(final BezierPath path1, final BezierPath path2) {
+	public static BezierPath intersect(final BezierPath path1,
+		final BezierPath path2)
+	{
 		return op(path1, path2, OP.INTERSECT);
 	}
 
-	public static BezierPath subtract(final BezierPath path1, final BezierPath path2) {
+	public static BezierPath subtract(final BezierPath path1,
+		final BezierPath path2)
+	{
 		return op(path1, path2, OP.SUBTRACT);
 	}
 
-	public static BezierPath op(final BezierPath path1, final BezierPath path2, final OP op) {
+	public static BezierPath op(final BezierPath path1, final BezierPath path2,
+		final OP op)
+	{
 		final Area area1 = new Area(path1.toGeneralPath());
 		final Area area2 = new Area(path2.toGeneralPath());
 		switch (op) {
-		case ADD: area1.add(area2); break;
-		case XOR: area1.exclusiveOr(area2); break;
-		case INTERSECT: area1.intersect(area2); break;
-		case SUBTRACT: area1.subtract(area2); break;
+			case ADD:
+				area1.add(area2);
+				break;
+			case XOR:
+				area1.exclusiveOr(area2);
+				break;
+			case INTERSECT:
+				area1.intersect(area2);
+				break;
+			case SUBTRACT:
+				area1.subtract(area2);
+				break;
 		}
 		return toBezierPath(area1.getPathIterator(new AffineTransform()));
 	}
@@ -150,37 +176,38 @@ public class BezierPathFunctions {
 
 	@SuppressWarnings("serial")
 	private static void show(final Shape shape) {
-		JFrame frame = new JFrame("debug");
-		JPanel panel = new JPanel() {
+		final JFrame frame = new JFrame("debug");
+		final JPanel panel = new JPanel() {
+
 			@Override
-			public void paint(Graphics graphics) {
-				Graphics2D graphics2D = (Graphics2D)graphics;
+			public void paint(final Graphics graphics) {
+				final Graphics2D graphics2D = (Graphics2D) graphics;
 				graphics2D.draw(shape);
 			}
 		};
 		frame.getContentPane().add(panel);
-		Rectangle bounds = shape.getBounds();
+		final Rectangle bounds = shape.getBounds();
 		frame.setSize(bounds.x + bounds.width, bounds.y + bounds.height);
 		frame.pack();
 		frame.setVisible(true);
 	}
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		BezierPath path1 = new BezierPath();
 		path1.moveTo(0, 0);
 		path1.lineTo(100, 0);
 		path1.lineTo(100, 100);
 		path1.lineTo(0, 0);
 
-		GeneralPath path2 = new GeneralPath();
+		final GeneralPath path2 = new GeneralPath();
 		path2.moveTo(0, 0);
 		path2.lineTo(100, 0);
 		path2.lineTo(100, 100);
 		path2.closePath();
 
-		//path1 = toBezierPath(path2.getPathIterator(new AffineTransform()));
+		// path1 = toBezierPath(path2.getPathIterator(new AffineTransform()));
 
-		BezierPath path3 = new BezierPath();
+		final BezierPath path3 = new BezierPath();
 		path3.moveTo(0, 100);
 		path3.lineTo(100, 0);
 		path3.lineTo(100, 100);
@@ -190,4 +217,5 @@ public class BezierPathFunctions {
 
 		show(path1.toGeneralPath());
 	}
+
 }
