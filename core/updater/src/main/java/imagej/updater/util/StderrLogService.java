5d8f2b0d2b7b35ab815db855ed270b8b1e32dea9
Curtis Rueden <ctrueden@wisc.edu>
2013-02-27 16:17:58 -0600

    StderrLogService: implement new dispose() method
    In the scijava-common framework, Service now extends Disposable, so that
    services (and their context) can properly clean up resources if needed.

--- core/updater/src/main/java/imagej/updater/util/StderrLogService.java
+++ core/updater/src/main/java/imagej/updater/util/StderrLogService.java
@@ -52,6 +52,7 @@ import org.scijava.log.LogService;
 @Deprecated
 public class StderrLogService implements LogService {
 	@Override public void initialize() { }
+	@Override public void dispose() { }
 	@Override public Context getContext() { return null; }
 	@Override public void setContext(Context context) { }
 	@Override public double getPriority() { return 0; }
