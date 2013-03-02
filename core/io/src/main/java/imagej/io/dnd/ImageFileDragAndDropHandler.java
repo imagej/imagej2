0aa3caa23df79445339cff85e25f420d3ca16139
Curtis Rueden <ctrueden@wisc.edu>
2013-03-02 09:22:09 -0600

    Add drag-and-drop handler for image files

---
+++ core/io/src/main/java/imagej/io/dnd/ImageFileDragAndDropHandler.java
@@ -0,0 +1,109 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2013 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.io.dnd;
+
+import imagej.data.Dataset;
+import imagej.display.Display;
+import imagej.display.DisplayService;
+import imagej.io.IOService;
+import imagej.ui.dnd.AbstractDragAndDropHandler;
+import imagej.ui.dnd.DragAndDropData;
+import imagej.ui.dnd.DragAndDropHandler;
+
+import java.io.File;
+import java.util.List;
+
+import net.imglib2.exception.IncompatibleTypeException;
+import net.imglib2.io.ImgIOException;
+
+import org.scijava.log.LogService;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Drag-and-drop handler for image files.
+ * 
+ * @author Curtis Rueden
+ * @author Barry DeZonia
+ */
+@Plugin(type = DragAndDropHandler.class)
+public class ImageFileDragAndDropHandler extends AbstractDragAndDropHandler {
+
+	private static final String MIME_TYPE =
+		"application/x-java-file-list; class=java.util.List";
+
+	@Override
+	public boolean isCompatible(final Display<?> display,
+		final DragAndDropData data)
+	{
+		for (final String mimeType : data.getMimeTypes()) {
+			if (MIME_TYPE.equals(mimeType)) return true;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean drop(final Display<?> display, final DragAndDropData data) {
+		final IOService ioService = getContext().getService(IOService.class);
+		final DisplayService displayService =
+			getContext().getService(DisplayService.class);
+		if (ioService == null || displayService == null) return false;
+
+		final LogService log = getContext().getService(LogService.class);
+
+		@SuppressWarnings("unchecked")
+		final List<File> files = (List<File>) data.getData(MIME_TYPE);
+
+		// load each file
+		boolean success = true;
+		for (final File file : files) {
+			final String path = file.getAbsolutePath();
+			try {
+				final Dataset dataset = ioService.loadDataset(file.getAbsolutePath());
+				displayService.createDisplay(dataset);
+			}
+			catch (final ImgIOException exc) {
+				if (log != null) log.error("Error opening file: " + path, exc);
+				success = false;
+			}
+			catch (final IncompatibleTypeException exc) {
+				if (log != null) log.error("Error opening file: " + path, exc);
+				success = false;
+			}
+		}
+		return success;
+	}
+
+}
