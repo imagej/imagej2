684a137112bedb3fb601dcfde04acdc8e1d9d643
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-06-26 14:57:46 -0500

    ij-legacy: stop the Java Agent whenever CodeHacker loads classes
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- core/legacy/src/main/java/imagej/legacy/CodeHacker.java
+++ core/legacy/src/main/java/imagej/legacy/CodeHacker.java
@@ -666,6 +666,12 @@ public class CodeHacker {
 	}
 
 	public void loadClasses() {
+		try {
+			LegacyJavaAgent.stop();
+		} catch (Throwable t) {
+			// ignore
+		}
+
 		final Iterator<CtClass> iter = handledClasses.iterator();
 		while (iter.hasNext()) {
 			final CtClass classRef = iter.next();
