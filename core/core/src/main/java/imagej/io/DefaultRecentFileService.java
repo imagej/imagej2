389a95854d0355dff306f8ea324f955f72492108
Curtis Rueden <ctrueden@wisc.edu>
2013-06-28 15:16:16 -0500

    Remove obsolete ij-io component
    As of the io-plugins merge (c239a613766ecc6d33a479d523c3fea003e1d790),
    the I/O system has been sufficiently modularized to avoid the need for a
    dedicated ij-io component at all.

--- core/core/src/main/java/imagej/io/DefaultRecentFileService.java
+++ core/core/src/main/java/imagej/io/DefaultRecentFileService.java
@@ -191,7 +191,7 @@ public final class DefaultRecentFileService extends AbstractService implements
 
 	/** Creates a {@link ModuleInfo} to reopen data at the given path. */
 	private ModuleInfo createInfo(final String path) {
-		// CTR FIXME: Avoid circular dependency between ij-io and ij-commands.
+		// CTR FIXME: Avoid circular dependency between ij-core and ij-commands.
 		final String commandClassName = "imagej.core.commands.io.OpenFile";
 		final CommandInfo info = new CommandInfo(commandClassName);
 
