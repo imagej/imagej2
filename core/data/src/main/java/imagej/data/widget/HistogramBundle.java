02834941baed03cf39dd396b31c62a5a8fe08d16
Barry DeZonia <bdezonia@wisc.edu>
2013-07-01 11:45:16 -0500

    Improve javadoc and style

--- core/data/src/main/java/imagej/data/widget/HistogramBundle.java
+++ core/data/src/main/java/imagej/data/widget/HistogramBundle.java
@@ -38,18 +38,22 @@ package imagej.data.widget;
 import net.imglib2.histogram.Histogram1d;
 
 /**
- * A wrapper class around {@link Histogram1d}.
- * 
- * This class provides inferred metadata: min and max.
+ * HistogramBundles marry a {@link Histogram1d} with rendering information.
  * 
  * @author Barry DeZonia
  */
 public class HistogramBundle {
 
+	// -- fields --
+
 	private Histogram1d<?> histogram;
-	private long min;
-	private long max;
+	private long min = -1;
+	private long max = -1;
+	private int preferredSizeX = 300;
+	private int preferredSizeY = 150;
 	private boolean hasChanges;
+
+	// -- constructors --
 
 	public HistogramBundle(Histogram1d<?> hist, long min, long max) {
 		this.histogram = hist;
@@ -58,37 +62,98 @@ public class HistogramBundle {
 		hasChanges = true;
 	}
 
+	// -- accessors --
+
+	/**
+	 * Sets the histogram of interest for this HistogramBundle.
+	 */
 	public void setHistogram(Histogram1d<?> hist) {
 		hasChanges |= hist != this.histogram;
 		this.histogram = hist;
 	}
 
+	/**
+	 * Gets the histogram of interest for this HistogramBundle.
+	 */
+	public Histogram1d<?> getHistogram() {
+		return histogram;
+	}
+
+	/**
+	 * Sets the bin number of the minimum value. Can be used to by renderers to
+	 * display the minimum line on the histogram for example. A value of -1 notes
+	 * that the minimum value is not of interest.
+	 */
 	public void setMin(long min) {
 		hasChanges |= min != this.min;
 		this.min = min;
 	}
 
+	/**
+	 * Gets the bin number of the minimum value. A value of -1 notes the minimum
+	 * value is not of interest.
+	 */
+	public long getMin() {
+		return min;
+	}
+
+	/**
+	 * Sets the bin number of the maximum value. Can be used to by renderers to
+	 * display the maximum line on the histogram for example. A value of -1 notes
+	 * that the maximum value is not of interest.
+	 */
 	public void setMax(long max) {
 		hasChanges |= max != this.max;
 		this.max = max;
 	}
 
-	public Histogram1d<?> getHistogram() {
-		return histogram;
-	}
-
-	public long getMin() {
-		return min;
-	}
-
+	/**
+	 * Gets the bin number of the maximum value. A value of -1 notes the maximum
+	 * value is not of interest.
+	 */
 	public long getMax() {
 		return max;
 	}
 
+	/**
+	 * Sets the preferred size for the rendering of this histogram as a widget.
+	 */
+	public void setPreferredSize(int x, int y) {
+		preferredSizeX = x;
+		preferredSizeY = y;
+	}
+
+	/**
+	 * Gets the preferred X size of the widget for the rendering of this
+	 * histogram.
+	 */
+	public int getPreferredSizeX() {
+		return preferredSizeX;
+	}
+
+	/**
+	 * Gets the preferred Y size of the widget for the rendering of this
+	 * histogram.
+	 */
+	public int getPreferredSizeY() {
+		return preferredSizeY;
+	}
+
+	// -- changes api --
+
+	/**
+	 * Returns true if the bundle has been changed via use of some of the field
+	 * setting api. This information can be used by renderers to update their UI
+	 * as appropriate.
+	 */
 	public boolean hasChanges() {
 		return hasChanges;
 	}
 
+	/**
+	 * Clears the changes flag. Called by renderers after they have updated their
+	 * UI to reflect previous changes.
+	 */
 	public void changesNoted() {
 		hasChanges = false;
 	}
