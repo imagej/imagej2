94cad9e9fd7e4031cd7ca2432d1a2c80cbc5b7df
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-07-06 14:59:42 -0500

    code-cleanup.eclipse.js: mention how to start it
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- bin/code-cleanup.eclipse.js
+++ bin/code-cleanup.eclipse.js
@@ -3,6 +3,10 @@
  *
  * Needs EclipseScript: Help>Install New Software..., Add
  * http://eclipsescript.org/updates/, select "EclipseScript".
+ *
+ * Then, Ctrl+4 (or on MacOSX, Command+4) -- *not* Ctrl+*F4*!
+ * -- opens a command launcher look-alike with which you can
+ * launch this script.
  */
 
 importClass(Packages.org.eclipse.core.runtime.NullProgressMonitor);
