6974951fdb327ec67f0ed094de1d5fcd7b151787
Barry DeZonia <bdezonia@wisc.edu>
2013-07-11 11:23:59 -0500

    Add DataTypes for complex numbers

---
+++ core/data/src/main/java/imagej/data/types/DataType64BitSignedComplexFloat.java
@@ -0,0 +1,131 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2013 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.data.types;
+
+import java.math.BigDecimal;
+
+import net.imglib2.type.numeric.complex.ComplexFloatType;
+
+import org.scijava.AbstractContextual;
+import org.scijava.plugin.Plugin;
+
+/**
+ * @author Barry DeZonia
+ */
+@Plugin(type = DataType.class)
+public class DataType64BitSignedComplexFloat extends AbstractContextual
+	implements DataType<ComplexFloatType>
+{
+
+	// -- fields --
+
+	private ComplexFloatType type = new ComplexFloatType();
+
+	// -- DataType methods --
+
+	@Override
+	public ComplexFloatType getType() {
+		return type;
+	}
+
+	@Override
+	public String name() {
+		return "64-bit complex";
+	}
+
+	@Override
+	public String description() {
+		return "A complex floating data type with 32-bit subcomponents";
+	}
+
+	@Override
+	public boolean isFloat() {
+		return true;
+	}
+
+	@Override
+	public boolean isSigned() {
+		return true;
+	}
+
+	@Override
+	public boolean isBoundedFully() {
+		return false;
+	}
+
+	@Override
+	public boolean isBoundedBelow() {
+		return false;
+	}
+
+	@Override
+	public boolean isBoundedAbove() {
+		return false;
+	}
+
+	@Override
+	public void lowerBound(ComplexFloatType dest) {
+		throw new UnsupportedOperationException("complex numbers are unbounded");
+	}
+
+	@Override
+	public void upperBound(ComplexFloatType dest) {
+		throw new UnsupportedOperationException("complex numbers are unbounded");
+	}
+
+	@Override
+	public int bitCount() {
+		return 64;
+	}
+
+	@Override
+	public ComplexFloatType createVariable() {
+		return type.createVariable();
+	}
+
+	@Override
+	public void cast(ComplexFloatType val, BigComplex dest) {
+		dest.setReal(BigDecimal.valueOf(val.getRealFloat()));
+		dest.setImag(BigDecimal.valueOf(val.getImaginaryFloat()));
+	}
+
+	@Override
+	public void cast(BigComplex val, ComplexFloatType dest) {
+		dest.setReal(val.getReal().floatValue());
+		dest.setImaginary(val.getImag().floatValue());
+	}
+
+}
