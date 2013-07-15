f99b90a0d28295359664b01f5a11256ac671e648
Curtis Rueden <ctrueden@wisc.edu>
2013-07-15 15:45:16 -0500

    LUTService: remove obsolete comment

--- core/data/src/main/java/imagej/data/lut/LUTService.java
+++ core/data/src/main/java/imagej/data/lut/LUTService.java
@@ -56,8 +56,6 @@ import org.scijava.service.Service;
  */
 public interface LUTService extends Service {
 
-	// CTR TODO: Convert this to an IOPlugin, probably?
-
 	/**
 	 * Gets whether the given file can be parsed as a color table by
 	 * {@link #loadLUT(File)}.
