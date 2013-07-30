1887af7204e21c22adfc6795595b61e5e60d5e74
Barry DeZonia <bdezonia@wisc.edu>
2013-07-30 14:31:17 -0500

    Restore TypeChangers ability to combine channels

--- plugins/commands/src/main/java/imagej/core/commands/typechange/TypeChanger.java
+++ plugins/commands/src/main/java/imagej/core/commands/typechange/TypeChanger.java
@@ -39,6 +39,7 @@ import imagej.command.Command;
 import imagej.command.DynamicCommand;
 import imagej.data.Dataset;
 import imagej.data.DatasetService;
+import imagej.data.display.ColorTables;
 import imagej.data.types.BigComplex;
 import imagej.data.types.DataType;
 import imagej.data.types.DataTypeService;
@@ -53,6 +54,10 @@ import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.img.ImgPlus;
 import net.imglib2.meta.Axes;
+import net.imglib2.meta.AxisType;
+import net.imglib2.ops.pointset.HyperVolumePointSet;
+import net.imglib2.ops.pointset.PointSet;
+import net.imglib2.ops.pointset.PointSetIterator;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 
@@ -88,11 +93,14 @@ public class TypeChanger<U extends RealType<U>, V extends RealType<V> & NativeTy
 	@Parameter
 	private DataTypeService dataTypeService;
 
+	@Parameter
+	private Dataset data;
+
 	@Parameter(label = "Type", persist = false, initializer = "init")
 	private String typeName;
 
-	@Parameter
-	private Dataset data;
+	@Parameter(label = "Combine channels", persist = false)
+	private boolean combineChannels;
 
 	// -- Command methods --
 
@@ -103,20 +111,82 @@ public class TypeChanger<U extends RealType<U>, V extends RealType<V> & NativeTy
 			dataTypeService.getTypeByClass(data.getImgPlus().firstElement()
 				.getClass());
 		DataType<V> outType = (DataType<V>) dataTypeService.getTypeByName(typeName);
-		Dataset newData =
-			datasetService.create(outType.createVariable(), data.getDims(),
-				"Converted Image", data.getAxes());
-		Cursor<U> inCursor = (Cursor<U>) data.getImgPlus().cursor();
-		RandomAccess<V> outAccessor =
-			(RandomAccess<V>) newData.getImgPlus().randomAccess();
-		BigComplex tmp = new BigComplex();
-		while (inCursor.hasNext()) {
-			inCursor.fwd();
-			outAccessor.setPosition(inCursor);
-			cast(inType, inCursor.get(), outType, outAccessor.get(), tmp);
+		int chAxis = data.getAxisIndex(Axes.CHANNEL);
+		long channelCount = (chAxis < 0) ? 1 : data.dimension(chAxis);
+		Dataset newData;
+		if (combineChannels && channelCount > 1 &&
+			channelCount <= Integer.MAX_VALUE)
+		{
+			int count = (int) channelCount;
+			BigComplex[] temps = new BigComplex[count];
+			for (int i = 0; i < count; i++) {
+				temps[i] = new BigComplex();
+			}
+			BigComplex combined = new BigComplex();
+			BigComplex divisor = new BigComplex(count, 0);
+			long[] dims = calcDims(data.getDims(), chAxis);
+			AxisType[] axes = calcAxes(data.getAxes(), chAxis);
+			newData =
+				datasetService.create(outType.createVariable(), dims,
+					"Converted Image", axes);
+			long[] span = data.getDims().clone();
+			span[chAxis] = 1;
+			PointSet combinedSpace = new HyperVolumePointSet(span);
+			PointSetIterator iter = combinedSpace.iterator();
+			RandomAccess<U> inAccessor =
+				(RandomAccess<U>) data.getImgPlus().randomAccess();
+			RandomAccess<V> outAccessor =
+				(RandomAccess<V>) newData.getImgPlus().randomAccess();
+			while (iter.hasNext()) {
+				long[] pos = iter.next();
+				inAccessor.setPosition(pos);
+				for (int i = 0; i < count; i++) {
+					inAccessor.setPosition(i, chAxis);
+					inType.cast(inAccessor.get(), temps[i]);
+				}
+				combined.setZero();
+				for (int i = 0; i < count; i++) {
+					combined.add(temps[i]);
+				}
+				int d = 0;
+				for (int i = 0; i < count; i++) {
+					if (i == chAxis) continue;
+					outAccessor.setPosition(pos[i], d++);
+				}
+				// TODO
+				// was:
+				// combined.div(divisor);
+				// outType.cast(combined, outAccessor.get());
+				//
+				// Would love to do a combined.div(divisor) but note that BigComplex can
+				// error out. Divide by 3 and you'll get an exception. We need
+				// BigRationals internally rather than BigDecimals. BigComplex is
+				// broken as a math type (but totally useful as a casting type). For now
+				// work in doubles causing possible precision loss.
+				double real = combined.getRealDouble() / count;
+				double imag = combined.getImaginaryDouble() / count;
+				outAccessor.get().setReal(real);
+				outAccessor.get().setImaginary(imag);
+			}
+			copyMetaDataChannelsCase(data.getImgPlus(), newData.getImgPlus());
 		}
-		copyMetaData(data.getImgPlus(), newData.getImgPlus());
+		else { // straight 1 for 1 pixel casting
+			newData =
+				datasetService.create(outType.createVariable(), data.getDims(),
+					"Converted Image", data.getAxes());
+			Cursor<U> inCursor = (Cursor<U>) data.getImgPlus().cursor();
+			RandomAccess<V> outAccessor =
+				(RandomAccess<V>) newData.getImgPlus().randomAccess();
+			BigComplex tmp = new BigComplex();
+			while (inCursor.hasNext()) {
+				inCursor.fwd();
+				outAccessor.setPosition(inCursor);
+				cast(inType, inCursor.get(), outType, outAccessor.get(), tmp);
+			}
+			copyMetaDataDefaultCase(data.getImgPlus(), newData.getImgPlus());
+		}
 		data.setImgPlus(newData.getImgPlus());
+		data.setRGBMerged(false);
 	}
 
 	// -- initializers --
@@ -161,7 +231,7 @@ public class TypeChanger<U extends RealType<U>, V extends RealType<V> & NativeTy
 		}
 	}
 
-	private void copyMetaData(ImgPlus<?> src, ImgPlus<?> dest) {
+	private void copyMetaDataDefaultCase(ImgPlus<?> src, ImgPlus<?> dest) {
 
 		// dims and axes already correct
 
@@ -191,5 +261,80 @@ public class TypeChanger<U extends RealType<U>, V extends RealType<V> & NativeTy
 			dest.setChannelMinimum(i, min);
 			dest.setChannelMaximum(i, max);
 		}
+	}
+
+	private void copyMetaDataChannelsCase(ImgPlus<?> src, ImgPlus<?> dest) {
+
+		int chAxis = src.getAxisIndex(Axes.CHANNEL);
+
+		// dims and axes already correct
+
+		// name
+		dest.setName(src.getName());
+
+		// calibrations
+		double[] cal = new double[src.numDimensions()];
+		src.calibration(cal);
+		dest.setCalibration(calcCalibration(cal, chAxis));
+
+		// color tables
+		// ACK what is best here?
+		int tableCount = (int) calcTableCount(src, chAxis);
+		dest.initializeColorTables(tableCount);
+		for (int i = 0; i < tableCount; i++) {
+			dest.setColorTable(ColorTables.GRAYS, i);
+		}
+
+		// channel min/maxes
+		double min = src.getChannelMinimum(0);
+		double max = src.getChannelMaximum(0);
+		int channels;
+		if (chAxis < 0) channels = 1;
+		else channels = (int) src.dimension(chAxis);
+		for (int i = 1; i < channels; i++) {
+			min = Math.min(min, src.getChannelMinimum(i));
+			max = Math.max(max, src.getChannelMaximum(i));
+		}
+		dest.setChannelMinimum(0, min);
+		dest.setChannelMaximum(0, max);
+	}
+
+	private long[] calcDims(long[] dims, int chAxis) {
+		long[] outputDims = new long[dims.length - 1];
+		int d = 0;
+		for (int i = 0; i < dims.length; i++) {
+			if (i == chAxis) continue;
+			outputDims[d++] = dims[i];
+		}
+		return outputDims;
+	}
+
+	private AxisType[] calcAxes(AxisType[] axes, int chAxis) {
+		AxisType[] outputAxes = new AxisType[axes.length - 1];
+		int d = 0;
+		for (int i = 0; i < axes.length; i++) {
+			if (i == chAxis) continue;
+			outputAxes[d++] = axes[i];
+		}
+		return outputAxes;
+	}
+
+	private double[] calcCalibration(double[] cal, int chAxis) {
+		double[] outputCal = new double[cal.length - 1];
+		int d = 0;
+		for (int i = 0; i < cal.length; i++) {
+			if (i == chAxis) continue;
+			outputCal[d++] = cal[i];
+		}
+		return outputCal;
+	}
+
+	private long calcTableCount(ImgPlus<?> src, int chAxis) {
+		long count = 1;
+		for (int i = 0; i < src.numDimensions(); i++) {
+			if (i == chAxis) continue;
+			count *= src.dimension(i);
+		}
+		return count;
 	}
 }
