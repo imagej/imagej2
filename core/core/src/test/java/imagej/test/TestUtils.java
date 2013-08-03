df2d13a3985eb920222321c5ca22c9593a1564d4
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-08-03 16:15:30 -0500

    Add a function to create temporary directories for tests
    When we execute tests in the Maven context, the target/ directory is a
    much better place to put stuff than /tmp/, especially when the tests are
    run by Jenkins: the workspace including the target/ directories can be
    accessed via Web, the /tmp/ directory cannot.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ core/core/src/test/java/imagej/test/TestUtils.java
@@ -0,0 +1,82 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2013 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.scijava.util.ClassUtils;
+import org.scijava.util.FileUtils;
+
+/**
+ * A bunch of helpful functions for unit tests.
+ * 
+ * @author Johannes Schindelin
+ */
+public class TestUtils {
+
+	/**
+	 * Makes a temporary directory for use with unit tests.
+	 * <p>
+	 * When the unit test runs in a Maven context, the temporary directory will be
+	 * created in the corresponding <i>target/</i> directory instead of
+	 * <i>/tmp/</i>.
+	 * </p>
+	 * 
+	 * @param prefix the prefix for the directory's name
+	 * @param forClass the class for context (to determine whether there's a
+	 *          <i>target/<i> directory)
+	 * @return the reference to the newly-created temporary directory
+	 * @throws IOException
+	 */
+	public static File createTemporaryDirectory(final String prefix,
+		final Class<?> forClass) throws IOException
+	{
+		final URL directory = ClassUtils.getLocation(forClass);
+		if (directory != null && "file".equals(directory.getProtocol())) {
+			final String path = directory.getPath();
+			if (path != null && path.endsWith("/target/test-classes/")) {
+				final File baseDirectory =
+					new File(path.substring(0, path.length() - 13));
+				final File file = File.createTempFile(prefix, "", baseDirectory);
+				if (file.delete() && file.mkdir()) return file;
+			}
+		}
+		return FileUtils.createTemporaryDirectory(prefix, "");
+	}
+
+}
