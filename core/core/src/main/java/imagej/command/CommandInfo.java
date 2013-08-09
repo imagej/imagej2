imagej/imagej2

commit 541005824c723dd05a6cb7e9842562ead0f952dd

CommandInfo: remove factory infrastructure
This infrastructure was put in place long ago to accommodate a specific
use case: 3D Viewer plugins, which were special extensions of Commands
with their own menu structure separate from vanilla ImageJ commands.

That use case can now be handled using the menuRoot attribute of the
@Plugin annotation; there is no more need to involve factories that
customize the wrapping of Commands to Module instances.

To reduce the complexity of the class hierarchy surrounding Commands and
Modules (which is really more than complicated enough already), we
remove this unused feature.

If the need should arise for it in the future, it can always be readded
at that time. But until then, we respect the "YAGNI" principle here.

core/core/src/main/java/imagej/command/CommandInfo.java
@@ -89,9 +89,6 @@ public class CommandInfo extends PluginInfo<Command> implements ModuleInfo,
 	/** List of items with fixed, preset values. */
 	private Map<String, Object> presets;
 
-	/** Factory used to create a module associated with the associated command. */
-	private CommandModuleFactory factory;
-
 	/**
 	 * Flag indicating whether the command parameters have been parsed. Parsing
 	 * the parameters requires loading the command class, so doing so is deferred
@@ -180,7 +177,6 @@ public class CommandInfo extends PluginInfo<Command> implements ModuleInfo,
 		super(className, commandClass, Command.class, annotation, null);
 		this.info = info;
 		setPresets(null);
-		setCommandModuleFactory(null);
 	}
 
 	// -- CommandInfo methods --
@@ -200,27 +196,16 @@ public class CommandInfo extends PluginInfo<Command> implements ModuleInfo,
 		return presets;
 	}
 
-	/** Sets the factory used to construct {@link CommandModule} instances. */
-	public void setCommandModuleFactory(final CommandModuleFactory factory) {
-		if (factory == null) {
-			this.factory = new DefaultCommandModuleFactory();
-		}
-		else {
-			this.factory = factory;
-		}
-	}
-
-	/** Gets the factory used to construct {@link CommandModule} instances. */
-	public CommandModuleFactory getCommandModuleFactory() {
-		return factory;
-	}
-
 	/**
 	 * Instantiates the module described by this module info, around the specified
 	 * existing command instance.
 	 */
 	public Module createModule(final Command commandInstance) {
-		return factory.createModule(this, commandInstance);
+		// if the command implements Module, return the instance directly
+		if (commandInstance instanceof Module) return (Module) commandInstance;
+
+		// command does not implement Module; wrap it in a CommandModule instance
+		return new CommandModule(this, commandInstance);
 	}
 
 	// -- PluginInfo methods --
@@ -317,7 +302,25 @@ public class CommandInfo extends PluginInfo<Command> implements ModuleInfo,
 
 	@Override
 	public Module createModule() throws ModuleException {
-		return factory.createModule(this);
+		// if the command implements Module, return a new instance directly
+		try {
+			final Class<?> commandClass = info.loadClass();
+			if (Module.class.isAssignableFrom(commandClass)) {
+				return (Module) commandClass.newInstance();
+			}
+		}
+		catch (final InstantiableException e) {
+			throw new ModuleException(e);
+		}
+		catch (final InstantiationException e) {
+			throw new ModuleException(e);
+		}
+		catch (final IllegalAccessException e) {
+			throw new ModuleException(e);
+		}
+
+		// command does not implement Module; wrap it in a CommandModule instance
+		return new CommandModule(this);
 	}
 
 	@Override

core/core/src/main/java/imagej/command/CommandModuleFactory.java
@@ -1,58 +1,0 @@
-/*
- * #%L
- * ImageJ software for multidimensional image processing and analysis.
- * %%
- * Copyright (C) 2009 - 2013 Board of Regents of the University of
- * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
- * Institute of Molecular Cell Biology and Genetics.
- * %%
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * 
- * The views and conclusions contained in the software and documentation are
- * those of the authors and should not be interpreted as representing official
- * policies, either expressed or implied, of any organization.
- * #L%
- */
-
-package imagej.command;
-
-import imagej.module.Module;
-import imagej.module.ModuleException;
-
-/**
- * A factory for instantiating a module from a {@link CommandInfo}.
- * 
- * @author Curtis Rueden
- */
-public interface CommandModuleFactory {
-
-	/** Constructs a module to work with the given {@link CommandInfo}. */
-	Module createModule(CommandInfo info)
-		throws ModuleException;
-
-	/**
-	 * Constructs a module to work with the given {@link CommandInfo}, around the
-	 * specified {@link Command} instance.
-	 */
-	Module createModule(CommandInfo info, final Command command);
-
-}

core/core/src/main/java/imagej/command/DefaultCommandModuleFactory.java
@@ -1,87 +1,0 @@
-/*
- * #%L
- * ImageJ software for multidimensional image processing and analysis.
- * %%
- * Copyright (C) 2009 - 2013 Board of Regents of the University of
- * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
- * Institute of Molecular Cell Biology and Genetics.
- * %%
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- * 
- * The views and conclusions contained in the software and documentation are
- * those of the authors and should not be interpreted as representing official
- * policies, either expressed or implied, of any organization.
- * #L%
- */
-
-package imagej.command;
-
-import imagej.module.Module;
-import imagej.module.ModuleException;
-
-import org.scijava.InstantiableException;
-
-/**
- * The default implementation of {@link CommandModuleFactory}, using a
- * {@link CommandModule}.
- * 
- * @author Curtis Rueden
- */
-public class DefaultCommandModuleFactory implements CommandModuleFactory {
-
-	@Override
-	public Module createModule(final CommandInfo info)
-		throws ModuleException
-	{
-		// if the command implements Module, return a new instance directly
-		try {
-			final Class<?> commandClass = info.loadClass();
-			if (Module.class.isAssignableFrom(commandClass)) {
-				return (Module) commandClass.newInstance();
-			}
-		}
-		catch (final InstantiableException e) {
-			throw new ModuleException(e);
-		}
-		catch (final InstantiationException e) {
-			throw new ModuleException(e);
-		}
-		catch (final IllegalAccessException e) {
-			throw new ModuleException(e);
-		}
-
-		// command does not implement Module; wrap it in a CommandModule instance
-		return new CommandModule(info);
-	}
-
-	@Override
-	public Module createModule(final CommandInfo info, final Command command) {
-		// if the command implements Module, return the instance directly
-		if (command instanceof Module) {
-			return (Module) command;
-		}
-
-		// command does not implement Module; wrap it in a CommandModule instance
-		return new CommandModule(info, command);
-	}
-
-}

commit 840d7d3836b297f2c01ea429baf630dd40805f28

CommandInfo: fix NPE in createModule method
This bug was introduced in 541005824c723dd05a6cb7e9842562ead0f952dd.

The meaning of the "info" variable changed (from CommandInfo to
PluginInfo) when the code was moved from DefaultCommandModuleFactory.

core/core/src/main/java/imagej/command/CommandInfo.java
@@ -304,7 +304,7 @@ public class CommandInfo extends PluginInfo<Command> implements ModuleInfo,
 	public Module createModule() throws ModuleException {
 		// if the command implements Module, return a new instance directly
 		try {
-			final Class<?> commandClass = info.loadClass();
+			final Class<?> commandClass = loadClass();
 			if (Module.class.isAssignableFrom(commandClass)) {
 				return (Module) commandClass.newInstance();
 			}