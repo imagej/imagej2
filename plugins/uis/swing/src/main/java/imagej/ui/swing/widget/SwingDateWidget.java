17416aa74557f2f06a5233538920ba74c50f7def
Curtis Rueden <ctrueden@wisc.edu>
2013-10-01 10:43:05 -0500

    Add input widget for java.util.Date objects
    The initial Swing implementation uses JDatePicker (jdatepicker.org,
    net.sourceforge.jdatepicker:jdatepicker:1.3.2) to get the job done.

---
+++ plugins/uis/swing/src/main/java/imagej/ui/swing/widget/SwingDateWidget.java
@@ -0,0 +1,116 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2013 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.ui.swing.widget;
+
+import imagej.widget.DateWidget;
+import imagej.widget.InputWidget;
+import imagej.widget.WidgetModel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
+
+import javax.swing.JPanel;
+
+import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
+import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
+import net.sourceforge.jdatepicker.impl.UtilDateModel;
+
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+/**
+ * Swing implementation of date selector widget.
+ * 
+ * @author Curtis Rueden
+ */
+@Plugin(type = InputWidget.class)
+public class SwingDateWidget extends SwingInputWidget<Date> implements
+	DateWidget<JPanel>, ActionListener
+{
+
+	@Parameter
+	private LogService log;
+
+	private UtilDateModel dateModel;
+
+	// -- InputWidget methods --
+
+	@Override
+	public Date getValue() {
+		return dateModel == null ? null : dateModel.getValue();
+	}
+
+	// -- WrapperPlugin methods --
+
+	@Override
+	public void set(final WidgetModel model) {
+		super.set(model);
+
+		dateModel = new UtilDateModel();
+		final JDatePickerImpl datePicker =
+			new JDatePickerImpl(new JDatePanelImpl(dateModel), null);
+		setToolTip(datePicker);
+		getComponent().add(datePicker);
+		datePicker.addActionListener(this);
+
+		refreshWidget();
+	}
+
+	// -- Typed methods --
+
+	@Override
+	public boolean supports(final WidgetModel model) {
+		return super.supports(model) && model.isType(Date.class);
+	}
+
+	// -- ActionListener methods --
+
+	@Override
+	public void actionPerformed(final ActionEvent e) {
+		updateModel();
+	}
+
+	// -- AbstractUIInputWidget methods ---
+
+	@Override
+	public void doRefresh() {
+		final Date date = (Date) get().getValue();
+		dateModel.setValue(date);
+	}
+
+}
