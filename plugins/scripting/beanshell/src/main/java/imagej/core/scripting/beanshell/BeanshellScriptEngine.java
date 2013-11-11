258ac15c13be84c2b63cf476e92f1d0c6db2ad5a
Curtis Rueden <ctrueden@wisc.edu>
2013-11-11 14:14:50 -0600

    Rename scripting implementations, for consistency
    Since each scripting language has its own artifact, it should live in
    its own package prefix. The Java language already did this
    (imagej.script.java) but the others all reused the imagej.script base
    package. This change moves each scripting language into a subpackage of
    imagej.core.scripting, for consistency with the rest of the plugins/
    component hierarchy.

---
+++ plugins/scripting/beanshell/src/main/java/imagej/core/scripting/beanshell/BeanshellScriptEngine.java
@@ -0,0 +1,120 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2013 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.core.scripting.beanshell;
+
+import imagej.script.AbstractScriptEngine;
+import imagej.util.LineOutputStream;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.Reader;
+import java.io.Writer;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+
+import bsh.EvalError;
+import bsh.Interpreter;
+
+/**
+ * Beanshell support for ImageJ
+ * 
+ * @author Johannes Schindelin
+ */
+public class BeanshellScriptEngine extends AbstractScriptEngine
+{
+
+	protected final Interpreter interpreter;
+
+	public BeanshellScriptEngine() {
+		interpreter = new Interpreter();
+		engineScopeBindings = new BeanshellBindings(interpreter);
+	}
+
+	@Override
+	public Object eval(final String script) throws ScriptException {
+		setup();
+		try {
+			return interpreter.eval(script);
+		}
+		catch (final EvalError e) {
+			throw new ScriptException(e);
+		}
+	}
+
+	@Override
+	public Object eval(final Reader reader) throws ScriptException {
+		setup();
+		try {
+			final String filename = (String) get(ScriptEngine.FILENAME);
+			return interpreter.eval(reader, interpreter.getNameSpace(), filename);
+		}
+		catch (final EvalError e) {
+			throw new ScriptException(e);
+		}
+	}
+
+	protected void setup() {
+		final ScriptContext context = getContext();
+		final Reader reader = context.getReader();
+		if (reader != null) {
+			log().warn("Beanshell does not support redirecting the input");
+		}
+		final Writer writer = context.getWriter();
+		if (writer != null) interpreter.setOut(new PrintStream(
+			new WriterOutputStream(writer)));
+		final Writer errorWriter = context.getErrorWriter();
+		if (errorWriter != null) interpreter.setErr(new PrintStream(
+			new WriterOutputStream(errorWriter)));
+	}
+
+	private static class WriterOutputStream extends LineOutputStream {
+
+		private Writer writer;
+
+		public WriterOutputStream(final Writer writer) {
+			this.writer = writer;
+		}
+
+		@Override
+		public void println(String line) throws IOException {
+			writer.write(line);
+			writer.write('\n');
+		}
+
+	}
+}
---
+++ plugins/scripting/clojure/src/test/java/imagej/core/scripting/clojure/ClojureTest.java
@@ -0,0 +1,79 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2013 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of any organization.
+ * #L%
+ */
+
+package imagej.core.scripting.clojure;
+
+import static org.junit.Assert.assertEquals;
+import imagej.core.scripting.clojure.ClojureScriptEngine;
+import imagej.script.ScriptService;
+
+import java.io.StringReader;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineFactory;
+
+import org.junit.Test;
+import org.scijava.Context;
+
+/**
+ * Clojure unit tests.
+ * 
+ * @author Johannes Schindelin
+ */
+public class ClojureTest {
+
+	@Test
+	public void testBasic() throws Exception {
+		final Context context = new Context(ScriptService.class);
+		final ScriptService scriptService = context.getService(ScriptService.class);
+
+		String script = "(+ 1 2)";
+		final Object result = scriptService.eval("add.clj", new StringReader(script));
+		assertEquals("3", result.toString());
+	}
+
+	@Test
+	public void testLocals() throws Exception {
+		final Context context = new Context(ScriptService.class);
+		final ScriptService scriptService = context.getService(ScriptService.class);
+
+		final ScriptEngineFactory factory = scriptService.getByFileExtension("clj");
+		final ScriptEngine engine = factory.getScriptEngine();
+		assertEquals(ClojureScriptEngine.class, engine.getClass());
+		engine.put("$hello", 17);
+		assertEquals("17", engine.eval("$hello").toString());
+		assertEquals("17", engine.get("$hello").toString());
+	}
+}
