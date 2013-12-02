762e678bbf5cfa2657727255a3580cebe49f3163
Curtis Rueden <ctrueden@wisc.edu>
2013-12-01 22:29:19 -0600

    JavaEngineTest: fix XML version number
    I have absolutely no idea how the version went from 1.0 to 2.0
    just because of an Eclipse style reformatting, though...

--- plugins/scripting/java/src/test/java/imagej/plugins/scripting/java/JavaEngineTest.java
+++ plugins/scripting/java/src/test/java/imagej/plugins/scripting/java/JavaEngineTest.java
@@ -198,7 +198,7 @@ public class JavaEngineTest {
 
 		final FileWriter pom = new FileWriter(new File(dir, "pom.xml"));
 		final String xml = "" + //
-			"<?xml version=\"2.0\" encoding=\"UTF-8\"?>\n" + //
+			"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + //
 			"<project xmlns=\"http://maven.apache.org/POM/4.0.0\"\n" + //
 			" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n" + //
 			" xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0\n" + //
