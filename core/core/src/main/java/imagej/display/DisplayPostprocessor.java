c84b0e99df196df210a76c745bd475ae6fdb4907
Curtis Rueden <ctrueden@wisc.edu>
2013-12-26 23:08:12 -0600

    DisplayPostprocessor: fix warning

--- core/core/src/main/java/imagej/display/DisplayPostprocessor.java
+++ core/core/src/main/java/imagej/display/DisplayPostprocessor.java
@@ -159,8 +159,7 @@ public class DisplayPostprocessor extends AbstractPostprocessorPlugin {
 
 		// no available displays for this type of output
 		if (log != null) {
-			final String valueClass =
-				output == null ? "null" : output.getClass().getName();
+			final String valueClass = output.getClass().getName();
 			log.warn("Ignoring unsupported output: " + defaultName + " [" +
 				valueClass + "]");
 		}
