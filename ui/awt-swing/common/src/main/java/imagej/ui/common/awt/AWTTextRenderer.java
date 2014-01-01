4c3f8746db4ba3af001bd4e3eddfed565825326b
Curtis Rueden <ctrueden@wisc.edu>
2014-01-01 10:12:15 -0600

    Happy New Year 2014!

--- ui/awt-swing/common/src/main/java/imagej/ui/common/awt/AWTTextRenderer.java
+++ ui/awt-swing/common/src/main/java/imagej/ui/common/awt/AWTTextRenderer.java
@@ -2,7 +2,7 @@
  * #%L
  * ImageJ software for multidimensional image processing and analysis.
  * %%
- * Copyright (C) 2009 - 2013 Board of Regents of the University of
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
  * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
  * Institute of Molecular Cell Biology and Genetics.
  * %%
