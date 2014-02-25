e14487f1b1e14e04f2f54d4a0d587a86cba5e0eb
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-02-24 21:00:58 -0600

    Add a default implementation of the text editor
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ core/legacy/src/main/java/imagej/legacy/plugin/DefaultLegacyEditor.java
@@ -0,0 +1,91 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+package imagej.legacy.plugin;
+
+import imagej.platform.event.AppQuitEvent;
+import imagej.script.editor.TextEditor;
+
+import java.io.File;
+
+import org.scijava.Context;
+import org.scijava.event.EventHandler;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = LegacyEditor.class)
+public class DefaultLegacyEditor implements LegacyEditor {
+
+	@Parameter
+	private Context context;
+
+	private TextEditor editor;
+
+	private synchronized TextEditor editor() {
+		if (editor == null) {
+			editor = new TextEditor(context) {
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public void dispose() {
+					synchronized (DefaultLegacyEditor.this) {
+						editor = null;
+						super.dispose();
+					}
+				}
+			};
+			editor.setVisible(true);
+		}
+		return editor;
+	}
+
+	@EventHandler
+	private synchronized void onEvent(final AppQuitEvent event) {
+		if (editor != null) {
+			editor.dispose();
+			editor = null;
+		}
+	}
+
+	/** @inherit */
+	@Override
+	public boolean open(File file) {
+		editor().open(file);
+		return true;
+	}
+
+	/** @inherit */
+	@Override
+	public boolean create(String title, String content) {
+		editor().createNewDocument(title, content);
+		return true;
+	}
+
+}
