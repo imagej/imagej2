SushilGC/imagej

Make LegacyJavaAgentIT less implementation-dependent
We are about to switch around a few things, in particular the assumption
that there is always a LegacyService available.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

core/legacy/src/test/java/imagej/legacy/LegacyJavaAgentIT.java
@@ -36,6 +36,7 @@
 package imagej.legacy;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
@@ -53,12 +54,10 @@ public class LegacyJavaAgentIT {
 	private StackTraceElement[] trace;
 
 	@Test
-	public void testAgentInit() {
+	public void testAgentInit() throws NoSuchMethodException {
 		assumeTrue("init".equals(System.getProperty("legacy.agent.mode")));
 		IJ.log("Now ij.IJ is loaded.");
-		final LegacyService legacyService =
-			(LegacyService)IJ.runPlugIn(LegacyService.class.getName(), null);
-		assertTrue(legacyService != null);
+		assertNotNull(IJ.class.getMethod("getContext"));
 	}
 
 	@Test

imagej/imagej2

core/legacy/src/test/java/imagej/legacy/LegacyJavaAgentIT.java
@@ -32,6 +32,7 @@
 package imagej.legacy;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assume.assumeTrue;
 
@@ -49,12 +50,10 @@ public class LegacyJavaAgentIT {
 	private StackTraceElement[] trace;
 
 	@Test
-	public void testAgentInit() {
+	public void testAgentInit() throws Exception {
 		assumeTrue("init".equals(System.getProperty("legacy.agent.mode")));
 		IJ.log("Now ij.IJ is loaded.");
-		final LegacyService legacyService =
-			(LegacyService)IJ.runPlugIn(LegacyService.class.getName(), null);
-		assertTrue(legacyService != null);
+		assertNotNull(IJ.class.getField("_hooks"));
 	}
 
 	@Test