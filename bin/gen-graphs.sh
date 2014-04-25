415a7d81915cc2e2c38ecb9f7fab52d96c635a02
Curtis Rueden <ctrueden@wisc.edu>
2014-04-25 15:41:23 -0500

    Fix gen-graphs.sh script
    The dependency graph is beautiful now, but should probably include
    dependencies of dependencies, as well as runtime components...

--- bin/gen-graphs.sh
+++ bin/gen-graphs.sh
@@ -13,7 +13,7 @@
 set -e
 
 ROOT=`cd "$(dirname $0)/.." ; pwd`
-COMPONENTS=(. app core plugins ui)
+cd "$ROOT"
 
 GRAPH_CMD="
   neato
@@ -25,9 +25,5 @@ GRAPH_CMD="
   dependency-graph.dot
 "
 
-for c in ${COMPONENTS[*]}
-do
-  cd $ROOT/$c
-  mvn graph:reactor
-  cd target && $GRAPH_CMD
-done
+mvn graph:reactor
+cd target && $GRAPH_CMD
