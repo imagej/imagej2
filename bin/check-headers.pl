556f46d46024293d82afd31a0fd7b408e3516158
Curtis Rueden <ctrueden@wisc.edu>
2014-06-10 15:33:48 -0500

    Consolidate Perl subroutines

--- bin/check-headers.pl
+++ bin/check-headers.pl
@@ -26,7 +26,6 @@ use constant {
 
 my $dir = `dirname "$0"`;
 chop $dir;
-require "$dir/subs.pl";
 
 my %knownAuthors = (
   "Adam Fraser" => 1,
@@ -228,4 +227,39 @@ sub process($) {
 
   # all OK
   return OK;
+}
+
+sub match($$$) {
+  my ($tRef, $dRef, $index) = @_;
+  my @template = @$tRef;
+  my @data = @$dRef;
+
+  my $i = $index;
+  my $result = 1;
+  for my $expected (@template) {
+    my $actual = $data[$i++];
+    if ($actual ne $expected) {
+      $result = 0;
+      last;
+    }
+  }
+  return $result;
+}
+
+sub readFile($) {
+  my ($file) = @_;
+  open FILE, "$file" or die "$file: $!";
+  my @data = <FILE>;
+  close(FILE);
+  for my $line (@data) {
+    chop $line;
+  }
+  return @data;
+}
+
+sub trim($) {
+  my ($line) = @_;
+  $line =~ s/^\s*//;
+  $line =~ s/\s*$//;
+  return $line;
 }
