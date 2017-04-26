0ee3c7bd551e8ed6d57ea2f3058822b5e7bd7f97
Curtis Rueden <ctrueden@wisc.edu>
2017-04-26 15:36:50 -0500

    Fix the Travis configuration
    Without this, failed builds of master trigger another "mvn install".
    
    See: https://gist.github.com/ctrueden/ae0f024a0cdf2cb53c915d75b0759553

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 dir="$(dirname "$0")"
-test "$TRAVIS_SECURE_ENV_VARS" = true \
+if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
-  -a "$TRAVIS_BRANCH" = master &&
-  mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml" ||
+  -a "$TRAVIS_BRANCH" = master ]
+then
+  mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml"
+else
   mvn install
+fi
