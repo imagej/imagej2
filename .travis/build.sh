fd13761533fc54b7bff4017e3363ec921c20a3eb
Curtis Rueden <ctrueden@wisc.edu>
2017-10-05 18:21:57 -0500

    Update Travis configuration
    This will hopefully reduce the need for future en masse updates.

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-dir="$(dirname "$0")"
-if [ "$TRAVIS_SECURE_ENV_VARS" = true \
-  -a "$TRAVIS_PULL_REQUEST" = false \
-  -a "$TRAVIS_BRANCH" = master ]
-then
-  mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml"
-else
-  mvn install
-fi
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
+sh travis-build.sh
--- .travis/notify.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-curl -fs "https://jenkins.imagej.net/job/$1/buildWithParameters?token=$TOKEN_NAME&repo=$TRAVIS_REPO_SLUG&commit=$TRAVIS_COMMIT&pr=$TRAVIS_PULL_REQUEST"
