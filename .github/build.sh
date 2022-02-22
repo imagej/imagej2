c50a254609e99b15f9dd4024b5998b9ada498959
Curtis Rueden <ctrueden@wisc.edu>
2022-02-22 11:40:41 -0600

    Update CI configuration

--- .github/build.sh
+++ .github/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-build.sh
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/main/ci-build.sh
 sh ci-build.sh
--- .github/setup.sh
+++ .github/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/ci-setup-github-actions.sh
+curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/main/ci-setup-github-actions.sh
 sh ci-setup-github-actions.sh
